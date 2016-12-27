9228f778d2d4f9098b7d0aae001b7ba1962cdf82
Ace <258613763@qq.com>
2016-12-27 21:45:25 +0800

    增加类型，兼容C#

---
+++ SYSTEM/driver/inc/Type.h
@@ -0,0 +1,23 @@
+/*
+基本类型表，在 C++基础上尽可能参考 C#/Net
+
+
+*/
+#pragma once
+
+#include <stdint.h>
+
+typedef uint8_t byte; //单字节 0~255
+//typedef  int16_t short;//短整型
+typedef uint16_t ushort; //无符号短整型 0~65535
+//typedef  int32_t int;//整型
+typedef uint32_t uint; //无符号整型
+typedef int64_t Int64; //长整型
+typedef uint64_t UInt64; //无符号长整型
+typedef uint32_t cstring; //C 格式字符串
+
+class CType
+{
+	
+};
+
---
+++ SYSTEM/driver/src/Type.cpp
@@ -0,0 +1,6 @@
+#include "Type.h"
+
+
+
+
+
