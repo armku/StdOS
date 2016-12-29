21274993540fdb6780c5169712b61e495193f4ac
Ace <258613763@qq.com>
2016-12-29 15:45:52 +0800

    优化任务调度

--- SYSTEM/CORE/inc/Task.h
+++ SYSTEM/CORE/inc/Task.h
@@ -1,8 +1,24 @@
 #pragma once
 #include "Type.h"
-#include "Thread_T.h"
 
 #define PROCESSCNT 20	//任务数量
+
+//仅限内部使用
+class CThread
+{
+	public:
+		CThread();
+		~CThread();
+	public:
+		uint TickCur;//当前计数值	
+		uint intervalms;//回调间隔
+		bool canRun;//可以执行
+		uint delaycnt;//延时时间		
+	public:
+		void (*callback)(void);//回调函数
+		void checkRun();//判断是否运行
+		void SetPara(void (*callback)(void),uint intervalms,uint delaycntms=0);//设置参数
+};
 
 class Task
 {
--- SYSTEM/CORE/src/Task.cpp
+++ SYSTEM/CORE/src/Task.cpp
@@ -30,3 +30,55 @@ void Task::AddTask(void(*callback)(void), uint delaycntms, uint intervalms)
     this->thread[this->NextID].SetPara(callback, intervalms, delaycntms);
     this->NextID++;
 }
+
+CThread::CThread()
+{
+	this->canRun=false;
+}
+CThread::~CThread()
+{
+}
+void CThread::checkRun()//判断是否运行
+{
+	//间隔运行
+	if(this->canRun)
+	{
+		//时间间隔为0，每次都运行
+		if(this->intervalms==0)
+		{
+			this->TickCur=0;
+			this->callback();
+		}
+		//到达间隔时间，运行
+		else if(this->TickCur>=this->intervalms)
+		{
+			this->TickCur=0;
+			this->callback();
+		}		
+		else{}
+	}
+	//第一次运行
+	else
+		{
+			if(this->TickCur>=this->delaycnt)
+			{
+				this->canRun=true;
+				this->TickCur=0;
+				this->callback();
+			}
+		}
+}
+void CThread::SetPara(void (*callback)(void),uint intervalms,uint delaycntms)//设置参数
+{
+	this->callback=callback;
+	this->intervalms=intervalms;
+	this->delaycnt=delaycntms;
+	if(delaycntms==0)
+	{
+		this->canRun=true;
+	}
+	else
+	{
+		this->canRun=false;
+	}
+}
