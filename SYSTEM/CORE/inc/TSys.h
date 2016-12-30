ca07e36148f18bf2f3b6bb3b0f6996eafd663fd9
Ace <258613763@qq.com>
2016-12-30 22:41:25 +0800

    优化任务调度

--- SYSTEM/CORE/inc/TSys.h
+++ SYSTEM/CORE/inc/TSys.h
@@ -10,22 +10,7 @@ typedef enum
 {
 	COM1=0,COM2,COM3,COM4,COM5,COM_NONE=0XFF
 }MessagePort_T;//端口号
-typedef struct Thread_T
-{
-    uint TickCur; //当前计数值	
-    uint periodMs; //回调间隔
-    bool canRun; //可以执行
-    uint firstMs; //延时时间	
-    uint ID; //编号
-    const char *Name; //名称
-    void(*callback)(void); //回调函数	
-} Thread_T;
 
-struct Node
-{
-    Thread_T data; //数据
-    Node *pNext; //下一节点
-};
 
 class TSys:public Object
 {
--- SYSTEM/CORE/inc/Task.h
+++ SYSTEM/CORE/inc/Task.h
@@ -2,6 +2,23 @@
 
 #include "Type.h"
 
+typedef struct Thread_T
+{
+    uint TickCur; //当前计数值	
+    uint periodMs; //回调间隔
+    bool canRun; //可以执行
+    uint firstMs; //延时时间	
+    uint ID; //编号
+    const char *Name; //名称
+    void(*callback)(void); //回调函数	
+} Thread_T;
+
+struct Node
+{
+    Thread_T data; //数据
+    Node *pNext; //下一节点
+};
+
 class Task
 {
 	public:
