a60323bd3ac807d9b7520b1e5567854c067ca5f3
Ace <258613763@qq.com>
2016-12-30 11:16:12 +0800

    Task增加链表功能

--- SYSTEM/CORE/inc/Task.h
+++ SYSTEM/CORE/inc/Task.h
@@ -18,11 +18,21 @@ class CThread
 		void SetPara(void (*callback)(void),uint intervalms,uint delaycntms=0);//设置参数
 };
 
+typedef struct Thread
+{
+	uint TickCur;//当前计数值	
+	uint intervalms;//回调间隔
+	bool canRun;//可以执行
+	uint delaycnt;//延时时间	
+	void (*callback)(void);//回调函数
+	Thread *pnext;
+	
+}Thread_T;
+
 class Task
 {
     public:
         Task();
-    public:
         uint ticks; //形同运行滴答数
     private:
         ushort	NextID; //下一个ID
