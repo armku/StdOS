5429f97f8a27b832b37c54ebc25e7751cebe98f0
Ace <258613763@qq.com>
2017-01-01 23:03:15 +0800

    代码整理

--- SYSTEM/Peripheral/src/sys.c
+++ SYSTEM/Peripheral/src/sys.c
@@ -1,3 +1,4 @@
+#include "stm32f10x.h"
 #include "sys.h"
 
 //////////////////////////////////////////////////////////////////////////////////	 
@@ -16,22 +17,25 @@
 //采用如下方法实现执行汇编指令WFI  
 void WFI_SET(void)
 {
-	__ASM volatile("wfi");		  
+    __ASM volatile("wfi");
 }
+
 //关闭所有中断
 void INTX_DISABLE(void)
-{		  
-	__ASM volatile("cpsid i");
+{
+    __ASM volatile("cpsid i");
 }
+
 //开启所有中断
 void INTX_ENABLE(void)
 {
-	__ASM volatile("cpsie i");		  
+    __ASM volatile("cpsie i");
 }
+
 //设置栈顶地址
 //addr:栈顶地址
-__asm void MSR_MSP(uint addr) 
+__asm void MSR_MSP(uint addr)
 {
-    MSR MSP, r0 			//set Main Stack value
+    MSR MSP, r0  //set Main Stack value
     BX r14
 }
--- SYSTEM/Peripheral/src/usart.cpp
+++ SYSTEM/Peripheral/src/usart.cpp
@@ -1,3 +1,4 @@
+#include "stm32f10x.h"
 #include "usart.h"
 #include "usart.hpp"
 #include "BasePort.h"
@@ -8,7 +9,7 @@ void uart_init(uint bound)
     USART_InitTypeDef USART_InitStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);	//使能USART1
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); //使能USART1
 
     BasePort tx1(PA9);
     BasePort rx1(PA10);
@@ -17,23 +18,23 @@ void uart_init(uint bound)
 
     //Usart1 NVIC 配置
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0 ; //抢占优先级3
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;		//子优先级3
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
-    NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; //抢占优先级3
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1; //子优先级3
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道使能
+    NVIC_Init(&NVIC_InitStructure); //根据指定的参数初始化VIC寄存器
 
     //USART 初始化设置
 
-    USART_InitStructure.USART_BaudRate = bound;//串口波特率
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
-    USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+    USART_InitStructure.USART_BaudRate = bound; //串口波特率
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b; //字长为8位数据格式
+    USART_InitStructure.USART_StopBits = USART_StopBits_1; //一个停止位
+    USART_InitStructure.USART_Parity = USART_Parity_No; //无奇偶校验位
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; //无硬件数据流控制
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx; //收发模式
 
     USART_Init(USART1, &USART_InitStructure); //初始化串口1
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启串口接收中断
-    USART_Cmd(USART1, ENABLE);                    //使能串口1
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); //开启串口接收中断
+    USART_Cmd(USART1, ENABLE); //使能串口1
     USART_ClearFlag(USART1, USART_FLAG_TC);
 
     //串口2设置
@@ -45,16 +46,16 @@ void uart_init(uint bound)
 
     //Usart2 NVIC 配置
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0 ; //抢占优先级3
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;		//子优先级3
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
-    NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; //抢占优先级3
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1; //子优先级3
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道使能
+    NVIC_Init(&NVIC_InitStructure); //根据指定的参数初始化VIC寄存器
 
     //USART 初始化设置
 
     USART_Init(USART2, &USART_InitStructure); //初始化串口2
-    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);//开启串口接受中断
-    USART_Cmd(USART2, ENABLE);                    //使能串口2
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); //开启串口接受中断
+    USART_Cmd(USART2, ENABLE); //使能串口2
     USART_ClearFlag(USART2, USART_FLAG_TC);
 
     //	 //串口3设置
@@ -120,39 +121,43 @@ void uart_init(uint bound)
     //    USART_Cmd(UART5, ENABLE);                    //使能串口5
     //USART_ClearFlag(UART5, USART_FLAG_TC);
 }
-#ifdef __cplusplus
-extern "C" {
-#endif
-#if EN_USART1_RX   //如果使能了接收
-//串口1中断服务程序
-//注意,读取USARTx->SR能避免莫名其妙的错误
-/*
-*/
-void USART1_IRQHandler(void)                	//串口1中断服务程序
-{
-    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收到一字节
-    {
-        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
-        byte  inch = USART1->DR;	//读取接收到的数据
-        
-		com1buf.Push(inch);
-		com1timeidle=0;//空闲计时器清零
-    }
-}
-#endif
-#if EN_USART2_RX   //如果使能了接收
 
-void USART2_IRQHandler(void)                	//串口1中断服务程序
-{
-    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
-    {
-        USART_ClearITPendingBit(USART2, USART_IT_RXNE);
-        byte  inch = USART_ReceiveData(USART2);	//读取接收到的数据
-
-        
-    }
-}
-#endif
 #ifdef __cplusplus
-}
+    extern "C"
+    {
+    #endif 
+    #if EN_USART1_RX   //如果使能了接收
+        //串口1中断服务程序
+        //注意,读取USARTx->SR能避免莫名其妙的错误
+        /*
+         */
+        void USART1_IRQHandler(void) //串口1中断服务程序
+        {
+            if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+            //接收到一字节
+            {
+                USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+                byte inch = USART1->DR; //读取接收到的数据
+
+                com1buf.Push(inch);
+                com1timeidle = 0; //空闲计时器清零
+            }
+        }
+    #endif 
+    #if EN_USART2_RX   //如果使能了接收
+
+        void USART2_IRQHandler(void) //串口1中断服务程序
+        {
+            if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+            //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+            {
+                USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+                byte inch = USART_ReceiveData(USART2); //读取接收到的数据
+
+
+            }
+        }
+    #endif 
+    #ifdef __cplusplus
+    }
 #endif
