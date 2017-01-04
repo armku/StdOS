dc23de64765d28c6786a3b0f9abe5db68245ce68
Ace <258613763@qq.com>
2017-01-04 14:41:05 +0800

    代码升级

--- SYSTEM/Peripheral/inc/Port.h
+++ SYSTEM/Peripheral/inc/Port.h
@@ -1,31 +1,15 @@
 #pragma once 
 
-#include "Type.h"
+#include "Sys.h"
+#include "stm32f10x.h"
 
-typedef enum
-{
-    AIN = 0x0,  //模拟输入 
-    INPUT = 0x04,  //输入
-    INPUT_PD = 0x28,  //浮空输入
-    INPUT_PU = 0x48,  //推挽式输入
-    OUTPUT_OD = 0x14,  //开漏输出
-    OUTPUT_PP = 0x10,  //推挽式输出
-    AF_OD = 0x1C,  //开漏复用输出
-    AF_PP = 0x18  //推挽复用输出
-} PIN_MODE;
-/* 针脚 ------------------------------------------------------------------*/
-//引脚定义
-typedef enum
-{
-	PA0,PA1,PA2,PA3,PA4,PA5,PA6,PA7,PA8,PA9,PA10,PA11,PA12,PA13,PA14,PA15,
-	PB0,PB1,PB2,PB3,PB4,PB5,PB6,PB7,PB8,PB9,PB10,PB11,PB12,PB13,PB14,PB15,
-	PC0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,PC11,PC12,PC13,PC14,PC15,
-	PD0,PD1,PD2,PD3,PD4,PD5,PD6,PD7,PD8,PD9,PD10,PD11,PD12,PD13,PD14,PD15,
-	PE0,PE1,PE2,PE3,PE4,PE5,PE6,PE7,PE8,PE9,PE10,PE11,PE12,PE13,PE14,PE15,
-	PF0,PF1,PF2,PF3,PF4,PF5,PF6,PF7,PF8,PF9,PF10,PF11,PF12,PF13,PF14,PF15,
-	PG0,PG1,PG2,PG3,PG4,PG5,PG6,PG7,PG8,PG9,PG10,PG11,PG12,PG13,PG14,PG15,
-	P0 = 0XFFFF
-}Pin;
+#ifdef STM32F4
+    #define GPIO_MAX_SPEED 100
+#else 
+    #define GPIO_MAX_SPEED 50
+#endif 
+
+
 
 class Port
 {
@@ -34,15 +18,73 @@ class Port
 		ushort  PinBit; // 组内引脚位。每个引脚一个位
 		Port& Set(Pin pin); // 设置引脚，并应用配置。
 		bool Empty() const { return _Pin == P0; }
-		//virtual void Config(); // 确定配置,确认用对象内部的参数进行初始化
+		virtual void Config(); // 确定配置,确认用对象内部的参数进行初始化
 		// 配置过程，由Config调用，最后GPIO_Init
 		//virtual void OnConfig(GPIO_InitTypeDef& gpio);
-	public:
-		Port();
-		
-		
-		
-		
-	
-    
+	protected:
+        Port();
+        virtual ~Port();
+};
+// 输出口
+class OutputPort: public Port
+{
+    public:
+        bool OpenDrain; // 是否开漏输出
+        bool Invert; // 是否倒置输入输出
+        uint Speed; // 速度
+
+        OutputPort()
+        {
+                Init();
+        }
+        // 普通输出一般采用开漏输出，需要倒置
+        OutputPort(Pin pin, bool invert = false, bool openDrain = false, uint speed = GPIO_MAX_SPEED)
+        {
+                Init(invert, openDrain, speed);
+                Set(pin);
+        }
+
+        // 整体写入所有包含的引脚
+        void Write(bool value);
+        void WriteGroup(ushort value); // 整组写入
+        void Up(uint ms); // 拉高一段时间后拉低
+        void Blink(uint times, uint ms); // 闪烁多次
+
+        ushort ReadGroup(); // 整组读取
+        // 读取指定索引引脚。索引按照从小到大，0xFF表示任意脚为true则返回true
+        bool Read(byte index);
+        bool Read(); // Read() ReadReal() 的区别在  前者读输出  一个读输入    在开漏输出的时候有很大区别
+        bool ReadInput();
+
+        static bool Read(Pin pin);
+        static void Write(Pin pin, bool value);
+
+        OutputPort &operator = (bool value)
+        {
+                Write(value);
+                return  *this;
+        }
+        OutputPort &operator = (OutputPort &port)
+        {
+                Write(port.Read());
+                return  *this;
+        }
+        operator bool()
+        {
+                return Read();
+        }
+
+    protected:
+        virtual void OnConfig(GPIO_InitTypeDef &gpio);
+
+        void Init(bool invert = false, bool openDrain = false, uint speed = GPIO_MAX_SPEED)
+        {
+                OpenDrain = openDrain;
+                Speed = speed;
+                Invert = invert;
+        }
+
+        #if DEBUG
+            virtual bool OnReserve(Pin pin, bool flag);
+        #endif 
 };
