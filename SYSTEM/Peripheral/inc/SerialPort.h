108e3c6627cab3e798f722074f8d039125ea3ae5
Ace <258613763@qq.com>
2017-01-04 10:36:45 +0800

    优化部分串口

--- SYSTEM/Peripheral/inc/SerialPort.h
+++ SYSTEM/Peripheral/inc/SerialPort.h
@@ -2,12 +2,23 @@
 
 #include "ITransport.h"
 #include "Sys.h"
+#include "AlternatePort.h"
+
 
 //回调函数定义 down指示按下还是弹起触发
 typedef void(*BtnInCallback)(ITransport* transport,Buffer & bs);
 
 class SerialPort:public ITransport
 {
+	private:
+    byte _index;
+    byte _parity;
+    byte _dataBits;
+    byte _stopBits;
+    int _baudRate;
+	
+	//AlternatePort _tx;
+	
 	public:
 		SerialPort(MessagePort_T comport=COM1,uint baudrate=115200);
 		void Open();//打开
