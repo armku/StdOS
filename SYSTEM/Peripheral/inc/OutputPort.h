1bbbc803e4001b37f05e412150f0f29bd30a69fb
Ace <258613763@qq.com>
2017-01-04 13:53:43 +0800

    代码优化

--- SYSTEM/Peripheral/inc/OutputPort.h
+++ SYSTEM/Peripheral/inc/OutputPort.h
@@ -2,13 +2,3 @@
 
 #include "Port.h"
 
-class OutputPort:public Port
-{
-	public:
-		OutputPort(Pin pin,byte invert=2);
-		void Init(Pin pin,byte invert=2);
-		void Write(bool onoff);//输出开关
-		void Open();		
-	private:
-		bool binvert;//端口反转
-};
