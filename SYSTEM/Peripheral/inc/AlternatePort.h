7586d99960f87c3d02608c7d6692e79f27d0f438
Ace <258613763@qq.com>
2017-01-07 15:23:53 +0800

    优化部分代码

--- SYSTEM/Peripheral/inc/AlternatePort.h
+++ SYSTEM/Peripheral/inc/AlternatePort.h
@@ -8,15 +8,10 @@ class AlternatePort: public OutputPort
 {
     public:
         AlternatePort(): OutputPort()
-        {
-                Init(false, false);
-        }
+        {}
         // 复用输出一般采用推挽输出，不需要倒置
-        AlternatePort(Pin pin, bool invert = false, bool openDrain = false, uint speed = GPIO_MAX_SPEED): OutputPort()
-        {
-                Init(invert, openDrain, speed);
-                Set(pin);
-        }
+        AlternatePort(Pin pin, bool invert = false, bool openDrain = false, uint speed = GPIO_MAX_SPEED): OutputPort(pin,invert,openDrain,speed)
+        {}
 
     protected:
         virtual void OnConfig(GPIO_InitTypeDef &gpio);
