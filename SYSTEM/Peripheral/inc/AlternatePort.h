824e2ed7b1de532bd65546c91d66708244d54221
Ace <258613763@qq.com>
2017-01-10 15:54:09 +0800

    修复端口初始化bug，pwm可用

--- SYSTEM/Peripheral/inc/AlternatePort.h
+++ SYSTEM/Peripheral/inc/AlternatePort.h
@@ -8,10 +8,15 @@ class AlternatePort: public OutputPort
 {
     public:
         AlternatePort(): OutputPort()
-        {}
+        {
+			Init(false,false);
+		}
         // 复用输出一般采用推挽输出，不需要倒置
         AlternatePort(Pin pin, bool invert = false, bool openDrain = false, uint speed = GPIO_MAX_SPEED): OutputPort(pin,invert,openDrain,speed)
-        {}
+        {
+			Init(invert,openDrain,speed);
+			Set(pin);
+		}
 
     protected:
         virtual void OnConfig(GPIO_InitTypeDef &gpio);
