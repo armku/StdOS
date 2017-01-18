f4be55d2da122deaecf97f04ded8f54913cfc0a2
Ace <258613763@qq.com>
2017-01-18 14:58:45 +0800

    结构优化

--- User/inc/Data.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void STDInit();//系统初始化
--- User/main.cpp
+++ User/main.cpp
@@ -1,4 +1,156 @@
-#include "Data.h"
+#include <stdio.h>
+#include "Type.h"
+#include "stm32f10x.h"
+#include "SerialPortOld.h"
+#include "W24xxx.h"
+#include "W25qxx.h"
+#include "multi_button.h"
+#include "InputPortOld.h"
+#include "Sys.h"
+#include "WatchDog.h"
+#include "Port.h"
+
+//sequence 序列号 cur_seq max_seq step
+
+void ledflash();
+void eepread();
+//1ms软件定时器
+void softTimers();
+OutputPort led1(PB0,true);
+OutputPort led2(PF7,true);
+OutputPort led3(PF8,true);
+CW24xxx  w24c02(PB6,PB7,AT24C02);
+//按键 PC13 PA0
+
+InputPortOld exti(PC13);//PA1 PB3
+ 
+WatchDog dog(3000);
+void feeddog()
+{
+	dog.Feed();
+}
+
+void OnKeyPress(Pin pin, bool onoff)
+{
+	led1.Write(onoff);
+	led2=!led2;
+	printf("中断引脚：%d 值%d \n",pin,onoff);
+}
+static uint OnUsartRead(ITransport* transport,Buffer& bs,void* para)
+{
+	SerialPortOld* sp =(SerialPortOld*)para;
+	debug_printf("%s 收到：[%d]",sp->Name,bs.Length());
+	bs.Show(true);		
+	sp->SendBuffer(bs.GetBuffer());
+	
+	return 0;
+}
+SerialPortOld sp1(COM1);
+SerialPortOld sp2(COM2);
+SerialPortOld sp3(COM3);
+/*
+KEY PA0 
+*/
+//系统初始化
+void ComTimers();
+void STDInit() 
+{    		
+	Sys.MessagePort=COM1;
+	Sys.Init();
+	Sys.ShowInfo();
+	sp1.Register(OnUsartRead,&sp1);
+	sp2.Register(OnUsartRead,&sp2);
+	sp3.Register(OnUsartRead,&sp3);
+	sp1.Open();
+	sp2.Open();
+	sp3.Open();
+	    
+	exti.InitOld();
+	exti.On();	
+	exti.RegisterOld(OnKeyPress);
+		
+	//w24c02.Test();
+		
+	Sys.AddTask(softTimers,0,1,1,"1毫秒软件定时器");//1毫秒周期循环
+	Sys.AddTask(ComTimers,0,1,1,"串口数据接收定时器");//1毫秒周期循环
+	Sys.AddTask(feeddog, 0, 0, 10, "看门狗"); //看门狗-喂狗
+	Sys.AddTask(ledflash,0,5,50,"状态指示灯");
+	Sys.AddTask(eepread,0,10,1000,"测试任务");
+	Sys.Start();
+}
+
+void ledflash()
+{
+	//led1=!led1;
+	//led2=!led2;
+	led3=!led3;	
+}
+
+void eepread()
+{
+	//sp1.SendBuffer("COM1\n");
+	sp2.SendBuffer("COM2\n");
+	sp3.SendBuffer("COM3\n");
+}
+
+//1ms软件定时器
+void softTimers()
+{
+    
+}
+byte USART_RX_BUF[100]; //接收缓冲,最大USART_REC_LEN个字节.
+extern uint com1timeidle; //串口1空闲时间
+extern CFIFORing com1buf; //串口1接收缓冲区
+
+extern uint com2timeidle; //串口2空闲时间
+extern CFIFORing com2buf; //串口2接收缓冲区
+
+extern uint com3timeidle; //串口3空闲时间
+extern CFIFORing com3buf; //串口3接收缓冲区
+//串口接收通信定时器
+void ComTimers()
+{
+	com1timeidle++;
+	com2timeidle++;
+	com3timeidle++;
+
+    if (com1timeidle > 3)
+    {
+        com1timeidle = 0;
+        ushort len = com1buf.GetLength();
+        if (len >= 1)
+        {
+            com1buf.Pop(USART_RX_BUF, 0, len);
+			sp1.OnUsartReceive(USART_RX_BUF,len);
+        }
+        com1buf.Reset();		
+    }
+	
+	if (com2timeidle > 3)
+    {
+        com2timeidle = 0;
+        ushort len = com2buf.GetLength();
+        if (len >= 1)
+        {
+            com2buf.Pop(USART_RX_BUF, 0, len);
+			sp2.OnUsartReceive(USART_RX_BUF,len);
+        }
+        com2buf.Reset();		
+    }
+	
+	if (com3timeidle > 3)
+    {
+        com3timeidle = 0;
+        ushort len = com3buf.GetLength();
+        if (len >= 1)
+        {
+            com3buf.Pop(USART_RX_BUF, 0, len);
+			sp3.OnUsartReceive(USART_RX_BUF,len);
+        }
+        com3buf.Reset();		
+    }
+}
+
 
 int main(void)
 {
