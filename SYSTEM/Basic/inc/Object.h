f097fadac0cd62ba755238cfb89e5fc9d8607808
Ace <258613763@qq.com>
2017-01-19 16:37:58 +0800

    代码整理

--- SYSTEM/Basic/inc/Object.h
+++ SYSTEM/Basic/inc/Object.h
@@ -5,20 +5,23 @@
 对象转字符串 ToStr/ToString 的默认实现时显示类名。
 显示对象 Show 的默认实现是通过板子的调试串口输出。
 
-*/
+ */
 #pragma once
 
 class CType;
+class String;
 
 //根对象
 class Object
 {
-	public:
-		//输出对象的字符串表示方式
-		//virtual String & ToStr(String& str) const;
-		//输出对象的字符串表示方式.支持RVO优化
-		//virtual String& ToString() const;
-		//显示对象。默认显示 ToString
-		virtual void Show(bool newLine=false) const;
-		const CType GetType() const;
+    public:
+        #if 0
+            //输出对象的字符串表示方式
+            virtual String &ToStr(String &str)const;
+            //输出对象的字符串表示方式.支持RVO优化
+            virtual String &ToString()const;
+        #endif 
+        //显示对象。默认显示 ToString
+        virtual void Show(bool newLine = false)const;
+        const CType GetType()const;
 };
--- SYSTEM/Basic/src/Object.cpp
+++ SYSTEM/Basic/src/Object.cpp
@@ -1,10 +1,20 @@
 #include "stdio.h"
 #include "Object.h"
 
-void Object::Show(bool newLine) const
+#if 0
+    //输出对象的字符串表示方式
+    virtual String &Object::ToStr(String &str)const{}
+    //输出对象的字符串表示方式.支持RVO优化
+    virtual String &Object::ToString()const{}
+#endif 
+void Object::Show(bool newLine)const
 {
-	if(newLine)
-	{
-		printf("\n");
-	}
+    if (newLine)
+    {
+        printf("\n");
+    }
 }
+
+#if 0
+    const CType Object::GetType()const{}
+#endif
