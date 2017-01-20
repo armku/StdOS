e9b51e8a6e9b096946a5ee0cfc3c8cd2cd658b0c
Ace <258613763@qq.com>
2017-01-20 11:34:05 +0800

    代码整理

--- SYSTEM/Basic/inc/DateTime.h
+++ SYSTEM/Basic/inc/DateTime.h
@@ -1,6 +1,18 @@
 #pragma once
+#include "Type.h"
+#include "Object.h"
 
 class DateTime
 {
 	public:
+		ushort Year;
+		byte Month;
+		byte Day;
+		byte Hour;
+		byte Minute;
+		byte Second;
+		ushort Ms;
+	
+		DateTime();
+	DateTime(ushort year,byte month,byte day);
 };
