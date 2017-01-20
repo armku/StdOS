ea77d8cb0a27d1fd4c7413b0cc48000612b09514
Ace <258613763@qq.com>
2017-01-20 12:27:44 +0800

    代码整理

--- SYSTEM/Basic/inc/DateTime.h
+++ SYSTEM/Basic/inc/DateTime.h
@@ -4,15 +4,37 @@
 
 class DateTime
 {
-	public:
-		ushort Year;
-		byte Month;
-		byte Day;
-		byte Hour;
-		byte Minute;
-		byte Second;
-		ushort Ms;
-	
-		DateTime();
-	DateTime(ushort year,byte month,byte day);
+    public:
+        ushort Year;
+        byte Month;
+        byte Day;
+        byte Hour;
+        byte Minute;
+        byte Second;
+        ushort Ms;
+
+        DateTime();
+        DateTime(ushort year, byte month, byte day);
+        DateTime(uint seconds);
+        DateTime(const DateTime &value);
+        #if 0
+            DateTime(DateTime && value);
+        #endif 
+        //重载等号运算符
+        DateTime &operator = (uint seconds);
+        DateTime &operator = (const DateTime &value);
+
+        DateTime &Parse(uint seconds);
+        DateTime &ParseMs(UInt64 ms);
+
+        uint TotalDays()const;
+        uint TotalSeconds()const;
+        UInt64 TotalMs()const;
+
+        //获取星期
+        byte DayOfWeek()const;
+        //取时间日期的日期部分
+        DateTime Date()const;
+		
+		static void Now();//当前时间
 };
--- SYSTEM/Basic/src/DateTime.cpp
+++ SYSTEM/Basic/src/DateTime.cpp
@@ -1,2 +1,66 @@
 #include "DateTime.h"
 
+DateTime::DateTime()
+{
+}
+DateTime::DateTime(ushort year, byte month, byte day)
+{
+	this->Year=year;
+	this->Month=month;
+	this->Day=day;
+}
+DateTime::DateTime(uint seconds)
+{
+	
+}
+DateTime::DateTime(const DateTime &value)
+{
+	
+}
+#if 0
+DateTime::DateTime(DateTime && value)
+{
+}
+#endif
+DateTime &DateTime::operator = (uint seconds)
+{
+	return *this;
+}
+DateTime &DateTime::operator = (const DateTime &value)
+{
+	return *this;
+}
+DateTime &DateTime::Parse(uint seconds)
+{
+	return *this;
+}
+DateTime &DateTime::ParseMs(UInt64 ms)
+{
+	return *this;
+}
+uint DateTime::TotalDays()const
+{
+	
+	return 0;
+}
+uint DateTime::TotalSeconds()const
+{	
+	return 0;
+}
+UInt64 DateTime::TotalMs()const
+{	
+	return 0;
+}
+byte DateTime::DayOfWeek()const
+{
+	return 0;
+}
+DateTime DateTime::Date()const
+{
+	return *this;
+}
+//当前时间
+void DateTime::Now()
+{
+	
+}
