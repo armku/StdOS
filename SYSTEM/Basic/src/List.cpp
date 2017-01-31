3a9e11f1d7c210dfd628c42fa0a2db552f8a3e33
Ace <258613763@qq.com>
2017-01-31 16:10:37 +0800

    代码整理

--- SYSTEM/Basic/src/List.cpp
+++ SYSTEM/Basic/src/List.cpp
@@ -1,5 +1,21 @@
 #include "List.h"
 #include "stdio.h"
+/*
+List<float> floatList;
+
+for(int i=1;i<7;i++)
+{
+floatList.Add(* new float(i+0.6));
+}
+floatList.PrintList();
+float b=3.6;
+float *pa=floatlist.Find(b);
+if(pa)
+{
+	floatList.Remove(*pa);
+}
+floatList.PrintList();
+*/
 
 template <class T> List<T>::List()
 {
