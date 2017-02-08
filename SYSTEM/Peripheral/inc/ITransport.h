e5f475d88cdf60604ce66fe7ea41e1ed4586a7d4
Ace <258613763@qq.com>
2017-02-08 16:32:17 +0800

    代码整理

--- SYSTEM/Peripheral/inc/ITransport.h
+++ SYSTEM/Peripheral/inc/ITransport.h
@@ -8,25 +8,36 @@ class SerialPortOld;
 #define NULL 0
 
 //回调函数定义,当有数据到达时触发
-typedef uint (*IOnUsartRead)(ITransport* transport,Buffer& bs,void* param);
+typedef uint(*IOnUsartRead)(ITransport *transport, Buffer &bs, void *param);
 
 
-typedef uint (*TransportHandler)(ITransport* transport,Buffer& bs,void* param); //临时不能用
+typedef uint(*TransportHandler)(ITransport *transport, Buffer &bs, void *param); //临时不能用
 
 class ITransport
 {
-	public:
-	void Open(){}//打开端口
-	bool HasHandler(){return false;}
-	uint Read(byte *buf,uint len){return 10;}
-	uint Write(byte *buf,uint len){return len;}
-	int OnReceive(byte *buf,uint len){return len;}
-	void SendData(byte data);		
-	void SendBuffer(byte *buff,int length=-1);//发送数据
-	void SendBuffer(char *buff,int length=-1);//发送数据
-	void Register(IOnUsartRead handler,void *param = NULL); // 注册数据到达事件
-	
-	
+    public:
+        void Open(){}
+        //打开端口
+        bool HasHandler()
+        {
+                return false;
+        }
+        uint Read(byte *buf, uint len)
+        {
+                return 10;
+        }
+        uint Write(byte *buf, uint len)
+        {
+                return len;
+        }
+        int OnReceive(byte *buf, uint len)
+        {
+                return len;
+        }
+        void SendData(byte data);
+        void SendBuffer(byte *buff, int length =  - 1); //发送数据
+        void SendBuffer(char *buff, int length =  - 1); //发送数据
+        void Register(IOnUsartRead handler, void *param = NULL); // 注册数据到达事件
+		bool Opened;//是否打开
+
 };
-
-
