616100e5403f69f0e65ded81b88f3178c8d77535
Ace <258613763@qq.com>
2017-02-08 16:18:52 +0800

    代码整理

--- SYSTEM/Peripheral/inc/SerialPort.h
+++ SYSTEM/Peripheral/inc/SerialPort.h
@@ -5,6 +5,7 @@
 #include "stm32f10x.h"
 #include "Port.h"
 #include "InputPort.h"
+#include "CString.h"
 
 // 串口类
 class SerialPort: public ITransport
@@ -56,12 +57,12 @@ class SerialPort: public ITransport
         bool Flush(uint times = 3000);
         void GetPins(Pin *txPin, Pin *rxPin);
         virtual void Register(TransportHandler handler, void *param = NULL);
-        #if 0
-            virtual string ToString()
-            {
-                    return Name;
-            }
-        #endif 		
+
+        virtual String ToString()
+        {
+                return Name;
+        }
+
         static SerialPort *GetMessagePort();
     protected:
         virtual bool OnOpen();
@@ -71,26 +72,26 @@ class SerialPort: public ITransport
     private:
         static void OnUsartReceive(ushort num, void *param);
 };
-class SerialPortOld:public ITransport
+class SerialPortOld: public ITransport
 {
     public:
-		//115200 无奇偶校验 8位数据长度 1位停止位
-        SerialPortOld(COM_Def index, int baudRate = 115200, byte parity = USART_Parity_No,byte dataBits = USART_WordLength_8b, byte stopBits = USART_StopBits_1);
-		char Name[5]; // 名称。COMx，后面1字节\0表示结束
-		void SendData(byte data);
-		void SendBuffer(byte *buff,int length=-1);//发送数据
-		void SendBuffer(char *buff,int length=-1);//发送数据
-		void Open();
-		void Register(IOnUsartRead handler,SerialPortOld *sp); // 注册数据到达事件	
-		void OnUsartReceive(byte * buf,ushort length);//从串口收到数据
-		OutputPort *RS485; // RS485使能引脚
+        //115200 无奇偶校验 8位数据长度 1位停止位
+        SerialPortOld(COM_Def index, int baudRate = 115200, byte parity = USART_Parity_No, byte dataBits = USART_WordLength_8b, byte stopBits = USART_StopBits_1);
+        char Name[5]; // 名称。COMx，后面1字节\0表示结束
+        void SendData(byte data);
+        void SendBuffer(byte *buff, int length =  - 1); //发送数据
+        void SendBuffer(char *buff, int length =  - 1); //发送数据
+        void Open();
+        void Register(IOnUsartRead handler, SerialPortOld *sp); // 注册数据到达事件	
+        void OnUsartReceive(byte *buf, ushort length); //从串口收到数据
+        OutputPort *RS485; // RS485使能引脚
     private:
         byte _index;
         byte _parity;
         byte _dataBits;
         byte _stopBits;
         int _baudRate;
-		IOnUsartRead OnRcv; 
-		AlternatePort tx;
-		InputPort rx;
+        IOnUsartRead OnRcv;
+        AlternatePort tx;
+        InputPort rx;
 };
