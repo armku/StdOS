1f41d9fe19d6eafc12877985665af94aa3b04e1a
Ace <258613763@qq.com>
2017-02-13 14:18:21 +0800

    代码整理

--- SYSTEM/CORE/src/TInterrupt.cpp
+++ SYSTEM/CORE/src/TInterrupt.cpp
@@ -32,81 +32,39 @@ extern SerialPort *_printf_sp;
      */
     void USART1_IRQHandler(void) //串口1中断服务程序
     {
-        #if 0
-            if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
-            //接收到一字节
-            {
-
-                USART_ClearITPendingBit(USART1, USART_IT_RXNE);
-                byte inch = USART1->DR; //读取接收到的数据
-                printf("COM1 in data \r\n");
-                com1buf.Push(inch);
-                com1timeidle = 0; //空闲计时器清零
-            }
-        #else 
-//            if (_printf_sp)
-//            {
-//				
-//                SerialPort::OnUsartReceive(0, _printf_sp);
-//            }
+        
 			if(onSerialPortRcv[0])
 			{
 				SerialPort::OnUsartReceive(0, onSerialPortRcv[0]);
 			}
-        #endif 
     }	
 void USART2_IRQHandler(void) //串口1中断服务程序
-    {
-		#if 0
-        if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
-        {
-            USART_ClearITPendingBit(USART2, USART_IT_RXNE);
-            byte inch = USART_ReceiveData(USART2); //读取接收到的数据
-
-//            com2buf.Push(inch);
-//            com2timeidle = 0; //空闲计时器清零
-        }
-		#else
+    {		
 		if(onSerialPortRcv[1])
 			{
 				SerialPort::OnUsartReceive(1, onSerialPortRcv[1]);
 			}
-		#endif
     }
     void USART3_IRQHandler(void) //串口1中断服务程序
-    {
-		#if 0
-        if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
-        {
-            USART_ClearITPendingBit(USART3, USART_IT_RXNE);
-            byte inch = USART_ReceiveData(USART3); //读取接收到的数据
-            printf("com3 in data \r\n");
-//            com3buf.Push(inch);
-//            com3timeidle = 0; //空闲计时器清零
-        }
-		#else
+    {		
 		if(onSerialPortRcv[2])
 			{
 				SerialPort::OnUsartReceive(2, onSerialPortRcv[2]);
 			}
-		#endif
     }
     void USART4_IRQHandler(void) //串口1中断服务程序
     {
-        if (USART_GetITStatus(UART4, USART_IT_RXNE) != RESET)
-        {
-            USART_ClearITPendingBit(UART4, USART_IT_RXNE);
-            byte inch = USART_ReceiveData(UART4); //读取接收到的数据
-        }
+        if(onSerialPortRcv[3])
+			{
+				SerialPort::OnUsartReceive(3, onSerialPortRcv[3]);
+			}
     }
     void USART5_IRQHandler(void) //串口1中断服务程序
     {
-        if (USART_GetITStatus(UART5, USART_IT_RXNE) != RESET)
-        //接收中断(接收到的数据必须是0x0d 0x0a结尾)
-        {
-            USART_ClearITPendingBit(UART5, USART_IT_RXNE);
-            byte inch = USART_ReceiveData(UART5); //读取接收到的数据
-        }
+        if(onSerialPortRcv[4])
+			{
+				SerialPort::OnUsartReceive(4, onSerialPortRcv[4]);
+			}
     }	
 		
 		    #ifdef __cplusplus
--- User/main.cpp
+++ User/main.cpp
@@ -39,59 +39,6 @@ void OnKeyPress(Pin pin, bool onoff)
     printf("中断引脚：%d 值%d \n", pin, onoff);
 }
 
-//SerialPortOld sp1(COM1);
-//SerialPortOld sp2(COM2);
-//SerialPortOld sp3(COM3);
-byte USART_RX_BUF[100]; //接收缓冲,最大USART_REC_LEN个字节.
-extern uint com1timeidle; //串口1空闲时间
-extern CFIFORing com1buf; //串口1接收缓冲区
-extern uint com2timeidle; //串口2空闲时间
-extern CFIFORing com2buf; //串口2接收缓冲区
-extern uint com3timeidle; //串口3空闲时间
-extern CFIFORing com3buf; //串口3接收缓冲区
-//串口接收通信定时器
-void ComTimers(void *param)
-{
-    com1timeidle++;
-    com2timeidle++;
-    com3timeidle++;
-
-    if (com1timeidle > 3)
-    {
-        com1timeidle = 0;
-        ushort len = com1buf.GetLength();
-        if (len >= 1)
-        {
-            com1buf.Pop(USART_RX_BUF, 0, len);
-            //sp1.OnUsartReceive(USART_RX_BUF, len);
-        }
-        com1buf.Reset();
-    }
-
-    if (com2timeidle > 3)
-    {
-        com2timeidle = 0;
-        ushort len = com2buf.GetLength();
-        if (len >= 1)
-        {
-            com2buf.Pop(USART_RX_BUF, 0, len);
-            //sp2.OnUsartReceive(USART_RX_BUF, len);
-        }
-        com2buf.Reset();
-    }
-
-    if (com3timeidle > 3)
-    {
-        com3timeidle = 0;
-        ushort len = com3buf.GetLength();
-        if (len >= 1)
-        {
-            com3buf.Pop(USART_RX_BUF, 0, len);
-            //sp3.OnUsartReceive(USART_RX_BUF, len);
-        }
-        com3buf.Reset();
-    }
-}
 /*
 ISO-V2:PB5控制485方向
 ISO-V3:PC2控制485方向
@@ -115,13 +62,7 @@ int main(void)
     Sys.MessagePort = COM3;
     Sys.Init();
     Sys.ShowInfo();
-    //sp1.Register(OnUsartRead, &sp1);
-    //sp2.Register(OnUsartRead, &sp2);
-    //sp3.Register(OnUsartRead, &sp3);
-    //sp1.Open();
-    //sp2.Open();
-    //sp3.Open();
-
+   
     //sp2.RS485 = &rs485;
     rs485 = 1;
     
@@ -140,7 +81,6 @@ int main(void)
 	
 	SerialPort::GetMessagePort()->Register(OnUsartRead);
 	
-    Sys.AddTask(ComTimers, 0, 1, 1, "串口接收"); //1毫秒周期循环
     Sys.AddTask(feeddog, 0, 0, 1000, "看门狗"); //看门狗-喂狗
     Sys.AddTask(ledflash, 0, 5, 500, "状态指示灯"); 
 
