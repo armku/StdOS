8c3d29c0b94deaadfcb7c6a36d1effc57cf88c67
Ace <258613763@qq.com>
2017-03-01 16:40:56 +0800

    任务显示优化

--- SYSTEM/CORE/src/Task.cpp
+++ SYSTEM/CORE/src/Task.cpp
@@ -25,82 +25,82 @@ if(ID) _Scheduler->Remove(ID);
 // 显示状态
 void Task::ShowStatus()
 {
-	debug_printf("Task::%-12s %2d", Name, ID);
-	if(Times>1000000)
-	{
-		debug_printf("[%d]",Times);
-	}
-	else if(Times>100000)
-	{
-		debug_printf("[%d] ",Times);
-	}
-	else if(Times>10000)
-	{
-		debug_printf("[%d]  ",Times);
-	}
-	else if(Times>1000)
-	{
-		debug_printf("[%d]   ",Times);
-	}
-	else if(Times>100)
-	{
-		debug_printf("[%d]    ",Times);
-	}
-	else if(Times>10)
-	{
-		debug_printf("[%d]     ",Times);
-	}
-	else
-	{
-		debug_printf("[%d]      ",Times);
-	}
-	float cpuPercent=0;
-	cpuPercent=0;
-	cpuPercent=this->Cost*this->Times/10;
-	cpuPercent/=Sys.Ms();
-	
-	if(cpuPercent>10)
-	{
-		debug_printf(" 平均[%4.1f%%] ", cpuPercent);
-	}
-	else  
-	{
-		debug_printf(" 平均[%0.2f%%] ", cpuPercent);
-	}
-	
-	if(Cost>1000)
-	{
-		debug_printf("%3ums", Cost/1000);
-	}
-	else
-	{
-		debug_printf("%3uus", Cost);
-	}
-	if(CpuTime>1000000)
-	{
-		debug_printf(" 最大 %3ds ", CpuTime/1000000);
-	}
-	else if(CpuTime>1000)
-	{
-		debug_printf(" 最大 %3dms", CpuTime/1000);
-	}
-	else
-	{
-		debug_printf(" 最大 %3dus", CpuTime);
-	}
+    debug_printf("Task::%-12s %2d", Name, ID);
+    if (Times >= 1000000)
+    {
+        debug_printf("[%d]", Times);
+    }
+    else if (Times >= 100000)
+    {
+        debug_printf("[%d] ", Times);
+    }
+    else if (Times >= 10000)
+    {
+        debug_printf("[%d]  ", Times);
+    }
+    else if (Times >= 1000)
+    {
+        debug_printf("[%d]   ", Times);
+    }
+    else if (Times >= 100)
+    {
+        debug_printf("[%d]    ", Times);
+    }
+    else if (Times >= 10)
+    {
+        debug_printf("[%d]     ", Times);
+    }
+    else
+    {
+        debug_printf("[%d]      ", Times);
+    }
+    float cpuPercent = 0;
+    cpuPercent = 0;
+    cpuPercent = this->Cost *this->Times / 10;
+    cpuPercent /= Sys.Ms();
+
+    if (cpuPercent >= 10)
+    {
+        debug_printf(" 平均[%4.1f%%] ", cpuPercent);
+    }
+    else
+    {
+        debug_printf(" 平均[%0.2f%%] ", cpuPercent);
+    }
+
+    if (Cost >= 1000)
+    {
+        debug_printf("%3ums", Cost / 1000);
+    }
+    else
+    {
+        debug_printf("%3uus", Cost);
+    }
+    if (CpuTime >= 1000000)
+    {
+        debug_printf(" 最大 %3ds ", CpuTime / 1000000);
+    }
+    else if (CpuTime >= 1000)
+    {
+        debug_printf(" 最大 %3dms", CpuTime / 1000);
+    }
+    else
+    {
+        debug_printf(" 最大 %3dus", CpuTime);
+    }
     if (this->Period >= 1000000)
     {
-        debug_printf(" 周期 %3lds ",  Period / 1000000);
+        debug_printf(" 周期 %3lds ", Period / 1000000);
     }
     else if (this->Period >= 1000)
     {
-        debug_printf(" 周期 %3ldms",  Period / 1000);
+        debug_printf(" 周期 %3ldms", Period / 1000);
     }
     else
     {
         debug_printf(" 周期 %3ldus", Period);
     }
-	debug_printf(" %s\r\n",this->Enable ? " " : "禁用");
+    debug_printf(" %s\r\n", this->Enable ? " " : "禁用");
 }
 
 bool Task::operator == (Task &tsk)
--- SYSTEM/CORE/src/TaskScheduler.cpp
+++ SYSTEM/CORE/src/TaskScheduler.cpp
@@ -27,15 +27,15 @@ TaskScheduler::~TaskScheduler()
 // 创建任务，返回任务编号。dueTime首次调度时间ms，period调度间隔ms，-1表示仅处理一次
 uint TaskScheduler::Add(Action func, void *param, long dueTime, long period, const char *name)
 {
-	if(dueTime >0)
-	{
-		dueTime*=1000;
-	}
-	if(period>0)
-	{
-		period*=1000;
-	}
-	
+    if (dueTime > 0)
+    {
+        dueTime *= 1000;
+    }
+    if (period > 0)
+    {
+        period *= 1000;
+    }
+
     Task *task = new Task(this);
     task->ID = mgid++;
     task->Callback = func;
@@ -43,36 +43,36 @@ uint TaskScheduler::Add(Action func, void *param, long dueTime, long period, con
     task->Period = period;
     task->NextTime = Time.Current() + dueTime;
     task->Name = name;
-	if(dueTime<0)
-	{
-		task->Enable=false;
-	}
+    if (dueTime < 0)
+    {
+        task->Enable = false;
+    }
 
     this->Count++;
     _Tasks.Add(task);
     #if DEBUG
-		debug_printf("%s::添加%2d %-11s",Name,task->ID,task->Name);
-		if(dueTime>=1000000)
-		{
-			debug_printf("First = %3lds ",dueTime/1000000);
-		}
-		else if(dueTime>=1000)
-		{
-			debug_printf("First = %3ldms",dueTime/1000);
-		}
-		else
-		{
-			debug_printf("First = %3ldus",dueTime);
-		}
-	
+        debug_printf("%s::添加%2d %-11s", Name, task->ID, task->Name);
+        if (dueTime >= 1000000)
+        {
+            debug_printf("First = %3lds ", dueTime / 1000000);
+        }
+        else if (dueTime >= 1000)
+        {
+            debug_printf("First = %3ldms", dueTime / 1000);
+        }
+        else
+        {
+            debug_printf("First = %3ldus", dueTime);
+        }
+
         // 输出长整型%ld，无符号长整型%llu
         if (period >= 1000000)
-        {           
-            debug_printf(" Period = %3lds \r\n",period/1000000);
+        {
+            debug_printf(" Period = %3lds \r\n", period / 1000000);
         }
         else if (period >= 1000)
         {
-            debug_printf(" Period = %3ldms\r\n", period/1000);
+            debug_printf(" Period = %3ldms\r\n", period / 1000);
         }
         else
         {
@@ -81,20 +81,22 @@ uint TaskScheduler::Add(Action func, void *param, long dueTime, long period, con
     #endif 
     return task->ID;
 }
+
 //设置任务执行、就绪状态
-void TaskScheduler::SetTask(uint taskid,bool onoff,long delaytime)
+void TaskScheduler::SetTask(uint taskid, bool onoff, long delaytime)
 {
-	if(taskid>this->Count)
-	{
-		//超范围，返回
-		return;
-	}
-	this->_Tasks[taskid]->Enable=onoff;
-	if(onoff)
-	{
-		this->_Tasks[taskid]->NextTime=Time.Current()+delaytime*1000;	
-	}
+    if (taskid > this->Count)
+    {
+        //超范围，返回
+        return ;
+    }
+    this->_Tasks[taskid]->Enable = onoff;
+    if (onoff)
+    {
+        this->_Tasks[taskid]->NextTime = Time.Current() + delaytime * 1000;
+    }
 }
+
 void TaskScheduler::Remove(uint taskid)
 {
     int i =  - 1;
@@ -122,7 +124,7 @@ void TaskScheduler::Start()
     }
 
     //Add(ShowTime, this, 2 *1000, 20 *1000, "时间显示");
-    Add(ShowStatus, this, 1*1000, 30*1000,"任务状态");
+    Add(ShowStatus, this, 1 *1000, 30 *1000, "任务状态");
 
     debug_printf("%s::准备就绪 开始循环处理%d个任务！\r\n\r\n", Name, Count);
 
@@ -151,7 +153,7 @@ void TaskScheduler::Execute(uint usMax)
 
     // 需要跳过当前正在执行任务的调度
     //Task* _cur = Current;
-	
+
     int i =  - 1;
     while (_Tasks.MoveNext(i))
     {
@@ -187,12 +189,12 @@ void TaskScheduler::Execute(uint usMax)
             }
             if (cost > 500000)
             {
-                debug_printf("Task::Execute 任务:%s %d [%d] 执行时间过长 %dus 睡眠 %dus\r\n",task->Name, task->ID, task->Times, cost, task->SleepTime);
+                debug_printf("Task::Execute 任务:%s %d [%d] 执行时间过长 %dus 睡眠 %dus\r\n", task->Name, task->ID, task->Times, cost, task->SleepTime);
             }
             // 如果只是一次性任务，在这里清理
             if (task->Period < 0)
             {
-				Remove(task->ID);
+                Remove(task->ID);
             }
         }
 
@@ -201,7 +203,7 @@ void TaskScheduler::Execute(uint usMax)
         {
             return ;
         }
-    }	
+    }
     // 如果有最小时间，睡一会吧
     now = Time.Current(); // 当前时间
     if (min != UInt64_Max && min > now)
@@ -219,54 +221,54 @@ void TaskScheduler::Execute(uint usMax)
 void TaskScheduler::ShowTime(void *param)
 {
     UInt64 curms = Time.Ms();
-    debug_printf("Time: %02lld:%02lld:%02lld.%03lld\n", curms / 3600000, curms / 60000 % 60, curms / 1000 % 60,curms%1000);
+    debug_printf("Time: %02lld:%02lld:%02lld.%03lld\n", curms / 3600000, curms / 60000 % 60, curms / 1000 % 60, curms % 1000);
 }
 
 // 显示状态
 void TaskScheduler::ShowStatus(void *param)
 {
-	static ulong runCounts=0;
-	float RunTimes=0;
-	float RunTimesAvg=0;
-	Task *tsk;
-	byte buf[1];
-	
-	runCounts++;
+    static ulong runCounts = 0;
+    float RunTimes = 0;
+    float RunTimesAvg = 0;
+    Task *tsk;
+    byte buf[1];
+
+    runCounts++;
     TaskScheduler *ts = (TaskScheduler*)param;
-	UInt64 curms = Time.Ms();
-	
-	//统计运行时间
-	RunTimes=0;
-	RunTimesAvg=0;
-	for(int j=0;j<ts->Count;j++)
-	{
-		tsk=ts->_Tasks[j];
-		RunTimes+=tsk->Cost*tsk->Times;
-		RunTimesAvg+=tsk->Cost;
-	}
-	RunTimesAvg/=ts->Count;
-	//SRAM   0X20000000-0X3FFFFFFF 共512MB
-	//SCODE  0X00000000-0X1FFFFFFF 共512MB
-	debug_printf("Task::%s [%llu]", "ShowStatus", runCounts);
-	debug_printf("负载 %0.2f%% ", RunTimes/10/curms);
-	if(RunTimesAvg>=1000)
-	{
-		debug_printf("平均 %3.1fms ", RunTimesAvg/ts->Count/1000);
-	}
-		else
-		{
-			debug_printf("平均 %3.0fus ", RunTimesAvg/ts->Count);
-		}
-	debug_printf("当前 1970-01-01 23 00:00");
-	debug_printf("启动 ");	
-    debug_printf("%02lld:%02lld:%02lld.%03lld ", curms / 3600000, curms / 60000 % 60, curms / 1000 % 60,curms%1000);
-	debug_printf("堆 %u/%u",&(buf[0])-0X20000000,1024);
-	debug_printf("\r\n");
-    
-	int i =  - 1;
-	Task *task=ts->Current;
-	task->ShowStatus();
-	while (ts->_Tasks.MoveNext(i))
+    UInt64 curms = Time.Ms();
+
+    //统计运行时间
+    RunTimes = 0;
+    RunTimesAvg = 0;
+    for (int j = 0; j < ts->Count; j++)
+    {
+        tsk = ts->_Tasks[j];
+        RunTimes += tsk->Cost *tsk->Times;
+        RunTimesAvg += tsk->Cost;
+    }
+    RunTimesAvg /= ts->Count;
+    //SRAM   0X20000000-0X3FFFFFFF 共512MB
+    //SCODE  0X00000000-0X1FFFFFFF 共512MB
+    debug_printf("Task::%s [%llu]", "ShowStatus", runCounts);
+    debug_printf("负载 %0.2f%% ", RunTimes / 10 / curms);
+    if (RunTimesAvg >= 1000)
+    {
+        debug_printf("平均 %3.1fms ", RunTimesAvg / ts->Count / 1000);
+    }
+    else
+    {
+        debug_printf("平均 %3.0fus ", RunTimesAvg / ts->Count);
+    }
+    debug_printf("当前 1970-01-01 23 00:00");
+    debug_printf("启动 ");
+    debug_printf("%02lld:%02lld:%02lld.%03lld ", curms / 3600000, curms / 60000 % 60, curms / 1000 % 60, curms % 1000);
+    debug_printf("堆 %u/%u", &(buf[0]) - 0X20000000, 1024);
+    debug_printf("\r\n");
+
+    int i =  - 1;
+    Task *task = ts->Current;
+    task->ShowStatus();
+    while (ts->_Tasks.MoveNext(i))
     {
         Task *task = ts->_Tasks[i];
         if (task)
