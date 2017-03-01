da888738b38f642b46fe25cc8a82bfc646ee3088
Ace <258613763@qq.com>
2017-03-01 16:32:03 +0800

    任务调度优化

--- SYSTEM/CORE/src/TaskScheduler.cpp
+++ SYSTEM/CORE/src/TaskScheduler.cpp
@@ -121,7 +121,7 @@ void TaskScheduler::Start()
         return ;
     }
 
-    Add(ShowTime, this, 2 *1000, 20 *1000, "时间显示");
+    //Add(ShowTime, this, 2 *1000, 20 *1000, "时间显示");
     Add(ShowStatus, this, 1*1000, 30*1000,"任务状态");
 
     debug_printf("%s::准备就绪 开始循环处理%d个任务！\r\n\r\n", Name, Count);
