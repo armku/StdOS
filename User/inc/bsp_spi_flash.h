f445a636a18dd657425e2a718a2a51f293fa838d
Ace <258613763@qq.com>
2017-03-03 18:41:18 +0800

    代码整理

--- User/inc/bsp_spi_flash.h
+++ User/inc/bsp_spi_flash.h
@@ -14,23 +14,6 @@
 #define macSPI_CS_PORT                              GPIOA
 #define macSPI_CS_PIN                               GPIO_Pin_4
 
-//#define macSPI_SCK_APBxClock_FUN                    RCC_APB2PeriphClockCmd
-//#define macSPI_SCK_CLK                              RCC_APB2Periph_GPIOA   
-//#define macSPI_SCK_PORT                             GPIOA   
-//#define macSPI_SCK_PIN                              GPIO_Pin_5
-
-//#define macSPI_MISO_APBxClock_FUN                   RCC_APB2PeriphClockCmd
-//#define macSPI_MISO_CLK                             RCC_APB2Periph_GPIOA    
-//#define macSPI_MISO_PORT                            GPIOA 
-//#define macSPI_MISO_PIN                             GPIO_Pin_6
-
-//#define macSPI_MOSI_APBxClock_FUN                   RCC_APB2PeriphClockCmd
-//#define macSPI_MOSI_CLK                             RCC_APB2Periph_GPIOA    
-//#define macSPI_MOSI_PORT                            GPIOA 
-//#define macSPI_MOSI_PIN                             GPIO_Pin_7
-
-
-
 /************************** SPI Flash º¯Êýºê¶¨Òå********************************/
 #define macSPI_FLASH_CS_ENABLE()                       GPIO_ResetBits( macSPI_CS_PORT, macSPI_CS_PIN )
 #define macSPI_FLASH_CS_DISABLE()                      GPIO_SetBits( macSPI_CS_PORT, macSPI_CS_PIN )
