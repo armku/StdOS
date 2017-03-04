b95ed54e7c8e9764688f14e2d199b20159a8cb3a
Ace <258613763@qq.com>
2017-03-04 13:29:52 +0800

    增加Flash操作框架

---
+++ SYSTEM/Peripheral/inc/Flash.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Type.h"
+/*
+单片机内部flash操作
+*/
+class Flash
+{
+	public:
+		int Erase(uint addr,int size);
+		int WriteBlock(uint addr,byte *pBuffer,int size);
+		int Read(uint addr,byte *pBuffer,int size);
+		int Write(uint addr,byte *pBuffer,int size);
+		
+		int Size;
+		int BytesPerBlock;
+};
+//#define TestFlsh
---
+++ SYSTEM/Peripheral/src/Flash.cpp
@@ -0,0 +1,73 @@
+#include "Flash.h"
+#include "Sys.h"
+#ifdef TestFlsh
+    #include "stdlib.h"
+#endif 
+
+int Flash::Erase(uint addr, int size)
+{
+    return 0;
+}
+
+int Flash::WriteBlock(uint addr, byte *pBuffer, int size)
+{
+    return 0;
+}
+
+int Flash::Read(uint addr, byte *pBuffer, int size)
+{
+    return 0;
+}
+
+int Flash::Write(uint addr, byte *pBuffer, int size)
+{
+    return 0;
+}
+
+#ifdef TestFlsh
+
+    void TestFlash()
+    {
+        debug_printf("\r\n\r\n");
+        debug_printf("TestFlash Start......\r\n");
+
+        uint addr = 0x08004000;
+
+        Flash flash;
+        debug_printf("FlashSize: %d Bytes  BytesPerBlock: %d Bytes\r\n", flash.Size, flash.BytesPerBlock);
+        flash.Erase(addr, 0x100);
+
+        byte buf[] = "STM32F10x SPI Firmware Library Example: communication with an AT45DB SPI FLASH";
+        uint size = ArrayLength(buf);
+        flash.WriteBlock(addr, buf, size);
+
+        byte *rx = (byte*)malloc(size);
+        flash.Read(addr, rx, size);
+
+        int n = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (buf[i] != rx[i])
+                n++;
+        }
+        debug_printf("diffent %d\r\n", n);
+
+        // ¼¯³É²âÊÔ
+        //flash.Erase(addr, 0x100);
+        flash.Write(addr, buf, size);
+
+        flash.Read(addr, rx, size);
+
+        n = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (buf[i] != rx[i])
+                n++;
+        }
+        debug_printf("diffent %d\r\n", n);
+
+        free(rx);
+
+        debug_printf("\r\nTestFlash Finish!\r\n");
+    }
+#endif
