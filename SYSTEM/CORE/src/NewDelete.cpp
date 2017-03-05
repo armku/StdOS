162471f94aacd056e41660a85d1846c90664f644
Ace <258613763@qq.com>
2017-03-05 14:11:51 +0800

    代码整理

--- SYSTEM/CORE/src/NewDelete.cpp
+++ SYSTEM/CORE/src/NewDelete.cpp
@@ -11,8 +11,8 @@
 
 extern "C"
 {
-    extern uint __heap_base;
-    extern uint __heap_limit;
+    //extern uint __heap_base;
+    //extern uint __heap_limit;
 }
 
 void *operator new(uint size)
@@ -25,9 +25,11 @@ void *operator new(uint size)
     {
         debug_printf(" 0X%08x", p);
         // 如果堆只剩下64字节，则报告失败，要求用户扩大堆空间以免不测
+		#if 0
         uint end = (uint) &__heap_limit;
         if ((uint)p + size + 0x40 >= end)
             debug_printf(" + %d near HeapEnd=0X%08x", size, end);
+		#endif
     }
 	debug_printf("\r\n");
     //assert_param(p);
@@ -44,9 +46,11 @@ void *operator new[](uint size)
     {
         debug_printf(" 0x%08x", p);
         // 如果堆只剩下64字节，则报告失败，要求用户扩大堆空间以免不测
+		#if 0
         uint end = (uint) &__heap_limit;
         if ((uint)p + size + 0x40 >= end)
             debug_printf(" + %d near HeapEnd=0x%08x", size, end);
+		#endif
     }
 	debug_printf("\r\n");
     //assert_param(p);
--- SYSTEM/CORE/src/Sys.cpp
+++ SYSTEM/CORE/src/Sys.cpp
@@ -15,9 +15,9 @@ Sys.ID 是12字节芯片唯一标识、也就是ChipID，同一批芯片仅前面几个字节不同
 #include "DateTime.h"
 extern "C"
 {
-    extern uint __heap_base;
-    extern uint __heap_limit;
-    extern uint __initial_sp;
+    //extern uint __heap_base;
+    //extern uint __heap_limit;
+    //extern uint __initial_sp;
 }
 
 //外部注册函数
@@ -205,18 +205,19 @@ void TSys::Reboot(uint msDelay){}
 //显示系统信息
 void TSys::ShowInfo()
 {
+	#if 0
     uint HeapSize = 0;
     uint StackSize = 0;
 
     HeapSize = ((uint) &__heap_limit - (uint) &__heap_base);
     StackSize = ((uint) &__initial_sp - (uint) &__heap_limit);
-
+	#endif
     printf("STD_Embedded_Team::STD0801 Code:Demo Ver:0.0.6113 Build:%s\n", __DATE__);
     printf("STDOS::%s %dMHz Flash:%dk RAM:%dk\n", this->CPUName->GetBuffer(),this->Clock/1000000, this->FlashSize, this->RamSize);
     printf("DevID:0X%04X RevID:0X%04X\n", this->DevID, this->RevID);
     printf("CPUID:0X%X ARM:ARMv7-M Cortex-M3: R1p2\n", this->CPUID);
-    printf("Heap :(0X%X, 0X%X) = 0X%X (%dk)\n", (uint) &__heap_base, (uint) &__heap_limit, HeapSize, HeapSize / 1024);
-    printf("Stack:(0X%X, 0X%X) = 0X%X (%dk)\n", (uint) &__heap_limit, (uint) &__initial_sp, StackSize, StackSize / 1024);
+    //printf("Heap :(0X%X, 0X%X) = 0X%X (%dk)\n", (uint) &__heap_base, (uint) &__heap_limit, HeapSize, HeapSize / 1024);
+    //printf("Stack:(0X%X, 0X%X) = 0X%X (%dk)\n", (uint) &__heap_limit, (uint) &__initial_sp, StackSize, StackSize / 1024);
     printf("ChipType:0x42455633 3\n");
     printf("ChipID:%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X\n", ID[0], ID[1], ID[2], ID[3], ID[4], ID[5], ID[6], ID[7], ID[8], ID[9], ID[10], ID[11]);
     printf("Time : ");
