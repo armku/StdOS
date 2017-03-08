3187384eb9a0b770b239ec5ce0fec8b77408a4b6
Ace <258613763@qq.com>
2017-03-08 17:00:16 +0800

    代码整理

--- SYSTEM/CORE/inc/TTime.h
+++ SYSTEM/CORE/inc/TTime.h
@@ -4,8 +4,28 @@
 
 class TTime
 {
+	private:
+		static void OnHandler(ushort num,void* param);
 	public:
-		TTime();
+		uint seconds;//全局秒数，系统启动后总秒数，累加
+		UInt64 Milliseconds;//全局毫秒数，系统启动后总毫秒(1000毫秒整部分，累加
+		uint BaseSeconds;//基准秒数，系统启动时相对于1970年的秒数，时间调节，加上Seconds得到当前时间Now();
+		byte Index;//定时器
+		#if !(defined(STM32F0)||()GD32F130)
+		byte Div;//分频系数，最大分频时无法让大于64M主频的芯片分配得到1k时钟
+		#endif
+		
+		Func OnInit;
+	Func OnLoad;
+	Func OnSave;
+	typedef void (*FuncInt)(int);
+	Function OnSleep;
+	
+	TTime();
+	public:
+		
+	
+		//以上为新内容
 		UInt64 Ms() const;//系统启动以来的毫秒数
 		UInt64 Current() const;//系统启动以来的毫秒
 		uint Seconds();//系统时间
