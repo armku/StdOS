e7fe4f8779276b1c9e1159178498afb6dbc7b607
Ace <258613763@qq.com>
2017-03-09 09:41:04 +0800

    代码整理

--- SYSTEM/Basic/inc/Buffer.h
+++ SYSTEM/Basic/inc/Buffer.h
@@ -4,93 +4,108 @@
 #include "Object.h"
 
 //拷贝的原则是尽力而为，有多少可用空间拷贝多少长度
-class Buffer:public Object
+class Buffer: public Object
 {
     public:
-		//打包一个指针和长度指定的数据区
-		Buffer(void* ptr,int len);    
-		//禁用拷贝构造函数
-		Buffer(const Buffer& buf)=delete;
-		//对象Mov操作，指针和长度归我，清空对方
-		Buffer(Buffer& rval);
-		
-		//从另一个对象拷贝数据和长度，长度不足时扩容失败时报错
-		Buffer& operator = (const Buffer&rhs);
-		//从指针拷贝，使用我的长度
-		Buffer& operator = (const void * prt);
-		//对象Mov操作，指针和长度归我，清空对方
-		Buffer& operator=(Buffer&& rval);
-		
-		//拿出指针供外部使用
-		inline byte* GetBuffer(){return (byte*)_Arr;}
-		inline const byte* GetBuffer() const{return (byte*)_Arr;}
-		inline int Length() const{return _Length;}
-		
-		virtual bool SetLength(int len);
-		//设置指定位置的值，长度不足时报错
-		bool SetAt(int index,byte value);
-		
-		//自我索引运算符[] 返回指定元素的第一个字节
-		byte operator[](int i)const;
-		//支持buf[i]=0x33的语法
-		byte& operator[](int i);
-		
-		//原始拷贝 清零 不检查边界
-		static void Copy(void* dest,const void* source,int len);
-		static void Zero(void * dest,int len);
-		//拷贝数据 默认-1长度表示当前长度
-		virtual void Copy(int destIndex, const void *src, int len=-1);//拷贝数据，默认-1长度表示当前长度   
-		virtual void CopyTo(int destIndex, const void *dest, int len=-1);//const;//把数据复制到目标缓冲区，默认-1长度表示当前长度
-		virtual void Copy(int destIndex, const Buffer &src, int srcIndex, int len);//拷贝数据，默认-1长度表示两者最小长度 
-		int Copy(const Buffer &src, int destIndex = 0);     
-		//用指定字节设置初始化一个区域
-		int Set(byte item,int index,int len);
-		void Clear(byte item=0);
-		
-		//截取一个子缓冲区，默认-1长度表示剩余全部		
-		Buffer Sub(int index,int length);//截取自缓冲区  
-		const Buffer Sub(int index,int len)const;
-		
-		//显示16禁止数据，指定分割字符和换行长度
-		String& ToHex(String& str,char sep=0,int newLine=0)const;
-		//显示16禁止数据，指定分割字符和换行长度
-		String ToHex(char sep=0,int newLine=0);
-		
-		
-		ushort ToUInt16() const;
-		uint ToUInt32() const;
-		UInt64 ToUInt64() const;
-		
-		void Write(ushort value,int index=0);
-		void Write(short value,int index=0);
-		void Write(uint value,int index=0);
-		void Write(int value,int index=0);
-		void Write(UInt64 value,int index=0);
-		//输出对象的字符串表示方式
-		virtual String& ToStr(String& str)const;
-		//包装为字符串对象
-		String AsString() const;
-		
-		explicit operator bool() const{return _Length>0;}
-		bool operator!()const {return _Length==0;}
-		
-		int CompareTo(const Buffer& bs)const;
-		int CompareTo(const void *ptr,int len=-1)const;
-		
-		friend bool operator==(const Buffer& bs1,const Buffer& bs2);
-		friend bool operator==(const Buffer& bs1,const void* ptr);
-		friend bool operator!=(const Buffer& bs1,const Buffer& bs2);
-		friend bool operator!=(const Buffer& bs1,const void* ptr);
-		
-		#if DEBUG
-		 static void Test();
-		#endif
-		protected:
-			byte Arr[0x40];//内部缓冲区			
-			virtual void* Alloc(int len);
-		//以下为猜测内容
-		int _Length;
-		void *_Arr;	
-		public:
-		virtual void Show(bool newLine=false) const;	
+        //打包一个指针和长度指定的数据区
+        Buffer(void *ptr, int len);
+        //禁用拷贝构造函数
+        Buffer(const Buffer &buf) = delete ;
+        //对象Mov操作，指针和长度归我，清空对方
+        Buffer(Buffer &rval);
+
+        //从另一个对象拷贝数据和长度，长度不足时扩容失败时报错
+        Buffer &operator = (const Buffer &rhs);
+        //从指针拷贝，使用我的长度
+        Buffer &operator = (const void *prt);
+        //对象Mov操作，指针和长度归我，清空对方
+        Buffer &operator = (Buffer && rval);
+
+        //拿出指针供外部使用
+        inline byte *GetBuffer()
+        {
+                return (byte*)_Arr;
+        }
+        inline const byte *GetBuffer()const
+        {
+                return (byte*)_Arr;
+        }
+        inline int Length()const
+        {
+                return _Length;
+        }
+
+        virtual bool SetLength(int len);
+        //设置指定位置的值，长度不足时报错
+        bool SetAt(int index, byte value);
+
+        //自我索引运算符[] 返回指定元素的第一个字节
+        byte operator[](int i)const;
+        //支持buf[i]=0x33的语法
+        byte &operator[](int i);
+
+        //原始拷贝 清零 不检查边界
+        static void Copy(void *dest, const void *source, int len);
+        static void Zero(void *dest, int len);
+        //拷贝数据 默认-1长度表示当前长度
+        virtual void Copy(int destIndex, const void *src, int len =  - 1); //拷贝数据，默认-1长度表示当前长度   
+        virtual void CopyTo(int destIndex, const void *dest, int len =  - 1); //const;//把数据复制到目标缓冲区，默认-1长度表示当前长度
+        virtual void Copy(int destIndex, const Buffer &src, int srcIndex, int len); //拷贝数据，默认-1长度表示两者最小长度 
+        int Copy(const Buffer &src, int destIndex = 0);
+        //用指定字节设置初始化一个区域
+        int Set(byte item, int index, int len);
+        void Clear(byte item = 0);
+
+        //截取一个子缓冲区，默认-1长度表示剩余全部		
+        Buffer Sub(int index, int length); //截取自缓冲区  
+        const Buffer Sub(int index, int len)const;
+
+        //显示16禁止数据，指定分割字符和换行长度
+        String &ToHex(String &str, char sep = 0, int newLine = 0)const;
+        //显示16禁止数据，指定分割字符和换行长度
+        String ToHex(char sep = 0, int newLine = 0);
+
+
+        ushort ToUInt16()const;
+        uint ToUInt32()const;
+        UInt64 ToUInt64()const;
+
+        void Write(ushort value, int index = 0);
+        void Write(short value, int index = 0);
+        void Write(uint value, int index = 0);
+        void Write(int value, int index = 0);
+        void Write(UInt64 value, int index = 0);
+        //输出对象的字符串表示方式
+        virtual String &ToStr(String &str)const;
+        //包装为字符串对象
+        String AsString()const;
+
+        explicit operator bool()const
+        {
+                return _Length > 0;
+        }
+        bool operator !()const
+        {
+                return _Length == 0;
+        }
+
+        int CompareTo(const Buffer &bs)const;
+        int CompareTo(const void *ptr, int len =  - 1)const;
+
+        friend bool operator == (const Buffer &bs1, const Buffer &bs2);
+        friend bool operator == (const Buffer &bs1, const void *ptr);
+        friend bool operator != (const Buffer &bs1, const Buffer &bs2);
+        friend bool operator != (const Buffer &bs1, const void *ptr);
+
+        #if DEBUG
+            static void Test();
+        #endif 
+    protected:
+        byte Arr[0x40]; //内部缓冲区			
+        virtual void *Alloc(int len);
+        //以下为猜测内容
+        int _Length;
+        void *_Arr;
+    public:
+        virtual void Show(bool newLine = false)const;
 };
--- SYSTEM/Basic/src/Buffer.cpp
+++ SYSTEM/Basic/src/Buffer.cpp
@@ -2,140 +2,147 @@
 #include "Buffer.h"
 #include "Sys.h"
 
-Buffer::Buffer(void* ptr,int len)
+Buffer::Buffer(void *ptr, int len)
 {
-	this->_Arr=ptr;
-	this->_Length=len;
+    this->_Arr = ptr;
+    this->_Length = len;
 }
 
-Buffer & Buffer::operator = (const void * prt)
+Buffer &Buffer::operator = (const void *prt)
 {
-	for(int  i=0;i<this->_Length;i++)
-	{
-		((byte *)(this->_Arr))[i]=((byte*)prt)[i];
-	}
-	return *this;
+    for (int i = 0; i < this->_Length; i++)
+    {
+        ((byte*)(this->_Arr))[i] = ((byte*)prt)[i];
+    }
+    return  *this;
 }
-Buffer& Buffer::operator=(Buffer&& rval)
+
+Buffer &Buffer::operator = (Buffer && rval)
 {
-	if(this->_Length<rval._Length)
-	{
-		debug_printf("Error: Buffer copy: Buffer length mismath src: %d ,dst: %d \n",rval._Length,this->_Length);
-	}
-	else
-	{
-		for(int i=0;i<rval.Length();i++)
-		{
-			((byte *)(this->_Arr))[i]=rval.GetBuffer()[i];
-		}
-	}
-	return *this;
+    if (this->_Length < rval._Length)
+    {
+        debug_printf("Error: Buffer copy: Buffer length mismath src: %d ,dst: %d \n", rval._Length, this->_Length);
+    }
+    else
+    {
+        for (int i = 0; i < rval.Length(); i++)
+        {
+            ((byte*)(this->_Arr))[i] = rval.GetBuffer()[i];
+        }
+    }
+    return  *this;
 }
-byte &Buffer::operator [] (int pos)
+
+byte &Buffer::operator[](int pos)
 {
-	if((pos<0)||(pos>this->_Length))
-	{
-		debug_printf("Error: [] length error");
-		return ((byte *)(this->_Arr))[0];
-	}
-	return ((byte *)(this->_Arr))[pos];
+    if ((pos < 0) || (pos > this->_Length))
+    {
+        debug_printf("Error: [] length error");
+        return ((byte*)(this->_Arr))[0];
+    }
+    return ((byte*)(this->_Arr))[pos];
 }
 
 //设置长度，可自动扩容 
 bool Buffer::SetLength(int len)
 {
-	if(this->_Length>=len)
-	{
-		this->_Length=len;
-	}
-	else
-	{
-		//自动扩容
-		this->_Arr=new byte[len];
-		this->_Length=len;
-		delete []((byte *)(this->_Arr));
-	}
-	return true;
+    if (this->_Length >= len)
+    {
+        this->_Length = len;
+    }
+    else
+    {
+        //自动扩容
+        this->_Arr = new byte[len];
+        this->_Length = len;
+        delete []((byte*)(this->_Arr));
+    }
+    return true;
 }
-void Buffer::Show(bool newLine) const
+
+void Buffer::Show(bool newLine)const
 {
-	if(newLine)
-	{
-		for(int i=0;i<this->_Length-1;i++)
-		{
-			printf("%02X ",((byte *)(this->_Arr))[i]);
-		}
-		printf("%02X",((byte *)(this->_Arr))[this->_Length-1]);
-	}
-	else
-	{
-		printf("%s",((byte *)(this->_Arr)));
-	}
-	//Object::Show(newLine);
-	Object::Show(true);
+    if (newLine)
+    {
+        for (int i = 0; i < this->_Length - 1; i++)
+        {
+            printf("%02X ", ((byte*)(this->_Arr))[i]);
+        }
+        printf("%02X", ((byte*)(this->_Arr))[this->_Length - 1]);
+    }
+    else
+    {
+        printf("%s", ((byte*)(this->_Arr)));
+    }
+    //Object::Show(newLine);
+    Object::Show(true);
 }
+
 //把数据复制到目标缓冲区，默认-1长度表示当前长度
 void Buffer::CopyTo(int destIndex, const void *dest, int len)
 {
-	if(len==-1)
-	{
-		len=this->_Length;
-	}
-	if(len<=0)
-	{
-		return;
-	}
-	for(int i=0;i<len;i++)
-	{
-		((byte*)dest)[destIndex+i]=((byte *)(this->_Arr))[i];
-	}
+    if (len ==  - 1)
+    {
+        len = this->_Length;
+    }
+    if (len <= 0)
+    {
+        return ;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        ((byte*)dest)[destIndex + i] = ((byte*)(this->_Arr))[i];
+    }
 }
+
 //拷贝数据，默认-1长度表示当前长度 
 void Buffer::Copy(int destIndex, const void *src, int len)
 {
-	if(len==-1)
-	{
-		len=this->_Length;
-	}
-	if(len<=0)
-	{
-		return;
-	}
-	for(int i=0;i<len;i++)
-	{
-	((byte *)(this->_Arr))[destIndex+i]=((byte*)src)[i];	
-	}	
+    if (len ==  - 1)
+    {
+        len = this->_Length;
+    }
+    if (len <= 0)
+    {
+        return ;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        ((byte*)(this->_Arr))[destIndex + i] = ((byte*)src)[i];
+    }
 }
+
 //拷贝数据，默认-1长度表示两者最小长度
 void Buffer::Copy(int destIndex, const Buffer &src, int srcIndex, int len)
 {
-	if(len==-1)
-	{		
-		len=this->_Length;
-		if(len<src._Length)
-		{
-			this->SetLength(src._Length);
-			len=this->_Length;
-		}
-	}
-	for(int i=0;i<len;i++)
-	{		
-		((byte*)this->_Arr)[destIndex+i] = ((byte*)(src._Arr))[srcIndex+i];
-	}	
+    if (len ==  - 1)
+    {
+        len = this->_Length;
+        if (len < src._Length)
+        {
+            this->SetLength(src._Length);
+            len = this->_Length;
+        }
+    }
+    for (int i = 0; i < len; i++)
+    {
+        ((byte*)this->_Arr)[destIndex + i] = ((byte*)(src._Arr))[srcIndex + i];
+    }
 }
+
 //截取自缓冲区  
-Buffer Buffer::Sub(int index,int length)
-{	
-	byte *pbufsub=new byte[length];
-	if(length > this->_Length)
-	{
-		length= this->_Length;
-	}
-	Buffer buf(pbufsub,length);
-	
-	for(int i=index;i<(index+length);i++)
-	{
-		pbufsub[i]=this->GetBuffer()[index+i];
-	}	
-	return buf;
+Buffer Buffer::Sub(int index, int length)
+{
+    byte *pbufsub = new byte[length];
+    if (length > this->_Length)
+    {
+        length = this->_Length;
+    }
+    Buffer buf(pbufsub, length);
+
+    for (int i = index; i < (index + length); i++)
+    {
+        pbufsub[i] = this->GetBuffer()[index + i];
+    }
+    return buf;
 }
