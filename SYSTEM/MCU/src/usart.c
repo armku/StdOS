5ff8b5791a1886dfa540df29abd5c40bac70a1c2
Ace <258613763@qq.com>
2017-03-09 10:25:00 +0800

    代码整理

--- SYSTEM/MCU/src/usart.c
+++ SYSTEM/MCU/src/usart.c
@@ -2,34 +2,36 @@
 #include "stm32f10x.h"
 //////////////////////////////////////////////////////////////////
 //加入以下代码,支持printf函数,而不需要选择use MicroLIB
-#pragma import(__use_no_semihosting)
-//标准库需要的支持函数
-struct __FILE
-{
-    int handle;
+#if 1
+    #pragma import(__use_no_semihosting)
+    //标准库需要的支持函数
+    struct __FILE
+    {
+        int handle;
 
-};
-#if 0
+    };
+
     FILE __stdout;
-#endif 
-//定义_sys_exit()以避免使用半主机模式
-void _sys_exit(int x)
-{
-    x = x;
-}
 
-//c++的
-void _ttywrch(int ch)
-{
-    FILE *fp;
-    fputc(ch, fp);
-}
+    //定义_sys_exit()以避免使用半主机模式
+    void _sys_exit(int x)
+    {
+        x = x;
+    }
 
-int GetKey(void)
-{
+    //c++的
+    void _ttywrch(int ch)
+    {
+        FILE *fp;
+        fputc(ch, fp);
+    }
 
-    while (!(USART1->SR &USART_FLAG_RXNE))
-        ;
+    int GetKey(void)
+    {
 
-    return ((int)(USART1->DR &0x1FF));
-}
+        while (!(USART1->SR &USART_FLAG_RXNE))
+            ;
+
+        return ((int)(USART1->DR &0x1FF));
+    }
+#endif
