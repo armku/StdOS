b285f5623d975fedb98a1344b90ca24d2c1a3c9c
Ace <258613763@qq.com>
2017-03-10 16:24:36 +0800

    代码整理

--- SYSTEM/Util/inc/CheckSum.h
+++ SYSTEM/Util/inc/CheckSum.h
@@ -4,21 +4,23 @@
 
 class CheckSum
 {
-	public:
-		byte CheckSum8(byte *buf,ushort len);
-		ushort CheckSum16(byte *buf,ushort len);
-		uint CheckSum32(byte *buf,ushort len);
-		byte IntelHexCheckSum(byte *buf,ushort len);
-		ushort NetCheckSum(byte *buf, ushort len);
-		byte BCCVerify(byte *buf,ushort len);
-		bool OddParity(byte *buf, ushort len);
-		bool EvenParity(byte *buf, ushort len);
-		byte CRC8( byte *buf, ushort len) ;
-		ushort CRC16Default(byte * pszBuf, ushort unLength);
-		ushort CRC16CCITT(byte * pszBuf, ushort unLength);
-		ushort CRC16RTU( byte * pszBuf, ushort unLength);
-		UInt64 CRC32Default(byte *pszBuf,UInt64 ulLength);
-	private:
-		unsigned long Reflect(unsigned long ref, char ch);
-		
+    public:
+        // 校验和
+        byte CheckSum8(byte *buf, ushort len);
+        ushort CheckSum16(byte *buf, ushort len);
+        uint CheckSum32(byte *buf, ushort len);
+        byte IntelHexCheckSum(byte *buf, ushort len);
+        ushort NetCheckSum(byte *buf, ushort len);
+        byte BCCVerify(byte *buf, ushort len);
+        //奇偶校验
+        bool OddParity(byte *buf, ushort len);
+        bool EvenParity(byte *buf, ushort len);
+        // 循环冗余校验
+        byte CRC8(byte *buf, ushort len);
+        ushort CRC16Default(byte *pszBuf, ushort unLength);
+        ushort CRC16CCITT(byte *pszBuf, ushort unLength);
+        ushort CRC16RTU(byte *pszBuf, ushort unLength);
+        UInt64 CRC32Default(byte *pszBuf, UInt64 ulLength);
+    private:
+        unsigned long Reflect(unsigned long ref, char ch);
 };
