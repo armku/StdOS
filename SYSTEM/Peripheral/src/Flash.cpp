4ea92704fdab77797d6fd46c56c6c417305fe3ac
Ace <258613763@qq.com>
2017-03-21 15:37:23 +0800

    代码整理

--- SYSTEM/Peripheral/src/Flash.cpp
+++ SYSTEM/Peripheral/src/Flash.cpp
@@ -27,15 +27,15 @@ void Flash::SetSectorSize(int bytesperblock, int size)
     {
         this->Size = 1024;
     }
-	else 
-	{
-		this->Size=size;
-	}
+    else
+    {
+        this->Size = size;
+    }
 }
 
 int Flash::Read(uint addr, void *pBuffer, int size)
 {
-	ushort tmp;
+    ushort tmp;
     if (size <= 0)
     {
         return 0;
@@ -47,8 +47,8 @@ int Flash::Read(uint addr, void *pBuffer, int size)
     }
     if (size % 2)
     {
-		tmp=this->ReadHalfWord(addr - 1);
-        ((byte*)pBuffer)[size - 1] = tmp&0xff;
+        tmp = this->ReadHalfWord(addr - 1);
+        ((byte*)pBuffer)[size - 1] = tmp &0xff;
     }
     return size;
 }
@@ -61,12 +61,12 @@ int Flash::Read(uint addr, void *pBuffer, int size)
 //返回值:对应数据.
 ushort Flash::ReadHalfWord(uint addr)
 {
-	ushort ret=0;
-	if(addr%2)
-	{
-		ret= *(volatile ushort*)addr;
-	}
-	return ret;
+    ushort ret = 0;
+    if (addr % 2)
+    {
+        ret = *(volatile ushort*)addr;
+    }
+    return ret;
 }
 
 //不检查的写入
@@ -90,105 +90,121 @@ void Flash::Write_NoCheck(uint addr, ushort *pBuffer, ushort size)
 
 /*
 --------|--------|--------|--------|  扇区
-               **************         数据
-            1        2          3     编号
+ **************         数据
+1        2          3     编号
 
-*/
+ */
 int Flash::Write(uint addr, void *pBuffer, int size)
 {
-	uint secpos;//扇区地址
-	ushort secoff;//扇区内偏移	
-	uint offaddr;////去掉0X08000000后的地址
-	
-	offaddr = addr - STM32_FLASH_BASE; //实际偏移地址
-	secpos = offaddr / this->BytesPerBlock; //扇区地址  0~127 for STM32F103RBT6
-	secoff = (offaddr % this->BytesPerBlock); //在扇区内的偏移(2个字节为基本单位.)
-    int size1=0;//第一部分字节大小
-	int pos=0;
-	size1=(secpos+1)*this->BytesPerBlock-addr;
-	
-	if(size1>this->BytesPerBlock)
-	{
-		//错误发生时，直接返回，放置造成意外伤害
-		return -2;
-	}
-	
-		
-	//读取
-	 FLASH_Unlock(); //解锁
-	//处理第一部分
-	this->Read(secpos*this->BytesPerBlock+STM32_FLASH_BASE,this->SectorBuffer.buf,this->BytesPerBlock);
-	for(int i=0;i<size1;i++)
-	{
-		this->SectorBuffer.buf[secoff+i]=((byte*)pBuffer)[i];
-	}
-	this->eraseSector(secpos *this->BytesPerBlock + STM32_FLASH_BASE); //擦除这个扇区
-	this->WriteSector(secpos*this->BytesPerBlock+STM32_FLASH_BASE, this->SectorBuffer.buf16);
-	size-=size1;
-	addr+=size1;
-	pos+=size1;
-	//处理第二部分
-	while(size>this->BytesPerBlock)
-	{
-		this->eraseSector(addr);
-		this->WriteSector(addr,(byte*)pBuffer+pos);
-		size-=this->BytesPerBlock;
-		pos+=this->BytesPerBlock;
-		addr+=this->BytesPerBlock;
-	}
-	//处理第三部分
-	this->Read(addr,this->SectorBuffer.buf,this->BytesPerBlock);
-	this->eraseSector(addr);
-	for(int i=0;i<size;i++)
-	{
-		this->SectorBuffer.buf[i]=((byte*)pBuffer)[pos+i];
-	}
-	this->WriteSector(addr,this->SectorBuffer.buf);
-	
-	 FLASH_Lock(); //上锁
+    uint secpos; //扇区地址
+    ushort secoff; //扇区内偏移	
+    uint offaddr; ////去掉0X08000000后的地址
+
+    offaddr = addr - STM32_FLASH_BASE; //实际偏移地址
+    secpos = offaddr / this->BytesPerBlock; //扇区地址  0~127 for STM32F103RBT6
+    secoff = (offaddr % this->BytesPerBlock); //在扇区内的偏移(2个字节为基本单位.)
+    int size1 = 0; //第一部分字节大小
+    int pos = 0;
+    if (offaddr % this->BytesPerBlock == 0)
+    {
+        size1 = 0;
+    }
+    else
+    {
+        size1 = (secpos + 1) *this->BytesPerBlock + STM32_FLASH_BASE - addr;
+    }
+    if (size1 > this->BytesPerBlock)
+    {
+        //错误发生时，直接返回，放置造成意外伤害
+        return  - 2;
+    }
+
+
+    //读取
+    FLASH_Unlock(); //解锁
+    //处理第一部分
+    if (size1)
+    {
+        //当第一部分需要处理时
+        this->Read(secpos *this->BytesPerBlock + STM32_FLASH_BASE, this->SectorBuffer.buf, this->BytesPerBlock);
+        for (int i = 0; i < size1; i++)
+        {
+            this->SectorBuffer.buf[secoff + i] = ((byte*)pBuffer)[i];
+        }
+        this->eraseSector(secpos *this->BytesPerBlock + STM32_FLASH_BASE); //擦除这个扇区
+        this->WriteSector(secpos *this->BytesPerBlock + STM32_FLASH_BASE, this->SectorBuffer.buf16);
+        size -= size1;
+        addr += size1;
+        pos += size1;
+    }
+    //处理第二部分
+    while (size > this->BytesPerBlock)
+    {
+        this->eraseSector(addr);
+        this->WriteSector(addr, (byte*)pBuffer + pos);
+        size -= this->BytesPerBlock;
+        pos += this->BytesPerBlock;
+        addr += this->BytesPerBlock;
+    }
+    //处理第三部分
+    if (size > 0)
+    {
+        //当第三部分需要处理时
+        this->Read(addr, this->SectorBuffer.buf, this->BytesPerBlock);
+        this->eraseSector(addr);
+        for (int i = 0; i < size; i++)
+        {
+            this->SectorBuffer.buf[i] = ((byte*)pBuffer)[pos + i];
+        }
+        this->WriteSector(addr, this->SectorBuffer.buf);
+    }
+    FLASH_Lock(); //上锁
     return size;
 }
 
 //扇区是否需要擦除
 bool Flash::sectorNeedErase(uint addr)
-{	
+{
     Read(addr, this->SectorBuffer.buf16, this->BytesPerBlock / 2); //读出整个扇区的内容
     for (int i = 0; i < this->BytesPerBlock / 2; i++)
     {
         if (this->SectorBuffer.buf16[i] != 0XFFFF)
             return true;
     }
-	
-	return false;
+
+    return false;
 }
+
 //擦除整个扇区
 int Flash::eraseSector(uint addr)
-{    
+{
     if (sectorNeedErase(addr))
     {
         FLASH_ErasePage(addr); //擦除这个扇区		
     }
     return this->BytesPerBlock;
 }
+
 //写扇区
 void Flash::WriteSector(uint addr, void *pBuffer)
 {
-  this->Write_NoCheck(addr, (ushort *)pBuffer, this->BytesPerBlock/2);
+    this->Write_NoCheck(addr, (ushort*)pBuffer, this->BytesPerBlock / 2);
 }
+
 #ifdef DEBUG
 
     void Flash::TestFlash()
     {
-        
-            debug_printf("\r\n\r\n");
-            debug_printf("TestFlash Start......\r\n");
 
-            uint addr = 0x08010000;
+        debug_printf("\r\n\r\n");
+        debug_printf("TestFlash Start......\r\n");
 
-            Flash flash;
-			flash.SetSectorSize(2048,512);
-            debug_printf("FlashSize: %d kBytes  BytesPerBlock: %d kBytes\r\n", flash.Size, flash.BytesPerBlock);			
-		#if 0
+        uint addr = 0x08019000;
+
+        Flash flash;
+        flash.SetSectorSize(2048, 512);
+        debug_printf("FlashSize: %d kBytes  BytesPerBlock: %d kBytes\r\n", flash.Size, flash.BytesPerBlock);
+        #if 0
             flash.Erase(addr, 0x100);
 
             byte buf[] = "STM32F10x SPI Firmware Library Example: communication with an AT45DB SPI FLASH";
@@ -224,23 +240,23 @@ void Flash::WriteSector(uint addr, void *pBuffer)
             free(rx);
 
             debug_printf("\r\nTestFlash Finish!\r\n");
-        #else            
+        #else 
             ushort buftest1[20];
-			debug_printf("测试开始\r\n");
+            debug_printf("测试开始\r\n");
             for (int i = 0; i < 20; i++)
             {
                 buftest1[i] = 1000+i;
             }
-			debug_printf("-1\r\n");
+            debug_printf("-1\r\n");
             flash.Write(addr, buftest1, 20);
-			debug_printf("0\r\n");
+            debug_printf("0\r\n");
             for (int i = 0; i < 20; i++)
             {
                 buftest1[i] = 0;
             }
-			debug_printf("1\r\n");
+            debug_printf("1\r\n");
             flash.Read(addr, buftest1, 20);
-			debug_printf("2\r\n");
+            debug_printf("2\r\n");
             for (int i = 0; i < 20; i++)
             {
                 debug_printf("%d:%d\r\n", i, buftest1[i]);
@@ -248,4 +264,4 @@ void Flash::WriteSector(uint addr, void *pBuffer)
             debug_printf("测试完成\r\n");
         #endif 
     }
-#endif 
+#endif
