5fb0a37b0d087121471e566b530bba207fb66155
Ace <258613763@qq.com>
2017-03-22 15:31:10 +0800

    代码整理

--- SYSTEM/Peripheral/src/stmflash.cpp
+++ SYSTEM/Peripheral/src/stmflash.cpp
@@ -116,7 +116,7 @@ void STMFLASH::STMFLASH_Read(uint addr, ushort *pBuffer, ushort NumToRead)
     void STMFLASH::Test()
     {
         ushort buftest1[120];
-        uint addr = 0X08070000;
+        uint addr = STM32_FLASH_BASE+0X00070000;
 
         debug_printf("\r\n\r\n");
         debug_printf("TestFlash Start......\r\n");
