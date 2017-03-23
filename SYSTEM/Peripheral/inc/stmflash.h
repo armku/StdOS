6865f0a4bf7767c50897e26a14d7af1041a77b0f
Ace <258613763@qq.com>
2017-03-23 13:58:54 +0800

    代码整理

--- SYSTEM/Peripheral/inc/stmflash.h
+++ SYSTEM/Peripheral/inc/stmflash.h
@@ -6,7 +6,6 @@ class STMFLASH
     public:       
 		STMFLASH();
 		void SetFlashSize(uint flashsize);
-        ushort ReadHalfWord(uint faddr); //读出半字  
         void Write(uint addr, ushort *pBuffer, ushort len); //从指定地址开始写入指定长度的数据
         void Read(uint addr, ushort *pBuffer, ushort len); //从指定地址开始读出指定长度的数据
 	#ifdef DEBUG
@@ -14,6 +13,7 @@ class STMFLASH
 	#endif	
 	private:
 		void Write_NoCheck(uint addr, ushort *pBuffer, ushort len);		
-		uint sectorSize;//扇区大小
+		ushort ReadHalfWord(uint faddr); //读出半字  
+        uint sectorSize;//扇区大小
 		uint flashSize;//Flash容量大小，单位K
 };
