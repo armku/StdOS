a31cff8c17e54cceeefaaa88f1e7440f69b71f38
Ace <258613763@qq.com>
2017-03-31 15:39:52 +0800

    代码整理

--- STDOS/Core/Array.cpp
+++ STDOS/Core/Array.cpp
@@ -3,3 +3,33 @@
 Array::Array()
 {
 }
+bool Array::Release()
+{
+	return true;
+}
+void* Array::Alloc(int len)
+{
+	return 0;
+}
+void Array::Clear()
+{
+}
+void Array::SetItemAt(int i, const void* item)
+{
+	
+}
+int Array::Copy(int destIndex, const Buffer& src, int srcIndex, int len)
+{
+	return true;
+}
+bool Array::SetLength(int len, bool bak)
+{
+	return true;
+}
+bool Array::SetLength(int len)
+{
+	return true;
+}
+Array::~Array()
+{
+}
