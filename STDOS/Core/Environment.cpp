53ca21180961d8cf6df749de198fc441b4f260d2
Ace <258613763@qq.com>
2017-04-09 13:47:17 +0800

    代码整理

--- STDOS/Core/Environment.cpp
+++ STDOS/Core/Environment.cpp
@@ -1,2 +1,26 @@
 #include "Type.h"
 #include "Environment.h"
+
+
+TEnvironment::TEnvironment()
+{
+}
+
+// 获取系统启动后经过的毫秒数
+UInt64 TEnvironment::TickCount()const
+{
+    return 0;
+}
+
+
+// 获取系统启动后经过的毫秒数
+UInt64 TEnvironment::Ms()const
+{
+    return 0;
+}
+
+// 获取当前计算机上的处理器数
+int TEnvironment::ProcessorCount()const
+{
+    return 1;
+}
