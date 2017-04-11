797b80e8ab144f18108907a92db810e81c8a398f
Ace <258613763@qq.com>
2017-04-11 16:20:50 +0800

    代码整理

--- STDOS/Storage/Storage.cpp
+++ STDOS/Storage/Storage.cpp
@@ -1,1 +1,37 @@
 #include "Storage.h"
+// 读取
+bool BlockStorage::Read(uint address, Buffer& bs) const
+	{
+		return false;
+	}
+    // 写入
+    bool BlockStorage::Write(uint address, const Buffer& bs) const
+	{
+		return false;
+	}
+	// 清空
+    bool BlockStorage::Memset(uint address, byte data, int len) const
+	{
+		return false;
+	}
+    // 擦除
+    bool BlockStorage::Erase(uint address, int len) const
+	{
+		return false;
+	}
+	// 指定块是否被擦除
+    bool BlockStorage::IsErased(uint address, int len) const
+	{
+		return false;
+	}
+	// 读取
+    bool CharStorage::Read(uint address, Buffer& bs) const
+	{
+		return false;
+	}
+		
+    // 写入
+    bool CharStorage::Write(uint address, const Buffer& bs) const
+	{
+		return false;
+	}
