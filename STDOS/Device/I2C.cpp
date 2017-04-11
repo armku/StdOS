b1ca5e16d099f5bebd306dbcfba75f2a23a2885d
Ace <258613763@qq.com>
2017-04-11 15:55:51 +0800

    代码整理

--- STDOS/Device/I2C.cpp
+++ STDOS/Device/I2C.cpp
@@ -1,16 +1,63 @@
 #include "Type.h"
 #include "I2C.h"
 
+I2C::~I2C(){}
+// 打开设备
+void I2C::Open(){}
+// 关闭设备
+void I2C::Close(){}
+// 新会话向指定地址写入
+bool I2C::Write(int addr, const Buffer &bs)
+{
+    return false;
+}
+
+bool I2C::Write(int addr, byte data)
+{
+    return false;
+}
+
+// 新会话从指定地址读取
+uint I2C::Read(int addr, Buffer &bs)
+{
+    return 0;
+}
+
+byte I2C::Read(int addr)
+{
+    return 0;
+}
+
+ushort I2C::Read2(int addr)
+{
+    return 0;
+}
+
+uint I2C::Read4(int addr)
+{
+    return 0;
+}
+
+bool I2C::SendAddress(int addr, bool tx)
+{
+    return false;
+}
+
+bool I2C::SendSubAddr(int addr)
+{
+    return false;
+}
+
 CSoftI2C::CSoftI2C(Pin pinscl, Pin pinsda)
 {
-	this->SCL.OpenDrain=true;
-    this->SDA.OpenDrain=true;
-	this->SCL.Set(pinscl);
+    this->SCL.OpenDrain = true;
+    this->SDA.OpenDrain = true;
+    this->SCL.Set(pinscl);
     this->SDA.Set(pinsda);
-    
-    this->SCL=0;
-    this->SDA=1;
-    this->SCL=1;
+
+    this->SCL = 0;
+    this->SDA = 1;
+    this->SCL = 1;
 }
 
 void CSoftI2C::Init(){
@@ -19,22 +66,22 @@ void CSoftI2C::Init(){
 void CSoftI2C::Start()
 {
     /* 当SCL高电平时，SDA出现一个下跳沿表示I2C总线启动信号 */
-    this->SDA=1;
-    this->SCL=1;
+    this->SDA = 1;
+    this->SCL = 1;
     this->Delay();
-    this->SDA=0;
+    this->SDA = 0;
     this->Delay();
-    this->SCL=0;
+    this->SCL = 0;
     this->Delay();
 }
 
 void CSoftI2C::Stop()
 {
     /* 当SCL高电平时，SDA出现一个上跳沿表示I2C总线停止信号 */
-    this->SDA=0;
-    this->SCL=1;
+    this->SDA = 0;
+    this->SCL = 1;
     this->Delay();
-    this->SDA=1;
+    this->SDA = 1;
 }
 
 void CSoftI2C::WriteByte(byte dat)
@@ -46,19 +93,19 @@ void CSoftI2C::WriteByte(byte dat)
     {
         if (dat &0x80)
         {
-            this->SDA=1;
+            this->SDA = 1;
         }
         else
         {
-            this->SDA=0;
+            this->SDA = 0;
         }
         this->Delay();
-        this->SCL=1;
+        this->SCL = 1;
         this->Delay();
-        this->SCL=0;
+        this->SCL = 0;
         if (i == 7)
         {
-            this->SDA=1; // 释放总线
+            this->SDA = 1; // 释放总线
         }
         dat <<= 1; /* 左移一个bit */
         this->Delay();
@@ -75,13 +122,13 @@ byte CSoftI2C::ReadByte()
     for (i = 0; i < 8; i++)
     {
         value <<= 1;
-        this->SCL=1;
+        this->SCL = 1;
         this->Delay();
         if (this->SDA.ReadInput())
         {
             value++;
         }
-        this->SCL=0;
+        this->SCL = 0;
         this->Delay();
     }
     return value;
@@ -94,9 +141,9 @@ bool CSoftI2C::WaitAck(int retry)
 {
     byte re;
 
-    this->SDA=1; /* CPU释放SDA总线 */
+    this->SDA = 1; /* CPU释放SDA总线 */
     this->Delay();
-    this->SCL=1; /* CPU驱动SCL = 1, 此时器件会返回ACK应答 */
+    this->SCL = 1; /* CPU驱动SCL = 1, 此时器件会返回ACK应答 */
     this->Delay();
 
     if (this->SDA.ReadInput())
@@ -108,73 +155,66 @@ bool CSoftI2C::WaitAck(int retry)
     {
         re = 0;
     }
-    this->SCL=0;
+    this->SCL = 0;
     this->Delay();
     return re;
 }
 
 void CSoftI2C::Ack(bool ack)
 {
-	if(ack)
-	{
-    this->SDA=0; /* CPU驱动SDA = 0 */
-    this->Delay();
-    this->SCL=1; /* CPU产生1个时钟 */
-    this->Delay();
-    this->SCL=0;
-    this->Delay();
-    this->SDA=1;
-    /*CPU释放SDA总线 */
-	}
-	else
-	{
-		this->SDA=1; /* CPU驱动SDA = 1 */
-    this->Delay();
-    this->SCL=1; /* CPU产生1个时钟 */
-    this->Delay();
-    this->SCL=0;
-    this->Delay();
-	}
+    if (ack)
+    {
+        this->SDA = 0; /* CPU驱动SDA = 0 */
+        this->Delay();
+        this->SCL = 1; /* CPU产生1个时钟 */
+        this->Delay();
+        this->SCL = 0;
+        this->Delay();
+        this->SDA = 1;
+        /*CPU释放SDA总线 */
+    }
+    else
+    {
+        this->SDA = 1; /* CPU驱动SDA = 1 */
+        this->Delay();
+        this->SCL = 1; /* CPU产生1个时钟 */
+        this->Delay();
+        this->SCL = 0;
+        this->Delay();
+    }
 }
-void CSoftI2C::OnOpen()
-{
+
+void CSoftI2C::OnOpen(){}
+void CSoftI2C::OnClose(){}
+void CSoftI2C::GetPin(Pin *scl, Pin *sda){
+
 }
-void CSoftI2C::OnClose()
-{
-}
-void CSoftI2C::GetPin(Pin* scl, Pin* sda)
-{
-	
-}
-void CSoftI2C::SetPin(Pin scl, Pin sda)
-{
-	
+void CSoftI2C::SetPin(Pin scl, Pin sda){
+
 }
 // 使用端口和最大速度初始化，因为需要分频，实际速度小于等于该速度
-CSoftI2C::CSoftI2C(uint speedHz)
-{
-	
+CSoftI2C::CSoftI2C(uint speedHz){
+
 }
-CSoftI2C::~CSoftI2C()
-{
-}
+CSoftI2C::~CSoftI2C(){}
 void CSoftI2C::Delay(int us)
 {
-	
-	#if 0
-	//Sys.Delay(us);
-	/*
-	因为有系统代码，导致Delay不是很准，经常大于1us
-	右移23位时：
-	48M = 5;
-	72M = 8;
-	108M= 12;
-	120m= 14;
-	*/
-	// 72M = 4
-	int t=Sys.Clock >> 21;
-	while(t-- > 0);
-	#endif
+
+    #if 0
+        //Sys.Delay(us);
+        /*
+        因为有系统代码，导致Delay不是很准，经常大于1us
+        右移23位时：
+        48M = 5;
+        72M = 8;
+        108M= 12;
+        120m= 14;
+         */
+        // 72M = 4
+        int t = Sys.Clock >> 21;
+        while (t-- > 0)
+            ;
+    #endif 
     byte i;
 
     /*　
