e1b1a3f1df76e5a9c29946fde5d891132d6298e9
Ace <258613763@qq.com>
2017-04-17 17:01:08 +0800

    代码整理

--- STDOS/App/FlushPort.cpp
+++ STDOS/App/FlushPort.cpp
@@ -1,2 +1,26 @@
 #include "FlushPort.h"
 
+FlushPort::FlushPort()
+{
+	
+}
+FlushPort::~FlushPort()
+{
+	
+}
+void FlushPort::Start(int ms)
+{
+	
+}
+// mode 0xff  为不改变Port状态  0为Write false  1为Write true
+void FlushPort::Stop(byte mode)
+{
+	
+}
+void FlushPort::Flush()
+{
+}
+int FlushPort::Write(byte* data)
+{
+	return 0;
+}
--- STDOS/App/IR.cpp
+++ STDOS/App/IR.cpp
@@ -1,2 +1,31 @@
 #include "IR.h"
 
+IR::IR(Pwm * pwm)
+{
+}
+
+bool IR::Open()
+{
+    return false;
+}
+
+bool IR::Close()
+{
+    return false;
+}
+
+bool IR::Send(const Buffer &bs)
+{
+    return false;
+}
+
+int IR::Receive(Buffer &bs, int sTimeout)
+{
+    return  - 1;
+}
+
+void IR::OnSend(void *sender, void *param)
+{
+	
+}
+
