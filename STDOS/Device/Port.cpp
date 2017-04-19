24c7d7bdc6c30a782bc01da409b7e3d6321fe7a5
Ace <258613763@qq.com>
2017-04-19 14:35:20 +0800

    代码整理

--- STDOS/Device/Port.cpp
+++ STDOS/Device/Port.cpp
@@ -140,6 +140,9 @@ bool Port::Open()
             #elif defined(STM32F4)
                 RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA << gi, ENABLE);
             #endif 
+            gpio.GPIO_Pin = _PORT(this->_Pin);
+            gpio.GPIO_Speed = GPIO_Speed_50MHz;
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOF, ENABLE);
 
             //        gpio.GPIO_Pin = PinBit;
 
@@ -161,7 +164,48 @@ bool Port::Open()
                 }
             #endif 
             this->OnOpen(&gpio);
+            GPIO_Init(GPIOF, &gpio);
             GPIO_Init(((GPIO_TypeDef*)this->State), &gpio);
+
+
+
+
+
+            /*定义一个GPIO_InitTypeDef类型的结构体*/
+            GPIO_InitTypeDef GPIO_InitStructure;
+
+            /*开启GPIOB和GPIOF的外设时钟*/
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOF, ENABLE);
+
+            /*选择要控制的GPIOB引脚*/
+            GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+
+            /*设置引脚模式为通用推挽输出*/
+            GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+
+            /*设置引脚速率为50MHz */
+            GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+            /*调用库函数，初始化GPIOB0*/
+            GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+            /*选择要控制的GPIOF引脚*/
+            GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+
+            /*调用库函数，初始化GPIOF7*/
+            GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+            /*选择要控制的GPIOF引脚*/
+            GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+
+            /*调用库函数，初始化GPIOF7*/
+            GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+            /* 关闭所有led灯	*/
+            GPIO_SetBits(GPIOB, GPIO_Pin_0);
+
+            /* 关闭所有led灯	*/
+            GPIO_SetBits(GPIOF, GPIO_Pin_7 | GPIO_Pin_8);
         }
 
         this->Opened = true;
