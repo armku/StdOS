cafdde1d257fdfe41fc64cd7e44c45272bf4092a
Ace <258613763@qq.com>
2017-04-19 21:08:45 +0800

    代码整理

--- User/main.cpp
+++ User/main.cpp
@@ -54,16 +54,50 @@ ISO-V3:PC2控制485方向
 //   bs.Show(true);
 //    return 0;
 //}
-
+InputPort key0(PA0);
 void LedTask(void *param)
 {
     OutputPort *leds = (OutputPort*)param;
     *leds = ! * leds;
+	led1=key0;
 }
 
 #define namee "StdOS"
 
-InputPort key0(PA0);
+void USART1_Config(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	
+	/* config USART1 clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+	
+	/* USART1 GPIO config */
+	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);    
+	/* Configure USART1 Rx (PA.10) as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	/* USART1 mode config */
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+	
+	/* 使能串口1接收中断 */
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	
+	USART_Cmd(USART1, ENABLE);
+}
+
 int main(void)
 {
     TSys &sys = (TSys &)(Sys);
@@ -81,6 +115,7 @@ int main(void)
     sys.Init();
         #if DEBUG
             Sys.MessagePort = COM1;
+			//USART1_Config();			
             Sys.ShowInfo();
 
     //        WatchDog::Start(20000, 10000);
