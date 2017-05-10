46df86a5a2e7534efd0f8f25c285b50648b99e7f
Ace <258613763@qq.com>
2017-05-10 17:23:58 +0800

    代码格式整理

--- User/main.cpp
+++ User/main.cpp
@@ -65,28 +65,32 @@ void LedTask(void *param)
 
 #define namee "StdOS"
 
-uint OnUsart1Read(ITransport *transport, Buffer &bs, void *param,void *param2)
+uint OnUsart1Read(ITransport *transport, Buffer &bs, void *param, void *param2)
 {
-	transport->Write(bs);
-	return bs.Length();
+    transport->Write(bs);
+    return bs.Length();
 }
+
 //按键事件
-void OnPress(InputPort& port,bool down)
+void OnPress(InputPort &port, bool down)
 {
-	debug_printf("Press P%c%d down=%d\r\n",_PIN_NAME(port._Pin),down);
+    debug_printf("Press P%c%d down=%d\r\n", _PIN_NAME(port._Pin), down);
 }
+
 void TIM2_Configuration(void);
 void TIM2_NVIC_Configuration(void);
 #include "stm32f10x_tim.h"
 volatile u32 time2cnt = 0; // ms 计时变量 
 void LedTest(void *param)
 {
-    if ( time2cnt >= 1000 ) /* 1000 * 1 ms = 1s 时间到 */
+    if (time2cnt >= 1000)
+     /* 1000 * 1 ms = 1s 时间到 */
     {
-      time2cnt = 0;   
-		led=!led; 
-    }    
+        time2cnt = 0;
+        led = !led;
+    }
 }
+
 int main(void)
 {
     SerialPort *sp1;
@@ -118,41 +122,41 @@ int main(void)
     //        Config::Current = &Config::CreateFlash();
     //    #endif 
 
-	InputPort key(PC13);
-	key.Press	= OnPress;
-	key.UsePress();
-	key.Open();
+    InputPort key(PC13);
+    key.Press = OnPress;
+    key.UsePress();
+    key.Open();
     //    exti.Register(OnKeyPress);
     //    exti1.Register(OnKeyPress);
     //	SerialPort::GetMessagePort()->Register(OnUsart1Read);
 
     // 初始化为输出
-	
-	/* TIM2 定时配置 */	
-  TIM2_Configuration();
-	
-	/* 实战定时器的中断优先级 */
-	TIM2_NVIC_Configuration();
 
-	/* TIM2 重新开时钟，开始计时 */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);
-	
-	
-	
+    /* TIM2 定时配置 */
+    TIM2_Configuration();
+
+    /* 实战定时器的中断优先级 */
+    TIM2_NVIC_Configuration();
+
+    /* TIM2 重新开时钟，开始计时 */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+
+
     //Sys.AddTask(LedTask, &led, 0, 500, "LedTask");
-	Sys.AddTask(LedTest,nullptr,0,10,"LedTest");
+    Sys.AddTask(LedTest, nullptr, 0, 10, "LedTest");
     Sys.Start();
 }
 
 /// TIM2中断优先级配置
 void TIM2_NVIC_Configuration(void)
 {
-    NVIC_InitTypeDef NVIC_InitStructure; 
-    
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);  													
-    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;	  
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;	
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 }
@@ -165,30 +169,30 @@ void TIM2_NVIC_Configuration(void)
  */
 void TIM2_Configuration(void)
 {
-    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-		
-		/* 设置TIM2CLK 为 72MHZ */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+    /* 设置TIM2CLK 为 72MHZ */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
     //TIM_DeInit(TIM2);
-	
-	/* 自动重装载寄存器周期的值(计数值) */
-    TIM_TimeBaseStructure.TIM_Period=1000;
-	
+
+    /* 自动重装载寄存器周期的值(计数值) */
+    TIM_TimeBaseStructure.TIM_Period = 1000;
+
     /* 累计 TIM_Period个频率后产生一个更新或者中断 */
-	  /* 时钟预分频数为72 */
-    TIM_TimeBaseStructure.TIM_Prescaler= 71;
-	
-		/* 对外部时钟进行采样的时钟分频,这里没有用到 */
-    TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;
-	
-    TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up; 
+    /* 时钟预分频数为72 */
+    TIM_TimeBaseStructure.TIM_Prescaler = 71;
+
+    /* 对外部时钟进行采样的时钟分频,这里没有用到 */
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-	
+
     TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-	
-    TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
-		
-    TIM_Cmd(TIM2, ENABLE);																		
-    
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , DISABLE);		/*先关闭等待使用*/    
+
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    TIM_Cmd(TIM2, ENABLE);
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, DISABLE); /*先关闭等待使用*/
 }
