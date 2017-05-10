ccdcdbe5bf5c0d6b71e0a7a6aab206cef0197a23
Ace <258613763@qq.com>
2017-05-10 17:03:31 +0800

    定时器代码整理

--- STDOS/Device/Timer.cpp
+++ STDOS/Device/Timer.cpp
@@ -1,77 +1,22 @@
 #include "Timer.h"
 #include "stm32f10x_rcc.h"
 
-#define TIMS {TIM1,TIM2,TIM3,TIM4,TIM5}
-
-
-static TIM_TypeDef *const g_Timers[] = TIMS;
 static Timer **Timers = NULL;// 已经实例化的定时器对象
+TIM_TypeDef *_port; 
+#define TIMS {TIM1,TIM2,TIM3,TIM4,TIM5}
+static TIM_TypeDef *const g_Timers[] = TIMS;
 const byte Timer::TimerCount = ArrayLength(g_Timers);
+
+void Timer::SetHandler(bool set)
+{
+	
+}
 
 Timer::Timer(TIMER index)
 {
 	
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-TIM_TypeDef *_port; 
-#if 0
-Timer::Timer(TIM_TypeDef *timer)
-{
-    assert_param(timer);
-
-    // 初始化静态数组
-    if (!Timers)
-    {
-        Timers = new Timer *[TimerCount];
-        #if 0
-            ArrayZero2(Timers, TimerCount);
-        #endif 
-    }
-
-    //TIM_TypeDef* g_Timers[] = TIMS;
-    byte idx = 0xFF;
-    #if 0
-        for (int i = 0; i < ArrayLength(g_Timers); i++)
-        {
-            if (g_Timers[i] == timer)
-            {
-                idx = i;
-                break;
-            }
-        }
-    #endif 
-    assert_param(idx <= ArrayLength(g_Timers));
-
-    Timers[idx] = this;
-
-    _index = idx;
-    _port = g_Timers[idx];
-
-    // 默认情况下，预分频到1MHz，然后1000个周期，即是1ms中断一次
-    /*Prescaler = Sys.Clock / 1000000;
-    Period = 1000;*/
-    SetFrequency(10);
-
-    Opened = false;
-    #if 0
-        _Handler = NULL;
-    #endif 
-}
-#endif
 Timer::~Timer()
 {
     if (Opened)
@@ -81,43 +26,6 @@ Timer::~Timer()
             Register(NULL);
     #endif 
     Timers[_index] = NULL;
-}
-
-// 创建指定索引的定时器，如果已有则直接返回，默认0xFF表示随机分配
-Timer *Timer::Create(byte index)
-{
-    // 特殊处理随机分配
-    if (index == 0xFF)
-    {
-        // 初始化静态数组
-        if (!Timers)
-        {
-            Timers = new Timer *[TimerCount];
-            #if 0
-                ArrayZero2(Timers, TimerCount);
-            #endif 
-        }
-
-        // 找到第一个可用的位置，没有被使用，并且该位置定时器存在
-        byte i = 0;
-        for (; i < TimerCount && (Timers[i] || !g_Timers[i]); i++)
-            ;
-
-        if (i >= TimerCount)
-        {
-            debug_printf("Timer::Create 失败！没有空闲定时器可用！\r\n");
-            return NULL;
-        }
-
-        index = i;
-    }
-
-    assert_param(index < TimerCount);
-
-    if (Timers[index])
-        return Timers[index];
-    else
-        return new Timer((TIMER)index);
 }
 void Timer::Open()	// 开始定时器
 {
@@ -184,15 +92,92 @@ void Timer::Close()	// 停止定时器
 void Timer::Config()
 {
 }
-void Timer::OnInit()
+// 设置频率，自动计算预分频
+void Timer::SetFrequency(uint frequency)
+{
+    // 获取当前频率
+    RCC_ClocksTypeDef clock;
+    RCC_GetClocksFreq(&clock);
+
+    #if defined(STM32F1) || defined(STM32F4)
+        uint clk = clock.PCLK1_Frequency;
+        if ((uint)_port &0x00010000)
+            clk = clock.PCLK2_Frequency;
+        clk <<= 1;
+    #elif defined(STM32F0)
+        uint clk = clock.PCLK_Frequency << 1;
+    #endif 
+
+    assert_param(frequency > 0 && frequency <= clk);
+
+    uint s = 1;
+    uint p = s / frequency;
+
+    uint pre = clk / s; // prescaler
+
+    //while (pre >= 0x10000 || p == 0) { // prescaler 太大
+    // 周期刚好为1也不行，配置的时候需要先减去1，就变成了0
+    while (pre >= 0x10000 || p <= 1)
+    {
+        // prescaler 太大
+        if (p >= 0x80000000)
+            return ;
+        s *= 10;
+        pre /= 10;
+        p = s / frequency;
+    }
+
+    if (_index + 1 != 2 && _index + 1 != 5)
+    {
+        // 16 bit timer
+        while (p >= 0x10000)
+        {
+            // period too large
+            if (pre > 0x8000)
+                return ;
+            pre <<= 1;
+            p >>= 1;
+        }
+    }
+
+    Prescaler = pre;
+    Period = p;
+
+    // 如果已启动定时器，则重新配置一下，让新设置生效
+    if (Opened)
+    {
+        TIM_TimeBaseInitTypeDef _timer;
+        TIM_TimeBaseStructInit(&_timer);
+        _timer.TIM_Period = Period - 1;
+        _timer.TIM_Prescaler = Prescaler - 1;
+        //_timer.TIM_ClockDivision = 0x0;
+        _timer.TIM_CounterMode = TIM_CounterMode_Up;
+        TIM_TimeBaseInit(_port, &_timer);
+    }
+}
+uint Timer::GetCounter()
+{
+	return 0;
+}
+void Timer::SetCounter(uint cnt)		// 设置计数器值
+{
+	
+}
+void Timer::Register(const Delegate<Timer&>& dlg)
 {
 }
-	void Timer::OnOpen()
-	{
-	}
-	void Timer::OnClose()
-	{
-	}
+void Timer::OnInterrupt()
+{
+    // 检查指定的 TIM 中断发生
+    if (TIM_GetITStatus(_port, TIM_IT_Update) == RESET)
+        return ;
+    // 必须清除TIMx的中断待处理位，否则会频繁中断
+    TIM_ClearITPendingBit(_port, TIM_IT_Update);
+    #if 0
+        if (_Handler)
+            _Handler(this, _Param);
+    #endif 
+}
 
 void Timer::ClockCmd(int idx, bool state)
 {
@@ -263,6 +248,130 @@ void Timer::ClockCmd(int idx, bool state)
             #endif 
     }
 }
+void Timer::OnHandler(ushort num, void *param)
+{
+    Timer *timer = (Timer*)param;
+    if (timer)
+        timer->OnInterrupt();
+}
+// 创建指定索引的定时器，如果已有则直接返回，默认0xFF表示随机分配
+Timer *Timer::Create(byte index)
+{
+    // 特殊处理随机分配
+    if (index == 0xFF)
+    {
+        // 初始化静态数组
+        if (!Timers)
+        {
+            Timers = new Timer *[TimerCount];
+            #if 0
+                ArrayZero2(Timers, TimerCount);
+            #endif 
+        }
+
+        // 找到第一个可用的位置，没有被使用，并且该位置定时器存在
+        byte i = 0;
+        for (; i < TimerCount && (Timers[i] || !g_Timers[i]); i++)
+            ;
+
+        if (i >= TimerCount)
+        {
+            debug_printf("Timer::Create 失败！没有空闲定时器可用！\r\n");
+            return NULL;
+        }
+
+        index = i;
+    }
+
+    assert_param(index < TimerCount);
+
+    if (Timers[index])
+        return Timers[index];
+    else
+        return new Timer((TIMER)index);
+}
+void Timer::OnInit()
+{
+}
+	void Timer::OnOpen()
+	{
+	}
+	void Timer::OnClose()
+	{
+	}
+const void* Timer::GetTimer(byte idx)
+{
+	return nullptr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+Timer::Timer(TIM_TypeDef *timer)
+{
+    assert_param(timer);
+
+    // 初始化静态数组
+    if (!Timers)
+    {
+        Timers = new Timer *[TimerCount];
+        #if 0
+            ArrayZero2(Timers, TimerCount);
+        #endif 
+    }
+
+    //TIM_TypeDef* g_Timers[] = TIMS;
+    byte idx = 0xFF;
+    #if 0
+        for (int i = 0; i < ArrayLength(g_Timers); i++)
+        {
+            if (g_Timers[i] == timer)
+            {
+                idx = i;
+                break;
+            }
+        }
+    #endif 
+    assert_param(idx <= ArrayLength(g_Timers));
+
+    Timers[idx] = this;
+
+    _index = idx;
+    _port = g_Timers[idx];
+
+    // 默认情况下，预分频到1MHz，然后1000个周期，即是1ms中断一次
+    /*Prescaler = Sys.Clock / 1000000;
+    Period = 1000;*/
+    SetFrequency(10);
+
+    Opened = false;
+    #if 0
+        _Handler = NULL;
+    #endif 
+}
+#endif
+
+
+
+
+
+
 
 // 设置预分频目标，比如1MHz
 /*void Timer::SetScaler(uint scaler)
@@ -273,69 +382,6 @@ uint ps = Sys.Clock / scaler;
 assert_param(ps > 0 && ps <= 0xFFFF);
 }*/
 
-// 设置频率，自动计算预分频
-void Timer::SetFrequency(uint frequency)
-{
-    // 获取当前频率
-    RCC_ClocksTypeDef clock;
-    RCC_GetClocksFreq(&clock);
-
-    #if defined(STM32F1) || defined(STM32F4)
-        uint clk = clock.PCLK1_Frequency;
-        if ((uint)_port &0x00010000)
-            clk = clock.PCLK2_Frequency;
-        clk <<= 1;
-    #elif defined(STM32F0)
-        uint clk = clock.PCLK_Frequency << 1;
-    #endif 
-
-    assert_param(frequency > 0 && frequency <= clk);
-
-    uint s = 1;
-    uint p = s / frequency;
-
-    uint pre = clk / s; // prescaler
-
-    //while (pre >= 0x10000 || p == 0) { // prescaler 太大
-    // 周期刚好为1也不行，配置的时候需要先减去1，就变成了0
-    while (pre >= 0x10000 || p <= 1)
-    {
-        // prescaler 太大
-        if (p >= 0x80000000)
-            return ;
-        s *= 10;
-        pre /= 10;
-        p = s / frequency;
-    }
-
-    if (_index + 1 != 2 && _index + 1 != 5)
-    {
-        // 16 bit timer
-        while (p >= 0x10000)
-        {
-            // period too large
-            if (pre > 0x8000)
-                return ;
-            pre <<= 1;
-            p >>= 1;
-        }
-    }
-
-    Prescaler = pre;
-    Period = p;
-
-    // 如果已启动定时器，则重新配置一下，让新设置生效
-    if (Opened)
-    {
-        TIM_TimeBaseInitTypeDef _timer;
-        TIM_TimeBaseStructInit(&_timer);
-        _timer.TIM_Period = Period - 1;
-        _timer.TIM_Prescaler = Prescaler - 1;
-        //_timer.TIM_ClockDivision = 0x0;
-        _timer.TIM_CounterMode = TIM_CounterMode_Up;
-        TIM_TimeBaseInit(_port, &_timer);
-    }
-}
 
 #if 0
     void Timer::Register(EventHandler handler, void *param)
@@ -353,22 +399,6 @@ void Timer::SetFrequency(uint frequency)
             Interrupt.Deactivate(irqs[_index]);
     }
 #endif 
-void Timer::OnHandler(ushort num, void *param)
-{
-    Timer *timer = (Timer*)param;
-    if (timer)
-        timer->OnInterrupt();
-}
 
-void Timer::OnInterrupt()
-{
-    // 检查指定的 TIM 中断发生
-    if (TIM_GetITStatus(_port, TIM_IT_Update) == RESET)
-        return ;
-    // 必须清除TIMx的中断待处理位，否则会频繁中断
-    TIM_ClearITPendingBit(_port, TIM_IT_Update);
-    #if 0
-        if (_Handler)
-            _Handler(this, _Param);
-    #endif 
-}
+
+
