43488892647786e9fb910475d5985d5c5e93b6f1
Ace <258613763@qq.com>
2017-05-11 16:46:46 +0800

    定时器代码整理

--- STDOS/Device/Timer.cpp
+++ STDOS/Device/Timer.cpp
@@ -91,9 +91,82 @@ void Timer::Close()	// 停止定时器
 
     Opened = false;
 }
+/*
+ * TIM_Period / Auto Reload Register(ARR) = 1000   TIM_Prescaler--71 
+ * 中断周期为 = 1/(72MHZ /72) * 1000 = 1ms
+ *
+ * TIMxCLK/CK_PSC --> TIMxCNT --> TIM_Period(ARR) --> 中断 且TIMxCNT重置为0重新计数 
+ */
 void Timer::Config()
 {
-	
+	switch(this->_index)
+		{
+			case Timer1:
+				break;
+			case Timer2:
+				TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+    /* 设置TIM2CLK 为 72MHZ */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    //TIM_DeInit(TIM2);
+
+    /* 自动重装载寄存器周期的值(计数值) */
+    TIM_TimeBaseStructure.TIM_Period = 1000;
+
+    /* 累计 TIM_Period个频率后产生一个更新或者中断 */
+    /* 时钟预分频数为72 */
+    TIM_TimeBaseStructure.TIM_Prescaler = 71;
+
+    /* 对外部时钟进行采样的时钟分频,这里没有用到 */
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    TIM_Cmd(TIM2, ENABLE);
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, DISABLE); /*先关闭等待使用*/		
+			
+				break;
+			case Timer3:
+				break;
+			case Timer4:
+				break;
+			case Timer5:
+				break;
+			case Timer6:
+				break;
+			case Timer7:
+				break;
+			case Timer8:
+				break;
+			case Timer9:
+				break;
+			case Timer10:
+				break;
+			case Timer11:
+				break;
+			case Timer12:
+				break;
+			case Timer13:
+				break;
+			case Timer14:
+				break;
+			case Timer15:
+				break;
+			case Timer16:
+				break;
+			case Timer17:
+				break;
+			case Timer18:
+				break;
+			default:
+				break;
+		}	
 }
 // 设置频率，自动计算预分频
 void Timer::SetFrequency(uint frequency)
--- User/main.cpp
+++ User/main.cpp
@@ -76,8 +76,6 @@ void OnPress(InputPort &port, bool down)
 {
     debug_printf("Press P%c%d down=%d\r\n", _PIN_NAME(port._Pin), down);
 }
-
-void TIM2_Configuration(void);
 void TIM2_NVIC_Configuration(void);
 #include "stm32f10x_tim.h"
 volatile u32 time2cnt = 0; // ms 计时变量 
@@ -133,9 +131,7 @@ int main(void)
     // 初始化为输出
 	
 	timer2.SetCounter(1000);
-    /* TIM2 定时配置 */
-    TIM2_Configuration();
-
+	timer2.Config();
     /* 实战定时器的中断优先级 */
     TIM2_NVIC_Configuration();
 
@@ -156,40 +152,4 @@ void TIM2_NVIC_Configuration(void)
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
-}
-
-/*
- * TIM_Period / Auto Reload Register(ARR) = 1000   TIM_Prescaler--71 
- * 中断周期为 = 1/(72MHZ /72) * 1000 = 1ms
- *
- * TIMxCLK/CK_PSC --> TIMxCNT --> TIM_Period(ARR) --> 中断 且TIMxCNT重置为0重新计数 
- */
-void TIM2_Configuration(void)
-{
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-
-    /* 设置TIM2CLK 为 72MHZ */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-    //TIM_DeInit(TIM2);
-
-    /* 自动重装载寄存器周期的值(计数值) */
-    TIM_TimeBaseStructure.TIM_Period = 1000;
-
-    /* 累计 TIM_Period个频率后产生一个更新或者中断 */
-    /* 时钟预分频数为72 */
-    TIM_TimeBaseStructure.TIM_Prescaler = 71;
-
-    /* 对外部时钟进行采样的时钟分频,这里没有用到 */
-    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-
-    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-
-    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-
-    TIM_Cmd(TIM2, ENABLE);
-
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, DISABLE); /*先关闭等待使用*/
 }
