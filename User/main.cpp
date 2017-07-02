702c4b42b270a2bcd76682987624a48573b08c2b
Ace <258613763@qq.com>
2017-07-02 21:05:03 +0800

    pwm代码整理

--- User/main.cpp
+++ User/main.cpp
@@ -88,42 +88,6 @@ uint8_t indexWave[] =
 /*    _______    ______     _____      ____       ___        __         _
  * |_|       |__|      |___|     |____|    |_____|   |______|  |_______| |________|
  */
-static void TIMx_Breathing_Init(void)
-{
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    TIM_OCInitTypeDef TIM_OCInitStructure;
-
-
-    /* 设置TIM3CLK 时钟为72MHZ */
-    //  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); 					//使能TIM3时钟
-//    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-    /* 基本定时器配置 */
-    TIM_TimeBaseStructure.TIM_Period = 255; //当定时器从0计数到255，即为266次，为一个定时周期
-    TIM_TimeBaseStructure.TIM_Prescaler = 1999; //设置预分频：
-    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //设置时钟分频系数：不分频(这里用不到)
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; //向上计数模式
-    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-
-    /* PWM模式配置 */
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //配置为PWM模式1
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //使能输出
-    TIM_OCInitStructure.TIM_Pulse = 0; //设置初始PWM脉冲宽度为0	
-    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low; //当定时器计数值小于CCR1_Val时为低电平
-
-    TIM_OC3Init(TIM3, &TIM_OCInitStructure); //使能通道3
-
-
-    TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
-
-    TIM_ARRPreloadConfig(TIM3, ENABLE); //使能TIM3重载寄存器ARR
-
-    /* TIM3 enable counter */
-//    TIM_Cmd(TIM3, ENABLE); //使能定时器3	
-
-//    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); //使能update中断
-}
-
 /* 呼吸灯中断服务函数 */
 void macTIMx_IRQHandler(void)
 {
