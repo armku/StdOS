d54c74da5cf22800032422faa8a409ce5fe41eac
Ace <258613763@qq.com>
2017-07-02 10:56:12 +0800

    代码整理

--- STDOS/Config.cpp
+++ STDOS/Config.cpp
@@ -1,2 +1,8 @@
 #include "Config.h"
-
+//    #if 0
+    //        //flash 最后一块作为配置区
+    //        Config::Current = &Config::CreateFlash();
+    //    #endif  
+	//    #if 0
+    //        sys.Codec = codec;
+    //	#endif
--- User/main.cpp
+++ User/main.cpp
@@ -16,36 +16,34 @@
 #include "Drivers\lcd_dr.h"
 #include "TInterrupt.h"
 
-const byte vers[]="yyyy-MM-dd HH:mm:ss";
+const byte vers[] = "yyyy-MM-dd HH:mm:ss";
 #if 1
-OutputPort led1(PB0);
-OutputPort led2(PF7);
-OutputPort led3(PF8);
-#else
-OutputPort led1(PD0);
-OutputPort led2(PD1);
-OutputPort led3(PD2);
-#endif
+    OutputPort led1(PB0);
+    OutputPort led2(PF7);
+    OutputPort led3(PF8);
+#else 
+    OutputPort led1(PD0);
+    OutputPort led2(PD1);
+    OutputPort led3(PD2);
+#endif 
 
 void LedTask(void *param)
 {
     OutputPort *leds = (OutputPort*)param;
     *leds = ! * leds;
-//    led2 = key0;
+    //    led2 = key0;
 }
+
 //#define namee "StdOS"
 //PWM ledLCD(PD12);
 
 int main(void)
 {
-    TSys &sys = (TSys &)(Sys);
-    //    #if 0
-    //        sys.Codec = codec;
-    //	#endif
+    TSys &sys = (TSys &)(Sys);   
 
-//    sys.Name = (char*)namee;
-	byte aa=vers[0];
-	aa=aa;
+    //    sys.Name = (char*)namee;
+    byte aa = vers[0];
+    aa = aa;
 
     sys.Init();
     #if DEBUG
@@ -55,12 +53,9 @@ int main(void)
     #else 
         WatchDog::Start();
     #endif 
-    //    #if 0
-    //        //flash 最后一块作为配置区
-    //        Config::Current = &Config::CreateFlash();
-    //    #endif  
-	
+    
+
     Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
-	
+
     Sys.Start();
 }
