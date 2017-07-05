fced16719d3886e8bcac8ec2e45801d218a85284
Ace <258613763@qq.com>
2017-07-05 19:13:59 +0800

    iic代码整理

--- STDOS/Drivers/AT24CXX.cpp
+++ STDOS/Drivers/AT24CXX.cpp
@@ -591,40 +591,6 @@ ushort AT24CXX::jsPageSize(uint type) //计算存储页大小
 #define I2C_WR	0		/* 写控制bit */
 #define I2C_RD	1		/* 读控制bit */
 AT24CXX at2402(PB6,PB7,AT24C02);
-SoftI2C *iic;
-
-
-void i2c_Start(void)
-{
-	iic->Start();
-}
-void i2c_Stop(void)
-{
-	iic->Stop();
-}
-
-void i2c_SendByte(uint8_t _ucByte)
-{
-	iic->WriteByte(_ucByte);
-}
-uint8_t i2c_ReadByte(void)
-{
-	return iic->ReadByte();
-
-}
-
-uint8_t i2c_WaitAck(void)
-{
-	return iic->WaitAck();
-}
-void i2c_Ack(void)
-{
-	iic->Ack();
-}
-void i2c_NAck(void)
-{
-	iic->Ack(false);
-}
 
 byte AT24CXX::checkDevice()
 {
@@ -660,34 +626,34 @@ uint8_t ee_ReadBytes(uint8_t *_pReadBuf, uint16_t _usAddress, uint16_t _usSize)
 	/* 采用串行EEPROM随即读取指令序列，连续读取若干字节 */
 	
 	/* 第1步：发起I2C总线启动信号 */
-	i2c_Start();
+	at2402.IIC->Start();
 	
 	/* 第2步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-	i2c_SendByte(EE_DEV_ADDR | I2C_WR);	/* 此处是写指令 */
+	at2402.IIC->WriteByte(EE_DEV_ADDR | I2C_WR);	/* 此处是写指令 */
 	
 	/* 第3步：发送ACK */
-	if (i2c_WaitAck() != 0)
+	if (at2402.IIC->WaitAck() != 0)
 	{
 		goto cmd_fail;	/* EEPROM器件无应答 */
 	}
 
 	/* 第4步：发送字节地址，24C02只有256字节，因此1个字节就够了，如果是24C04以上，那么此处需要连发多个地址 */
-	i2c_SendByte((uint8_t)_usAddress);
+	at2402.IIC->WriteByte((uint8_t)_usAddress);
 	
 	/* 第5步：发送ACK */
-	if (i2c_WaitAck() != 0)
+	if (at2402.IIC->WaitAck() != 0)
 	{
 		goto cmd_fail;	/* EEPROM器件无应答 */
 	}
 	
 	/* 第6步：重新启动I2C总线。前面的代码的目的向EEPROM传送地址，下面开始读取数据 */
-	i2c_Start();
+	at2402.IIC->Start();
 	
 	/* 第7步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-	i2c_SendByte(EE_DEV_ADDR | I2C_RD);	/* 此处是读指令 */
+	at2402.IIC->WriteByte(EE_DEV_ADDR | I2C_RD);	/* 此处是读指令 */
 	
 	/* 第8步：发送ACK */
-	if (i2c_WaitAck() != 0)
+	if (at2402.IIC->WaitAck() != 0)
 	{
 		goto cmd_fail;	/* EEPROM器件无应答 */
 	}	
@@ -695,25 +661,25 @@ uint8_t ee_ReadBytes(uint8_t *_pReadBuf, uint16_t _usAddress, uint16_t _usSize)
 	/* 第9步：循环读取数据 */
 	for (i = 0; i < _usSize; i++)
 	{
-		_pReadBuf[i] = i2c_ReadByte();	/* 读1个字节 */
+		_pReadBuf[i] = at2402.IIC->ReadByte();	/* 读1个字节 */
 		
 		/* 每读完1个字节后，需要发送Ack， 最后一个字节不需要Ack，发Nack */
 		if (i != _usSize - 1)
 		{
-			i2c_Ack();	/* 中间字节读完后，CPU产生ACK信号(驱动SDA = 0) */
+			at2402.IIC->Ack();	/* 中间字节读完后，CPU产生ACK信号(驱动SDA = 0) */
 		}
 		else
 		{
-			i2c_NAck();	/* 最后1个字节读完后，CPU产生NACK信号(驱动SDA = 1) */
+			at2402.IIC->Ack(false);	/* 最后1个字节读完后，CPU产生NACK信号(驱动SDA = 1) */
 		}
 	}
 	/* 发送I2C总线停止信号 */
-	i2c_Stop();
+	at2402.IIC->Stop();
 	return 1;	/* 执行成功 */
 
 cmd_fail: /* 命令执行失败后，切记发送停止信号，避免影响I2C总线上其他设备 */
 	/* 发送I2C总线停止信号 */
-	i2c_Stop();
+	at2402.IIC->Stop();
 	return 0;
 }
 uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize)
@@ -735,7 +701,7 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 		if ((i == 0) || (usAddr & (EE_PAGE_SIZE - 1)) == 0)
 		{
 			/*　第０步：发停止信号，启动内部写操作　*/
-			i2c_Stop();
+			at2402.IIC->Stop();
 			
 			/* 通过检查器件应答的方式，判断内部写操作是否完成, 一般小于 10ms 			
 				CLK频率为200KHz时，查询次数为30次左右
@@ -743,13 +709,13 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 			for (m = 0; m < 100; m++)
 			{				
 				/* 第1步：发起I2C总线启动信号 */
-				i2c_Start();
+				at2402.IIC->Start();
 				
 				/* 第2步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-				i2c_SendByte(EE_DEV_ADDR | I2C_WR);	/* 此处是写指令 */
+				at2402.IIC->WriteByte(EE_DEV_ADDR | I2C_WR);	/* 此处是写指令 */
 				
 				/* 第3步：发送一个时钟，判断器件是否正确应答 */
-				if (i2c_WaitAck() == 0)
+				if (at2402.IIC->WaitAck() == 0)
 				{
 					break;
 				}
@@ -760,20 +726,20 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 			}
 		
 			/* 第4步：发送字节地址，24C02只有256字节，因此1个字节就够了，如果是24C04以上，那么此处需要连发多个地址 */
-			i2c_SendByte((uint8_t)usAddr);
+			at2402.IIC->WriteByte((uint8_t)usAddr);
 			
 			/* 第5步：发送ACK */
-			if (i2c_WaitAck() != 0)
+			if (at2402.IIC->WaitAck() != 0)
 			{
 				goto cmd_fail;	/* EEPROM器件无应答 */
 			}
 		}
 	
 		/* 第6步：开始写入数据 */
-		i2c_SendByte(_pWriteBuf[i]);
+		at2402.IIC->WriteByte(_pWriteBuf[i]);
 	
 		/* 第7步：发送ACK */
-		if (i2c_WaitAck() != 0)
+		if (at2402.IIC->WaitAck() != 0)
 		{
 			goto cmd_fail;	/* EEPROM器件无应答 */
 		}
@@ -782,12 +748,12 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 	}
 	
 	/* 命令执行成功，发送I2C总线停止信号 */
-	i2c_Stop();
+	at2402.IIC->Stop();
 	return 1;
 
 cmd_fail: /* 命令执行失败后，切记发送停止信号，避免影响I2C总线上其他设备 */
 	/* 发送I2C总线停止信号 */
-	i2c_Stop();
+	at2402.IIC->Stop();
 	return 0;
 }
 
@@ -821,9 +787,7 @@ printf("\r\n");
 //	i2c_CfgGpio();
 //	at2402.Init();
 //	at2402.Test();
-	
-	iic=at2402.IIC;
-	
+		
 	uint16_t i;
 	uint8_t write_buf[EE_SIZE];
   uint8_t read_buf[EE_SIZE];
