fde20cc905d4b25f7608798ff7b8acb8c9e93de4
Ace <258613763@qq.com>
2017-07-05 20:59:48 +0800

    代码整理

--- User/main.cpp
+++ User/main.cpp
@@ -12,14 +12,14 @@
 
 const byte vers[] = "yyyy-MM-dd HH:mm:ss";
 #if 1
-    OutputPort led1(PB0);
-    OutputPort led2(PF7);
-    OutputPort led3(PF8);
+    OutputPort led1(PB0,false);
+    OutputPort led2(PF7,false);
+    OutputPort led3(PF8,false);
 #else 
-    OutputPort led1(PD0);
-    OutputPort led2(PD1);
-    OutputPort led3(PD2);
-	OutputPort ledLCD(PD12);
+    OutputPort led1(PD0,false);
+    OutputPort led2(PD1,false);
+    OutputPort led3(PD2,false);
+	OutputPort ledLCD(PD12,false);
 #endif 
 
 void LedTask(void *param)
