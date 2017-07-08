e8b080a17ea6dc95e6f6ddcea8235e3838acca4b
Ace <258613763@qq.com>
2017-07-08 10:37:48 +0800

    增加液晶驱动测试

--- STDOS/Drivers/ST7565R.cpp
+++ STDOS/Drivers/ST7565R.cpp
@@ -799,3 +799,19 @@ void CLcd::ShadowOn(ushort x, ushort y, ushort width, ushort height, byte id)
     this->ShadowRect[id].width = width;
     this->ShadowRect[id].height = height;
 }
+#if 0
+	CLcd lcddr1(PD3, PD6, PD7, PB3, PB4);
+	void ST7565Rtest()
+	{
+		
+		lcddr1.Init();
+		for(int i=0;i<10;i++)
+		{
+			lcddr1.Test(0x55,0xAA);
+			Sys.Sleep(500);
+			lcddr1.Test(0x00,0x00);
+			Sys.Sleep(500);
+		}
+		lcddr1.Test(0x55,0xAA);
+	}
+#endif
--- STDOS/Drivers/lcd_dr.cpp
+++ STDOS/Drivers/lcd_dr.cpp
@@ -415,3 +415,20 @@ ushort CLcd_DR::SearchhzIndex(byte lo, byte hi, Font font) //²éÕÒºº×Ö±àÂëÎ»ÖÃ
     }
     return 0;
 }
+#if 0
+	CLcd_DR lcddr(PD3, PD6, PD7, PB3, PB4);
+	void CLcd_DRtest()
+	{
+		
+		lcddr.Init();
+		for(int i=0;i<10;i++)
+		{
+			lcddr.Test(0x55,0xAA);
+			Sys.Sleep(500);
+			lcddr.Test(0x00,0x00);
+			Sys.Sleep(500);
+		}
+		lcddr.DisplayStr(0,0,(byte *)"ST7565Rtest");
+		lcddr.Flush();
+	}
+#endif
