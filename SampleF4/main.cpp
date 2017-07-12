6aea66a0073d8301886303e71546b18a5f9d09f2
Ace <258613763@qq.com>
2017-07-12 19:27:21 +0800

    代码整理 F4 led闪烁OK

--- SampleF4/main.cpp
+++ SampleF4/main.cpp
@@ -3,12 +3,22 @@
 OutputPort led1(PF9,false);
 OutputPort led2(PF10,false);
 
+void LedTask(void *param)
+{
+    OutputPort *leds = (OutputPort*)param;
+    *leds = ! * leds;
+    //    led2 = key0;
+}
 int main(void)
 {
-    led1=0;
-	led2=0;
-    while (1)
-	{
+	TSys &sys = (TSys &)(Sys);
+	sys.Init();
+    #if DEBUG
+        Sys.MessagePort = COM1;
+        Sys.ShowInfo();
+    #endif 	
+	//W25Q64Test();
+	Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
 
-    }
+    Sys.Start();
 }
