548082a0bc2cdc7f232c0b8f8889c9fa831d2a2e
Ace <258613763@qq.com>
2017-07-17 20:39:37 +0800

    优化IIC

--- STDOS/Device/I2C.cpp
+++ STDOS/Device/I2C.cpp
@@ -91,7 +91,7 @@ bool I2C::SendSubAddr(int addr)
 // 使用端口和最大速度初始化，因为需要分频，实际速度小于等于该速度
 SoftI2C::SoftI2C(uint speedHz)
 {
-	this->_delay=5;
+	this->_delay=4;
 	this->SCL.OpenDrain = true;
     this->SDA.OpenDrain = true;
 	this->SCL.Invert = false;
