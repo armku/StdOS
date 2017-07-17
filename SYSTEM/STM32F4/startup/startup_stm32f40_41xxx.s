a98e980d6f4df4fa9d1858ad22728cbf30f25d6d
Ace <258613763@qq.com>
2017-07-17 13:30:04 +0800

    代码整理库从1.4升级到1.8 F4

--- SYSTEM/STM32F4/startup/startup_stm32f40_41xxx.s
+++ SYSTEM/STM32F4/startup/startup_stm32f40_41xxx.s
@@ -1,8 +1,8 @@
-;******************** (C) COPYRIGHT 2014 STMicroelectronics ********************
+;******************** (C) COPYRIGHT 2016 STMicroelectronics ********************
 ;* File Name          : startup_stm32f40_41xxx.s
 ;* Author             : MCD Application Team
-;* @version           : V1.4.0
-;* @date              : 04-August-2014
+;* @version           : V1.8.0
+;* @date              : 09-November-2016
 ;* Description        : STM32F40xxx/41xxx devices vector table for MDK-ARM toolchain. 
 ;*                      This module performs:
 ;*                      - Set the initial SP
