caeb03fa1e809b45515255508bd8126f9ca32def
Ace <258613763@qq.com>
2017-07-30 17:29:23 +0800

    代码整理

--- STDOS/Drivers/AD7124.cpp
+++ STDOS/Drivers/AD7124.cpp
@@ -1,93 +1,5 @@
 #include "AD7124.h"
 
-
-#if AD7124SPISOFT
-AD7124::AD7124(SpiSoft *spi)
-#else
-AD7124::AD7124(Spi *spi)
-#endif
-{
-	
-}
-//返回-1,表示转换未完成
-int AD7124::Read(void)
-{
-	return 0;
-}
-//读取AD转换是否正常
-bool AD7124::GetFlag(void)
-{
-	return true;
-}
-
-
-
-
-
-
-
-
-
-
-
-#ifdef _AD7124_H_1
-
-#ifndef __AD7124_SPI_H
-#define __AD7124_SPI_H
-
-#include "stdint.h"
-
-/*
-	(SPI1 : AD7124-8)
-	PA4		:	SPI1-NSS	
-	PA5		:	SPI1-SCK
-	PA6		:	SPI1-MISO
-	PA7		:	SPI1-MOSI
-*/
-
-/*********************** 模拟SPI通信引脚 **************************************/
-#define SPI_NSS_PIN			GPIO_Pin_4
-#define SPI_NSS_PORT		GPIOA
-
-#define SPI_SCK_PIN			GPIO_Pin_5
-#define SPI_SCK_PORT		GPIOA
-
-#define SPI_MISO_PIN		GPIO_Pin_6
-#define SPI_MISO_PORT		GPIOA
-
-#define SPI_MOSI_PIN		GPIO_Pin_7
-#define SPI_MOSI_PORT		GPIOA
-
-/********************  **************************************/
-#define SPI_NSS_L()		(SPI_NSS_PORT->BRR = SPI_NSS_PIN)
-#define SPI_NSS_H()		(SPI_NSS_PORT->BSRR = SPI_NSS_PIN)
-#define SPI_SCK_L()		(SPI_SCK_PORT->BRR = SPI_SCK_PIN)
-#define SPI_SCK_H()		(SPI_SCK_PORT->BSRR = SPI_SCK_PIN)
-#define SPI_MOSI_L()	(SPI_MOSI_PORT->BRR = SPI_MOSI_PIN)
-#define SPI_MOSI_H()	(SPI_MOSI_PORT->BSRR = SPI_MOSI_PIN)
-
-#define SPI_MISO()		GPIO_ReadInputDataBit(SPI_MISO_PORT,SPI_MISO_PIN)
-
-void AD7124_SPI_Config(void);
-uint8_t spi8_run(uint8_t sendData);
-uint16_t spi16_run(uint16_t sendData);
-uint32_t spi24_run(uint32_t sendData);
-uint32_t spi32_run(uint32_t sendData);
-
-#endif
-#ifndef __AD7124_8_H
-#define __AD7124_8_H
-
-/******************************************************************************/
-/* Include Files                                                              */
-/******************************************************************************/
-#include "stdint.h"
-
-extern uint32_t ad7124Buf[];
-/************************ AD7124 COMMS寄存器读写控制 *************************************/
-#define AD7124_WR				0x00
-#define AD7124_RD				0x40
-
 /* AD7124 Register Map */
 #define AD7124_COMM_REG      0x00
 #define AD7124_STATUS_REG    0x00
@@ -345,6 +257,93 @@ extern uint32_t ad7124Buf[];
 #define AD7124_FILT_REG_POST_FILTER(x)    (((x) & 0x7) << 17)
 #define AD7124_FILT_REG_SINGLE_CYCLE      (1 << 16)
 #define AD7124_FILT_REG_FS(x)             (((x) & 0x7FF) << 0)
+
+#if AD7124SPISOFT
+AD7124::AD7124(SpiSoft *spi)
+#else
+AD7124::AD7124(Spi *spi)
+#endif
+{
+	
+}
+//返回-1,表示转换未完成
+int AD7124::Read(void)
+{
+	return 0;
+}
+//读取AD转换是否正常
+bool AD7124::GetFlag(void)
+{
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+#ifdef _AD7124_H_1
+
+#ifndef __AD7124_SPI_H
+#define __AD7124_SPI_H
+
+#include "stdint.h"
+
+/*
+	(SPI1 : AD7124-8)
+	PA4		:	SPI1-NSS	
+	PA5		:	SPI1-SCK
+	PA6		:	SPI1-MISO
+	PA7		:	SPI1-MOSI
+*/
+
+/*********************** 模拟SPI通信引脚 **************************************/
+#define SPI_NSS_PIN			GPIO_Pin_4
+#define SPI_NSS_PORT		GPIOA
+
+#define SPI_SCK_PIN			GPIO_Pin_5
+#define SPI_SCK_PORT		GPIOA
+
+#define SPI_MISO_PIN		GPIO_Pin_6
+#define SPI_MISO_PORT		GPIOA
+
+#define SPI_MOSI_PIN		GPIO_Pin_7
+#define SPI_MOSI_PORT		GPIOA
+
+/********************  **************************************/
+#define SPI_NSS_L()		(SPI_NSS_PORT->BRR = SPI_NSS_PIN)
+#define SPI_NSS_H()		(SPI_NSS_PORT->BSRR = SPI_NSS_PIN)
+#define SPI_SCK_L()		(SPI_SCK_PORT->BRR = SPI_SCK_PIN)
+#define SPI_SCK_H()		(SPI_SCK_PORT->BSRR = SPI_SCK_PIN)
+#define SPI_MOSI_L()	(SPI_MOSI_PORT->BRR = SPI_MOSI_PIN)
+#define SPI_MOSI_H()	(SPI_MOSI_PORT->BSRR = SPI_MOSI_PIN)
+
+#define SPI_MISO()		GPIO_ReadInputDataBit(SPI_MISO_PORT,SPI_MISO_PIN)
+
+void AD7124_SPI_Config(void);
+uint8_t spi8_run(uint8_t sendData);
+uint16_t spi16_run(uint16_t sendData);
+uint32_t spi24_run(uint32_t sendData);
+uint32_t spi32_run(uint32_t sendData);
+
+#endif
+#ifndef __AD7124_8_H
+#define __AD7124_8_H
+
+/******************************************************************************/
+/* Include Files                                                              */
+/******************************************************************************/
+#include "stdint.h"
+
+extern uint32_t ad7124Buf[];
+/************************ AD7124 COMMS寄存器读写控制 *************************************/
+#define AD7124_WR				0x00
+#define AD7124_RD				0x40
 
 /******************************************************************************/
 /* Functions Prototypes                                                       */
