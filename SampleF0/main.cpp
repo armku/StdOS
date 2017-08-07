280fcb99dabc83550d25f46acd190eceb31d6cb2
Ace <258613763@qq.com>
2017-08-07 20:55:27 +0800

    代码整理

--- SampleF0/main.cpp
+++ SampleF0/main.cpp
@@ -1,9 +1,12 @@
 #include "Port.h"
 #include "stm32f0xx.h"
 
-void BSP_Configuration(void);
+void LED_GPIO(void);
 void LED_Demo(void);
 OutputPort led1(PC6, false);
+OutputPort led2(PC7, false);
+OutputPort led3(PC8, false);
+OutputPort led4(PC9, false);
 void LedTask(void *param)
 {
     OutputPort *leds = (OutputPort*)param;
@@ -23,7 +26,7 @@ void main()
         Sys.ShowInfo();
     #endif 
 
-    BSP_Configuration(); //硬件驱动初始化函数
+    LED_GPIO(); //硬件驱动初始化函数
 //    Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
 
 
@@ -39,31 +42,6 @@ void main()
 #define LED_G_PIN GPIO_Pin_8 //定义GPIO_Pin_8引脚为LED_G_PIN，相当于重新命名。
 #define LED_R_PIN GPIO_Pin_9 //定义GPIO_Pin_9引脚为LED_R_PIN，相当于重新命名。
 #define LED_PORT GPIOC  //定义GPIOC端口为LED_PORT，相当于重新命名。
-
-//如果想用其他管脚控制LED,那么只需更改上面对应的端口与引脚编号即可
-
-//#define LED_1_L GPIO_ResetBits(LED_PORT,LED_1_PIN);    //定义LED_1_L,调用LED_1_L命令，PC6引脚输出低电平
-//#define LED_1_H GPIO_SetBits(LED_PORT,LED_1_PIN);      //定义LED_1_H,调用LED_1_H命令，PC6引脚输出低高平
-#define LED_1_T GPIO_WriteBit(LED_PORT,LED_1_PIN,(BitAction)(1 - GPIO_ReadOutputDataBit(LED_PORT, LED_1_PIN)));//定义LED_1_T,调用LED_1_T命令，PC6引脚输出电平反转
-
-#define LED_B_L GPIO_ResetBits(LED_PORT,LED_B_PIN);    //定义LED_B_L,调用LED_B_L命令，PC7引脚输出低电平
-#define LED_B_H GPIO_SetBits(LED_PORT,LED_B_PIN);      //定义LED_B_H,调用LED_B_H命令，PC7引脚输出低高平
-#define LED_B_T GPIO_WriteBit(LED_PORT,LED_B_PIN,(BitAction)(1 - GPIO_ReadOutputDataBit(LED_PORT, LED_B_PIN)));//定义LED_1_T,调用LED_1_T命令，PC7引脚输出电平反转
-
-#define LED_G_L GPIO_ResetBits(LED_PORT,LED_G_PIN);    //定义LED_G_L,调用LED_G_L命令，PC8引脚输出低电平
-#define LED_G_H GPIO_SetBits(LED_PORT,LED_G_PIN);      //定义LED_G_H,调用LED_G_H命令，PC8引脚输出低高平
-#define LED_G_T GPIO_WriteBit(LED_PORT,LED_G_PIN,(BitAction)(1 - GPIO_ReadOutputDataBit(LED_PORT, LED_G_PIN)));//定义LED_1_T,调用LED_1_T命令，PC8引脚输出电平反转
-
-#define LED_R_L GPIO_ResetBits(LED_PORT,LED_R_PIN);    //定义LED_R_L,调用LED_R_L命令，PC9引脚输出低电平
-#define LED_R_H GPIO_SetBits(LED_PORT,LED_R_PIN);      //定义LED_R_H,调用LED_R_H命令，PC9引脚输出低高平
-#define LED_R_T GPIO_WriteBit(LED_PORT,LED_R_PIN,(BitAction)(1 - GPIO_ReadOutputDataBit(LED_PORT, LED_R_PIN)));//定义LED_1_T,调用LED_1_T命令，PC9引脚输出电平反转
-
-
-
-void RCC_Configuration(void) //时钟初始化函数
-{
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-}
 
 void LED_GPIO(void)
 {
@@ -89,21 +67,9 @@ void LED_GPIO(void)
     GPIO_Init(LED_PORT, &GPIO_InitStructure);
 
     led1=1; //拉高熄灭
-    LED_B_H; //拉高熄灭
-    LED_G_H; //拉高熄灭
-    LED_R_H; //拉高熄灭
-}
-
-/***********************************************************************
- *   函 数 名: BSP_Configuration
- *   功能说明: 初始化硬件设备。只需要调用一次。该函数配置CPU寄存器和外设的寄存器并初始化一些全局变量。
- *   形    参：无
- *   返 回 值: 无
- ***********************************************************************/
-void BSP_Configuration(void) //硬件驱动初始化函数
-{
-    RCC_Configuration(); //时钟初始化函数
-    LED_GPIO(); //LED初始化函数
+    led2=1; //拉高熄灭
+    led3=1; //拉高熄灭
+    led4=1; //拉高熄灭
 }
 
 void delay_ms(uint16_t nms)
@@ -128,20 +94,20 @@ void LED_Demo(void)
     led1=1; //拉高PC6引脚，LED1发光二极管(熄灭)
     delay_ms(500);
 
-//    LED_R_L; //拉低PC9引脚，RGB发光二极管(发红色光)
-//    delay_ms(500);
-//    LED_R_H; //拉高PC9引脚，RGB发光二极管(红色熄灭)
-//    delay_ms(500);
+    led4=0; //拉低PC9引脚，RGB发光二极管(发红色光)
+    delay_ms(500);
+    led4=1; //拉高PC9引脚，RGB发光二极管(红色熄灭)
+    delay_ms(500);
 
-//    LED_G_L; //拉低PC8引脚，RGB发光二极管(发绿色光)
-//    delay_ms(500);
-//    LED_G_H; //拉低PC8引脚，RGB发光二极管(绿色熄灭)
-//    delay_ms(500);
+    led3=0; //拉低PC8引脚，RGB发光二极管(发绿色光)
+    delay_ms(500);
+    led3=1; //拉低PC8引脚，RGB发光二极管(绿色熄灭)
+    delay_ms(500);
 
-//    LED_B_L; //拉低PC7引脚，RGB发光二极管(发蓝色光)
-//    delay_ms(500);
-//    LED_B_H; //拉高PC7引脚，RGB发光二极管(蓝色熄灭)
-//    delay_ms(500);
+    led2=0; //拉低PC7引脚，RGB发光二极管(发蓝色光)
+    delay_ms(500);
+    led2=1; //拉高PC7引脚，RGB发光二极管(蓝色熄灭)
+    delay_ms(500);
 }
 
 
