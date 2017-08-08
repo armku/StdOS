c0f5f667050e47a2da85b21e757294192c82ab7f
Ace <258613763@qq.com>
2017-08-08 13:42:16 +0800

    F0 RTC代码整理

--- STDOS/Device/RTC.cpp
+++ STDOS/Device/RTC.cpp
@@ -2,59 +2,59 @@
 #include "TTime.h"
 
 #ifdef STM32F0
-	#include "stm32f0xx.h"
+    #include "stm32f0xx.h"
 #elif defined STM32F1
-	#include "stm32f10x.h"
+    #include "stm32f10x.h"
 #elif defined STM32F4
-	#include "stm32f4xx.h"
-#endif
+    #include "stm32f4xx.h"
+#endif 
 
 HardRTC::HardRTC()
 {
-	this->LowPower	= true;
-	this->External	= false;
+    this->LowPower = true;
+    this->External = false;
 }
 
 void HardRTC::Init()
 {
     NVIC_InitTypeDef NVIC_InitStructure;
 
-	#ifdef STM32F0
-	#elif defined STM32F1
-	/* Configure one bit for preemption priority */
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-	#elif defined STM32F4
-    /* Configure one bit for preemption priority */
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-	#endif
+    #ifdef STM32F0
+    #elif defined STM32F1
+        /* Configure one bit for preemption priority */
+        NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    #elif defined STM32F4
+        /* Configure one bit for preemption priority */
+        NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    #endif 
     /* Enable the RTC Interrupt */
-	#ifdef STM32F0
+    #ifdef STM32F0
     #elif defined STM32F1
         NVIC_InitStructure.NVIC_IRQChannel = RTC_IRQn;
-		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;    
-	#elif defined STM32F4
-		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;    
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    #elif defined STM32F4
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     #endif 
     NVIC_Init(&NVIC_InitStructure);
     /* Enable PWR and BKP clocks */
-	#ifdef STM32F0
+    #ifdef STM32F0
     #elif defined STM32F1
         RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
-	#elif defined STM32F4
-	
+    #elif defined STM32F4
+
     #endif 
     /* Allow access to BKP Domain */
     PWR_BackupAccessCmd(ENABLE);
 
     /* Reset Backup Domain */
-	#ifdef STM32F0
+    #ifdef STM32F0
     #elif defined STM32F1	
         BKP_DeInit();
-	#elif defined STM32F4
+    #elif defined STM32F4
     #endif 
 
     /* Enable LSE */
@@ -76,13 +76,13 @@ void HardRTC::Init()
 
     /* Wait until last write operation on RTC registers has finished */
     #ifdef STM32F0
-	#elif defined STM32F1
+    #elif defined STM32F1
         RTC_WaitForLastTask();
-	#elif defined STM32F4
+    #elif defined STM32F4
     #endif 
 
     /* Enable the RTC Second */
-	#ifdef STM32F0
+    #ifdef STM32F0
     #elif defined STM32F1
         RTC_ITConfig(RTC_IT_SEC, ENABLE);
 
@@ -94,33 +94,33 @@ void HardRTC::Init()
 
         /* Wait until last write operation on RTC registers has finished */
         RTC_WaitForLastTask();
-	#elif defined STM32F4
-    #endif 	
+    #elif defined STM32F4
+    #endif 
 }
 
 void HardRTC::LoadTime()
 {
-	uint totalSeconds=0;
-	#ifdef STM32F0
+    uint totalSeconds = 0;
+    #ifdef STM32F0
     #elif defined STM32F1
         totalSeconds = RTC_GetCounter();
-		Time.Seconds=totalSeconds;
-		if(totalSeconds>Time.BaseSeconds)
-		{
-			Time.Seconds=totalSeconds-Time.BaseSeconds;
-		}
-		else
-		{
-			Time.Seconds=0;
-		}
-	#elif defined STM32F4
+        Time.Seconds = totalSeconds;
+        if (totalSeconds > Time.BaseSeconds)
+        {
+            Time.Seconds = totalSeconds - Time.BaseSeconds;
+        }
+        else
+        {
+            Time.Seconds = 0;
+        }
+    #elif defined STM32F4
     #endif 
 }
 
 void HardRTC::SaveTime()
 {
-    uint seconds = Time.Seconds+Time.BaseSeconds;
-	#ifdef STM32F0
+    uint seconds = Time.Seconds + Time.BaseSeconds;
+    #ifdef STM32F0
     #elif defined STM32F1
         RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE); //使能PWR和BKP外设时钟  
         PWR_BackupAccessCmd(ENABLE); //使能RTC和后备寄存器访问 
@@ -128,7 +128,7 @@ void HardRTC::SaveTime()
 
         RTC_WaitForLastTask(); //等待最近一次对RTC寄存器的写操作完成  
     #elif defined STM32F4
-	#endif 
+    #endif 
 }
 
 int HardRTC::Sleep(int ms)
@@ -143,19 +143,19 @@ uint HardRTC::ReadBackup(byte addr)
 
 void HardRTC::WriteBackup(byte addr, uint value)
 {
-	addr=addr;
-	value=value;
+    addr = addr;
+    value = value;
 }
 
 HardRTC *HardRTC::Instance()
 {
-    return  new HardRTC();
+    return new HardRTC();
 }
 
 void HardRTC::Start(bool lowpower, bool external)
 {
-	lowpower=lowpower;
-	external=external;
+    lowpower = lowpower;
+    external = external;
 }
 
 /* 秒中断标志，进入秒中断时置1，当时间被刷新之后清0 */
@@ -167,7 +167,7 @@ __IO uint32_t TimeDisplay = 0;
     #endif 
     void RTC_IRQHandler(void)
     {
-		#ifdef STM32F0
+        #ifdef STM32F0
         #elif defined STM32F1
             if (RTC_GetITStatus(RTC_IT_SEC) != RESET)
             {
@@ -181,7 +181,7 @@ __IO uint32_t TimeDisplay = 0;
                 RTC_WaitForLastTask();
             }
         #elif defined STM32F4
-		#endif 
+        #endif 
     }
     #ifdef __cplusplus
     }
@@ -194,19 +194,19 @@ __IO uint32_t TimeDisplay = 0;
     {
         HardRTC *rtc = (HardRTC*)param;
         rtc->LoadTime();
-        now = Time.BaseSeconds+Time.Seconds;
+        now = Time.BaseSeconds + Time.Seconds;
         now.Show();
-		debug_printf("  BaseSeconds:%d Seconds:%d\r\n",Time.BaseSeconds,Time.Seconds);
+        debug_printf("  BaseSeconds:%d Seconds:%d\r\n", Time.BaseSeconds, Time.Seconds);
     }
     void RTCtest()
     {
-		HardRTC* rtc=HardRTC::Instance();
+        HardRTC *rtc = HardRTC::Instance();
         rtc->LowPower = false;
         rtc->External = false;
 
-		rtc->LoadTime();
-		Time.BaseSeconds=Time.Seconds+Time.BaseSeconds;
-        now = Time.BaseSeconds+Time.Seconds;
+        rtc->LoadTime();
+        Time.BaseSeconds = Time.Seconds + Time.BaseSeconds;
+        now = Time.BaseSeconds + Time.Seconds;
         if (now.TotalSeconds() < 100)
         {
             now.Year = 2017;
@@ -215,11 +215,252 @@ __IO uint32_t TimeDisplay = 0;
             now.Hour = 14;
             now.Minute = 17;
 
-            rtc->Init();//设置时间时初始化，否则错乱
-            Time.BaseSeconds = now.TotalSeconds()-Time.Seconds;
-			rtc->LoadTime();
+            rtc->Init(); //设置时间时初始化，否则错乱
+            Time.BaseSeconds = now.TotalSeconds() - Time.Seconds;
+            rtc->LoadTime();
             rtc->SaveTime();
         }
         Sys.AddTask(TimeRefresh, rtc, 100, 1000, "TimeUp");
     }
+#endif 
+
+#ifdef STM32F01
+    /* 类型定义 ------------------------------------------------------------------*/
+    typedef struct
+    {
+        uint8_t Year;
+        uint8_t Month;
+        uint8_t Date;
+        uint8_t Week;
+        uint8_t Hour;
+        uint8_t Minute;
+        uint8_t Second;
+    } RTC_DateTimeTypeDef;
+
+    typedef enum
+    {
+        RTC_OK = 0x00,  //正确
+        RTC_ERR,  //错误
+        RTC_TIMEOUT,  //超时
+        RTC_EXIT,  //退出
+        RTC_OVERFLOW,  //溢出
+    } RTC_RESULT;
+
+    /* 宏定义 --------------------------------------------------------------------*/
+    #define SEC_DAY                   (24*3600)                //一天秒数
+    #define SEC_YEAR                  (365*SEC_DAY)            //一年秒数(平年)
+
+    /* 变量 ----------------------------------------------------------------------*/
+    __IO uint32_t AsynchPrediv = 0, SynchPrediv = 0;
+
+    #define RTC_CLOCK_SOURCE_LSI                     //使用LSI时钟
+    #define RTC_FLAG_BKP              0xA55A         //标志
+
+    /************************************************
+    函数名称 ： RTC_Configuration
+    功    能 ： RTC配置
+    参    数 ： 无
+    返 回 值 ： 无
+    作    者 ： MCD Application Team
+     *************************************************/
+    void RTC_Configuration(void)
+    {
+        /* 1、使用LSI时钟 */
+        #if defined (RTC_CLOCK_SOURCE_LSI)
+
+            RCC_LSICmd(ENABLE); //使能LSI时钟
+
+            while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET)
+            {
+                //等待LSI就绪
+            }
+
+            RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI); //选择LSI时钟
+
+            SynchPrediv = 0x18F;
+            AsynchPrediv = 0x63;
+
+            /* 2、使用LSE时钟 */
+        #elif defined (RTC_CLOCK_SOURCE_LSE)
+
+            RCC_LSEConfig(RCC_LSE_ON); //使能LSI时钟
+
+            while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET)
+            {
+                //等待LSI就绪
+            }
+
+            RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE); //选择LSI时钟
+
+            SynchPrediv = 0xFF;
+            AsynchPrediv = 0x7F;
+
+        #else 
+            #error Please select the RTC Clock source inside the main.c file
+        #endif 
+
+        RCC_RTCCLKCmd(ENABLE); //使能RTC
+        RTC_WaitForSynchro(); //等待同步
+    }
+
+    /************************************************
+    函数名称 ： RTC_DateRegulate
+    功    能 ： RTC日期矫正
+    参    数 ： RTC_DateTimeStructure -- RTC结构体
+    返 回 值 ： RTC_OK ----------------- 成功
+    RTC_ERR ---------------- 错误
+    作    者 ： Huang Fugui
+     *************************************************/
+    RTC_RESULT RTC_DateRegulate(RTC_DateTimeTypeDef RTC_DateTimeStructure)
+    {
+        RTC_DateTypeDef RTC_DateStructure;
+
+        if (RTC_SetDate(RTC_Format_BIN, &RTC_DateStructure) == SUCCESS)
+        {
+            return RTC_OK;
+        }
+        else
+        {
+            return RTC_ERR;
+        }
+    }
+
+    /************************************************
+    函数名称 ： RTC_TimeRegulate
+    功    能 ： RTC时间矫正
+    参    数 ： RTC_DateTimeStructure -- RTC结构体
+    返 回 值 ： RTC_OK ----------------- 成功
+    RTC_ERR ---------------- 错误
+    作    者 ： Huang Fugui
+     *************************************************/
+    RTC_RESULT RTC_TimeRegulate(RTC_DateTimeTypeDef RTC_DateTimeStructure)
+    {
+        RTC_TimeTypeDef RTC_TimeStructure;
+
+        RTC_TimeStructure.RTC_H12 = RTC_H12_AM;
+        RTC_TimeStructure.RTC_Hours = RTC_DateTimeStructure.Hour;
+        RTC_TimeStructure.RTC_Minutes = RTC_DateTimeStructure.Minute;
+        RTC_TimeStructure.RTC_Seconds = RTC_DateTimeStructure.Second;
+
+        if (RTC_SetTime(RTC_Format_BIN, &RTC_TimeStructure) == SUCCESS)
+        {
+            return RTC_OK;
+        }
+        else
+        {
+            return RTC_ERR;
+        }
+    }
+    /************************************************
+    函数名称 ： RTC_SetDateTime
+    功    能 ： RTC日期时间设置
+    参    数 ： RTC_DateTimeStructure -- RTC结构体
+    返 回 值 ： RTC_OK ----------------- 成功
+    RTC_ERR ---------------- 错误
+    作    者 ： Huang Fugui
+     *************************************************/
+    RTC_RESULT RTC_SetDateTime(RTC_DateTimeTypeDef RTC_DateTimeStructure)
+    {
+        if (RTC_ERR == RTC_DateRegulate(RTC_DateTimeStructure))
+        {
+            return RTC_ERR;
+        }
+
+        if (RTC_ERR == RTC_TimeRegulate(RTC_DateTimeStructure))
+        {
+            return RTC_ERR;
+        }
+
+        return RTC_OK;
+    }
+
+    /************************************************
+    函数名称 ： RTC_Initializes
+    功    能 ： RTC初始化
+    参    数 ： 无
+    返 回 值 ： 无
+    作    者 ： Huang Fugui
+     *************************************************/
+    void RTC_Initializes(void)
+    {
+        RTC_InitTypeDef RTC_InitStructure;
+        RTC_DateTimeTypeDef RTC_DateTimeStructure;
+
+        PWR_BackupAccessCmd(ENABLE); //允许操作RTC
+
+        if (RTC_ReadBackupRegister(RTC_BKP_DR0) != RTC_FLAG_BKP)
+        {
+            RTC_Configuration(); //配置RTC
+
+            RTC_InitStructure.RTC_AsynchPrediv = AsynchPrediv;
+            RTC_InitStructure.RTC_SynchPrediv = SynchPrediv;
+            RTC_InitStructure.RTC_HourFormat = RTC_HourFormat_24;
+            if (RTC_Init(&RTC_InitStructure) == ERROR)
+            {
+                while (1)
+                    ;
+                //初始化失败
+            }
+
+            RTC_DateTimeStructure.Hour = 23;
+            RTC_DateTimeStructure.Minute = 59;
+            RTC_DateTimeStructure.Second = 59;
+            RTC_SetDateTime(RTC_DateTimeStructure);
+            RTC_WriteBackupRegister(RTC_BKP_DR0, RTC_FLAG_BKP);
+        }
+        else
+        {
+            #ifdef RTC_CLOCK_SOURCE_LSI
+                RCC_LSICmd(ENABLE);
+            #endif 
+
+            RTC_WaitForSynchro(); //等待RTC与RTC_APB时钟同步
+        }
+    }
+
+
+    /************************************************
+    函数名称 ： RTC_GetDateTime
+    功    能 ： 读取RTC日期时间（含日历、星期）
+    参    数 ： RTC_DateTimeStructure -- RTC结构体
+    返 回 值 ： 无
+    作    者 ： Huang Fugui
+     *************************************************/
+    void RTC_GetDateTime(DateTime &dt)
+    {
+        RTC_TimeTypeDef RTC_TimeStructure;
+        RTC_DateTypeDef RTC_DateStructure;
+
+        RTC_GetTime(RTC_Format_BIN, &RTC_TimeStructure);
+        RTC_GetDate(RTC_Format_BIN, &RTC_DateStructure);
+
+        dt.Year = RTC_DateStructure.RTC_Year + 2000;
+        dt.Month = RTC_DateStructure.RTC_Month;
+        dt.Day = RTC_DateStructure.RTC_Date;
+
+        dt.Hour = RTC_TimeStructure.RTC_Hours;
+        dt.Minute = RTC_TimeStructure.RTC_Minutes;
+        dt.Second = RTC_TimeStructure.RTC_Seconds;
+    }
+    DateTime now;
+    void RTC_Demo(void *param)
+    {
+        RTC_GetDateTime(now); //读取当前时间
+        now.Show();
+        printf("\r\n");
+    }
+
+    void BSP_Configuration(void) //硬件初始化函数
+    {
+        //	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);//设置ADC端口端口复用使能	
+
+        RTC_Initializes();
+    }
+
+    void rtcf0test()
+    {
+        BSP_Configuration(); //调用硬件初始化函数
+        Sys.AddTask(RTC_Demo, 0, 0, 1000, "RTC_Demo");
+    }
+
 #endif
