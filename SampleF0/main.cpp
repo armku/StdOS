81907c6fcde0717ab020e2598d5c3a483b3b58a0
Ace <258613763@qq.com>
2017-08-08 10:27:02 +0800

    F0串口代码整理

--- SampleF0/main.cpp
+++ SampleF0/main.cpp
@@ -53,27 +53,10 @@ void RS232_GPIO(void)
   GPIO_Init(GPIOA, &GPIO_InitStructure);  
 }
 
-void RS232_Configuration(void)
-{
-	USART_InitTypeDef USART_InitStructure;
-	
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART1, &USART_InitStructure);
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	USART_Cmd(USART1, ENABLE);
-}
-
 void BSP_Configuration(void)//硬件初始化函数
 {	
 	RCC_Configuration();
 	RS232_GPIO();
-	RS232_Configuration();
 }
 void Company_Information(void)//printf打印字符设计
 {
