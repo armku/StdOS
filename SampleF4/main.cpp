d6866e47fb13075f43504f6651b1a2d2514caaf2
Ace <258613763@qq.com>
2017-08-12 15:04:10 +0800

    代码整理

--- SampleF4/main.cpp
+++ SampleF4/main.cpp
@@ -1,4 +1,5 @@
 #include "SerialPort.h"
+#include "Timer.h"
 
 OutputPort led1(PF9,false);
 OutputPort led2(PF10,false);
@@ -13,17 +14,19 @@ void LedTask(void *param)
     *leds = ! * leds;	
     led2 = keyup;
 }
-void SerialTest(void *param)
-{
-	static int i=0;
-	debug_printf("hello %4d\r\n",i++);
-}
+
 uint OnUsart1Read(ITransport *transport, Buffer &bs, void *para, void *para2)
 {
     bs.Show(true);
     return 0;
 }
-
+uint time6cnt;
+void TimerTask(void *param)
+{
+    static int i = 0;
+    printf("\r\n%d: cnt:%d", i++, time6cnt);
+}
+void TimeTest();
 int main(void)
 {
 	Sys.Init();
@@ -32,9 +35,27 @@ int main(void)
         Sys.ShowInfo();
     #endif 		
 	SerialPort::GetMessagePort()->Register(OnUsart1Read);
-	//test();
+	TimeTest();
 	Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
-	//Sys.AddTask(SerialTest,0,0,1000,"Serialtest");
+	Sys.AddTask(TimerTask, &led1, 0, 1000, "TimerTask");
 
     Sys.Start();
+}
+
+Delegate < Timer & > abc;
+void tim2refesh(void *param)
+{
+    time6cnt++;
+}
+
+Timer *timer2;
+void TimeTest()
+{
+    // 初始化为输出
+    timer2 = new Timer(Timer6);
+    abc.Bind(tim2refesh);
+    timer2->Register(abc);
+    timer2->Open();
+    //        timer2->SetFrequency(1000);
+    //        timer2->Config();
 }
