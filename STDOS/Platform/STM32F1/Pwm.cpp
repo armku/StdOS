1bfb690071efed0b74b71f146d069f5b2b4aa448
Ace <258613763@qq.com>
2017-08-29 13:50:10 +0800

    代码整理

--- STDOS/Platform/STM32F1/Pwm.cpp
+++ STDOS/Platform/STM32F1/Pwm.cpp
@@ -1,135 +1,150 @@
 #include "Pwm.h"
 
 #ifdef STM32F0
-	#include "stm32f0xx.h"
+    #include "stm32f0xx.h"
 #elif defined STM32F1
-	#include "stm32f10x.h"
+    #include "stm32f10x.h"
 #elif defined STM32F4
-	#include "stm32f4xx.h"
-#endif
+    #include "stm32f4xx.h"
+#endif 
+
+
+/*
+TIM2 PA0,PA1,PA2,PA3
+TIM3 PA6,PA7,PB0,PB1
+TIM4 PB6,PB7,PB8,PB9
+TIM5 
+remap
+
+TIM1 PE9,PE11,PE13,PE14
+TIM2 PA15,PB3,PB10,PB11
+TIM3 PC6,PC7,PC8,PC9
+TIM4 PD12,PD13,PD14,PD15
+TIM5 
+*/
 
 void Pwm::Open()
 {
-	/*
-	REMAP
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+    /*
+    REMAP
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
     GPIO_PinRemapConfig(GPIO_FullRemap_TIM2, ENABLE);
-	*/
-	TIM_OCInitTypeDef TIM_OCInitStructure;
-	
-	Timer::Open();
-	
-	/* PWM模式配置 */
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //配置为PWM模式1
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //使能输出
-	TIM_OCInitStructure.TIM_Pulse = 0; //设置初始PWM脉冲宽度为0	
-	TIM_OCInitStructure.TIM_OCPolarity = this->Polarity?TIM_OCPolarity_Low:TIM_OCPolarity_High; //当定时器计数值小于CCR1_Val时为低电平
+     */
+    TIM_OCInitTypeDef TIM_OCInitStructure;
 
-	switch (this->_index)
-	{
-		case Timer1:
+    Timer::Open();
+
+    /* PWM模式配置 */
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //配置为PWM模式1
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //使能输出
+    TIM_OCInitStructure.TIM_Pulse = 0; //设置初始PWM脉冲宽度为0	
+    TIM_OCInitStructure.TIM_OCPolarity = this->Polarity ? TIM_OCPolarity_Low : TIM_OCPolarity_High; //当定时器计数值小于CCR1_Val时为低电平
+
+    switch (this->_index)
+    {
+        case Timer1:
             break;
         case Timer2:
-            if(this->Enabled[0])
-			{
-				TIM_OC1Init(TIM2, &TIM_OCInitStructure); //使能通道3
-				TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[1])
-			{
-				TIM_OC2Init(TIM2, &TIM_OCInitStructure); //使能通道3
-				TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[2])
-			{
-				TIM_OC3Init(TIM2, &TIM_OCInitStructure); //使能通道3
-				TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[3])
-			{
-				TIM_OC4Init(TIM2, &TIM_OCInitStructure); //使能通道3
-				TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
-			}
-			
-			TIM_ARRPreloadConfig(TIM2, ENABLE); //使能TIM3重载寄存器ARR
+            if (this->Enabled[0])
+            {
+                TIM_OC1Init(TIM2, &TIM_OCInitStructure); //使能通道3
+                TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[1])
+            {
+                TIM_OC2Init(TIM2, &TIM_OCInitStructure); //使能通道3
+                TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[2])
+            {
+                TIM_OC3Init(TIM2, &TIM_OCInitStructure); //使能通道3
+                TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[3])
+            {
+                TIM_OC4Init(TIM2, &TIM_OCInitStructure); //使能通道3
+                TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable); //使能预装载	
+            }
+
+            TIM_ARRPreloadConfig(TIM2, ENABLE); //使能TIM3重载寄存器ARR
             break;
         case Timer3:
-			if(this->Enabled[0])
-			{
-				TIM_OC1Init(TIM3, &TIM_OCInitStructure); //使能通道3
-				TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[1])
-			{
-				TIM_OC2Init(TIM3, &TIM_OCInitStructure); //使能通道3
-				TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[2])
-			{
-				TIM_OC3Init(TIM3, &TIM_OCInitStructure); //使能通道3
-				TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[3])
-			{
-				TIM_OC4Init(TIM3, &TIM_OCInitStructure); //使能通道3
-				TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
-			}
-			
-			TIM_ARRPreloadConfig(TIM3, ENABLE); //使能TIM3重载寄存器ARR
+            if (this->Enabled[0])
+            {
+                TIM_OC1Init(TIM3, &TIM_OCInitStructure); //使能通道3
+                TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[1])
+            {
+                TIM_OC2Init(TIM3, &TIM_OCInitStructure); //使能通道3
+                TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[2])
+            {
+                TIM_OC3Init(TIM3, &TIM_OCInitStructure); //使能通道3
+                TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
+            }
+            if (this->Enabled[3])
+            {
+                TIM_OC4Init(TIM3, &TIM_OCInitStructure); //使能通道3
+                TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable); //使能预装载	
+            }
+
+            TIM_ARRPreloadConfig(TIM3, ENABLE); //使能TIM3重载寄存器ARR
             break;
         case Timer4:
-			#if defined(STM32F1) || defined(STM32F4)			
-            if(this->Enabled[0])
-			{
-				TIM_OC1Init(TIM4, &TIM_OCInitStructure); //使能通道3
-				TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[1])
-			{
-				TIM_OC2Init(TIM4, &TIM_OCInitStructure); //使能通道3
-				TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
-			}
-			
-			if(this->Enabled[2])
-			{
-				TIM_OC3Init(TIM4, &TIM_OCInitStructure); //使能通道3
-				TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[3])
-			{
-				TIM_OC4Init(TIM4, &TIM_OCInitStructure); //使能通道3
-				TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
-			}			
-			TIM_ARRPreloadConfig(TIM4, ENABLE); //使能TIM3重载寄存器ARR
+            #if defined(STM32F1) || defined(STM32F4)			
+                if (this->Enabled[0])
+                {
+                    TIM_OC1Init(TIM4, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
+                }
+                if (this->Enabled[1])
+                {
+                    TIM_OC2Init(TIM4, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
+                }
+
+                if (this->Enabled[2])
+                {
+                    TIM_OC3Init(TIM4, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
+                }
+                if (this->Enabled[3])
+                {
+                    TIM_OC4Init(TIM4, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable); //使能预装载	
+                }
+                TIM_ARRPreloadConfig(TIM4, ENABLE); //使能TIM3重载寄存器ARR
             #elif defined STM32F0
-			#endif
-			break;
+            #endif 
+            break;
         case Timer5:
-			#if defined(STM32F1) || defined(STM32F4)
-            if(this->Enabled[0])
-			{
-				TIM_OC1Init(TIM5, &TIM_OCInitStructure); //使能通道3
-				TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[1])
-			{
-				TIM_OC2Init(TIM5, &TIM_OCInitStructure); //使能通道3
-				TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[2])
-			{
-				TIM_OC3Init(TIM5, &TIM_OCInitStructure); //使能通道3
-				TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
-			}
-			if(this->Enabled[3])
-			{
-				TIM_OC4Init(TIM5, &TIM_OCInitStructure); //使能通道3
-				TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
-			}			
-			TIM_ARRPreloadConfig(TIM5, ENABLE); //使能TIM3重载寄存器ARR
+            #if defined(STM32F1) || defined(STM32F4)
+                if (this->Enabled[0])
+                {
+                    TIM_OC1Init(TIM5, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
+                }
+                if (this->Enabled[1])
+                {
+                    TIM_OC2Init(TIM5, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
+                }
+                if (this->Enabled[2])
+                {
+                    TIM_OC3Init(TIM5, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
+                }
+                if (this->Enabled[3])
+                {
+                    TIM_OC4Init(TIM5, &TIM_OCInitStructure); //使能通道3
+                    TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Enable); //使能预装载	
+                }
+                TIM_ARRPreloadConfig(TIM5, ENABLE); //使能TIM3重载寄存器ARR
             #elif defined STM32F0
-			#endif
-			break;
+            #endif 
+            break;
         case Timer6:
             break;
         case Timer7:
@@ -157,98 +172,227 @@ void Pwm::Open()
         case Timer18:
             break;
         default:
-			break;
-	}
+            break;
+    }
 }
 
 
 
 void Pwm::Close()
 {
-	Timer::Close();
+    Timer::Close();
 }
+
 // 刷新输出
-void Pwm::Flush()
-{
-}
+void Pwm::Flush(){}
 void Pwm::Config()
 {
-	Timer::Config();
+    Timer::Config();
+    switch (this->_index)
+    {
+        case Timer1:
+            break;
+        case Timer2:
+            if (this->Enabled[0])
+            {
+                this->Ports[0] = new AlternatePort(PA0, false, true);
+            }
+            if (this->Enabled[1])
+            {
+                this->Ports[1] = new AlternatePort(PA1, false, true);
+            }
+            if (this->Enabled[2])
+            {
+                this->Ports[2] = new AlternatePort(PA2, false, true);
+            }
+            if (this->Enabled[3])
+            {
+                this->Ports[3] = new AlternatePort(PA3, false, true);
+            }
+            break;
+        case Timer3:
+            if (this->Enabled[0])
+            {
+                this->Ports[0] = new AlternatePort(PA6, false, true);
+            }
+            if (this->Enabled[1])
+            {
+                this->Ports[1] = new AlternatePort(PA7, false, true);
+            }
+            if (this->Enabled[2])
+            {
+                this->Ports[2] = new AlternatePort(PB0, false, true);
+            }
+            if (this->Enabled[3])
+            {
+                this->Ports[3] = new AlternatePort(PB1, false, true);
+            }
+            break;
+        case Timer4:
+            if (this->Enabled[0])
+            {
+                this->Ports[0] = new AlternatePort(PB6, false, true);
+            }
+            if (this->Enabled[1])
+            {
+                this->Ports[1] = new AlternatePort(PB7, false, true);
+            }
+            if (this->Enabled[2])
+            {
+                this->Ports[2] = new AlternatePort(PB8, false, true);
+            }
+            if (this->Enabled[3])
+            {
+                this->Ports[3] = new AlternatePort(PB9, false, true);
+            }
+            break;
+        case Timer5:
+            if (this->Enabled[0])
+            {
+                this->Ports[0] = new AlternatePort();
+            }
+            if (this->Enabled[1])
+            {
+                this->Ports[1] = new AlternatePort();
+            }
+            if (this->Enabled[2])
+            {
+                this->Ports[2] = new AlternatePort();
+            }
+            if (this->Enabled[3])
+            {
+                this->Ports[3] = new AlternatePort();
+            }
+            break;
+        case Timer6:
+            break;
+        case Timer7:
+            break;
+        case Timer8:
+            break;
+        case Timer9:
+            break;
+        case Timer10:
+            break;
+        case Timer11:
+            break;
+        case Timer12:
+            break;
+        case Timer13:
+            break;
+        case Timer14:
+            break;
+        case Timer15:
+            break;
+        case Timer16:
+            break;
+        case Timer17:
+            break;
+        case Timer18:
+            break;
+        default:
+            break;
+    }
+}
+
+void PwmData::OnInterrupt(){
+
+}
+void PwmData::Config(){}
+/////////////////////////////////////////////////////////////
+////////////////////////////////////////以下为添加
+void Pwm::SetPulse(int idx, ushort pulse)
+{
+	if(idx>3)
+	{
+		return;
+	}
+	this->Pulse[idx]=pulse;
 	switch (this->_index)
 	{
 		case Timer1:
             break;
         case Timer2:
-            if(this->Enabled[0])
+            switch(idx)
 			{
-				this->Ports[0]=new AlternatePort(PA0, false, true);
-			}
-			if(this->Enabled[1])
-			{
-				this->Ports[1]=new AlternatePort(PA1, false, true);
-			}
-			if(this->Enabled[2])
-			{
-				this->Ports[2]=new AlternatePort(PA2, false, true);
-			}
-			if(this->Enabled[3])
-			{
-				this->Ports[3]=new AlternatePort(PA3, false, true);
+				case 0:
+					TIM2->CCR1=this->Pulse[idx];//根据PWM表修改定时器的比较寄存器值
+					break;
+				case 1:
+					TIM2->CCR2=this->Pulse[idx];
+					break;
+				case 2:
+					TIM2->CCR3=this->Pulse[idx];
+					break;
+				case 3:
+					TIM2->CCR4=this->Pulse[idx];
+					break;
+				default:
+					break;
 			}
 			break;
-        case Timer3:
-			if(this->Enabled[0])
+        case Timer3:			
+			switch(idx)
 			{
-				this->Ports[0]=new AlternatePort(PA6, false, true);
-			}
-			if(this->Enabled[1])
-			{
-				this->Ports[1]=new AlternatePort(PA7, false, true);
-			}
-			if(this->Enabled[2])
-			{
-				this->Ports[2]=new AlternatePort(PB0, false, true);
-			}
-			if(this->Enabled[3])
-			{
-				this->Ports[3]=new AlternatePort(PB1, false, true);
+				case 0:
+					TIM3->CCR1=this->Pulse[idx];//根据PWM表修改定时器的比较寄存器值
+					break;
+				case 1:
+					TIM3->CCR2=this->Pulse[idx];
+					break;
+				case 2:
+					TIM3->CCR3=this->Pulse[idx];
+					break;
+				case 3:
+					TIM3->CCR4=this->Pulse[idx];
+					break;
+				default:
+					break;
 			}
             break;
         case Timer4:
-            if(this->Enabled[0])
+			#if defined(STM32F1) || defined(STM32F4)
+            switch(idx)
 			{
-				this->Ports[0]=new AlternatePort(PB6, false, true);
+				case 0:
+					TIM4->CCR1=this->Pulse[idx];//根据PWM表修改定时器的比较寄存器值
+					break;
+				case 1:
+					TIM4->CCR2=this->Pulse[idx];
+					break;
+				case 2:
+					TIM4->CCR3=this->Pulse[idx];
+					break;
+				case 3:
+					TIM4->CCR4=this->Pulse[idx];
+					break;
+				default:
+					break;
 			}
-			if(this->Enabled[1])
-			{
-				this->Ports[1]=new AlternatePort(PB7, false, true);
-			}
-			if(this->Enabled[2])
-			{
-				this->Ports[2]=new AlternatePort(PB8, false, true);
-			}
-			if(this->Enabled[3])
-			{
-				this->Ports[3]=new AlternatePort(PB9, false, true);
-			}
+			#elif defined STM32F0
+			#endif
 			break;
         case Timer5:
-            if(this->Enabled[0])
+			#if defined(STM32F1) || defined(STM32F4)
+            switch(idx)
 			{
-				this->Ports[0]=new AlternatePort();
+				case 0:
+					TIM5->CCR1=this->Pulse[idx];//根据PWM表修改定时器的比较寄存器值
+					break;
+				case 1:
+					TIM5->CCR2=this->Pulse[idx];
+					break;
+				case 2:
+					TIM5->CCR3=this->Pulse[idx];
+					break;
+				case 3:
+					TIM5->CCR4=this->Pulse[idx];
+					break;
+				default:
+					break;
 			}
-			if(this->Enabled[1])
-			{
-				this->Ports[1]=new AlternatePort();
-			}
-			if(this->Enabled[2])
-			{
-				this->Ports[2]=new AlternatePort();
-			}
-			if(this->Enabled[3])
-			{
-				this->Ports[3]=new AlternatePort();
-			}
+			#elif defined STM32F0
+			#endif
 			break;
         case Timer6:
             break;
@@ -280,23 +424,67 @@ void Pwm::Config()
 			break;
 	}
 }
-void PwmData::OnInterrupt()
-{
-	
-}
-void PwmData::Config()
-{
-}
 
 
+#if 0
+	//呼吸灯例程
+    Pwm pwm(Timer3);
+    Delegate < Timer & > abc;
+    void macTIMx_IRQHandler(void);
+    /* LED亮度等级 PWM表 */
+    uint8_t indexWave[] = 
+    {
+        1, 1, 2, 2, 3, 4, 6, 8, 10, 14, 19, 25, 33, 44, 59, 80, 107, 143, 191, 255, 255, 191, 143, 107, 80, 59, 44, 33, 25, 19, 14, 10, 8, 6, 4, 3, 2, 2, 1, 1
+    };
 
+    /*
+     * TIMxCLK/CK_PSC --> TIMxCNT --> TIMx_ARR --> 中断 & TIMxCNT 重新计数
+     *                    TIMx_CCR(电平发生变化)
+     *
+     * 信号周期=(TIMx_ARR +1 ) * 时钟周期
+     * 
+     */
 
+    /*    _______    ______     _____      ____       ___        __         _
+     * |_|       |__|      |___|     |____|    |_____|   |______|  |_______| |________|
+     */
+    void tim2refesh(void *param)
+    {
+        static uint8_t pwm_index = 0; //用于PWM查表
+        static uint8_t period_cnt = 0; //用于计算周期数
 
+        period_cnt++;
+        if (period_cnt >= 10)
+        //若输出的周期数大于10，输出下一种脉冲宽的PWM波
+        {
 
+            pwm.SetPulse(2, indexWave[pwm_index]);
+            pwm_index++; //标志PWM表的下一个元素
 
+            if (pwm_index >= 40)
+            //若PWM脉冲表已经输出完成一遍，重置PWM查表标志
+            {
+                pwm_index = 0;
+            }
 
+            period_cnt = 0; //重置周期计数标志
+        }
+    }
 
-
-
-
-
+    void breathledtest()
+    {
+        abc.Bind(tim2refesh);
+        pwm.Enabled[0] = false;
+        pwm.Enabled[1] = false;
+        pwm.Enabled[2] = true;
+        pwm.Enabled[3] = false;
+        pwm.Pulse[0] = 0;
+        pwm.Pulse[1] = 0;
+        pwm.Pulse[2] = 0;
+        pwm.Pulse[3] = 0;
+        pwm.Register(abc);
+        pwm.Period = 255; //默认1秒一次
+        pwm.Prescaler = 1999;
+        pwm.Open();
+    }
+#endif
--- STDOS/Platform/STM32F1/Timer.cpp
+++ STDOS/Platform/STM32F1/Timer.cpp
@@ -1,3 +1,31 @@
+/*
+硬件定时器驱动，兼容STM32F0/F1/F4/GD32F10x/GD32F1x0
+F0
+基本定时器 
+TIM6 TIM7 16为向上计数，仅仅计数功能
+通用定时器 
+TIM2	32位上下计数器，4捕获通道 0互补输出
+TIM3	16位上下计数器，4捕获通道	0互补输出
+TIM14	16位上计数器，1捕获通道	0互补输出
+TIM15	16位上计数器，2捕获通道	1互补输出
+TIM16 TIM17	16位上计数器，1捕获通道	1互补输出
+高级定时器
+TIM1	16位上下计数器，4捕获通道 3互补输出
+
+F1:
+Timer6 Timer7 为基本定时器，只具备基础的定时功能
+Timer2 Timer3 Timer4 Timer5 为通用定时器，可以脉宽测量PWM 具有编码器接口
+Timer1 Timer8 高级定时器，具有基本定时器 通用定时器所有功能，三相六步电机，刹车功能，PWM死区控制
+
+F4
+基本定时器
+TIM6 TIM7
+通用定时器
+TIM2-TIM5 TIM9-TIM14
+高级定时器
+TIM1 TIM8
+
+*/
 #include "Timer.h"
 #include "TInterrupt.h"
 
@@ -8,7 +36,6 @@
 #elif defined STM32F4
     #include "stm32f4xx.h"
 #endif 
-
 
 uint Timer::GetCounter()
 {
@@ -274,6 +301,23 @@ void Timer::OnOpen()
             break;
     }
     this->Opened = true;
+	#if DEBUG
+        // 获取当前频率
+        RCC_ClocksTypeDef clock;
+        RCC_GetClocksFreq(&clock);
+
+        #if defined(STM32F1) || defined(STM32F4)
+            uint clk = clock.PCLK1_Frequency;
+            if ((uint)this->_index &0x00010000)
+                clk = clock.PCLK2_Frequency;
+            clk <<= 1;
+        #elif defined(STM32F0)
+            uint clk = clock.PCLK_Frequency << 1;
+        #endif 
+
+        uint fre = clk / (Prescaler + 1) / Period;
+        debug_printf("Timer%d::Start Prescaler=%d Period=%d Frequency=%d\r\n", _index + 1, Prescaler + 1, Period, fre);
+    #endif 
 }
 
 void Timer::OnClose()
@@ -436,3 +480,175 @@ void Timer::OnHandler(ushort num, void *param)
     if (timer)
         timer->OnInterrupt();
 }
+//////////////////////////////////////////////////////////////
+/////////////////////////////////以下为添加////////////////////
+/* 定时器针脚 ------------------------------------------------------------------*/
+#if defined(STM32F1) || defined(STM32F4) 
+    #define TIMS {TIM1, TIM2, TIM3, TIM4, TIM5, TIM6, TIM7, TIM8}
+#elif defined STM32F0
+    #define TIMS {TIM1, TIM2, TIM3}
+#endif 
+#define TIM_PINS {\
+PA8, PA9, PA10,PA11,/* TIM1 */	\
+PA0, PA1, PA2, PA3, /* TIM2 */	\
+PA6, PA7, PB0, PB1, /* TIM3 */	\
+PB6, PB7, PB8, PB9, /* TIM4 */	\
+PA0, PA1, PA2, PA3, /* TIM5 */	\
+P0,  P0,  P0,  P0,	/* TIM6 */	\
+P0,  P0,  P0,  P0,	/* TIM7 */	\
+PC6, PC7, PC8, PC9	/* TIM8 */	\
+}
+#define TIM_PINS_FULLREMAP {\
+PE9, PE11,PE13,PE14,/* TIM1 AFIO_MAPR_TIM1_REMAP_FULLREMAP */	\
+PA15,PB3, PB10,PB11,/* TIM2 AFIO_MAPR_TIM2_REMAP_FULLREMAP */	\
+PC6, PC7, PC8, PC9, /* TIM3 AFIO_MAPR_TIM3_REMAP_FULLREMAP */	\
+PD12,PD13,PD14,PD15,/* TIM4 AFIO_MAPR_TIM4_REMAP */	\
+PA0, PA1, PA2, PA3, /* TIM5 */	\
+P0,  P0,  P0,  P0,	/* TIM6 */	\
+P0,  P0,  P0,  P0,	/* TIM7 */	\
+PC6, PC7, PC8, PC9	/* TIM8 */	\
+}
+
+static TIM_TypeDef *const g_Timers[] = TIMS;
+const byte Timer::TimerCount = ArrayLength(g_Timers);
+extern Timer **Timers; // 已经实例化的定时器对象
+// 创建指定索引的定时器，如果已有则直接返回，默认0xFF表示随机分配
+Timer *Timer::Create(byte index)
+{
+    // 特殊处理随机分配
+    if (index == 0xFF)
+    {
+        // 初始化静态数组
+        if (!Timers)
+        {
+            Timers = new Timer *[TimerCount];
+            #if 0
+                ArrayZero2(Timers, TimerCount);
+            #endif 
+        }
+
+        // 找到第一个可用的位置，没有被使用，并且该位置定时器存在
+        byte i = 0;
+        for (; i < TimerCount && (Timers[i] || !g_Timers[i]); i++)
+            ;
+
+        if (i >= TimerCount)
+        {
+            debug_printf("Timer::Create 失败！没有空闲定时器可用！\r\n");
+            return NULL;
+        }
+
+        index = i;
+    }
+
+    assert_param(index < TimerCount);
+
+    if (Timers[index])
+        return Timers[index];
+    else
+        return new Timer((TIMER)index);
+}
+
+void Timer::Register(const Delegate < Timer & >  &dlg)
+{
+    this->OnTick = dlg;
+    switch (this->_index)
+    {
+        case Timer1:
+            break;
+        case Timer2:
+            Interrupt.Activate(TIM2_IRQn, Timer::OnHandler, this);
+            break;
+        case Timer3:
+            Interrupt.Activate(TIM3_IRQn, Timer::OnHandler, this);
+            break;
+        case Timer4:
+            #if defined(STM32F1) || defined(STM32F4)
+                Interrupt.Activate(TIM4_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer5:
+            #if defined(STM32F1) || defined(STM32F4)
+                Interrupt.Activate(TIM5_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer6:
+            #if defined(STM32F1) 
+                Interrupt.Activate(TIM6_IRQn, Timer::OnHandler, this);
+			#elif defined STM32F0 || defined(STM32F4)
+				Interrupt.Activate(TIM6_DAC_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer7:
+            Interrupt.Activate(TIM7_IRQn, Timer::OnHandler, this);
+            break;
+        case Timer8:
+            break;
+        case Timer9:
+            break;
+        case Timer10:
+            break;
+        case Timer11:
+            break;
+        case Timer12:
+            break;
+        case Timer13:
+            break;
+        case Timer14:
+			#if defined STM32F0
+				Interrupt.Activate(TIM14_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer15:
+            #if defined STM32F0
+				Interrupt.Activate(TIM15_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer16:
+            #if defined STM32F0
+				Interrupt.Activate(TIM16_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer17:
+            #if defined STM32F0
+				Interrupt.Activate(TIM17_IRQn, Timer::OnHandler, this);
+            #endif 
+            break;
+        case Timer18:
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
