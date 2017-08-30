e389b15ee60248a5fb02492d206ac0730f3fb3ee
Ace <258613763@qq.com>
2017-08-30 14:18:57 +0800

    代码整理

--- STDOS/Core/Array.cpp
+++ STDOS/Core/Array.cpp
@@ -1,38 +1,101 @@
 #include "Array.h"
 
-Array::Array(void *data, int len): Buffer(data, len){
-
-}
-Array::Array(const void *data, int len): Buffer((void*)data, len){
-
-}
-bool Array::Release()
+Array::Array(void *data, int len): Buffer(data, len)
 {
-    return true;
+
+}
+Array::Array(const void *data, int len): Buffer((void*)data, len)
+{
+
+}
+//Array::Array(Array&& rval)
+//{
+//}
+//Array::Array(const Buffer& rhs)
+//{
+//}
+Array::~Array()
+{
+}
+Array& Array::operator = (const Buffer& rhs)
+{
+}
+Array& Array::operator = (const void* p)
+{
+}
+Array& Array::operator = (Array&& rval)
+{
+}
+// 设置数组长度。容量足够则缩小Length，否则扩容以确保数组容量足够大避免多次分配内存
+bool Array::SetLength(int len)
+{
+}
+bool Array::SetLength(int len, bool bak)
+{
+}
+// 拷贝数据，默认-1长度表示使用右边最大长度，左边不足时自动扩容
+int Array::Copy(int destIndex, const Buffer& src, int srcIndex, int len)
+{
 }
 
+// 设置数组元素为指定值，自动扩容
+bool Array::SetItem(const void* data, int index, int count)
+{
+}
+// 设置数组。直接使用指针，不拷贝数据
+bool Array::Set(void* data, int len)
+{
+}
+// 设置数组。直接使用指针，不拷贝数据
+bool Array::Set(const void* data, int len)
+{
+}
+// 清空已存储数据。
+void Array::Clear()
+{
+}
+// 设置指定位置的值，不足时自动扩容
+void Array::SetItemAt(int i, const void* item)
+{
+}
+
+// 重载索引运算符[]，返回指定元素的第一个字节
+byte Array::operator[](int i) const
+{
+}
+byte& Array::operator[](int i)
+{
+}
+
+bool operator==(const Array& bs1, const Array& bs2)
+{
+}
+bool operator!=(const Array& bs1, const Array& bs2)
+{
+}
+
+#if DEBUG
+	void Array::Test()
+	{
+	}
+#endif
+void Array::Init()
+{
+}
+void Array::move(Array& rval)
+{
+}
+
+// 检查容量。如果不足则扩大，并备份指定长度的数据
+bool Array::CheckCapacity(int len, int bak)
+{
+}
 void *Array::Alloc(int len)
 {
     return 0;
 }
-
-void Array::Clear(){}
-void Array::SetItemAt(int i, const void *item){
-
-}
-int Array::Copy(int destIndex, const Buffer &src, int srcIndex, int len)
+// 释放已占用内存
+bool Array::Release()
 {
     return true;
 }
-
-bool Array::SetLength(int len, bool bak)
-{
-    return true;
-}
-
-bool Array::SetLength(int len)
-{
-    return true;
-}
-
-Array::~Array(){}
