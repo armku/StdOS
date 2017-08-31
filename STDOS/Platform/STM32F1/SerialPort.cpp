4bbf2ce1111d30d68380cd610d2158c39c631125
Ace <258613763@qq.com>
2017-08-31 09:08:48 +0800

    代码整理

--- STDOS/Platform/STM32F1/SerialPort.cpp
+++ STDOS/Platform/STM32F1/SerialPort.cpp
@@ -48,10 +48,11 @@
 	#define UART_PINS {\
     /* TX   RX   CTS  RTS */	\
     PA9, PA10,PA11,PA12,/* USART1 */	\
-    PA2, PA3, PA0, PA1, /* USART2 */	\
-    PB10,PB11,PB13,PB14,/* USART3 */	\
+    PD5, PD6, PD7, PD4, /* USART2 */	\
+    PD8,PD9,PB13,PB14,/* USART3 */	\
     PC10,PC11,P0,  P0,  /* UART4  */	\
     PC12, PD2,P0,  P0,  /* UART5  */	\
+	PC6, PC7,PG7,  PG8,  /* USART6  */	\
     }
     #define UART_PINS_FULLREMAP {\
     /* TX   RX   CTS  RTS */	\
