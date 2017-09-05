a0c93bd4a40c582d5f51b2c8f38669c340a92fc8
Ace <258613763@qq.com>
2017-09-05 17:27:35 +0800

    代码整理

--- STDOS/Core/Queue.cpp
+++ STDOS/Core/Queue.cpp
@@ -84,7 +84,45 @@ int Queue::Write(const Buffer &bs)
 
 // ÅúÁ¿¶ÁÈ¡
 int Queue::Read(Buffer &bs)
-
 {
-    return 0;
+	int ret=0;
+	if(this->_size!=0)
+	{
+		int buflen=bs.Length();
+		if(buflen!=0)
+		{
+			if(this->_size<buflen)
+			{
+				buflen=this->_size;
+			}
+			int v6=0;
+			while(1)
+			{
+				int v7 = this->Capacity();
+				int v8=v7-this->_tail;
+				if(buflen<=v8)
+					break;
+				//this->_s.CopyTo();
+				buflen-=v8;
+				v6+=v8;
+				this->_tail+=buflen;
+				if(this->_tail>=v7)
+				this->_tail-=v7;
+				//this->_s.CopyTo(,v9);
+				EnterCritical();
+				//this->_size-=v9;
+				ExitCritical();
+				//ret=v9;
+			}
+		}
+		else
+		{
+			ret=0;
+		}
+	}
+	else
+	{
+		ret=0;
+	}
+    return ret;
 }
