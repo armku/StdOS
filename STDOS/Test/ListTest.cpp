8e1acddec4f36efec9ec518a6aba1f3f0c31bab2
Ace <258613763@qq.com>
2017-09-18 23:37:01 +0800

    代码整理

--- STDOS/Test/ListTest.cpp
+++ STDOS/Test/ListTest.cpp
@@ -2,22 +2,8 @@
 #include <stdio.h>
 #include "Sys.h"
 
-class TaskTest
-{
-	public:
-		int aa;
-		int bb;
-	void show()
-	{
-		debug_printf("aa:%d,bb:%d \r\n",aa,bb);
-	}
-};
-
-
 #if DEBUG
-	#define SmartOS_printf debug_printf
-	List<TaskTest*>	_Tasks;
-    void IList::Test()
+	void IList::Test()
     {
 //		TraceStack ts("TestList");
 //		SmartOS_printf("TestList......\r\n");		
@@ -25,19 +11,8 @@ class TaskTest
 		
 		
 		
-		debug_printf("\r\n\r\n TestList begin");
-			
+		debug_printf("\r\n\r\n TestList begin");			
 		
-		TaskTest *t1=new TaskTest();
-		t1->aa=1;
-		t1->bb=2;
-		debug_printf("\r\n 原始t1:\r\n");
-		t1->show();
-		
-		
-		_Tasks.Add(t1);
-		debug_printf("\r\n 取出t1:\r\n");
-		_Tasks[0]->show();
 		
 		debug_printf("\r\n\r\n TestList end\r\n\r\n");
     }
