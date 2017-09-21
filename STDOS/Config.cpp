215818517c22624d37952dde4c2c0fbbe5a4dad9
Ace <258613763@qq.com>
2017-09-21 10:00:25 +0800

    删除部分代码，项目配置优化

--- STDOS/Config.cpp
+++
@@ -1,8 +0,0 @@
-#include "Config.h"
-//    #if 0
-    //        //flash 最后一块作为配置区
-    //        Config::Current = &Config::CreateFlash();
-    //    #endif  
-	//    #if 0
-    //        sys.Codec = codec;
-    //	#endif
