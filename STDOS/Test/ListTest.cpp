3aee5288da901458fb2962f5ea097ab1302342ea
Ace <258613763@qq.com>
2017-09-30 15:23:35 +0800

    代码整理

--- STDOS/Test/ListTest.cpp
+++ STDOS/Test/ListTest.cpp
@@ -4,21 +4,21 @@
 #include "Task.h"
 
 #ifdef DEBUG
-	
+	List<Task*>	_Tasks;	// 任务列表	
 	void IList::Test()
     {
 //		TraceStack ts("TestList");
 //		SmartOS_printf("TestList......\r\n");		
 //		IList list;
 		
-		List<Task*>	_Tasks;	// 任务列表		
+			
 		debug_printf("\r\n\r\n TestList begin\r\n");	
-		Task k1;
-		k1.ID=1;
-		debug_printf("ID:%d\r\n",k1.ID);
-		_Tasks.Add(&k1);
+		Task *k1=new Task();
+		k1->ID=1;
+		debug_printf("ID:%d\r\n",k1->ID);
+		_Tasks.Add(k1);
 		Task *kk1;
-		//kk1=_Tasks[0];
+		kk1=_Tasks[0];
 		debug_printf("\r\n ID:%d",kk1->ID);
 		
 		
