0a2790cfec35ffa503d4dc9d671b6e39c590a0ad
Ace <258613763@qq.com>
2017-09-30 15:20:07 +0800

    代码整理 测试List

--- STDOS/Test/ListTest.cpp
+++ STDOS/Test/ListTest.cpp
@@ -11,15 +11,15 @@
 //		SmartOS_printf("TestList......\r\n");		
 //		IList list;
 		
-//		List<Task*>	_Tasks;	// 任务列表		
-//		debug_printf("\r\n\r\n TestList begin");	
-//		Task k1;
-//		k1.ID=1;
-//		debug_printf("\r\n ID:%d",k1.ID);
-//		_Tasks.Add(&k1);
-//		Task *kk1;
-//		kk1=_Tasks[0];
-//		debug_printf("\r\n ID:%d",kk1->ID);
+		List<Task*>	_Tasks;	// 任务列表		
+		debug_printf("\r\n\r\n TestList begin\r\n");	
+		Task k1;
+		k1.ID=1;
+		debug_printf("ID:%d\r\n",k1.ID);
+		_Tasks.Add(&k1);
+		Task *kk1;
+		//kk1=_Tasks[0];
+		debug_printf("\r\n ID:%d",kk1->ID);
 		
 		
 		debug_printf("\r\n\r\n TestList end\r\n\r\n");
--- SampleF1/main.cpp
+++ SampleF1/main.cpp
@@ -59,7 +59,7 @@ uint time6cnt;
 void TimerTask(void *param)
 {
     static int i = 0;
-    printf("\r\n%d: cnt:%d", i++, time6cnt++);
+    //printf("\r\n%d: cnt:%d", i++, time6cnt++);
 }
 
 SerialPort *sp1;
@@ -80,7 +80,7 @@ int main(void)
     Sys.AddTask(TimerTask, &led1, 0, 1000, "TimerTask");
     Sys.AddTask(Test12, 0, 600, 1000, "Test");
 	
-	//IList::Test();
+	IList::Test();
 	
 	//InterruptTest();
 		
