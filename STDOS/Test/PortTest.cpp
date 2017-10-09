caddf7b5a4752e084959f6fa4253c630f813bdc3
Ace <258613763@qq.com>
2017-10-09 12:53:31 +0800

    外部中断测试 STM32F4

--- STDOS/Test/PortTest.cpp
+++ STDOS/Test/PortTest.cpp
@@ -32,142 +32,219 @@ K3:PE4
     {
         led2 = !led2;
     }
+    void EXTIX_Init();
     void InterruptTest()
     {
-        key0.Press = OnPress;
-        key0.UsePress();
-        key0.Open();
+        #if defined STM32F1
+            key0.Press = OnPress;
+            key0.UsePress();
+            key0.Open();
 
-        key.Press = OnPress;
-        key.UsePress();
-        key.Open();
-		//EXTIX_Init();
+            key.Press = OnPress;
+            key.UsePress();
+            key.Open();
+            //EXTIX_Init();
 
-        Sys.AddTask(led2flash, &led2, 0, 200, "led2");
+            Sys.AddTask(led2flash, &led2, 0, 200, "led2");
+        #elif defined STM32F4
+            EXTIX_Init(); //初始化外部中断输入 
+        #endif 
     }
 #endif 
 #ifdef STM32F4
-#include "stm32f4xx.h" 
+    #include "stm32f4xx.h" 
 
-//外部中断0服务程序
-//void EXTI0_IRQHandler(void)
-//{
-//    delay_ms(10); //消抖
-//    if (WK_UP == 1)
-//    {
-//        BEEP = !BEEP; //蜂鸣器翻转 
-//    }
-//    EXTI_ClearITPendingBit(EXTI_Line0); //清除LINE0上的中断标志位 
-//}
+    #define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+    #define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+    #define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+    //IO口地址映射
+    #define GPIOA_ODR_Addr    (GPIOA_BASE+20) //0x40020014
+    #define GPIOB_ODR_Addr    (GPIOB_BASE+20) //0x40020414 
+    #define GPIOC_ODR_Addr    (GPIOC_BASE+20) //0x40020814 
+    #define GPIOD_ODR_Addr    (GPIOD_BASE+20) //0x40020C14 
+    #define GPIOE_ODR_Addr    (GPIOE_BASE+20) //0x40021014 
+    #define GPIOF_ODR_Addr    (GPIOF_BASE+20) //0x40021414    
+    #define GPIOG_ODR_Addr    (GPIOG_BASE+20) //0x40021814   
+    #define GPIOH_ODR_Addr    (GPIOH_BASE+20) //0x40021C14    
+    #define GPIOI_ODR_Addr    (GPIOI_BASE+20) //0x40022014     
 
-////外部中断2服务程序
-//void EXTI2_IRQHandler(void)
-//{
-//    delay_ms(10); //消抖
-//    if (KEY2 == 0)
-//    {
-//        LED0 = !LED0;
-//    }
-//    EXTI_ClearITPendingBit(EXTI_Line2); //清除LINE2上的中断标志位 
-//}
+    #define GPIOA_IDR_Addr    (GPIOA_BASE+16) //0x40020010 
+    #define GPIOB_IDR_Addr    (GPIOB_BASE+16) //0x40020410 
+    #define GPIOC_IDR_Addr    (GPIOC_BASE+16) //0x40020810 
+    #define GPIOD_IDR_Addr    (GPIOD_BASE+16) //0x40020C10 
+    #define GPIOE_IDR_Addr    (GPIOE_BASE+16) //0x40021010 
+    #define GPIOF_IDR_Addr    (GPIOF_BASE+16) //0x40021410 
+    #define GPIOG_IDR_Addr    (GPIOG_BASE+16) //0x40021810 
+    #define GPIOH_IDR_Addr    (GPIOH_BASE+16) //0x40021C10 
+    #define GPIOI_IDR_Addr    (GPIOI_BASE+16) //0x40022010 
+    #define KEY0 		GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4) //PE4
+    #define KEY1 		GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_3)	//PE3 
+    #define KEY2 		GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_2) //PE2
+    #define WK_UP 	GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)	//PA0
+    //IO口操作,只对单一的IO口!
+    //确保n的值小于16!
+    #define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+    #define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
 
-////外部中断3服务程序
-//void EXTI3_IRQHandler(void)
-//{
-//    delay_ms(10); //消抖
-//    if (KEY1 == 0)
-//    {
-//        LED1 = !LED1;
-//    }
-//    EXTI_ClearITPendingBit(EXTI_Line3); //清除LINE3上的中断标志位  
-//}
+    #define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+    #define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
 
-////外部中断4服务程序
-//void EXTI4_IRQHandler(void)
-//{
-//    delay_ms(10); //消抖
-//    if (KEY0 == 0)
-//    {
-//        LED0 = !LED0;
-//        LED1 = !LED1;
-//    }
-//    EXTI_ClearITPendingBit(EXTI_Line4); //清除LINE4上的中断标志位  
-//}
-void KEY_Init(void)
-{
+    #define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
+    #define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
 
-    GPIO_InitTypeDef GPIO_InitStructure;
+    #define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
+    #define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
 
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOE, ENABLE); //使能GPIOA,GPIOE时钟
+    #define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
+    #define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4; //KEY0 KEY1 KEY2对应引脚
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN; //普通输入模式
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; //100M
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
-    GPIO_Init(GPIOE, &GPIO_InitStructure); //初始化GPIOE2,3,4
+    #define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
+    #define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
 
+    #define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
+    #define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
 
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; //WK_UP对应引脚PA0
-//    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN; //下拉
-//    GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化GPIOA0
+    #define PHout(n)   BIT_ADDR(GPIOH_ODR_Addr,n)  //输出 
+    #define PHin(n)    BIT_ADDR(GPIOH_IDR_Addr,n)  //输入
 
-}
-//外部中断初始化程序
-//初始化PE2~4,PA0为中断输入.
-void EXTIX_Init(void)
-{
-    NVIC_InitTypeDef NVIC_InitStructure;
-    EXTI_InitTypeDef EXTI_InitStructure;
+    #define PIout(n)   BIT_ADDR(GPIOI_ODR_Addr,n)  //输出 
+    #define PIin(n)    BIT_ADDR(GPIOI_IDR_Addr,n)  //输入
 
-    KEY_Init(); //按键对应的IO口初始化
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE); //使能SYSCFG时钟
+    #define BEEP PFout(8)	// 蜂鸣器控制IO 
+    #define LED0 PFout(9)	// DS0
+    #define LED1 PFout(10)	// DS1	
+    /*下面方式是通过位带操作方式读取IO*/
+    /*
+    #define KEY0 		PEin(4)   	//PE4
+    #define KEY1 		PEin(3)		//PE3 
+    #define KEY2 		PEin(2)		//P32
+    #define WK_UP 	PAin(0)		//PA0
+     */
 
 
-    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource2); //PE2 连接到中断线2
-    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource3); //PE3 连接到中断线3
-    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource4); //PE4 连接到中断线4
-    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0); //PA0 连接到中断线0
+    #define KEY0_PRES 	1
+    #define KEY1_PRES	2
+    #define KEY2_PRES	3
+    #define WKUP_PRES   4
+    void delay_ms(int ms){}
+    void KEY_Init()
+    {
 
-    /* 配置EXTI_Line0 */
-    EXTI_InitStructure.EXTI_Line = EXTI_Line0; //LINE0
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt; //中断事件
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising; //上升沿触发 
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE; //使能LINE0
-    EXTI_Init(&EXTI_InitStructure); //配置
+        GPIO_InitTypeDef GPIO_InitStructure;
 
-    /* 配置EXTI_Line2,3,4 */
-    EXTI_InitStructure.EXTI_Line = EXTI_Line2 | EXTI_Line3 | EXTI_Line4;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt; //中断事件
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling; //下降沿触发
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE; //中断线使能
-    EXTI_Init(&EXTI_InitStructure); //配置
+        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOE, ENABLE); //使能GPIOA,GPIOE时钟
 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn; //外部中断0
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00; //抢占优先级0
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
-    NVIC_Init(&NVIC_InitStructure); //配置
-
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn; //外部中断2
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x03; //抢占优先级3
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
-    NVIC_Init(&NVIC_InitStructure); //配置
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4; //KEY0 KEY1 KEY2对应引脚
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN; //普通输入模式
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; //100M
+        GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
+        GPIO_Init(GPIOE, &GPIO_InitStructure); //初始化GPIOE2,3,4
 
 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn; //外部中断3
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02; //抢占优先级2
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
-    NVIC_Init(&NVIC_InitStructure); //配置
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; //WK_UP对应引脚PA0
+        GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN; //下拉
+        GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化GPIOA0
+
+    }
+
+    //外部中断0服务程序
+    void EXTI0_IRQHandler()
+    {
+        delay_ms(10); //消抖
+        if (WK_UP == 1)
+        {
+            BEEP = !BEEP; //蜂鸣器翻转 
+        }
+        EXTI_ClearITPendingBit(EXTI_Line0); //清除LINE0上的中断标志位 
+    }
+    //外部中断2服务程序
+    void EXTI2_IRQHandler()
+    {
+        delay_ms(10); //消抖
+        if (KEY2 == 0)
+        {
+            LED0 = !LED0;
+        }
+        EXTI_ClearITPendingBit(EXTI_Line2); //清除LINE2上的中断标志位 
+    }
+    //外部中断3服务程序
+    void EXTI3_IRQHandler()
+    {
+        delay_ms(10); //消抖
+        if (KEY1 == 0)
+        {
+            LED1 = !LED1;
+        }
+        EXTI_ClearITPendingBit(EXTI_Line3); //清除LINE3上的中断标志位  
+    }
+    //外部中断4服务程序
+    void EXTI4_IRQHandler()
+    {
+        delay_ms(10); //消抖
+        if (KEY0 == 0)
+        {
+            LED0 = !LED0;
+            LED1 = !LED1;
+        }
+        EXTI_ClearITPendingBit(EXTI_Line4); //清除LINE4上的中断标志位  
+    }
+
+    //外部中断初始化程序
+    //初始化PE2~4,PA0为中断输入.
+    void EXTIX_Init()
+    {
+        NVIC_InitTypeDef NVIC_InitStructure;
+        EXTI_InitTypeDef EXTI_InitStructure;
+
+        KEY_Init(); //按键对应的IO口初始化
+
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE); //使能SYSCFG时钟
 
 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn; //外部中断4
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01; //抢占优先级1
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
-    NVIC_Init(&NVIC_InitStructure); //配置
+        SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource2); //PE2 连接到中断线2
+        SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource3); //PE3 连接到中断线3
+        SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource4); //PE4 连接到中断线4
+        SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0); //PA0 连接到中断线0
 
-}
+        /* 配置EXTI_Line0 */
+        EXTI_InitStructure.EXTI_Line = EXTI_Line0; //LINE0
+        EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt; //中断事件
+        EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising; //上升沿触发 
+        EXTI_InitStructure.EXTI_LineCmd = ENABLE; //使能LINE0
+        EXTI_Init(&EXTI_InitStructure); //配置
+
+        /* 配置EXTI_Line2,3,4 */
+        EXTI_InitStructure.EXTI_Line = EXTI_Line2 | EXTI_Line3 | EXTI_Line4;
+        EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt; //中断事件
+        EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling; //下降沿触发
+        EXTI_InitStructure.EXTI_LineCmd = ENABLE; //中断线使能
+        EXTI_Init(&EXTI_InitStructure); //配置
+
+        NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn; //外部中断0
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00; //抢占优先级0
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
+        NVIC_Init(&NVIC_InitStructure); //配置
+
+        NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn; //外部中断2
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x03; //抢占优先级3
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
+        NVIC_Init(&NVIC_InitStructure); //配置
+
+
+        NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn; //外部中断3
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02; //抢占优先级2
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
+        NVIC_Init(&NVIC_InitStructure); //配置
+
+
+        NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn; //外部中断4
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01; //抢占优先级1
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02; //子优先级2
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //使能外部中断通道
+        NVIC_Init(&NVIC_InitStructure); //配置
+
+    }
 #endif
