68974efdcca7d56f3e7f5ece265755fd2369364c
Ace <258613763@qq.com>
2017-10-18 22:08:53 +0800

    代码格式整理

--- STDOS/Drivers/ADS1232.cpp
+++ STDOS/Drivers/ADS1232.cpp
@@ -5,47 +5,120 @@ void ADS1232::SetPin(Pin pdout, Pin psclk, Pin ppwdn)
     this->dout.Set(pdout);
     this->sclk.Set(psclk);
     this->pwdn.Set(ppwdn);
-	
-	this->dout.Invert=false;
-	this->sclk.Invert=false;
-	this->pwdn.Invert=false;
-	
-//	this->dout.OpenDrain=false;
-	this->sclk.OpenDrain=false;
-	this->pwdn.OpenDrain=false;
-	
-	this->dout.Open();
-	this->sclk.Open();
-	this->pwdn.Open();
-	
-//	this->dout = 1;
-	this->sclk = 0;
-	this->pwdn = 1;
+
+    this->dout.Invert = false;
+    this->sclk.Invert = false;
+    this->pwdn.Invert = false;
+
+    //	this->dout.OpenDrain=false;
+    this->sclk.OpenDrain = false;
+    this->pwdn.OpenDrain = false;
+
+    this->dout.Open();
+    this->sclk.Open();
+    this->pwdn.Open();
+
+    //	this->dout = 1;
+    this->sclk = 0;
+    this->pwdn = 1;
 }
+
+void ADS1232::SetPinSpeed(Pin pin)
+{
+    this->pSpeed.Set(pin);
+
+    this->pSpeed.Invert = false;
+
+    this->pSpeed.OpenDrain = false;
+
+    this->pSpeed.Open();
+}
+
+void ADS1232::SetPinGain(Pin gain0, Pin gain1)
+{
+    this->pGain0.Set(gain0);
+    this->pGain1.Set(gain1);
+
+    this->pGain0.Invert = false;
+    this->pGain1.Invert = false;
+
+    this->pGain0.OpenDrain = false;
+    this->pGain1.OpenDrain = false;
+
+    this->pGain0.Open();
+    this->pGain1.Open();
+}
+
+void ADS1232::SetSpeed(Speed speed)
+{
+    if (this->pSpeed._Pin != P0)
+    {
+        switch (speed)
+        {
+            case SPEED10:
+                this->pSpeed = 0;
+                break;
+            case SPEED80:
+                this->pSpeed = 1;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void ADS1232::SetGain(Gain gain)
+{
+    if ((this->pGain0._Pin != P0) && (this->pGain1._Pin != P0))
+    {
+        switch (gain)
+        {
+            case GAIN1:
+                this->pGain0 = 0;
+                this->pGain1 = 0;
+                break;
+            case GAIN2:
+                this->pGain0 = 1;
+                this->pGain1 = 0;
+                break;
+            case GAIN64:
+                this->pGain0 = 0;
+                this->pGain1 = 1;
+                break;
+            case GAIN128:
+                this->pGain0 = 1;
+                this->pGain1 = 1;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 void ADS1232::test()
 {
-	//this->dout=!this->dout;
+    //this->dout=!this->dout;
 }
 
 void ADS1232::Init()
 {
-	//return;
-	this->pwdn = 0; //复位1232，操作前先复位
-	Sys.Delay(20);
+    //return;
+    this->pwdn = 0; //复位1232，操作前先复位
+    Sys.Delay(20);
     this->sclk = 0;
     Sys.Delay(20);
     Sys.Delay(20);
-//    this->dout = 1; //初始化引脚
+    //    this->dout = 1; //初始化引脚
     this->pwdn = 1; //开启1232
     Sys.Delay(20);
-	this->readCnt=0;
+    this->readCnt = 0;
 }
 
 
 int ADS1232::Read()
 {
-	return;
-	int temp = 0;
+    return ;
+    int temp = 0;
     byte i = 0;
     //        for(temp=0;temp<500;temp++)
     //        {
@@ -97,6 +170,6 @@ int ADS1232::Read()
         }
         this->oldval = temp;
     }
-	this->readCnt++;
+    this->readCnt++;
     return this->oldval;
 }
--- STDOS/Drivers/ADS1232.h
+++ STDOS/Drivers/ADS1232.h
@@ -1,21 +1,38 @@
 #ifndef _ADS1232_H
-    #define _ADS1232_H
+#define _ADS1232_H
     #include "Device\Port.h"
 
     class ADS1232
     {
         public:
+            enum Speed  //AD转换速度
+            {
+                    SPEED10,  //10hZ
+                    SPEED80  //80Hz
+            };
+            enum Gain  //放大倍数
+            {
+                    GAIN1, GAIN2, GAIN64, GAIN128
+            };
+        public:
             void Init();
             void SetPin(Pin pdout, Pin psclk, Pin ppwdn);
-            int	Read();
-			void test();
+            void SetPinSpeed(Pin pin);
+            void SetPinGain(Pin gain0, Pin gain1);
+            void SetSpeed(Speed speed = SPEED10);
+            void SetGain(Gain gain = GAIN1);
+            int Read();
+            void test();
         private:
             InputPort dout;
             OutputPort sclk;
             OutputPort pwdn;
-		private:
-			int readCnt;//读取数量
-			int oldval;
+            OutputPort pSpeed;
+            OutputPort pGain0;
+            OutputPort pGain1;
+        private:
+            int readCnt; //读取数量
+            int oldval;
     };
 
 #endif
