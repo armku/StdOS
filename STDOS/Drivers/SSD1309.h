049722f6521d7e820cb5e1bb70a52932c08305eb
Ace <258613763@qq.com>
2017-10-26 17:01:39 +0800

    oled代码整理

--- STDOS/Drivers/SSD1309.h
+++ STDOS/Drivers/SSD1309.h
@@ -7,21 +7,21 @@
     {
         public:
             void SetPin(Pin cs, Pin res, Pin dc, Pin wr, Pin rd, Pin sclk, Pin sdin);
-            //OLED¿ØÖÆÓÃº¯Êý
-            void OLED_WR_Byte(byte dat, byte cmd);
-            void OLED_Display_On(void);
-            void OLED_Display_Off(void);
-            void OLED_Init(void);
-            void OLED_Clear(void);
-            void OLED_DrawPoint(byte x, byte y, byte t);
-            void OLED_Fill(byte x1, byte y1, byte x2, byte y2, byte dot);
-            void OLED_ShowChar(byte x, byte y, byte chr);
-            void OLED_ShowNum(byte x, byte y, uint num, byte len, byte size);
-            void OLED_ShowString(byte x, byte y, byte *p);
-            void OLED_Set_Pos(unsigned char x, unsigned char y);
-            void OLED_ShowCHinese(byte x, byte y, byte no);
-            void OLED_DrawBMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[]);
+            void DisplayOn();
+            void DisplayOff();
+            void Init();
+            void Clear();
+//            void DrawPoint(byte x, byte y, byte t);
+//            void Fill(byte x1, byte y1, byte x2, byte y2, byte dot);
+            void ShowChar(byte x, byte y, byte chr);
+            void ShowNum(byte x, byte y, uint num, byte len, byte size);
+            void ShowString(byte x, byte y, byte *p);
+            void SetPos(unsigned char x, unsigned char y);
+            void ShowCHinese(byte x, byte y, byte no);
+            void DrawBMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[]);
         private:
+			void WRByte(byte dat, byte cmd);            
+		private:
             uint oled_pow(byte m, byte n);
 
             OutputPort _cs;
--- STDOS/Test/SSD1309Test.cpp
+++ STDOS/Test/SSD1309Test.cpp
@@ -11,44 +11,44 @@
         {
             t = 0;
         }
-        ssd1309.OLED_Clear();
+        ssd1309.Clear();
         switch (++cnt)
         {
             case 1:
-                ssd1309.OLED_ShowCHinese(0, 0, 0); //中
-                ssd1309.OLED_ShowCHinese(18, 0, 1); //景
-                ssd1309.OLED_ShowCHinese(36, 0, 2); //园
-                ssd1309.OLED_ShowCHinese(54, 0, 3); //电
-                ssd1309.OLED_ShowCHinese(72, 0, 4); //子
-                ssd1309.OLED_ShowCHinese(90, 0, 5); //科
-                ssd1309.OLED_ShowCHinese(108, 0, 6); //技
-                ssd1309.OLED_ShowString(0, 3, (byte*)"1.54' OLED TEST");
+                ssd1309.ShowCHinese(0, 0, 0); //中
+                ssd1309.ShowCHinese(18, 0, 1); //景
+                ssd1309.ShowCHinese(36, 0, 2); //园
+                ssd1309.ShowCHinese(54, 0, 3); //电
+                ssd1309.ShowCHinese(72, 0, 4); //子
+                ssd1309.ShowCHinese(90, 0, 5); //科
+                ssd1309.ShowCHinese(108, 0, 6); //技
+                ssd1309.ShowString(0, 3, (byte*)"1.54' OLED TEST");
                 //OLED_ShowString(8,2,"ZHONGJINGYUAN");  
                 //	OLED_ShowString(20,4,"2014/05/01");  
-                ssd1309.OLED_ShowString(0, 6, (byte*)"ASCII:");
-                ssd1309.OLED_ShowString(63, 6, (byte*)"CODE:");
-                ssd1309.OLED_ShowChar(48, 6, t); //显示ASCII字符	  
-                ssd1309.OLED_ShowNum(103, 6, t, 3, 16); //显示ASCII字符的码值 
+                ssd1309.ShowString(0, 6, (byte*)"ASCII:");
+                ssd1309.ShowString(63, 6, (byte*)"CODE:");
+                ssd1309.ShowChar(48, 6, t); //显示ASCII字符	  
+                ssd1309.ShowNum(103, 6, t, 3, 16); //显示ASCII字符的码值 
                 break;
             case 2:
-                ssd1309.OLED_ShowString(30, 0, (byte*)"OLED TEST");
-                ssd1309.OLED_ShowString(8, 2, (byte*)"ZHONGJINGYUAN");
-                ssd1309.OLED_ShowString(20, 4, (byte*)"2014/05/01");
-                ssd1309.OLED_ShowString(0, 6, (byte*)"ASCII:");
-                ssd1309.OLED_ShowString(63, 6, (byte*)"CODE:");
-                ssd1309.OLED_ShowCHinese(0, 0, 0); //中
-                ssd1309.OLED_ShowCHinese(18, 0, 1); //景
-                ssd1309.OLED_ShowCHinese(36, 0, 2); //园
-                ssd1309.OLED_ShowCHinese(54, 0, 3); //电
-                ssd1309.OLED_ShowCHinese(72, 0, 4); //子
-                ssd1309.OLED_ShowCHinese(90, 0, 5); //科
-                ssd1309.OLED_ShowCHinese(108, 0, 6); //技
+                ssd1309.ShowString(30, 0, (byte*)"OLED TEST");
+                ssd1309.ShowString(8, 2, (byte*)"ZHONGJINGYUAN");
+                ssd1309.ShowString(20, 4, (byte*)"2014/05/01");
+                ssd1309.ShowString(0, 6, (byte*)"ASCII:");
+                ssd1309.ShowString(63, 6, (byte*)"CODE:");
+                ssd1309.ShowCHinese(0, 0, 0); //中
+                ssd1309.ShowCHinese(18, 0, 1); //景
+                ssd1309.ShowCHinese(36, 0, 2); //园
+                ssd1309.ShowCHinese(54, 0, 3); //电
+                ssd1309.ShowCHinese(72, 0, 4); //子
+                ssd1309.ShowCHinese(90, 0, 5); //科
+                ssd1309.ShowCHinese(108, 0, 6); //技
                 break;
             case 3:
-                ssd1309.OLED_DrawBMP(0, 0, 128, 8, BMP1); //图片显示(图片显示慎用，生成的字表较大，会占用较多空间，FLASH空间8K以下慎用) 
+                ssd1309.DrawBMP(0, 0, 128, 8, BMP1); //图片显示(图片显示慎用，生成的字表较大，会占用较多空间，FLASH空间8K以下慎用) 
                 break;
             case 4:
-                ssd1309.OLED_DrawBMP(0, 0, 128, 8, BMP2);
+                ssd1309.DrawBMP(0, 0, 128, 8, BMP2);
 				cnt=0;
                 break;
             default:
@@ -60,8 +60,7 @@
     void ssd1309Test()
     {
 		ssd1309.SetPin(PD3,PD4,PD5,PD14,PD13,PD6,PD7);
-        ssd1309.OLED_Init(); //初始化OLED  
-        ssd1309.OLED_Clear();
+        ssd1309.Init(); //初始化OLED 
 
         Sys.AddTask(ssd1309routin, 0, 100, 1000, "ssd1309routin");
     }
