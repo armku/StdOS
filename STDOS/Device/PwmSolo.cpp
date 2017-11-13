a5ed718cf760eaed4e9d194ef6fd9b3f75a99b93
Ace <258613763@qq.com>
2017-11-13 16:30:40 +0800

    增加类PwmSolo

---
+++ STDOS/Device/PwmSolo.cpp
@@ -0,0 +1,16 @@
+#include "PwmSolo.h"
+#include "stm32f10x.h"
+
+
+PwmSolo::PwmSolo(Pin pin, uint frq, uint duty): AlternatePort(pin)
+{
+    this->freq = frq;
+    this->duty = duty;
+}
+
+//ÉèÖÃÊä³ö±ÈÀı£¬0-100
+void PwmSolo::SetOutPercent(float per)
+{
+    this->duty = this->freq *per / 100;
+    this->SetOut(this->duty);
+}
---
+++ STDOS/Device/PwmSolo.h
@@ -0,0 +1,17 @@
+#ifndef _PWMSOLO_H
+    #define _PWMSOLO_H
+    #include "Type.h"
+    #include "Port.h"
+
+    class PwmSolo: private AlternatePort
+    {
+        public:
+            PwmSolo(Pin pin, uint frq = 10000, uint duty = 50);
+            void Init();
+            void SetOut(ushort pwmValue); //设置输出值
+            void SetOutPercent(float per); //设置输出比例，0-100		
+        private:
+            uint freq; //频率
+            uint duty; //输出值
+    };
+#endif
