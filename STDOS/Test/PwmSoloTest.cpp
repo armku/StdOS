ad640f678e6bf17a0812b29dd2383f0ea062ebc2
Ace <258613763@qq.com>
2017-11-14 13:55:33 +0800

    pwm代码优化

--- STDOS/Test/PwmSoloTest.cpp
+++ STDOS/Test/PwmSoloTest.cpp
@@ -2,10 +2,20 @@
 
 #ifdef DEBUG
 	PwmSolo pwm(10000, 2700,false);
+	PwmSolo pwm1[3];
 	void PwmSoloTest()
 	{
-		pwm.Set(PA6);
+		pwm.Set(PA3);
 		pwm.Open();
 		pwm.SetOutPercent(5);
+		
+		pwm1[0].Set(PA2);
+		pwm1[1].Set(PA6);
+		pwm1[2].Set(PA7);
+		for(int i=0;i<3;i++)
+		{
+			pwm1[i].Open();
+			pwm1[i].SetOutPercent(i*10+10);
+		}
 	}
 #endif
