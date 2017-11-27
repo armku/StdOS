6c2541791a1189cace0cc59f393b64a99602a876
Ace <258613763@qq.com>
2017-11-27 09:55:59 +0800

    代码整理

--- STDOS/Test/ESP8266Test.cpp
+++ STDOS/Test/ESP8266Test.cpp
@@ -76,33 +76,4 @@
             }
         }
     }
-    #ifdef __cplusplus
-        extern "C"
-        {
-        #endif 
-        void USART3_IRQHandler(void)
-        {
-            uint8_t ucCh;
-
-            if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
-            {
-                ucCh = USART_ReceiveData(USART3);
-
-                if (strEsp8266_Fram_Record .Length < (RX_BUF_MAX_LEN - 1))
-                //预留1个字节写结束符
-                    strEsp8266_Fram_Record .RxBuf[strEsp8266_Fram_Record .Length++] = ucCh;
-            }
-
-            if (USART_GetITStatus(USART3, USART_IT_IDLE) == SET)
-            //数据帧接收完毕
-            {
-                strEsp8266_Fram_Record .FlagFinish = 1;
-
-                ucCh = USART_ReceiveData(USART3); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR)
-                esp.FlagTcpClosed = strstr(strEsp8266_Fram_Record .RxBuf, "CLOSED\r\n") ? 1 : 0;
-            }
-        }
-        #ifdef __cplusplus
-        }
-    #endif 
 #endif
