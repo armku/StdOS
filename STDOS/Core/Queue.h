7f313b0399a5d1a2df23413e50cb57444a7172e9
Ace <258613763@qq.com>
2017-11-28 18:37:19 +0800

    代码格式整理

--- STDOS/Core/Queue.h
+++ STDOS/Core/Queue.h
@@ -8,24 +8,24 @@
     class Queue
     {
         private:
-            byte *pBuf; /* 缓冲区 */
-            int _write; /* 缓冲区写指针 */
-            int _capacity; /* 缓冲区大小 */
-            int _read; /* 缓冲区读指针 */
-            int _count; /* 数据个数 */
+            byte *pBuf; 	/* 缓冲区 */
+            int _write; 	/* 缓冲区写指针 */
+            int _capacity; 	/* 缓冲区大小 */
+            int _read; 		/* 缓冲区读指针 */
+            int _count; 	/* 数据个数 */
         public:
             Queue();
-            void SetBuf(void *buf, int len);
-            bool Empty()const{return _count == 0;} // 队列空
-            int Capacity()const{return _capacity;} // 队列容量
-            bool Full()const{return this->_count >= this->_capacity;} // 队列满
-            int Length()const{return _count;} // 队列大小
+            void SetBuf(void *buf, int len);							// 设置缓冲区
+            bool Empty()const{return _count == 0;} 						// 队列空
+            int Capacity()const{return _capacity;} 						// 队列容量
+            bool Full()const{return this->_count >= this->_capacity;} 	// 队列满
+            int Length()const{return _count;} 							// 队列大小
             void Clear();
 
             int Enqueue(byte da);
             byte Dequeue();
 
-            int Write(const Buffer &bs); // 批量写入
-            int Read(Buffer &bs); // 批量读取
+            int Write(const Buffer &bs); 								// 批量写入
+            int Read(Buffer &bs); 										// 批量读取
     };
 #endif
