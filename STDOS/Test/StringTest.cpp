72337d0ba8e3149861ede6c9377b7e3c5e99dd0e
Ace <258613763@qq.com>
2017-12-14 15:03:10 +0800

    代码整理

--- STDOS/Test/StringTest.cpp
+++ STDOS/Test/StringTest.cpp
@@ -6,22 +6,25 @@
 #define STRINGTEST
 
 #ifdef STRINGTEST
-	String str("Hello world");
+	void TestNum10();
+	
 	void StrintTest()
 	{
+		TestNum10();
 		
-		debug_printf("\r\nString Test Begin\r\n");
-				
-//		str.Show();
-//		str.ToLower();
-//		str.Show();
-//		str.ToUpper();
-//		str.Show();
-		
-		debug_printf("\r\nString Test End\r\n");
 	}
+	
 	void TestNum10()
 	{
+		String str("Hello world");
+		debug_printf("\r\nString Test Begin\r\n");
+				
+		str.Show();
+		str.ToLower();
+		str.Show();
+		str.ToUpper();
+		str.Show();
 		
+		debug_printf("\r\nString Test End\r\n");
 	}
 #endif
