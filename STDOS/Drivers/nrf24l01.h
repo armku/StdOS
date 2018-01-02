66be1d0d21027c9cd4e32e78e03eea5f0def9feb
Ace <258613763@qq.com>
2018-01-02 12:52:39 +0800

    代码整理

--- STDOS/Drivers/nrf24l01.h
+++ STDOS/Drivers/nrf24l01.h
@@ -1,11 +1,22 @@
 #ifndef _NRF24L01_H
     #define _NRF24L01_H
 
-	#include "Type.h"
+	#include "Spi.h"
 
     #define MAX_RT      0x10 //达到最大重发次数中断标志位
     #define TX_DS		0x20 //发送完成中断标志位	  // 
     #define RX_DR		0x40 //接收到数据中断标志位
+	
+	class NRF24L01 
+	{
+		public:
+			
+		private:
+			Spi*		_spi;
+			OutputPort	_CE;
+			InputPort	Irq;
+			OutputPort	_Power;	// 设置控制2401电源的引脚  直接进行对2401的通断电操作，以免死机对setPower无效
+	};
 
     void SPI_NRF_Init();
     byte SPI_NRF_RW(byte dat);
