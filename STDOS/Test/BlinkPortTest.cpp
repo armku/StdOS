b5fbaebc40aaf3e98f5d93012395c74706be5c41
Ace <258613763@qq.com>
2018-01-04 08:43:35 +0800

    优化测试

--- STDOS/Test/BlinkPortTest.cpp
+++ STDOS/Test/BlinkPortTest.cpp
@@ -3,12 +3,18 @@
 #define _BLINKPORT_H
 
 #ifdef _BLINKPORT_H
+	void BlinkRoutin(void * param)
+	{
+		BlinkPort* bph=(BlinkPort*)param;
+		bph->Start();
+	}
 	OutputPort ledBlink(PB0, true);
 	BlinkPort bp;
 	void BlinkPortTest()
 	{
 		bp.Add(&ledBlink);
 		bp.Start();
+		Sys.AddTask(BlinkRoutin,&bp,1000,3000,"blink");
 	}
 
 #endif
