87a5d20f32199b66dfa585c9313a63117f9c012f
Ace <258613763@qq.com>
2018-01-11 20:17:32 +0800

    增加文件

---
+++ STDOS/Core/Environment.h
@@ -0,0 +1,23 @@
+﻿#ifndef __Environment_H__
+#define __Environment_H__
+
+// 环境变量
+class TEnvironment
+{
+public:
+	TEnvironment();
+	
+	// 获取系统启动后经过的毫秒数
+	UInt64 TickCount() const;
+
+	// 获取系统启动后经过的毫秒数
+	UInt64 Ms() const;
+
+	// 获取当前计算机上的处理器数
+	int ProcessorCount() const;
+};
+
+// 全局环境变量
+extern const TEnvironment Environment;
+
+#endif
---
+++ STDOS/Core/TimeSpan.cpp
@@ -0,0 +1,94 @@
+#include "SString.h"
+#include "TimeSpan.h"
+
+TimeSpan::TimeSpan(Int64 ms)
+{
+
+}
+TimeSpan::TimeSpan(int hours, int minutes, int seconds)
+{
+
+}
+TimeSpan::TimeSpan(int days, int hours, int minutes, int seconds)
+{
+
+}
+
+int TimeSpan::Days() const
+{
+	return 0;
+}
+int TimeSpan::Hours() const
+{
+	return 0;
+}
+int TimeSpan::Minutes() const
+{
+	return 0;
+}
+int TimeSpan::Seconds() const
+{
+	return 0;
+}
+int TimeSpan::Ms() const
+{
+	return 0;
+}
+int TimeSpan::TotalDays() const
+{
+	return 0;
+}
+int TimeSpan::TotalHours() const
+{
+	return 0;
+}
+int TimeSpan::TotalMinutes() const
+{
+	return 0;
+}
+int TimeSpan::TotalSeconds() const
+{
+	return 0;
+}
+Int64 TimeSpan::TotalMs() const
+{
+	return 0;
+}
+
+int TimeSpan::CompareTo(const TimeSpan& value) const
+{
+	return 0;
+}
+bool operator==	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+bool operator!=	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+bool operator>	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+bool operator<	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+bool operator>=	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+bool operator<=	(const TimeSpan& left, const TimeSpan& right)
+{
+	return false;
+}
+
+String TimeSpan::ToString() const
+{
+	return "";
+}
+void TimeSpan::Show(bool newLine) const
+{
+
+}
