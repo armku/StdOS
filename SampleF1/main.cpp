16aca328295e7ea99d847fcc82408b5fa8b5429d
Ace <258613763@qq.com>
2018-01-13 17:09:06 +0800

    删除无效代码

--- SampleF1/main.cpp
+++ SampleF1/main.cpp
@@ -12,26 +12,8 @@
 #include "TTime.h"
 #include "Drivers\RX8025T.h"
 
-
-class TTTTime
-{
-    public:
-        uint Seconds; // 全局秒数，系统启动后总秒数。累加
-        UInt64 Milliseconds; // 全局毫秒数，系统启动后总毫秒（1000ms整部分）。累加
-        uint CurrentTicks; // 当前滴答时钟
-        UInt64 Current; // 当前毫秒数
-};
-TTTTime now123;
 //InputPort paa11(PA11,false);
 OutputPort paaa1(PA11);
-void Test12(void *param)
-{
-    //	int aa=paa11;aa++;
-    now123.Seconds = Time.Seconds;
-    now123.Milliseconds = Time.Milliseconds;
-    now123.Current = Time.Current();
-    now123.CurrentTicks = Time.CurrentTicks();
-}
 
 #if 1
     OutputPort led1(PB0, false);
@@ -69,49 +51,8 @@ uint OnUsart3Read(ITransport *transport, Buffer &bs, void *para, void *para2)
     return 0;
 }
 
-
-//void TimerTask(void *param)
-//{
-////    static int i = 0;
-////    debug_printf("\r\n%d: cnt:%d", i++, time6cnt);
-//}
-
 //SerialPort *sp2;
 //SerialPort *sp3;
-
-void InterruptTest();
-void ADS1232Test();
-void ModbusTest();
-void streamtest();
-
-void ESP8266Test();
-void ssd1309Test();
-void ds18b20test();
-void ST7565Rtest();
-void keyTest();
-void FifoTest();
-void SerialPortTest();
-void PwmSoloTest();
-void WaitHandleTest();
-void StrintTest();
-void n24l01Test();
-void BlinkPortTest();
-void FlushPortTest();
-
-//void Test(void * param)
-//{
-//	auto aa="helloworld";
-//	Buffer bs((char *)aa,ArrayLength(aa));
-////	sp2->Write(bs);
-//	bs.Show();
-//}
-void Test(void * param)
-{
-	int cur=Time.Current();	
-	Sys.Sleep(20);
-	int cur2=Time.Current();
-	debug_printf("\r\n aa:%ld--%ld %ld",cur,cur2,cur2-cur);
-}
 
 int main(void)
 {
@@ -119,8 +60,7 @@ int main(void)
     #if DEBUG
         Sys.MessagePort = COM1;
         Sys.ShowInfo();
-    #endif 
-	
+    #endif 	
 
     SerialPort::GetMessagePort()->Register(OnUsart1Read);
 		
@@ -129,27 +69,6 @@ int main(void)
 //	sp3->Open();
 	
     //Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
-//    Sys.AddTask(TimerTask, &led1, 0, 1000, "TimerTask");
-//	Sys.AddTask(Test,0,2000,2000,"Test");
-    //Sys.AddTask(Test12, 0, 600, 1000, "Test");
-	Sys.AddTask(Test, 0, 600, 1000, "Test");
-	
-	//StrintTest();
-    //IList::Test();
-    //ADS1232Test();
-    //streamtest();
-    //ModbusTest();
-    //InterruptTest();
-	//ssd1309Test();
-    //ESP8266Test();
-	//ds18b20test();
-	//n24l01Test();
-	//keyTest();
-	//SerialPortTest();
-	//ST7565Rtest();
-	//PwmSoloTest();
-	//WaitHandleTest();
-	//BlinkPortTest();
-	FlushPortTest();
+		
     Sys.Start();
 }
