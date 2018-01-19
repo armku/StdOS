3f6a8ed13498f7d2bc1a0331c14684ea8d2d89d4
Ace <258613763@qq.com>
2018-01-19 16:16:22 +0800

    删除无用代码

--- STDOS/Drivers/OCM240128.cpp
+++ STDOS/Drivers/OCM240128.cpp
@@ -58,10 +58,6 @@ extern const byte  GB3212[][32];
 #define  LCD_CMD_GPIO     GPIOA
 #define  GPIOCLK_CMD      RCC_APB2Periph_GPIOA
 
-#define  STA0      GPIO_Pin_8
-#define  STA1      GPIO_Pin_9
-#define  STA3      GPIO_Pin_11
-
 #define  LCD_GPIO_DAT   GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15
 #define  LCD_GPIO_CMD	  GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7
 
