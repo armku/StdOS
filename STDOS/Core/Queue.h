96a92b4f62772ecce4c327c01babda7a3c9cb8e1
Ace <258613763@qq.com>
2018-01-21 18:10:01 +0800

    Queue优化

--- STDOS/Core/Queue.h
+++ STDOS/Core/Queue.h
@@ -1,7 +1,7 @@
 #ifndef _Queue_H_
     #define _Queue_H_
 
-    #include "Buffer.h"
+    #include "Array.h"
 
     // 队列
     // 内有一个缓冲区，游标位置，数据长度。实际有效数据仅占用缓冲区中间部分，头尾都可能有剩余
@@ -11,7 +11,7 @@
             byte *pBuf; 	/* 缓冲区 */
             int _write; 	/* 缓冲区写指针 */
             int _capacity; 	/* 缓冲区大小 */
-            int _read; 		/* 缓冲区读指针 */
+			int _read; 		/* 缓冲区读指针 */
             int _count; 	/* 数据个数 */
         public:
             Queue();
