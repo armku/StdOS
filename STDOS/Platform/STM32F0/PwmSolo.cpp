f7dc5b8e35700d5e258c687c94d87f2cd416a243
Ace <258613763@qq.com>
2018-03-18 12:44:12 +0800

    优化pwmsolo

--- STDOS/Platform/STM32F0/PwmSolo.cpp
+++ STDOS/Platform/STM32F0/PwmSolo.cpp
@@ -268,3 +268,20 @@ void PwmSolo::SetOut(ushort pwmValue)
 			break;
 	}	
 }
+PwmSolo::PwmSolo(uint frq, uint duty, bool invert)
+{
+	this->_freq = frq;
+	this->_duty = duty;
+	this->_invert = invert;
+}
+void PwmSolo::Open()
+{
+	AlternatePort::Open();
+	this->OnOpen2();
+}
+//ÉèÖÃÊä³ö±ÈÀı£¬0-100
+void PwmSolo::SetOutPercent(float per)
+{
+	this->_duty = this->_freq *per / 100;
+	this->SetOut(this->_duty);
+}
