f8c9fcba8ecf9e1da9d2fbc003a99c54c2d27c8a
Ace <258613763@qq.com>
2018-04-12 21:57:35 +0800

    代码整理

--- STDOS/Core/Type.cpp
+++ STDOS/Core/Type.cpp
@@ -6,10 +6,6 @@ void assert_failed(uint8_t *file, uint32_t line, char *errstr)
     StdPrintf("%s(%d):    %s\n", file, line, errstr);
 }
 
-//String &Object::ToStr(String &str)const
-//{
-//    return str;
-//}
 
 String Object::ToString()const
 {
@@ -24,24 +20,3 @@ void Object::Show(bool newLine)const
 //	this->ToStr(strtmp);	
 //	strtmp.Show(newLine);
 }
-
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//const Type Object::GetType()const
-//{
-//    Type ret;
-//    ret._info = "Object";
-//    ret.Size = 7;
-//    return ret;
-//}
-
-//Type::Type()
-//{
-
-//}
-//// Ãû³Æ
-//const String Type::Name()const
-//{
-//    String ret((cstring)(this->_info), this->Size);
-//    return ret;
-//}
