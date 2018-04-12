363285af86c9d84bda8d7b7023a02fb37e5f1a00
Ace <258613763@qq.com>
2018-04-12 22:02:17 +0800

    代码整理

--- STDOS/Core/Type.cpp
+++ STDOS/Core/Type.cpp
@@ -5,18 +5,3 @@ void assert_failed(uint8_t *file, uint32_t line, char *errstr)
 {
     StdPrintf("%s(%d):    %s\n", file, line, errstr);
 }
-
-
-String Object::ToString()const
-{
-    return String("Object");
-}
-
-void Object::Show(bool newLine)const
-{
-//	char bufch[512];
-//	String strtmp(bufch,ArrayLength(bufch));
-//	strtmp.SetLength(0);
-//	this->ToStr(strtmp);	
-//	strtmp.Show(newLine);
-}
