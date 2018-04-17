86b0eec477ecbaa602f7d7b3936a987d3582b29d
Ace <258613763@qq.com>
2018-04-17 13:04:20 +0800

    代码整理

--- STDOS/Device/Spi.cpp
+++ STDOS/Device/Spi.cpp
@@ -341,6 +341,235 @@ void SpiSoft::Stop()
     }
 }
 #if defined STM32F0
+#include "Sys.h"
+#include "Spi.h"
+#include "Platform\stm32.h"
+
+// 基础读写
+uint8_t Spi::Write(uint8_t data)
+{
+	int retry;
+	switch (this->_index)
+	{
+	case Spi1:
+	case Spi2:
+	case Spi3:
+		retry = Retry;
+		while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_TXE) == RESET)
+		{
+			if (--retry <= 0)
+				return ++Error;
+			// 超时处理
+		}
+		SPI_SendData8((SPI_TypeDef*)(this->_SPI), data);
+		//是否发送成功
+		retry = Retry;
+		while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_RXNE) == RESET)
+		{
+			if (--retry <= 0)
+				return ++Error;
+			// 超时处理
+		}
+
+		return SPI_ReceiveData8((SPI_TypeDef*)(this->_SPI)); //返回通过SPIx最近接收的数据            
+	default:
+		return  0;
+	}
+}
+
+int Spi::GetPre(int index, uint32_t &speedHz)
+{
+	// 自动计算稍低于速度speedHz的分频
+	uint16_t pre = SPI_BaudRatePrescaler_2;
+	uint32_t clock = Sys.Clock >> 1;
+	while (pre <= SPI_BaudRatePrescaler_256)
+	{
+		if (clock <= speedHz)
+			break;
+		clock >>= 1;
+		pre += (SPI_BaudRatePrescaler_4 - SPI_BaudRatePrescaler_2);
+	}
+	if (pre > SPI_BaudRatePrescaler_256)
+	{
+		debug_printf("Spi%d::Init Error! speedHz=%d mush be dived with %dMHz\r\n", index, speedHz, Sys.Clock);
+		return  -1;
+	}
+
+	speedHz = clock;
+	return pre;
+}
+void Spi::OnInit()
+{
+	//以上为历史内容
+	switch (this->_index)
+	{
+	case Spi1:
+		//				// SPI都在GPIO_AF_0分组内
+		//        GPIO_PinAFConfig(_GROUP(ps[1]), _PIN(ps[1]), GPIO_AF_0);
+		//        GPIO_PinAFConfig(_GROUP(ps[2]), _PIN(ps[2]), GPIO_AF_0);
+		//        GPIO_PinAFConfig(_GROUP(ps[3]), _PIN(ps[3]), GPIO_AF_0)
+		this->SetPin(PB3, PB4, PB5);
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_0); //PB3复用为 SPI1
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_0); //PB4复用为 SPI1
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_0); //PB5复用为 SPI1
+		this->_SPI = SPI1;
+		break;
+	case Spi2:
+		this->SetPin(PB13, PB14, PB15);
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, GPIO_AF_0); //PB13复用为 SPI2
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_0); //PB14复用为 SPI2
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, GPIO_AF_0); //PB15复用为 SPI2
+		this->_SPI = SPI2;
+		break;
+	case Spi3:
+		break;
+	default:
+		break;
+	}
+	/////////////////////////////////////////////////////////////
+	SPI_InitTypeDef SPI_InitStructure;
+	SPI_StructInit(&SPI_InitStructure);
+	switch (this->_index)
+	{
+	case Spi1:
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+		break;
+	case Spi2:
+		RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+		break;
+	case Spi3:
+		break;
+	default:
+		break;
+	}
+	//this->Stop();
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //双线全双工
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Master; // 主模式
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b; // 数据大小8位 SPI发送接收8位帧结构
+	if (this->CPOL == CPOL_Low)
+	{
+		SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low; // 时钟极性，空闲时为高
+	}
+	else if (this->CPOL == CPOL_High)
+	{
+		SPI_InitStructure.SPI_CPOL = SPI_CPOL_High; // 时钟极性，空闲时为高
+	}
+	else {}
+	if (this->CPHA == CPHA_1Edge)
+	{
+		SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge; // 第1个边沿有效，上升沿为采样时刻
+	}
+	else if (this->CPHA == CPHA_2Edge)
+	{
+		SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge; // 第2个边沿有效，上升沿为采样时刻
+	}
+	else {}
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;// NSS信号由硬件（NSS管脚）还是软件（使用SSI位）管理:内部NSS信号有SSI位控制    
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前。指定数据传输从MSB位还是LSB位开始:数据传输从MSB位开始
+	SPI_InitStructure.SPI_CRCPolynomial = 7; // CRC值计算的多项式
+	switch (this->_index)
+	{
+	case Spi1:
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+		assert_param(IS_SPI_BAUDRATE_PRESCALER(SPI_BaudRatePrescaler)); //判断有效性
+		SPI1->CR1 &= 0XFFC7; //位3-5清零，用来设置波特率
+		SPI1->CR1 |= SPI_BaudRatePrescaler_64; //设置SPI1速度 设置为21M时钟,高速模式 
+		SPI_Cmd(SPI1, ENABLE); //使能SPI1
+		break;
+	case Spi2:
+		RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+		break;
+	case Spi3:
+		break;
+	default:
+		break;
+	}
+	switch (this->_index)
+	{
+	case Spi1:
+	case Spi2:
+	case Spi3:
+		SPI_Init((SPI_TypeDef*)(this->_SPI), &SPI_InitStructure);
+		SPI_RxFIFOThresholdConfig((SPI_TypeDef*)(this->_SPI), SPI_RxFIFOThreshold_QF);
+		break;
+	default:
+		break;
+	}
+}
+
+void Spi::OnOpen()
+{
+	switch (this->_index)
+	{
+	case Spi1:
+	case Spi2:
+	case Spi3:
+		SPI_Cmd((SPI_TypeDef*)(this->_SPI), ENABLE);
+		break;
+	default:
+		break;
+	}
+}
+
+void Spi::OnClose()
+{
+	this->Stop();
+	switch (this->_index)
+	{
+	case Spi1:
+	case Spi2:
+	case Spi3:
+		SPI_Cmd((SPI_TypeDef*)(this->_SPI), DISABLE);
+		break;
+	default:
+		break;
+	}
+	SPI_I2S_DeInit((SPI_TypeDef*)(this->_SPI));
+	debug_printf("    CLK : ");
+	this->_clk.Set(P0);
+	debug_printf("    MISO: ");
+	this->_miso.Set(P0);
+	debug_printf("    MOSI: ");
+	this->_mosi.Set(P0);
+	debug_printf("    NSS : ");
+	this->_nss.Set(P0);
+
+	this->Pins[0] = P0;
+	this->Pins[1] = P0;
+	this->Pins[2] = P0;
+	this->Pins[3] = P0;
+}
+uint16_t Spi::Write16(uint16_t data)
+{
+	int retry;
+	switch (this->_index)
+	{
+	case Spi1:
+	case Spi2:
+	case Spi3:
+		// 双字节操作，超时次数加倍
+		retry = Retry << 1;
+		while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_TXE) == RESET)
+		{
+			if (--retry <= 0)
+				return ++Error;
+			// 超时处理
+		}
+		SPI_I2S_SendData16((SPI_TypeDef*)(this->_SPI), data);
+
+		retry = Retry << 1;
+		while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_RXNE) == RESET)
+		{
+			if (--retry <= 0)
+				return ++Error;
+			// 超时处理
+		}
+		return SPI_I2S_ReceiveData16((SPI_TypeDef*)(this->_SPI));
+	default:
+		return 0;
+	}
+}
 
 #elif defined STM32F1
 #include "Sys.h"
@@ -569,5 +798,248 @@ uint16_t Spi::Write16(uint16_t data)
 #endif
 
 #elif defined STM32F4
+#include "Sys.h"
+#include "Spi.h"
+#include "Platform\stm32.h"
+
+// 基础读写
+uint8_t Spi::Write(uint8_t data)
+{
+	int retry;
+    switch (this->_index)
+    {
+        case Spi1:
+        case Spi2:
+        case Spi3:
+            retry = Retry;
+            while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_TXE) == RESET)
+            {
+                if (--retry <= 0)
+                    return ++Error;
+                // 超时处理
+            }
+            SPI_I2S_SendData((SPI_TypeDef*)(this->_SPI), data);
+            
+            //是否发送成功
+            retry = Retry;
+            while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_RXNE) == RESET)
+            {
+                if (--retry <= 0)
+                    return ++Error;
+                // 超时处理
+            }
+
+            return SPI_I2S_ReceiveData((SPI_TypeDef*)(this->_SPI));             
+        default:
+            return  0;
+    }
+}
+
+int Spi::GetPre(int index, uint32_t &speedHz)
+{
+    // 自动计算稍低于速度speedHz的分频
+    uint16_t pre = SPI_BaudRatePrescaler_2;
+    uint32_t clock = Sys.Clock >> 1;
+    while (pre <= SPI_BaudRatePrescaler_256)
+    {
+        if (clock <= speedHz)
+            break;
+        clock >>= 1;
+        pre += (SPI_BaudRatePrescaler_4 - SPI_BaudRatePrescaler_2);
+    }
+    if (pre > SPI_BaudRatePrescaler_256)
+    {
+        debug_printf("Spi%d::Init Error! speedHz=%d mush be dived with %dMHz\r\n", index, speedHz, Sys.Clock);
+        return  - 1;
+    }
+
+    speedHz = clock;
+    return pre;
+}
+void Spi::OnInit()
+{
+	//以上为历史内容
+    switch (this->_index)
+    {
+        case Spi1:
+            this->SetPin(PB3, PB4, PB5);//this->SetPin(PB3, PB4, PB5, PB14);//this->SetPin(PA5, PA6, PA7, PA4);/原生SPI1
+                //				uint8_t afs[] = 
+                //        {
+                //            GPIO_AF_SPI1, GPIO_AF_SPI2, GPIO_AF_SPI3, GPIO_AF_SPI4, GPIO_AF_SPI5, GPIO_AF_SPI6
+                //        };
+                //        GPIO_PinAFConfig(_GROUP(ps[1]), _PIN(ps[1]), afs[_index]);
+                //        GPIO_PinAFConfig(_GROUP(ps[2]), _PIN(ps[2]), afs[_index]);
+                //        GPIO_PinAFConfig(_GROUP(ps[3]), _PIN(ps[3]), afs[_index]);
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI1); //PB3复用为 SPI1
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_SPI1); //PB4复用为 SPI1
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_SPI1); //PB5复用为 SPI1
+            this->_SPI = SPI1;
+            break;
+        case Spi2:
+                this->SetPin(PB13, PB14, PB15);//this->SetPin(PB13, PB14, PB15, PB12); //原生SPI2
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, GPIO_AF_SPI2); //PB13复用为 SPI2
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_SPI2); //PB14复用为 SPI2
+                GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, GPIO_AF_SPI2); //PB15复用为 SPI2
+            this->_SPI = SPI2;
+            break;
+        case Spi3:
+                this->SetPin(PC10, PC11, PC12);//this->SetPin(PC10, PC11, PC12, PA15);
+                GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_SPI3); //PC10复用为 SPI3
+                GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_SPI3); //PC11复用为 SPI3
+                GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_SPI3); //PC12复用为 SPI3
+            this->_SPI = SPI3;
+			break;
+        default:
+            break;
+    }
+    /////////////////////////////////////////////////////////////
+    SPI_InitTypeDef SPI_InitStructure;
+    SPI_StructInit(&SPI_InitStructure);
+    switch (this->_index)
+    {
+        case Spi1:
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+            break;
+        case Spi2:
+            RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+            break;
+        case Spi3:
+			RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+			break;
+        default:
+            break;
+    }
+    //this->Stop();
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //双线全双工
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master; // 主模式
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b; // 数据大小8位 SPI发送接收8位帧结构
+	if(this->CPOL==CPOL_Low)
+	{
+		SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low; // 时钟极性，空闲时为高
+	}
+	else if(this->CPOL==CPOL_High)
+	{
+		SPI_InitStructure.SPI_CPOL = SPI_CPOL_High; // 时钟极性，空闲时为高
+	}
+	else{}
+	if(this->CPHA==CPHA_1Edge)
+	{
+		SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge; // 第1个边沿有效，上升沿为采样时刻
+	}
+	else if(this->CPHA==CPHA_2Edge)
+	{
+		SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge; // 第2个边沿有效，上升沿为采样时刻
+	}
+	else{}
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;// NSS信号由硬件（NSS管脚）还是软件（使用SSI位）管理:内部NSS信号有SSI位控制    
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; //定义波特率预分频的值:波特率预分频值为256
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前。指定数据传输从MSB位还是LSB位开始:数据传输从MSB位开始
+    SPI_InitStructure.SPI_CRCPolynomial = 7; // CRC值计算的多项式
+    switch (this->_index)
+    {
+        case Spi1:
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+            //SPI1速度设置函数
+                //SPI速度=fAPB2/分频系数
+                //@ref SPI_BaudRate_Prescaler:SPI_BaudRatePrescaler_2~SPI_BaudRatePrescaler_256  
+                //fAPB2时钟一般为84Mhz：
+                assert_param(IS_SPI_BAUDRATE_PRESCALER(SPI_BaudRatePrescaler)); //判断有效性
+                SPI1->CR1 &= 0XFFC7; //位3-5清零，用来设置波特率
+                SPI1->CR1 |= SPI_BaudRatePrescaler_4; //设置SPI1速度 设置为21M时钟,高速模式 
+                SPI_Cmd(SPI1, ENABLE); //使能SPI1
+            break;
+        case Spi2:
+            RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+            break;
+        case Spi3:
+			RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+			break;
+        default:
+            break;
+    }
+    switch (this->_index)
+    {
+        case Spi1:
+        case Spi2:
+        case Spi3:
+            SPI_Init((SPI_TypeDef*)(this->_SPI), &SPI_InitStructure);
+			break;
+        default:
+            break;
+    }
+}
+
+void Spi::OnOpen()
+{
+    switch (this->_index)
+    {
+        case Spi1:
+        case Spi2:
+        case Spi3:
+            SPI_Cmd((SPI_TypeDef*)(this->_SPI), ENABLE);
+            break;
+        default:
+            break;
+    }
+}
+
+void Spi::OnClose()
+{
+    this->Stop();
+    switch (this->_index)
+    {
+        case Spi1:
+        case Spi2:
+        case Spi3:
+            SPI_Cmd((SPI_TypeDef*)(this->_SPI), DISABLE);
+            break;
+        default:
+            break;
+    }
+    SPI_I2S_DeInit((SPI_TypeDef*)(this->_SPI));
+    debug_printf("    CLK : ");
+    this->_clk.Set(P0);
+    debug_printf("    MISO: ");
+    this->_miso.Set(P0);
+    debug_printf("    MOSI: ");
+    this->_mosi.Set(P0);
+    debug_printf("    NSS : ");
+    this->_nss.Set(P0);
+
+    this->Pins[0] = P0;
+    this->Pins[1] = P0;
+    this->Pins[2] = P0;
+    this->Pins[3] = P0;
+}
+uint16_t Spi::Write16(uint16_t data)
+{
+	int retry;
+    switch (this->_index)
+    {
+        case Spi1:
+        case Spi2:
+        case Spi3:
+            // 双字节操作，超时次数加倍
+            retry = Retry << 1;
+            while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_TXE) == RESET)
+            {
+                if (--retry <= 0)
+                    return ++Error;
+                // 超时处理
+            }
+            SPI_I2S_SendData((SPI_TypeDef*)(this->_SPI), data);
+            
+            retry = Retry << 1;
+            while (SPI_I2S_GetFlagStatus((SPI_TypeDef*)(this->_SPI), SPI_I2S_FLAG_RXNE) == RESET)
+            {
+                if (--retry <= 0)
+                    return ++Error;
+                // 超时处理
+            }
+            return SPI_I2S_ReceiveData((SPI_TypeDef*)(this->_SPI));            
+        default:
+            return 0;
+    }
+}
 
 #endif
