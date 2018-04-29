90a951e480828c50f439a8e418258413cc11ccf7
Ace <258613763@qq.com>
2018-04-29 18:23:39 +0800

    代码整理

--- STDOS/Device/WatchDog.cpp
+++ STDOS/Device/WatchDog.cpp
@@ -52,6 +52,84 @@ void WatchDog::Feed()
 // 配置看门狗喂狗重置时间，超过该时间将重启MCU
 bool WatchDog::Config(uint32_t ms)
 {
+	#if defined STM32F0
+	if (ms == 0)
+    {
+        debug_printf("WatchDog msTimeout %dms must larger than 0ms\r\n", ms);
+        return false;
+    }
+    RCC_LSICmd(ENABLE);
+    /* 检查系统是否从IWDG重置恢复 */
+    if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
+    {
+        /* 清除重置标识 */
+        RCC_ClearFlag();
+    }
+    /* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
+    IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+
+    uint8_t pre = IWDG_Prescaler_4;
+    int mul = 4;
+    // 计数器12位 0~0x0FFF，有reload = ms/1000 / (1/(LSI/mul)) = ms * LSI / (mul*1000) = ms * 40 / mul
+    // 考虑到reload溢出的可能，每种分频最大ms = reload * mul / 40 ~= 102 * mul
+    int i = 0;
+    /*
+    #define IWDG_Prescaler_4            ((uint8_t)0x00)
+    #define IWDG_Prescaler_8            ((uint8_t)0x01)
+    #define IWDG_Prescaler_16           ((uint8_t)0x02)
+    #define IWDG_Prescaler_32           ((uint8_t)0x03)
+    #define IWDG_Prescaler_64           ((uint8_t)0x04)
+    #define IWDG_Prescaler_128          ((uint8_t)0x05)
+    #define IWDG_Prescaler_256          ((uint8_t)0x06)
+     */
+    for (i = IWDG_Prescaler_4; i <= IWDG_Prescaler_256; i++)
+    {
+        pre = i;
+        mul = 1 << (i + 2);
+
+        // 判断是否在范围之内
+        if (ms *40 / mul < 0x0FFF)
+            break;
+    }
+    if (i > IWDG_Prescaler_256)
+    {
+        debug_printf("WatchDog msTimeout must smaller than %dms\r\n", 0x0FFF *256 / 40);
+        return false;
+    }
+    IWDG_SetPrescaler(pre);
+    /*if(ms < (0x0FFF * 32 / 40))
+    {
+    // IWDG计数器时钟: LSI/32=40KHz/32=1250Hz，每周期0.8ms
+    IWDG_SetPrescaler(IWDG_Prescaler_32);
+    }
+    else
+    {
+    // IWDG计数器时钟: LSI/64=40KHz/64=625Hz，每周期0.4ms
+    IWDG_SetPrescaler(IWDG_Prescaler_64);
+
+    // 直接除以2，后面不用重复计算
+    ms >>= 2;
+    }*/
+
+    /* 设置计数器重载值为超时时间
+    Counter Reload Value = ms / 1000 / IWDG计数器时钟周期
+    = ms / 1000 / (1/(LSI/mul))
+    = ms * LSI / (mul * 1000)
+    = ms * 40k / (mul * 1000)
+    = ms * 40 / mul
+     */
+    IWDG_SetReload(ms *40 / mul);
+
+    /* 重载 IWDG 计数器 */
+    IWDG_ReloadCounter();
+
+    /* 打开 IWDG (LSI将由硬件打开) */
+    IWDG_Enable();
+
+    Timeout = ms;
+
+    return true;
+	#elif defined STM32F1
     if (ms == 0)
     {
         debug_printf("WatchDog msTimeout %dms must larger than 0ms\r\n", ms);
@@ -128,63 +206,8 @@ bool WatchDog::Config(uint32_t ms)
     Timeout = ms;
 
     return true;
-}
-// 看门狗无法关闭，只能设置一个最大值
-void WatchDog::ConfigMax()
-{
-    /* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
-    IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-
-    // 独立看门狗无法关闭
-    IWDG_SetPrescaler(IWDG_Prescaler_256);
-    IWDG_SetReload(0x0FFF); 
-    IWDG_ReloadCounter();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if defined STM32F0
-int SetWatchDog( unsigned int a2)
-{
-//  int v3; // r0@1
-//  int v4; // r0@1
-
-//  IWDG_SetPrescaler();
-//  v3 = IWDG_SetReload(a2);
-//  v4 = IWDG_ReloadCounter(v3);
-//  return IWDG_Enable(v4);
-	return 0;
-}
-int OpenWatchDog(void)
-{
-//  RCC_LSICmd(1);
-//  if ( RCC_GetFlagStatus(125) )
-//    RCC_ClearFlag();
-//  return IWDG_WriteAccessCmd(21845);
-	return 0;
-}
-// 喂狗
-void WatchDog::Feed()
-{
-    IWDG_ReloadCounter();
-}
-// 配置看门狗喂狗重置时间，超过该时间将重启MCU
-bool WatchDog::Config(uint32_t ms)
-{
-    if (ms == 0)
+	#elif defined STM32F4
+	 if (ms == 0)
     {
         debug_printf("WatchDog msTimeout %dms must larger than 0ms\r\n", ms);
         return false;
@@ -260,10 +283,20 @@ bool WatchDog::Config(uint32_t ms)
     Timeout = ms;
 
     return true;
+	#endif
 }
 // 看门狗无法关闭，只能设置一个最大值
 void WatchDog::ConfigMax()
 {
+	#if defined STM32F0
+	/* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
+    IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+
+    // 独立看门狗无法关闭
+    IWDG_SetPrescaler(IWDG_Prescaler_256);
+    IWDG_SetReload(0x0FFF); 
+    IWDG_ReloadCounter();
+	#elif defined STM32F1
     /* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
     IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
 
@@ -271,125 +304,13 @@ void WatchDog::ConfigMax()
     IWDG_SetPrescaler(IWDG_Prescaler_256);
     IWDG_SetReload(0x0FFF); 
     IWDG_ReloadCounter();
-}
-
-#elif defined STM32F1
-
-#elif defined STM32F4
-int SetWatchDog( unsigned int a2)
-{
-//  int v3; // r0@1
-//  int v4; // r0@1
-
-//  IWDG_SetPrescaler();
-//  v3 = IWDG_SetReload(a2);
-//  v4 = IWDG_ReloadCounter(v3);
-//  return IWDG_Enable(v4);
-	return 0;
-}
-int OpenWatchDog(void)
-{
-//  RCC_LSICmd(1);
-//  if ( RCC_GetFlagStatus(125) )
-//    RCC_ClearFlag();
-//  return IWDG_WriteAccessCmd(21845);
-	return 0;
-}
-// 喂狗
-void WatchDog::Feed()
-{
-    IWDG_ReloadCounter();
-}
-// 配置看门狗喂狗重置时间，超过该时间将重启MCU
-bool WatchDog::Config(uint32_t ms)
-{
-    if (ms == 0)
-    {
-        debug_printf("WatchDog msTimeout %dms must larger than 0ms\r\n", ms);
-        return false;
-    }
-    RCC_LSICmd(ENABLE);
-    /* 检查系统是否从IWDG重置恢复 */
-    if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
-    {
-        /* 清除重置标识 */
-        RCC_ClearFlag();
-    }
-    /* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
-    IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-
-    uint8_t pre = IWDG_Prescaler_4;
-    int mul = 4;
-    // 计数器12位 0~0x0FFF，有reload = ms/1000 / (1/(LSI/mul)) = ms * LSI / (mul*1000) = ms * 40 / mul
-    // 考虑到reload溢出的可能，每种分频最大ms = reload * mul / 40 ~= 102 * mul
-    int i = 0;
-    /*
-    #define IWDG_Prescaler_4            ((uint8_t)0x00)
-    #define IWDG_Prescaler_8            ((uint8_t)0x01)
-    #define IWDG_Prescaler_16           ((uint8_t)0x02)
-    #define IWDG_Prescaler_32           ((uint8_t)0x03)
-    #define IWDG_Prescaler_64           ((uint8_t)0x04)
-    #define IWDG_Prescaler_128          ((uint8_t)0x05)
-    #define IWDG_Prescaler_256          ((uint8_t)0x06)
-     */
-    for (i = IWDG_Prescaler_4; i <= IWDG_Prescaler_256; i++)
-    {
-        pre = i;
-        mul = 1 << (i + 2);
-
-        // 判断是否在范围之内
-        if (ms *40 / mul < 0x0FFF)
-            break;
-    }
-    if (i > IWDG_Prescaler_256)
-    {
-        debug_printf("WatchDog msTimeout must smaller than %dms\r\n", 0x0FFF *256 / 40);
-        return false;
-    }
-    IWDG_SetPrescaler(pre);
-    /*if(ms < (0x0FFF * 32 / 40))
-    {
-    // IWDG计数器时钟: LSI/32=40KHz/32=1250Hz，每周期0.8ms
-    IWDG_SetPrescaler(IWDG_Prescaler_32);
-    }
-    else
-    {
-    // IWDG计数器时钟: LSI/64=40KHz/64=625Hz，每周期0.4ms
-    IWDG_SetPrescaler(IWDG_Prescaler_64);
-
-    // 直接除以2，后面不用重复计算
-    ms >>= 2;
-    }*/
-
-    /* 设置计数器重载值为超时时间
-    Counter Reload Value = ms / 1000 / IWDG计数器时钟周期
-    = ms / 1000 / (1/(LSI/mul))
-    = ms * LSI / (mul * 1000)
-    = ms * 40k / (mul * 1000)
-    = ms * 40 / mul
-     */
-    IWDG_SetReload(ms *40 / mul);
-
-    /* 重载 IWDG 计数器 */
-    IWDG_ReloadCounter();
-
-    /* 打开 IWDG (LSI将由硬件打开) */
-    IWDG_Enable();
-
-    Timeout = ms;
-
-    return true;
-}
-// 看门狗无法关闭，只能设置一个最大值
-void WatchDog::ConfigMax()
-{
-    /* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
+	#elif defined STM32F4
+	/* 打开IWDG_PR和IWDG_RLR寄存器的写访问 */
     IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
 
     // 独立看门狗无法关闭
     IWDG_SetPrescaler(IWDG_Prescaler_256);
     IWDG_SetReload(0x0FFF); 
     IWDG_ReloadCounter();
+	#endif
 }
-
-#endif
