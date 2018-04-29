7a8d1a024a161509c5c42d8cb78f37b81acbf17c
Ace <258613763@qq.com>
2018-04-29 17:57:55 +0800

    代码整理

--- STDOS/Kernel/Time.cpp
+++ STDOS/Kernel/Time.cpp
@@ -184,7 +184,38 @@ uint32_t TTime::UsToTicks(uint32_t us)const
 extern int gTicks; //
 void TTime::Init()
 {
-	#if defined STM32F0
+	#if defined STM32F0	
+    // 初始化为输出
+        this->Index = 6;
+	
+	gTicks = (Sys.Clock >> 3) / 0xF4240u;
+	
+    timerTick = new Timer((TIMER)this->Index);
+    abc.Bind(timTickrefesh);
+    timerTick->Register(abc);
+    timerTick->Open();
+    //timerTick->SetFrequency(1000);
+    //        timer2->Config();
+
+    //初始化延迟函数
+    //SYSTICK的时钟固定为HCLK时钟的1/8
+    //SYSCLK:系统时钟
+    SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
+    SysTick_Config(9000); //配置SysTick tick is 9ms	9000
+    NVIC_SetPriority(SysTick_IRQn, 0);
+	switch(this->Index)
+	{
+		case 2:
+			NVIC_SetPriority(TIM3_IRQn, 0);
+			break;
+		case 5:
+			break;
+		case 6:
+			//NVIC_SetPriority(TIM7_IRQn, 0);
+			break;
+		default:
+			break;
+	}
 	#elif defined STM32F1
 this->Index = 5;
     gTicks = (Sys.Clock >> 3) / 0xF4240u;
@@ -194,6 +225,41 @@ this->Index = 5;
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
     GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE); //关闭jtag，保留swd	
 	#elif defined STM32F4
+	void TTime::Init()
+{
+    // 初始化为输出
+        this->Index = 2;
+	
+	gTicks = (Sys.Clock >> 3) / 0xF4240u;
+	
+    timerTick = new Timer((TIMER)this->Index);
+    abc.Bind(timTickrefesh);
+    timerTick->Register(abc);
+    timerTick->Open();
+    //timerTick->SetFrequency(1000);
+    //        timer2->Config();
+
+    //初始化延迟函数
+    //SYSTICK的时钟固定为HCLK时钟的1/8
+    //SYSCLK:系统时钟
+//        uint32_t SYSCLK = 168;
+	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);  
+	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
+	SysTick_Config(9000); //配置SysTick tick is 9ms	9000
+    NVIC_SetPriority(SysTick_IRQn, 0);
+	switch(this->Index)
+	{
+		case 2:
+			NVIC_SetPriority(TIM3_IRQn, 0);
+			break;
+		case 5:
+			break;
+		case 6:
+			NVIC_SetPriority(TIM7_IRQn, 0);
+			break;
+		default:
+			break;
+	}
 	#endif
 }
 
@@ -266,40 +332,7 @@ void TTime::DelayUs(int nus) const
 #if defined STM32F0
 extern int gTicks; //
 void timTickrefesh(void *param);
-void TTime::Init()
-{
-    // 初始化为输出
-        this->Index = 6;
-	
-	gTicks = (Sys.Clock >> 3) / 0xF4240u;
-	
-    timerTick = new Timer((TIMER)this->Index);
-    abc.Bind(timTickrefesh);
-    timerTick->Register(abc);
-    timerTick->Open();
-    //timerTick->SetFrequency(1000);
-    //        timer2->Config();
 
-    //初始化延迟函数
-    //SYSTICK的时钟固定为HCLK时钟的1/8
-    //SYSCLK:系统时钟
-    SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
-    SysTick_Config(9000); //配置SysTick tick is 9ms	9000
-    NVIC_SetPriority(SysTick_IRQn, 0);
-	switch(this->Index)
-	{
-		case 2:
-			NVIC_SetPriority(TIM3_IRQn, 0);
-			break;
-		case 5:
-			break;
-		case 6:
-			//NVIC_SetPriority(TIM7_IRQn, 0);
-			break;
-		default:
-			break;
-	}
-}
 #elif defined STM32F1
 	
 #elif defined STM32F4
@@ -307,40 +340,5 @@ extern Timer *timerTick;
 extern Delegate < Timer & > abc;
 extern int gTicks; //
 void timTickrefesh(void *param);
-void TTime::Init()
-{
-    // 初始化为输出
-        this->Index = 2;
-	
-	gTicks = (Sys.Clock >> 3) / 0xF4240u;
-	
-    timerTick = new Timer((TIMER)this->Index);
-    abc.Bind(timTickrefesh);
-    timerTick->Register(abc);
-    timerTick->Open();
-    //timerTick->SetFrequency(1000);
-    //        timer2->Config();
 
-    //初始化延迟函数
-    //SYSTICK的时钟固定为HCLK时钟的1/8
-    //SYSCLK:系统时钟
-//        uint32_t SYSCLK = 168;
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);  
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
-	SysTick_Config(9000); //配置SysTick tick is 9ms	9000
-    NVIC_SetPriority(SysTick_IRQn, 0);
-	switch(this->Index)
-	{
-		case 2:
-			NVIC_SetPriority(TIM3_IRQn, 0);
-			break;
-		case 5:
-			break;
-		case 6:
-			NVIC_SetPriority(TIM7_IRQn, 0);
-			break;
-		default:
-			break;
-	}
-}
 #endif
