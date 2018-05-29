203cbfd1ed00e7ac46a84a05a3a6cc8767018541
Ace <258613763@qq.com>
2018-05-29 21:45:40 +0800

    增加代码类

---
+++ STDOS/Device/DevicesHelper.cpp
@@ -0,0 +1,526 @@
+#include "DeviceConfigHelper.h"
+#include "Platform\stm32.h"
+
+extern "C"
+{
+	void SysTick_Handler(void)
+	{
+
+	}
+
+	void USART1_IRQHandler(void)
+	{
+#if USECOM1
+		volatile uint8_t ch;
+		if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+		{
+			ch = USART_ReceiveData(USART1);
+			Rxx1.Enqueue(ch);
+		}
+		if (USART_GetITStatus(USART1, USART_IT_IDLE) == SET)
+		{
+			//数据帧接收完毕
+			ch = USART_ReceiveData(USART1); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR)
+			if (DeviceConfigHelper::PRcvCOM1)
+			{
+				(*DeviceConfigHelper::PRcvCOM1)();
+			}
+		}
+		/* 处理发送缓冲区空中断 */
+		if (USART_GetITStatus(USART1, USART_IT_TXE) != RESET)
+		{
+			if (Txx1.Empty())
+			{
+				/* 发送缓冲区的数据已取完时， 禁止发送缓冲区空中断 （注意：此时最后1个数据还未真正发送完毕）*/
+				USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+				/* 使能数据发送完毕中断 */
+				USART_ITConfig(USART1, USART_IT_TC, ENABLE);
+			}
+			else
+			{
+				/* 从发送FIFO取1个字节写入串口发送数据寄存器 */
+				USART_SendData(USART1, Txx1.Dequeue());
+			}
+
+		}
+		/* 数据bit位全部发送完毕的中断 */
+		else if (USART_GetITStatus(USART1, USART_IT_TC) != RESET)
+		{
+			if (Txx1.Empty())
+			{
+				/* 如果发送FIFO的数据全部发送完毕，禁止数据发送完毕中断 */
+				USART_ITConfig(USART1, USART_IT_TC, DISABLE);
+
+				/* 回调函数, 一般用来处理RS485通信，将RS485芯片设置为接收模式，避免抢占总线 */
+				Txx1.Clear();
+				if (DeviceConfigHelper::pCOM1Rx485)
+				{
+					*DeviceConfigHelper::pCOM1Rx485 = 0;
+				}
+			}
+			else
+			{
+				/* 正常情况下，不会进入此分支 */
+				/* 如果发送FIFO的数据还未完毕，则从发送FIFO取1个数据写入发送数据寄存器 */
+				USART_SendData(USART1, Txx1.Dequeue());
+			}
+		}
+#endif
+	}
+	void USART2_IRQHandler(void)
+	{
+#if USECOM2
+		volatile uint8_t ch;
+		if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+		{
+			ch = USART_ReceiveData(USART2);
+			Rxx2.Enqueue(ch);
+		}
+		if (USART_GetITStatus(USART2, USART_IT_IDLE) == SET)
+		{
+			//数据帧接收完毕
+			ch = USART_ReceiveData(USART2); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR)    
+			if (DeviceConfigHelper::PRcvCOM2)
+			{
+				(*DeviceConfigHelper::PRcvCOM2)();
+			}
+		}
+		/* 处理发送缓冲区空中断 */
+		if (USART_GetITStatus(USART2, USART_IT_TXE) != RESET)
+		{
+			if (Txx2.Empty())
+			{
+				/* 发送缓冲区的数据已取完时， 禁止发送缓冲区空中断 （注意：此时最后1个数据还未真正发送完毕）*/
+				USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
+				/* 使能数据发送完毕中断 */
+				USART_ITConfig(USART2, USART_IT_TC, ENABLE);
+			}
+			else
+			{
+				/* 从发送FIFO取1个字节写入串口发送数据寄存器 */
+				USART_SendData(USART2, Txx2.Dequeue());
+			}
+
+		}
+		/* 数据bit位全部发送完毕的中断 */
+		else if (USART_GetITStatus(USART2, USART_IT_TC) != RESET)
+		{
+			if (Txx2.Empty())
+			{
+				/* 如果发送FIFO的数据全部发送完毕，禁止数据发送完毕中断 */
+				USART_ITConfig(USART2, USART_IT_TC, DISABLE);
+
+				/* 回调函数, 一般用来处理RS485通信，将RS485芯片设置为接收模式，避免抢占总线 */
+				Txx2.Clear();
+				if (DeviceConfigHelper::pCOM2Rx485)
+				{
+					*DeviceConfigHelper::pCOM2Rx485 = 0;
+				}
+			}
+			else
+			{
+				/* 正常情况下，不会进入此分支 */
+				/* 如果发送FIFO的数据还未完毕，则从发送FIFO取1个数据写入发送数据寄存器 */
+				USART_SendData(USART2, Txx2.Dequeue());
+			}
+		}
+#endif
+	}
+	void USART3_IRQHandler(void)
+	{
+#if USECOM3
+		volatile uint8_t ch;
+		if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+		{
+			ch = USART_ReceiveData(USART3);
+			Rxx3.Enqueue(ch);
+		}
+		if (USART_GetITStatus(USART3, USART_IT_IDLE) == SET)
+		{
+			//数据帧接收完毕
+			ch = USART_ReceiveData(USART3); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR) 
+			if (DeviceConfigHelper::PRcvCOM3)
+			{
+				(*DeviceConfigHelper::PRcvCOM3)();
+			}
+		}
+		/* 处理发送缓冲区空中断 */
+		if (USART_GetITStatus(USART3, USART_IT_TXE) != RESET)
+		{
+			if (Txx3.Empty())
+			{
+				/* 发送缓冲区的数据已取完时， 禁止发送缓冲区空中断 （注意：此时最后1个数据还未真正发送完毕）*/
+				USART_ITConfig(USART3, USART_IT_TXE, DISABLE);
+				/* 使能数据发送完毕中断 */
+				USART_ITConfig(USART3, USART_IT_TC, ENABLE);
+			}
+			else
+			{
+				/* 从发送FIFO取1个字节写入串口发送数据寄存器 */
+				USART_SendData(USART3, Txx3.Dequeue());
+			}
+
+		}
+		/* 数据bit位全部发送完毕的中断 */
+		else if (USART_GetITStatus(USART3, USART_IT_TC) != RESET)
+		{
+			if (Txx3.Empty())
+			{
+				/* 如果发送FIFO的数据全部发送完毕，禁止数据发送完毕中断 */
+				USART_ITConfig(USART3, USART_IT_TC, DISABLE);
+
+				/* 回调函数, 一般用来处理RS485通信，将RS485芯片设置为接收模式，避免抢占总线 */
+				Txx3.Clear();
+				if (DeviceConfigHelper::pCOM3Rx485)
+				{
+					*DeviceConfigHelper::pCOM3Rx485 = 0;
+				}
+			}
+			else
+			{
+				/* 正常情况下，不会进入此分支 */
+				/* 如果发送FIFO的数据还未完毕，则从发送FIFO取1个数据写入发送数据寄存器 */
+				USART_SendData(USART3, Txx3.Dequeue());
+			}
+		}
+#endif
+	}
+	void UART4_IRQHandler(void)
+	{
+#if USECOM4
+		volatile uint8_t ch;
+		if (USART_GetITStatus(UART4, USART_IT_RXNE) != RESET)
+		{
+			ch = USART_ReceiveData(UART4);
+			Rxx3.Enqueue(ch);
+		}
+		if (USART_GetITStatus(UART4, USART_IT_IDLE) == SET)
+		{
+			//数据帧接收完毕
+			ch = USART_ReceiveData(UART4); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR) 
+			if (DeviceConfigHelper::PRcvCOM4)
+			{
+				(*DeviceConfigHelper::PRcvCOM4)();
+			}
+		}
+		/* 处理发送缓冲区空中断 */
+		if (USART_GetITStatus(UART4, USART_IT_TXE) != RESET)
+		{
+			if (Txx4.Empty())
+			{
+				/* 发送缓冲区的数据已取完时， 禁止发送缓冲区空中断 （注意：此时最后1个数据还未真正发送完毕）*/
+				USART_ITConfig(UART4, USART_IT_TXE, DISABLE);
+				/* 使能数据发送完毕中断 */
+				USART_ITConfig(UART4, USART_IT_TC, ENABLE);
+			}
+			else
+			{
+				/* 从发送FIFO取1个字节写入串口发送数据寄存器 */
+				USART_SendData(UART4, Txx4.Dequeue());
+			}
+
+		}
+		/* 数据bit位全部发送完毕的中断 */
+		else if (USART_GetITStatus(UART4, USART_IT_TC) != RESET)
+		{
+			if (Txx4.Empty())
+			{
+				/* 如果发送FIFO的数据全部发送完毕，禁止数据发送完毕中断 */
+				USART_ITConfig(UART4, USART_IT_TC, DISABLE);
+
+				/* 回调函数, 一般用来处理RS485通信，将RS485芯片设置为接收模式，避免抢占总线 */
+				Txx4.Clear();
+				if (DeviceConfigHelper::pCOM4Rx485)
+				{
+					*DeviceConfigHelper::pCOM4Rx485 = 0;
+				}
+			}
+			else
+			{
+				/* 正常情况下，不会进入此分支 */
+				/* 如果发送FIFO的数据还未完毕，则从发送FIFO取1个数据写入发送数据寄存器 */
+				USART_SendData(UART4, Txx4.Dequeue());
+			}
+		}
+#endif
+	}
+	void UART5_IRQHandler(void)
+	{
+#if USECOM5
+		volatile uint8_t ch;
+		if (USART_GetITStatus(UART5, USART_IT_RXNE) != RESET)
+		{
+			ch = USART_ReceiveData(UART5);
+			Rxx5.Enqueue(ch);
+		}
+		if (USART_GetITStatus(UART5, USART_IT_IDLE) == SET)
+		{
+			//数据帧接收完毕
+			ch = USART_ReceiveData(UART5); //由软件序列清除中断标志位(先读USART_SR，然后读USART_DR)  
+			if (DeviceConfigHelper::PRcvCOM5)
+			{
+				(*DeviceConfigHelper::PRcvCOM5)();
+			}
+		}
+		/* 处理发送缓冲区空中断 */
+		if (USART_GetITStatus(UART5, USART_IT_TXE) != RESET)
+		{
+			if (Txx5.Empty())
+			{
+				/* 发送缓冲区的数据已取完时， 禁止发送缓冲区空中断 （注意：此时最后1个数据还未真正发送完毕）*/
+				USART_ITConfig(UART5, USART_IT_TXE, DISABLE);
+				/* 使能数据发送完毕中断 */
+				USART_ITConfig(UART5, USART_IT_TC, ENABLE);
+			}
+			else
+			{
+				/* 从发送FIFO取1个字节写入串口发送数据寄存器 */
+				USART_SendData(UART5, Txx3.Dequeue());
+			}
+
+		}
+		/* 数据bit位全部发送完毕的中断 */
+		else if (USART_GetITStatus(UART5, USART_IT_TC) != RESET)
+		{
+			if (Txx5.Empty())
+			{
+				/* 如果发送FIFO的数据全部发送完毕，禁止数据发送完毕中断 */
+				USART_ITConfig(UART5, USART_IT_TC, DISABLE);
+
+				/* 回调函数, 一般用来处理RS485通信，将RS485芯片设置为接收模式，避免抢占总线 */
+				Txx5.Clear();
+				if (DeviceConfigHelper::pCOM5Rx485)
+				{
+					*DeviceConfigHelper::pCOM5Rx485 = 0;
+				}
+			}
+			else
+			{
+				/* 正常情况下，不会进入此分支 */
+				/* 如果发送FIFO的数据还未完毕，则从发送FIFO取1个数据写入发送数据寄存器 */
+				USART_SendData(UART5, Txx5.Dequeue());
+			}
+		}
+#endif
+	}
+
+	void TIM2_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM2, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim2Update)
+			{
+				(*DeviceConfigHelper::PTim2Update)();
+			}
+		}
+	}
+	void TIM3_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM3, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim3Update)
+			{
+				(*DeviceConfigHelper::PTim3Update)();
+			}
+		}
+	}
+	void TIM4_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM4, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim4Update)
+			{
+				(*DeviceConfigHelper::PTim4Update)();
+			}
+		}
+	}
+	void TIM5_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM5, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim5Update)
+			{
+				(*DeviceConfigHelper::PTim5Update)();
+			}
+		}
+	}
+
+	void TIM6_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM6, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM6, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim6Update)
+			{
+				(*DeviceConfigHelper::PTim6Update)();
+			}
+		}
+	}
+
+	void TIM7_IRQHandler(void)
+	{
+		if (TIM_GetITStatus(TIM7, TIM_IT_Update) != RESET)
+		{
+			TIM_ClearITPendingBit(TIM7, TIM_FLAG_Update);
+			if (DeviceConfigHelper::PTim7Update)
+			{
+				(*DeviceConfigHelper::PTim7Update)();
+			}
+		}
+	}
+	void EXTI0_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line0) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line0);
+			if (DeviceConfigHelper::PExit0)
+			{
+				(*DeviceConfigHelper::PExit0)();
+			}
+		}
+	}
+	void EXTI1_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line1) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line1);
+			if (DeviceConfigHelper::PExit1)
+			{
+				(*DeviceConfigHelper::PExit1)();
+			}
+		}
+	}
+
+	void EXTI2_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line2) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line2);
+			if (DeviceConfigHelper::PExit2)
+			{
+				(*DeviceConfigHelper::PExit2)();
+			}
+		}
+	}
+	void EXTI3_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line3) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line3);
+			if (DeviceConfigHelper::PExit3)
+			{
+				(*DeviceConfigHelper::PExit3)();
+			}
+		}
+	}
+	void EXTI4_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line4) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line4);
+			if (DeviceConfigHelper::PExit4)
+			{
+				(*DeviceConfigHelper::PExit4)();
+			}
+		}
+	}
+
+	void EXTI9_5_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line5) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line5);
+			if (DeviceConfigHelper::PExit5)
+			{
+				(*DeviceConfigHelper::PExit5)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line6) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line6);
+			if (DeviceConfigHelper::PExit6)
+			{
+				(*DeviceConfigHelper::PExit6)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line7) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line7);
+			if (DeviceConfigHelper::PExit7)
+			{
+				(*DeviceConfigHelper::PExit7)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line8) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line8);
+			if (DeviceConfigHelper::PExit8)
+			{
+				(*DeviceConfigHelper::PExit8)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line9) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line9);
+			if (DeviceConfigHelper::PExit9)
+			{
+				(*DeviceConfigHelper::PExit9)();
+			}
+		}
+	}
+	void EXTI15_10_IRQHandler(void)
+	{
+		if (EXTI_GetITStatus(EXTI_Line10) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line10);
+			if (DeviceConfigHelper::PExit10)
+			{
+				(*DeviceConfigHelper::PExit10)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line11) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line11);
+			if (DeviceConfigHelper::PExit11)
+			{
+				(*DeviceConfigHelper::PExit11)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line12) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line12);
+			if (DeviceConfigHelper::PExit12)
+			{
+				(*DeviceConfigHelper::PExit12)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line13) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line13);
+			if (DeviceConfigHelper::PExit13)
+			{
+				(*DeviceConfigHelper::PExit13)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line14) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line14);
+			if (DeviceConfigHelper::PExit14)
+			{
+				(*DeviceConfigHelper::PExit14)();
+			}
+		}
+		if (EXTI_GetITStatus(EXTI_Line15) != RESET)
+		{
+			EXTI_ClearITPendingBit(EXTI_Line15);
+			if (DeviceConfigHelper::PExit15)
+			{
+				(*DeviceConfigHelper::PExit15)();
+			}
+		}
+	}
+}
