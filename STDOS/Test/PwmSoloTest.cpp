6b0a52bc66b90765f46e9ffb8a6febb82486ec6c
Ace <258613763@qq.com>
2018-06-01 16:02:08 +0800

    代码整理

--- STDOS/Test/PwmSoloTest.cpp
+++ STDOS/Test/PwmSoloTest.cpp
@@ -42,17 +42,17 @@ static void TIMx_GPIO_Config(void)
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	/*GPIOB Configuration: TIM3 channel 2 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	///*GPIOB Configuration: TIM3 channel 2 as alternate function push-pull */
+	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	//GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	/*GPIOB Configuration: TIM3 channel 3 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	///*GPIOB Configuration: TIM3 channel 3 as alternate function push-pull */
+	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	//GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-	/*GPIOB Configuration: TIM3 channel 4 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	///*GPIOB Configuration: TIM3 channel 4 as alternate function push-pull */
+	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	//GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 }
 
@@ -100,24 +100,24 @@ static void TIMx_Mode_Config(void)
 	TIM_OC1Init(TIM3, &TIM_OCInitStructure);	 //使能通道1
 	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	/* PWM1 Mode configuration: Channel2 */
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR2_Val;	  //设置通道2的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC2Init(TIM3, &TIM_OCInitStructure);	  //使能通道2
-	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	///* PWM1 Mode configuration: Channel2 */
+	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	//TIM_OCInitStructure.TIM_Pulse = CCR2_Val;	  //设置通道2的电平跳变值，输出另外一个占空比的PWM
+	//TIM_OC2Init(TIM3, &TIM_OCInitStructure);	  //使能通道2
+	//TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	/* PWM1 Mode configuration: Channel3 */
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR3_Val;	//设置通道3的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC3Init(TIM3, &TIM_OCInitStructure);	 //使能通道3
-	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	///* PWM1 Mode configuration: Channel3 */
+	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	//TIM_OCInitStructure.TIM_Pulse = CCR3_Val;	//设置通道3的电平跳变值，输出另外一个占空比的PWM
+	//TIM_OC3Init(TIM3, &TIM_OCInitStructure);	 //使能通道3
+	//TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	/* PWM1 Mode configuration: Channel4 */
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;	//设置通道4的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC4Init(TIM3, &TIM_OCInitStructure);	//使能通道4
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-	TIM_ARRPreloadConfig(TIM3, ENABLE);			 // 使能TIM3重载寄存器ARR
+	///* PWM1 Mode configuration: Channel4 */
+	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	//TIM_OCInitStructure.TIM_Pulse = CCR4_Val;	//设置通道4的电平跳变值，输出另外一个占空比的PWM
+	//TIM_OC4Init(TIM3, &TIM_OCInitStructure);	//使能通道4
+	//TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	//TIM_ARRPreloadConfig(TIM3, ENABLE);			 // 使能TIM3重载寄存器ARR
 
 													 /* TIM3 enable counter */
 	TIM_Cmd(TIM3, ENABLE);                   //使能定时器3	
