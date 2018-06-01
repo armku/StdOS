4d67d3e0187efd6ffab1f8b94b3059f38d6b6de1
Ace <258613763@qq.com>
2018-06-01 16:19:38 +0800

    代码整理

--- STDOS/Test/PwmSoloTest.cpp
+++ STDOS/Test/PwmSoloTest.cpp
@@ -10,7 +10,7 @@ void PwmTestRoutin123(void *param)
 	static int i = 0;
 	if (i++ > 100)
 		i = 0;
-	pwm.SetOutPercent(i);
+	pwm.SetOutPercent(50);
 }
 
 void PwmSoloTestInit()
@@ -33,26 +33,13 @@ static void TIMx_GPIO_Config(void)
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	/* GPIOA and GPIOB clock enable */
-	RCC_APB2PeriphClockCmd(( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB ), ENABLE);
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA , ENABLE);
 
 	/*GPIOA Configuration: TIM3 channel 1 as alternate function push-pull */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		    // 复用推挽输出
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	///*GPIOB Configuration: TIM3 channel 2 as alternate function push-pull */
-	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-	//GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	///*GPIOB Configuration: TIM3 channel 3 as alternate function push-pull */
-	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	//GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	///*GPIOB Configuration: TIM3 channel 4 as alternate function push-pull */
-	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-	//GPIO_Init(GPIOB, &GPIO_InitStructure);
-
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	
 }
 
 /**
@@ -73,16 +60,6 @@ static void TIMx_Mode_Config(void)
 
 	/* PWM信号电平跳变值 */
 	u16 CCR1_Val = 500;
-	u16 CCR2_Val = 375;
-	u16 CCR3_Val = 250;
-	u16 CCR4_Val = 125;
-
-	/* -----------------------------------------------------------------------
-	macTIMx Channel1 duty cycle = (macTIMx_CCR1/ macTIMx_ARR+1)* 100% = 50%
-	macTIMx Channel2 duty cycle = (macTIMx_CCR2/ macTIMx_ARR+1)* 100% = 37.5%
-	macTIMx Channel3 duty cycle = (macTIMx_CCR3/ macTIMx_ARR+1)* 100% = 25%
-	macTIMx Channel4 duty cycle = (macTIMx_CCR4/ macTIMx_ARR+1)* 100% = 12.5%
-	----------------------------------------------------------------------- */
 
 	/* Time base configuration */
 	TIM_TimeBaseStructure.TIM_Period = 999;       //当定时器从0计数到999，即为1000次，为一个定时周期
@@ -98,27 +75,7 @@ static void TIMx_Mode_Config(void)
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;  //当定时器计数值小于CCR1_Val时为高电平
 	TIM_OC1Init(TIM3, &TIM_OCInitStructure);	 //使能通道1
 	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-	///* PWM1 Mode configuration: Channel2 */
-	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	//TIM_OCInitStructure.TIM_Pulse = CCR2_Val;	  //设置通道2的电平跳变值，输出另外一个占空比的PWM
-	//TIM_OC2Init(TIM3, &TIM_OCInitStructure);	  //使能通道2
-	//TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-	///* PWM1 Mode configuration: Channel3 */
-	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	//TIM_OCInitStructure.TIM_Pulse = CCR3_Val;	//设置通道3的电平跳变值，输出另外一个占空比的PWM
-	//TIM_OC3Init(TIM3, &TIM_OCInitStructure);	 //使能通道3
-	//TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-	///* PWM1 Mode configuration: Channel4 */
-	//TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	//TIM_OCInitStructure.TIM_Pulse = CCR4_Val;	//设置通道4的电平跳变值，输出另外一个占空比的PWM
-	//TIM_OC4Init(TIM3, &TIM_OCInitStructure);	//使能通道4
-	//TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-	//TIM_ARRPreloadConfig(TIM3, ENABLE);			 // 使能TIM3重载寄存器ARR
-
-													 /* TIM3 enable counter */
+															 /* TIM3 enable counter */
 	TIM_Cmd(TIM3, ENABLE);                   //使能定时器3	
 }
 
