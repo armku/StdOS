5fd6f53034e22d8b57ec3bed89aaf062a256d5ec
Ace <258613763@qq.com>
2018-06-01 15:58:51 +0800

    代码优化

--- STDOS/Test/PwmSoloTest.cpp
+++ STDOS/Test/PwmSoloTest.cpp
@@ -22,24 +22,6 @@ void PwmSoloTestInit()
 }
 
 #include "stm32f10x.h"
-
-
-/********************TIM参数定义，只限TIM2、3、4或5**************************/
-#define             macTIMx                                TIM3
-#define             macTIM_APBxClock_FUN                   RCC_APB1PeriphClockCmd
-#define             macTIM_CLK                             RCC_APB1Periph_TIM3
-#define             macTIM_GPIO_APBxClock_FUN              RCC_APB2PeriphClockCmd
-#define             macTIM_GPIO_CLK                        ( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB )
-#define             macTIM_CH1_PORT                        GPIOA
-#define             macTIM_CH1_PIN                         GPIO_Pin_6
-#define             macTIM_CH2_PORT                        GPIOA
-#define             macTIM_CH2_PIN                         GPIO_Pin_7
-#define             macTIM_CH3_PORT                        GPIOB
-#define             macTIM_CH3_PIN                         GPIO_Pin_0
-#define             macTIM_CH4_PORT                        GPIOB
-#define             macTIM_CH4_PIN                         GPIO_Pin_1
-
-
 /************************** TIM 函数声明********************************/
 void               TIMx_PWM_Init(void);
 /**
@@ -53,28 +35,28 @@ static void TIMx_GPIO_Config(void)
 
 	/* 设置TIM3CLK 为 72MHZ */
 	//  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); 
-	macTIM_APBxClock_FUN(macTIM_CLK, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	/* GPIOA and GPIOB clock enable */
-	macTIM_GPIO_APBxClock_FUN(macTIM_GPIO_CLK, ENABLE);
+	RCC_APB2PeriphClockCmd(( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB ), ENABLE);
 
 	/*GPIOA Configuration: TIM3 channel 1 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = macTIM_CH1_PIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		    // 复用推挽输出
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(macTIM_CH1_PORT, &GPIO_InitStructure);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	/*GPIOB Configuration: TIM3 channel 2 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = macTIM_CH2_PIN;
-	GPIO_Init(macTIM_CH2_PORT, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	/*GPIOB Configuration: TIM3 channel 3 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = macTIM_CH3_PIN;
-	GPIO_Init(macTIM_CH3_PORT, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	/*GPIOB Configuration: TIM3 channel 4 as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = macTIM_CH4_PIN;
-	GPIO_Init(macTIM_CH4_PORT, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 }
 
@@ -112,37 +94,37 @@ static void TIMx_Mode_Config(void)
 	TIM_TimeBaseStructure.TIM_Prescaler = 0;	    //设置预分频：不预分频，即为72MHz
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;	//设置时钟分频系数：不分频(这里用不到)
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //向上计数模式
-	TIM_TimeBaseInit(macTIMx, &TIM_TimeBaseStructure);
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
 	/* PWM1 Mode configuration: Channel1 */
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;	    //配置为PWM模式1
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR1_Val;	   //设置跳变值，当计数器计数到这个值时，电平发生跳变
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;  //当定时器计数值小于CCR1_Val时为高电平
-	TIM_OC1Init(macTIMx, &TIM_OCInitStructure);	 //使能通道1
-	TIM_OC1PreloadConfig(macTIMx, TIM_OCPreload_Enable);
+	TIM_OC1Init(TIM3, &TIM_OCInitStructure);	 //使能通道1
+	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	/* PWM1 Mode configuration: Channel2 */
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR2_Val;	  //设置通道2的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC2Init(macTIMx, &TIM_OCInitStructure);	  //使能通道2
-	TIM_OC2PreloadConfig(macTIMx, TIM_OCPreload_Enable);
+	TIM_OC2Init(TIM3, &TIM_OCInitStructure);	  //使能通道2
+	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	/* PWM1 Mode configuration: Channel3 */
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR3_Val;	//设置通道3的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC3Init(macTIMx, &TIM_OCInitStructure);	 //使能通道3
-	TIM_OC3PreloadConfig(macTIMx, TIM_OCPreload_Enable);
+	TIM_OC3Init(TIM3, &TIM_OCInitStructure);	 //使能通道3
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	/* PWM1 Mode configuration: Channel4 */
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;	//设置通道4的电平跳变值，输出另外一个占空比的PWM
-	TIM_OC4Init(macTIMx, &TIM_OCInitStructure);	//使能通道4
-	TIM_OC4PreloadConfig(macTIMx, TIM_OCPreload_Enable);
-	TIM_ARRPreloadConfig(macTIMx, ENABLE);			 // 使能TIM3重载寄存器ARR
+	TIM_OC4Init(TIM3, &TIM_OCInitStructure);	//使能通道4
+	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_ARRPreloadConfig(TIM3, ENABLE);			 // 使能TIM3重载寄存器ARR
 
 													 /* TIM3 enable counter */
-	TIM_Cmd(macTIMx, ENABLE);                   //使能定时器3	
+	TIM_Cmd(TIM3, ENABLE);                   //使能定时器3	
 }
 
 /**
