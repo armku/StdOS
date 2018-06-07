42d370bcb77ee5f39c4e62e71305459ef16ac1d7
Ace <258613763@qq.com>
2018-06-07 08:32:20 +0800

    串口优化

--- STDOS/Device/DeviceConfigHelper.cpp
+++ STDOS/Device/DeviceConfigHelper.cpp
@@ -469,21 +469,10 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 #if COM1RCVIDLEINTFLAG
 	USART_ITConfig(USART1, USART_IT_IDLE, ENABLE); //使能串口总线空闲中断 
 #endif
-
-
+	
 	USART_Cmd(USART1, ENABLE);
 	USART_ClearFlag(USART1, USART_FLAG_TC);
-
-	Txx1.SetBuf(com1tx, ArrayLength(com1tx));
-	Rxx1.SetBuf(com1rx, ArrayLength(com1rx));
-
-	SerialPort_GetPins(&Pins[0], &Pins[1], COM1);
-	Ports[0] = new AlternatePort();
-	Ports[1] = new InputPort();
-	Ports[0]->Set(Pins[0]);
-	Ports[1]->Set(Pins[1]);
-	Ports[0]->Open();
-	Ports[1]->Open();
+		
 #elif defined STM32F4
 	//GPIO端口设置
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -492,12 +481,10 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //使能GPIOA时钟
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);//使能USART1时钟
-
-														  //串口1对应引脚复用映射
+															  //串口1对应引脚复用映射
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1); //GPIOA9复用为USART1
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1); //GPIOA10复用为USART1
-
-															   //USART1端口配置
+																   //USART1端口配置
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
@@ -519,13 +506,17 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 	//USART_ClearFlag(USART1, USART_FLAG_TC);
 
 	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启相关中断
-
-												  //Usart1 NVIC 配置
+													  //Usart1 NVIC 配置
 	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;//串口1中断通道
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;//抢占优先级3
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
 	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器、
+		
+#endif
+
+	Txx1.SetBuf(com1tx, ArrayLength(com1tx));
+	Rxx1.SetBuf(com1rx, ArrayLength(com1rx));
 
 	SerialPort_GetPins(&Pins[0], &Pins[1], COM1);
 	Ports[0] = new AlternatePort();
@@ -534,7 +525,7 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 	Ports[1]->Set(Pins[1]);
 	Ports[0]->Open();
 	Ports[1]->Open();
-#endif
+
 #if  COM1RCVIDLEINTFLAG
 #else
 	Sys.AddTask(Com1RcvRoutin, 0, 100, 1, "RcvCom1");
