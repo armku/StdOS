6eada122cf624f63f8378187fdf2675ac3aafdce
Ace <258613763@qq.com>
2018-06-08 15:56:20 +0800

    F4串口代码优化

--- STDOS/Device/DeviceConfigHelper.cpp
+++ STDOS/Device/DeviceConfigHelper.cpp
@@ -427,7 +427,7 @@ void DeviceConfigCenter::com5send()
 
 #endif
 }
-
+#define EN_USART1_RX 1
 void DeviceConfigCenter::configCOM1(int baudRate)
 {
 	Port*		Ports[2];	// Tx/Rx
@@ -474,23 +474,25 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 	USART_ClearFlag(USART1, USART_FLAG_TC);
 		
 #elif defined STM32F4
-	//GPIO端口设置
+							//GPIO端口设置
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //使能GPIOA时钟
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);//使能USART1时钟
-															  //串口1对应引脚复用映射
+
+														  //串口1对应引脚复用映射
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1); //GPIOA9复用为USART1
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1); //GPIOA10复用为USART1
-																   //USART1端口配置
-	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
-	//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
-	//GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
-	//GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽复用输出
-	//GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
-	//GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化PA9，PA10
+
+															   //USART1端口配置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽复用输出
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
+	GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化PA9，PA10
 
 										   //USART1 初始化设置
 	USART_InitStructure.USART_BaudRate = baudRate;//波特率设置
@@ -503,15 +505,19 @@ void DeviceConfigCenter::configCOM1(int baudRate)
 
 	USART_Cmd(USART1, ENABLE);  //使能串口1 
 
-	//USART_ClearFlag(USART1, USART_FLAG_TC);
+								//USART_ClearFlag(USART1, USART_FLAG_TC);
 
+#if EN_USART1_RX	
 	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启相关中断
-													  //Usart1 NVIC 配置
+
+												  //Usart1 NVIC 配置
 	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;//串口1中断通道
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;//抢占优先级3
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
 	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器、
+
+#endif
 		
 #endif
 
