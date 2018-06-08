12d6d537c5b283ef16af42ffc01d5bd282e76160
Ace <258613763@qq.com>
2018-06-08 15:31:15 +0800

    F4定时器代码优化

--- STDOS/Device/DeviceConfigHelper.cpp
+++ STDOS/Device/DeviceConfigHelper.cpp
@@ -1044,32 +1044,7 @@ void DeviceConfigCenter::TimeTickInit()//系统用定时器初始化
 	NVIC_SetPriority(SysTick_IRQn, 0);
 	NVIC_SetPriority(TIM2_IRQn, 0);
 #elif defined STM32F4
-	//arr：自动重装值。
-	//psc：时钟预分频数
-	//定时器溢出时间计算方法 : Tout = ((arr + 1)*(psc + 1)) / Ft us.
-	//Ft=定时器工作频率,单位:Mhz
-	u16 arr = 10000 - 1;
-	u16 psc = 8400 - 1;
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);  ///使能TIM时钟
-
-	TIM_TimeBaseInitStructure.TIM_Period = arr; 	//自动重装载值
-	TIM_TimeBaseInitStructure.TIM_Prescaler = psc;  //定时器分频
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; //向上计数模式
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);//初始化TIM
-
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); //允许定时器更新中断
-	TIM_Cmd(TIM2, ENABLE); //使能定时器
-
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn; //定时器中断
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01; //抢占优先级1
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03; //子优先级3
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
+	Timer2Config(10000);
 #endif
 }
 
