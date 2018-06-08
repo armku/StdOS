49c6a729a942e4fb66686da5a280bd2425fa3acb
Ace <258613763@qq.com>
2018-06-08 15:02:40 +0800

    F4代码优化

--- User/BspPlatform/BspPlatform.cpp
+++ User/BspPlatform/BspPlatform.cpp
@@ -28,26 +28,7 @@ void BspPlatformInit()
 //这里使用的是定时器3!
 void TIM2_Int_Init(u16 arr, u16 psc)
 {
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);  ///使能TIM时钟
-
-	TIM_TimeBaseInitStructure.TIM_Period = arr; 	//自动重装载值
-	TIM_TimeBaseInitStructure.TIM_Prescaler = psc;  //定时器分频
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; //向上计数模式
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);//初始化TIM
-
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); //允许定时器更新中断
-	TIM_Cmd(TIM2, ENABLE); //使能定时器
-
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn; //定时器中断
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01; //抢占优先级1
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03; //子优先级3
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
+	
 
 }
 int DeviceConfigCenter::CurrentTick()
