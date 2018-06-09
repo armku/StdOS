54aca62d2eab48b9537da6784072f88233c8a3c4
Ace <258613763@qq.com>
2018-06-09 11:23:50 +0800

    增加部分代码

---
+++ STDOS/Drivers/ESP8266.h
@@ -0,0 +1,16 @@
+#ifndef _ESP8266_H
+#include "Sys.h"
+
+class ESP8266
+{
+public:
+	ESP8266();
+	~ESP8266();
+
+private:
+
+};
+
+
+
+#endif // !_ESP8266_H
---
+++ STDOS/Test/ESP8266Test.cpp
@@ -0,0 +1,11 @@
+#include "Drivers\ESP8266.h"
+
+#define _ESP8266_TEST_CPP
+
+#ifdef _ESP8266_TEST_CPP
+
+void Esp8266TestInit()
+{
+
+}
+#endif
