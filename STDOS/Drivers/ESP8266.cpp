bf0744f7644ed55b98e18af0d825e6cdd6d5fba3
Ace <258613763@qq.com>
2018-06-17 10:55:14 +0800

    代码整理

--- STDOS/Drivers/ESP8266.cpp
+++ STDOS/Drivers/ESP8266.cpp
@@ -21,3 +21,30 @@ void ESP8266::Init()
 	DeviceConfigCenter::PRcvCOM3 = com3rcv;
 	DeviceConfigCenter::ConfigCom(COM3, 115200);
 }
+void ESP8266::SetPin(Pin prst, Pin pch)
+{
+	this->pRst.Set(prst);
+	this->pRst.Invert = false;
+	this->pRst.OpenDrain = false;
+
+	this->pCH.Set(pch);
+	this->pCH.Invert = false;
+	this->pCH.OpenDrain = false;
+
+	this->pRst.Open();
+	this->pCH.Open();
+
+	this->pRst = 1;
+	this->pCH = 0;
+}
+void ESP8266::Rst()
+{
+#if 0
+	this->Cmd("AT+RST", "OK", "ready", 2500);
+
+#else 
+	this->pRst = 0;
+	Sys.Sleep(500);
+	this->pRst = 1;
+#endif 
+}
--- STDOS/Drivers/ESP8266.h
+++ STDOS/Drivers/ESP8266.h
@@ -1,14 +1,18 @@
 #ifndef _ESP8266_H
 #include "Sys.h"
+#include "Port.h"
 
 class ESP8266
 {
 public:
 	ESP8266();
 	void Init();
+	void SetPin(Pin prst, Pin pch);
+	void Rst();
 
 private:
-
+	OutputPort pRst;
+	OutputPort pCH;
 };
 
 
