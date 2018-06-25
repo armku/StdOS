5cf268c50ec4e00361d3ef944ce51a72094aabb5
Ace <258613763@qq.com>
2018-06-25 22:56:56 +0800

    代码整理

--- STDOS/Handlers/IPipeline.cpp
+++ STDOS/Handlers/IPipeline.cpp
@@ -5,7 +5,15 @@
 /// <returns></returns>
 void IPipeline::AddFirst(IHandler *handler)
 {
-
+	if (Head == NULL)
+	{
+		handler->Next = NULL;
+		handler->Prev = NULL;
+		Head = handler;
+		Tail = handler;
+	}
+	else
+		AddBefore(Head, handler);
 }
 
 /// <summary>添加处理器到末尾</summary>
@@ -13,7 +21,15 @@ void IPipeline::AddFirst(IHandler *handler)
 /// <returns></returns>
 void IPipeline::AddLast(IHandler *handler)
 {
-
+	if (Tail == NULL)
+	{
+		handler->Next = NULL;
+		handler->Prev = NULL;
+		Head = handler;
+		Tail = handler;
+	}
+	else
+		AddAfter(Tail, handler);
 }
 
 /// <summary>添加处理器到指定名称之前</summary>
@@ -22,7 +38,12 @@ void IPipeline::AddLast(IHandler *handler)
 /// <returns></returns>
 void IPipeline::AddBefore(IHandler *baseHandler, IHandler *handler)
 {
+	handler->Next = baseHandler;
+	handler->Prev = baseHandler->Prev;
+	if (baseHandler->Prev != NULL) baseHandler->Prev->Next = handler;
+	baseHandler->Prev = handler;
 
+	if (baseHandler == Head) Head = handler;
 }
 
 /// <summary>添加处理器到指定名称之后</summary>
@@ -31,7 +52,12 @@ void IPipeline::AddBefore(IHandler *baseHandler, IHandler *handler)
 /// <returns></returns>
 void IPipeline::AddAfter(IHandler* baseHandler, IHandler* handler)
 {
+	handler->Next = baseHandler->Next;
+	handler->Prev = baseHandler;
+	if (baseHandler->Next != NULL) baseHandler->Next->Prev = handler;
+	baseHandler->Next = handler;
 
+	if (baseHandler == Tail) Tail = handler;
 }
 
 /// <summary>删除处理器</summary>
@@ -39,13 +65,31 @@ void IPipeline::AddAfter(IHandler* baseHandler, IHandler* handler)
 /// <returns></returns>
 void IPipeline::Remove(IHandler* handler)
 {
+	if (handler->Prev != NULL)
+		handler->Prev->Next = handler->Next;
+	else
+		Head = handler->Next;
 
+	if (handler->Next != NULL)
+		handler->Next->Prev = handler->Prev;
+	else
+		Tail = handler->Prev;
 }
 
 
 void IPipeline::Read(Buffer &bs)
 {
-
+	/*auto rs = Head->Read(bs);
+	if (rs != NULL)
+	{
+		auto next = Head->Next;
+		while (next != NULL)
+		{
+			if (rs != NULL)
+				rs = next->Read(rs);
+			next = next->Next;
+		}
+	}*/
 }
 
 /// <summary>
@@ -54,6 +98,16 @@ void IPipeline::Read(Buffer &bs)
 /// <param name="buf"></param>
 void IPipeline::Write(Buffer& bs)
 {
-
+	/*auto rs = Tail->Write(bs);
+	if (rs != NULL)
+	{
+		auto next = Tail ->Prev;
+		while (next != NULL)
+		{
+			if (rs != NULL)
+				rs = next->Write(rs);
+			next = next->Next;
+		}
+	}*/
 }
 
