b21ee8d6293c9395f635c28dd54430d6328b72d4
Ace <258613763@qq.com>
2018-06-25 22:26:16 +0800

    代码整理

--- STDOS/Handlers/IPipeline.h
+++ STDOS/Handlers/IPipeline.h
@@ -14,29 +14,29 @@ public:
 	/// <summary>添加处理器到开头</summary>
 	/// <param name="handler">处理器</param>
 	/// <returns></returns>
-	void AddFirst(IHandler handler);
+	void AddFirst(IHandler *handler);
 
 	/// <summary>添加处理器到末尾</summary>
 	/// <param name="handler">处理器</param>
 	/// <returns></returns>
-	void AddLast(IHandler handler);
+	void AddLast(IHandler *handler);
 
 	/// <summary>添加处理器到指定名称之前</summary>
 	/// <param name="baseHandler">基准处理器</param>
 	/// <param name="handler">处理器</param>
 	/// <returns></returns>
-	void AddBefore(IHandler baseHandler, IHandler handler);
+	void AddBefore(IHandler *baseHandler, IHandler *handler);
 
 	/// <summary>添加处理器到指定名称之后</summary>
 	/// <param name="baseHandler">基准处理器</param>
 	/// <param name="handler">处理器</param>
 	/// <returns></returns>
-	void AddAfter(IHandler baseHandler, IHandler handler);
+	void AddAfter(IHandler* baseHandler, IHandler* handler);
 
 	/// <summary>删除处理器</summary>
 	/// <param name="handler">处理器</param>
 	/// <returns></returns>
-	void Remove(IHandler handler);
+	void Remove(IHandler* handler);
 
 	
 	void Read(Buffer &bs);
