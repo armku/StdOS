826deebb91d0176628f545ca505e586f5029d7d1
Ace <258613763@qq.com>
2018-06-25 22:08:54 +0800

    代码整理

---
+++ STDOS/Handlers/IHandler.h
@@ -0,0 +1,31 @@
+#ifndef _IPIPELINE_H
+#define _IPIPELINE_H
+#include "Sys.h"
+
+class IHandler
+{
+public:
+	
+	/// <summary>上一个处理器</summary>
+	IHandler *Prev;
+
+		/// <summary>下一个处理器</summary>
+	IHandler *Next;
+		/// <summary>
+		/// 读取数据处理
+		/// </summary>
+		/// <param name="buf"></param>
+		/// <returns></returns>
+	Buffer& Read(Buffer &bs);
+	/// <summary>
+	/// 写入数据处理
+	/// </summary>
+	/// <param name="buf"></param>
+	/// <returns></returns>
+	Buffer& Write(Buffer& bs);
+
+private:
+
+};
+
+#endif // !_IPIPELINE_H
---
+++ STDOS/Handlers/IPipeline.h
@@ -0,0 +1,13 @@
+#ifndef _IPIPELINE_H
+#define _IPIPELINE_H
+
+class IPipeline
+{
+public:
+	
+
+private:
+
+};
+
+#endif // !_IPIPELINE_H
