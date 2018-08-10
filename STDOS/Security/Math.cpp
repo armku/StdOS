4293188fef8825da17575bb597578098b3746817
Ace <258613763@qq.com>
2018-08-10 12:01:13 +0800

    增加部分类

--- STDOS/Security/Math.cpp
+++ STDOS/Security/Math.cpp
@@ -1,17 +1,17 @@
 #include "Math.h"
 
 //计算和
-float Math::Sum(float buf[], int len)
+float Math::Sum(float buf[], int len,int pos)
 {
 	float ret = 0;
-	for (int i = 0; i < len;i++)
+	for (int i = pos; i < len;i++)
 	{
 		ret += buf[len];
 	}
 	return ret;
 }
 //计算平均值
-float Math::Avg(float buf[], int len)
+float Math::Avg(float buf[], int len,int pos)
 {
 	if (len > 0)
 		return Math::Sum(buf, len) / len;
@@ -19,29 +19,49 @@ float Math::Avg(float buf[], int len)
 		return 0;
 }
 //最大值
-float Math::Max(float buf[], int len)
+float Math::Max(float buf[], int len,int pos)
 {
 	if (len <= 0)
-		return buf[0];
+		return buf[pos];
 
-	float max = buf[0];
-	for (int i = 0; i < len; i++)
+	auto max = buf[pos];
+	for (int i = pos; i < len; i++)
 	{
-		max = MAX(max, buf[i]);
+		max = MAX(max, buf[pos+i]);
 	}
 	return max;
 	
 }
 //最小值
-float Math::Min(float buf[], int len)
+float Math::Min(float buf[], int len,int pos)
 {
 	if (len <= 0)
-		return buf[0];
+		return buf[pos];
 
-	float min = buf[0];
-	for (int i = 0; i < len; i++)
+	auto min = buf[pos];
+	for (int i = pos; i < len; i++)
 	{
-		min = MIN(min, buf[i]);
+		min = MIN(min, buf[pos+i]);
 	}
 	return min;
 }
+//排序，升序
+void Math::SortUp(float buf[],int len,int pos)
+{
+	int maxpos=pos;
+	for(int i=pos;i<pos+len;i++)
+	{
+		if(buf[maxpos]<buf[i])
+			maxpos=i;
+	}
+}
+//排序 降序
+void Math::SortDown(float buf[],int len,int pos)
+{
+	
+}
+
+
+
+
+
--- STDOS/Security/Math.h
+++ STDOS/Security/Math.h
@@ -8,10 +8,12 @@
 class Math
 {
 public:
-	static float Sum(float buf[], int len);//计算和
-	static float Avg(float buf[], int len);//计算平均值
-	static float Max(float buf[], int len);//最大值
-	static float Min(float buf[], int len);//最小值
+	static float Sum(float buf[], int len,int pos=0);//计算和
+	static float Avg(float buf[], int len,int pos=0);//计算平均值
+	static float Max(float buf[], int len,int pos=0);//最大值
+	static float Min(float buf[], int len,int pos=0);//最小值
+	static void SortUp(float buf[],int len,int pos=0);//排序，升序
+	static void SortDown(float buf[],int len,int pos=0);//排序 降序
 	
 private:
 
