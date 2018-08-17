14bd077dbf8c484a574835799a87daadb2891fff
Ace <258613763@qq.com>
2018-08-17 15:21:38 +0800

    代码整理 RingBuffer完成

--- STDOS/Core/RingBuffer.cpp
+++ STDOS/Core/RingBuffer.cpp
@@ -1,2 +1,48 @@
 #include "RingBuffer.h"
 
+void RingBuffer::Init(void* buffer, int bufsize)
+{
+	this->buf = (uint8_t*)buffer;
+	this->length = bufsize;
+	this->head = 0;
+	this->tail = 0;
+	this->fillcnt = 0;
+}
+int RingBuffer::Put(char data)
+{
+	if (this->fillcnt >= this->length)
+	{
+		return;
+	}
+	else {}
+	this->buf[this->head] = data;
+	this->head++;
+	this->fillcnt++;
+	this->head %= this->length;
+	return true;
+}
+int RingBuffer::Put(char *c, int len)
+{
+	for (int i = 0; i < len; i++)
+		this->Put(c[i]);
+}
+int RingBuffer::Get(char *c, int len)
+{
+	if (this->fillcnt <= 0)
+	{
+		return false;
+	}
+	else {}
+	if (len > this->length)
+	{
+		len = this->length;
+	}
+	for (int i = 0; i < len; i++)
+	{
+		this->fillcnt--;
+		*c = this->buf[this->tail++];
+		c++;
+		this->tail %= this->length;
+	}
+	return true;
+}
--- STDOS/Core/RingBuffer.h
+++ STDOS/Core/RingBuffer.h
@@ -1,4 +1,24 @@
 #ifndef _RING_BUFFER_H
 #define _RING_BUFFER_H
 
+#include "Type.h"
+
+class RingBuffer
+{
+public:
+	void Init(void* buffer, int bufsize);
+	int Put(char data);
+	int Put(char *c, int len);
+	int Get(char *c, int len);
+
+	uint8_t * buf;
+	int length;
+	int head;
+	int tail;
+	int fillcnt;
+
+private:
+
+};
+
 #endif // !_RING_BUFFER_H
