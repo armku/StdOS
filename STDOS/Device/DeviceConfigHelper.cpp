9e12330e377bd5abcb75fbe08d07366d967c5634
Ace <258613763@qq.com>
2018-09-02 23:36:25 +0800

    整理

--- STDOS/Device/DeviceConfigHelper.cpp
+++ STDOS/Device/DeviceConfigHelper.cpp
@@ -247,6 +247,59 @@ void DeviceConfigCenter::com1send(Buffer& bs)
 	{
 		SendBuff[i] = bs[i];
 	}
+	//DMA发送
+	DMA_InitTypeDef DMA_InitStructure;
+
+	/*开启DMA时钟*/
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+	/*设置DMA源：串口数据寄存器地址*/
+	//		DMA_InitStructure.DMA_PeripheralBaseAddr = USART1_DR_Base;	  
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)(&(USART1->DR));
+
+	/*内存地址(要传输的变量的指针)*/
+	DMA_InitStructure.DMA_MemoryBaseAddr = (u32)SendBuff;
+
+	/*方向：从内存到外设*/
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+
+	/*传输大小DMA_BufferSize=SENDBUFF_SIZE*/
+	DMA_InitStructure.DMA_BufferSize = bs.Length();
+
+	/*外设地址不增*/
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+
+	/*内存地址自增*/
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+
+	/*外设数据单位*/
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+
+	/*内存数据单位 8bit*/
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+
+	/*DMA模式：不断循环*/
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	//DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+
+	/*优先级：中*/
+	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+
+	/*禁止内存到内存的传输	*/
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+
+	/*配置DMA1的4通道*/
+	DMA_Init(DMA1_Channel4, &DMA_InitStructure);
+
+	/*使能DMA*/
+	DMA_Cmd(DMA1_Channel4, ENABLE);
+	//DMA_ITConfig(DMA1_Channel4,DMA_IT_TC,ENABLE);  //配置DMA发送完成后产生中断
+
+
+
+
+
+
 	/* USART1 向 DMA发出TX请求 */
 	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
 #elif COM1SENDINTFLAG
