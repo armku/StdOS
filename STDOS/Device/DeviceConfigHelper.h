40969382c0f6a9665ce4acb4333a754226cce269
Ace <258613763@qq.com>
2018-09-08 20:39:30 +0800

    整理

--- STDOS/Device/DeviceConfigHelper.h
+++ STDOS/Device/DeviceConfigHelper.h
@@ -21,6 +21,10 @@
 #define COM2TXDMAFLAG 1 //串口2DMA发送
 #define COM3TXDMAFLAG 1 //串口3DMA发送
 
+#define COM1RXDMAFLAG 1 //串口1DMA接收
+#define COM2RXDMAFLAG 1 //串口2DMA接收
+#define COM3RXDMAFLAG 1 //串口3DMA接收
+
 #define COM1RCVIDLEINTFLAG	1//串口1空闲中断接收
 #define COM2RCVIDLEINTFLAG	1//串口2空闲中断接收
 #define COM3RCVIDLEINTFLAG	1//串口3空闲中断接收
