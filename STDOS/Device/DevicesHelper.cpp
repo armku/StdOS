02ce7e6b33112cc44e8039d0cb59f422f1c1b35b
Ace <258613763@qq.com>
2018-09-08 22:01:46 +0800

    代码整理

--- STDOS/Device/DevicesHelper.cpp
+++ STDOS/Device/DevicesHelper.cpp
@@ -49,12 +49,11 @@ extern "C"
 #if USECOM1
 
 #if COM1RXDMAFLAG
-		int Usart1_Rec_Cnt;
-		int DMA_Rec_Len;
+		int Usart1_Rec_Cnt;		
 		if (USART_GetITStatus(USART1, USART_IT_IDLE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
 		{
 			USART_ReceiveData(USART1);//读取数据 注意：这句必须要，否则不能够清除中断标志位。我也不知道为啥！
-			Usart1_Rec_Cnt = DMA_Rec_Len - DMA_GetCurrDataCounter(DMA1_Channel5);	//算出接本帧数据长度
+			Usart1_Rec_Cnt = 256 - DMA_GetCurrDataCounter(DMA1_Channel5);	//算出接本帧数据长度
 
 			//***********帧数据处理函数************//
 			debug_printf("The lenght:%d\r\n", Usart1_Rec_Cnt);
