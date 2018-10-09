27a8a6409ad26a7be47b03820e472f40cd9b7a09
Ace <258613763@qq.com>
2018-10-09 15:15:39 +0800

    增加部分测试代码

--- Libraries/App/ModbusLink.h
+++ Libraries/App/ModbusLink.h
@@ -1,6 +1,23 @@
 #include "DataFrameModbus.h"
+#include "stm32f10x.h"
+#include "FIFOBuffer.h"
+#include "USART.h"
 
 #ifndef _MODBUSLINK_H
 #define _MODBUSLINK_H
+
+class ModbusSlaveLink
+{
+public:
+	DataFrame txFrame;
+	DataFrame rxFrame;
+	USART &com;
+public:
+	ModbusSlaveLink(USART &uart);
+	bool CheckFrame();
+	bool Send();
+private:
+
+};
 
 #endif // !_MODBUSLINK_H
