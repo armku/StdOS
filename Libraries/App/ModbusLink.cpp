4b0da5ae3d823d7bc4f49783d6603978d5aa4f19
Ace <258613763@qq.com>
2018-10-10 13:50:25 +0800

    删除无用内容

--- Libraries/App/ModbusLink.cpp
+++ Libraries/App/ModbusLink.cpp
@@ -37,19 +37,7 @@ bool ModbusSlaveLink::CheckFrame()
 bool ModbusSlaveLink::Send()
 {
 	if (!txFrame.isUpdated) //no new frame data, no need to send
-		return false;
-	//if (!com.SendByte(txFrame.header))  //send frame header
-	//	return false;
-	//if (txFrame.fnCode > MAX_FN_CODE || !com.SendByte(txFrame.fnCode))  //send function code
-	//	return false;
-	//txFrame.dataLength = DATA_LENGTH[txFrame.fnCode][DIRECTION_SEND];
-	//if (!com.SendByte(txFrame.dataLength))  //send data length
-	//	return false;
-	//if (!com.SendBytes(txFrame.data, txFrame.dataLength)) //send data;
-	//	return false;
-	//txFrame.CreateCheckCode();
-	//if (!com.SendByte(txFrame.checkSum))    //send check code
-	//	return false;
+		return false;	
 	txFrame.data[0] = txFrame.devid;
 	txFrame.data[1] = txFrame.fnCode;
 	auto crc = Crc::CRC16RTU(txFrame.data, txFrame.frameLength - 2);
