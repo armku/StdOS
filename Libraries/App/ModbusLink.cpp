7314349c92b16b3a80ae1f8ba0ae42f9c3963bf8
Ace <258613763@qq.com>
2018-10-10 10:55:55 +0800

    代码整理

--- Libraries/App/ModbusLink.cpp
+++ Libraries/App/ModbusLink.cpp
@@ -15,48 +15,48 @@ bool ModbusSlaveLink::CheckFrame()
 		rxFrame.dataLength += rxlen;		
 	}
 
-	if (rxFrame.dataLength >= 8)
-	{	
-		this->rxFrame.devid = this->rxFrame.data[0];
-		this->rxFrame.fnCode = this->rxFrame.data[1];
-		this->rxFrame.regAddr = this->rxFrame.data[2];
-		this->rxFrame.regAddr <<= 8;
-		this->rxFrame.regAddr |= this->rxFrame.data[3];
-		this->rxFrame.regLength = this->rxFrame.data[4];
-		this->rxFrame.regLength <<= 8;
-		this->rxFrame.regLength |= this->rxFrame.data[5];
+	return rxFrame.CheckFrame();
+}
+bool DataFrameModbus::CheckFrame()
+{
+	if (dataLength >= 8)
+	{
+		this->devid = this->data[0];
+		this->fnCode = this->data[1];
+		this->regAddr = this->data[2];
+		this->regAddr <<= 8;
+		this->regAddr |= this->data[3];
+		this->regLength = this->data[4];
+		this->regLength <<= 8;
+		this->regLength |= this->data[5];
 
-		debug_printf("devid:%d fnCode:%d regAddr:%d reglen:%d\n",this->rxFrame.devid,this->rxFrame.fnCode,this->rxFrame.regAddr,this->rxFrame.regLength);
+		debug_printf("devid:%d fnCode:%d regAddr:%d reglen:%d\n", this->devid, this->fnCode, this->regAddr, this->regLength);
 		int needlen = 8;
-		switch (this->rxFrame.fnCode)
+		switch (this->fnCode)
 		{
 		case 0x0F://多个写 0 区输出继电器(指令代码: 0X0F) 
 		case 0x10://多个写 4 区输出寄存器(指令代码: 0X10) 
-			needlen = rxFrame.data[6] + needlen;
+			needlen = data[6] + needlen;
 			break;
 		default:
 			break;
 		}
 		//需要的长度不够，直接返回
-		if (needlen > this->rxFrame.dataLength)
+		if (needlen > this->dataLength)
 			return false;
 
-		auto crc11 = Crc::CRC16RTU(rxFrame.data, rxFrame.dataLength - 2);
+		auto crc11 = Crc::CRC16RTU(data, dataLength - 2);
 		debug_printf("crc cal:%04X\n", crc11);
 
 		//com.SendBytes(buf485, len);
 		debug_printf("rcb one frame\n");
-		Buffer bf(rxFrame.data, txFrame.dataLength);
+		Buffer bf(data, dataLength);
 		bf.ShowHex(true);
-		debug_printf("datalen:%d crc:%04X \n",rxFrame.dataLength,rxFrame.checkSum);
-		return rxFrame.VerifyCheckCode();
+		debug_printf("datalen:%d crc:%04X \n", dataLength, checkSum);
+		return VerifyCheckCode();
 	}
 	//return com.CheckFrame(rxFrame);
 	return false;
-}
-bool DataFrameModbus::CheckFrame()
-{
-
 }
 
 bool ModbusSlaveLink::Send()
