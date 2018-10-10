eeeb74c76e998f91a95fa73798e67b0b77f2d354
Ace <258613763@qq.com>
2018-10-10 08:55:11 +0800

    增加部分测试代码

--- Libraries/Test/ModbusLinkTest.cpp
+++ Libraries/Test/ModbusLinkTest.cpp
@@ -1,14 +1,26 @@
 #include "App/ModbusLink.h"
 #include "Sys.h"
+#include "BspPlatform/Interrupt.h"
 
 
 #define _MODBUSLINKEST_CPP
 #ifdef _MODBUSLINKEST_CPP
 
+USART usart222(USART2, 115200);
+uint8_t buf485[20];
+
 void ModbusSlaveLinkRoutin(void* param)
 {
 	static int i = 0;
 	debug_printf("hello:%d\n", i++);
+	if (pCOM2->RxSize() > 0)
+	{
+		int len = pCOM2->RxSize();
+		pCOM2->GetBytes(buf485, len);
+		//pCOM2->ClearRxBuf();
+		pCOM2->SendBytes(buf485, len);
+		debug_printf("rcb one frame\n");
+	}
 }
 
 void ModbusSlaveLinkTestInit()
