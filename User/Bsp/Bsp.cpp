8d1a8a8afed7892ff17412e3043772f59eb3526b
Ace <258613763@qq.com>
2018-10-25 11:29:11 +0800

    代码整理

--- User/Bsp/Bsp.cpp
+++ User/Bsp/Bsp.cpp
@@ -69,54 +69,14 @@ void LedTask(void *param)
 	//led2 = keyup;
 #endif 
 }
-
-uint8_t chbuf[256];
-
-void com1rcv()
-{
-//	int len= Rxx1.Read(chbuf, ArrayLength(chbuf));
-//	Rxx1.Clear();
-
-//	debug_printf("COM1RCV:\n");
-//	Buffer(chbuf,len).ShowHex(true);
-}
-void comtestrcv()
-{
-//	int len= Rxx3.Read(chbuf, ArrayLength(chbuf));
-//	
-//	debug_printf("COM3RCV:\n");
-//	Buffer(chbuf,len).ShowHex(true);
-}
 #include "OnChip/USART.h"
 #include "BspPlatform/Interrupt.h"
 
 USART usart111(USART1, 115200);
-void routsendtest(void * param)
-{
-	uint8_t buf[200];
-	static int icnt = 0;
-	(*pCOM1) << icnt++ << "hello world\n";
-	(*pCOM1) <<"Rxd:"<< pCOM1->RxSize();
-	if (pCOM1->RxSize() > 0)
-	{
-		int len = pCOM1->RxSize();
-		pCOM1->GetBytes(buf, len);
-		//pCOM1->ClearRxBuf();
-		pCOM1->SendBytes(buf, len);
-	}
-}
-void PwmSoloTestInit();
-void DemoLinkTestInit();
-void ModbusSlaveLinkTestInit();
 
 void BspInit()
 {
 	led1 = 1;
 	led2 = 1;
 	Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
-		
-	//Sys.AddTask(routsendtest, 0, 0, 1000, "routsendtest");
-	//PwmSoloTestInit();	
-	//DemoLinkTestInit();
-	ModbusSlaveLinkTestInit();
 }
