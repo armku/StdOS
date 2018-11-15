dd2e397eac75ff7409d1526dd03282a5109f1476
Ace <258613763@qq.com>
2018-11-15 09:59:29 +0800

    稳定版本0.7.2018.1115

--- Libraries/Kernel/Sys.h
+++ Libraries/Kernel/Sys.h
@@ -82,7 +82,7 @@ public:
 
 extern TSys Sys;		// 创建一个全局的Sys对象  会在main函数之前执行构造函数（！！！！！）
 
-#define STDOS_VERSION "0.7.2018.1114" //系统版本号
+#define STDOS_VERSION "0.7.2018.1115" //系统版本号
 extern char* AppVersion;//需要定义软件版本号实现 如：char *AppVersion = "0.1.2018.1114";
 
 #endif //_Sys_H_
