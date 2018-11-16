6b74ce13ae86e628e249e5f2edef9bd48405d4ca
Ace <258613763@qq.com>
2018-11-16 21:20:07 +0800

    增加部分测试代码

---
+++ Libraries/DriversCom/EspDemoLink.cpp
@@ -0,0 +1,33 @@
+#include "EspDemoLink.h"
+
+
+EspDemoLink::EspDemoLink(USART &uart) :com(uart)
+{
+
+}
+
+bool EspDemoLink::CheckFrame()
+{
+
+	return com.CheckFrame(rxFrame);
+}
+
+bool EspDemoLink::Send()
+{
+	if (!txFrame.isUpdated) //no new frame data, no need to send
+		return false;
+	if (!com.SendByte(txFrame.header))  //send frame header
+		return false;
+	if (txFrame.fnCode > MAX_FN_CODE || !com.SendByte(txFrame.fnCode))  //send function code
+		return false;
+	txFrame.dataLength = DATA_LENGTH[txFrame.fnCode][DIRECTION_SEND];
+	if (!com.SendByte(txFrame.dataLength))  //send data length
+		return false;
+	if (!com.SendBytes(txFrame.data, txFrame.dataLength)) //send data;
+		return false;
+	txFrame.CreateCheckCode();
+	if (!com.SendByte(txFrame.checkSum))    //send check code
+		return false;
+	txFrame.isUpdated = false;
+	return true;
+}
---
+++ Libraries/DriversCom/EspDemoLink.h
@@ -0,0 +1,23 @@
+#ifndef _ESPDEMOLINK_H
+#define _ESPDEMOLINK_H
+
+#include "stm32f10x.h"
+#include "FIFOBuffer.h"
+#include "USART.h"
+
+class EspDemoLink
+{
+public:
+	DataFrame txFrame;
+	DataFrame rxFrame;
+	USART &com;
+public:
+	EspDemoLink(USART &uart);
+	bool CheckFrame();
+	bool Send();
+private:
+
+};
+
+
+#endif // !_ESPDEMOLINK_H
