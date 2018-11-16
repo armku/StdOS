15f2ae39a39ea883170462e6f788fda5a968129b
Ace <258613763@qq.com>
2018-11-16 22:53:49 +0800

    整理

--- Libraries/DriversCom/DataFrameEspDemoLink.h
+++ Libraries/DriversCom/DataFrameEspDemoLink.h
@@ -9,14 +9,9 @@
 struct DataFrameEspDemoLink
 {
 public:
-	uint8_t devid;
-	uint8_t fnCode;
 	uint8_t dataLength;
-	uint16_t regAddr;
-	uint16_t regLength;
 	uint8_t data[MAX_FRAMEMODBUS_DATA_LENGTH];
 	bool isUpdated;
-	uint16_t checkSum;
 	int frameLength;//当前数据帧长度
 	int Cnt;//数据帧数量
 
@@ -25,27 +20,6 @@ public:
 	{
 		isUpdated = false;
 		Cnt = 0;
-	}
-
-	bool VerifyCheckCode()
-	{
-		auto crcnew = Crc::CRC16RTU(data, frameLength-2);
-		if (crcnew == this->checkSum)
-			return true;
-		else
-			return false;
-	}
-	//设置传输寄存器值，地址从0开始编码
-	void SetReg(uint8_t regaddr, uint16_t val)
-	{
-		this->data[3 + regaddr * 2] = val >> 8;
-		this->data[3 + regaddr * 2 + 1] = val & 0xff;
-	}
-	void SetRegLen(uint8_t len)
-	{
-		this->data[2] = len * 2;
-		this->frameLength = 5 + len * 2;
-		this->dataLength = this->frameLength;
 	}
 	bool RemoveOneFrame()
 	{
@@ -59,53 +33,27 @@ public:
 		this->frameLength = 0;
 		return true;
 	}
+	//查找AT指令
+	int SearchATEnd(int len)
+	{
+		for (int i = 0; i < len-1; i++)
+		{
+			if (data[i] == '\r'&&data[i + 1] == '\n')
+				return i;
+		}
+		return -1;
+	}
 	bool CheckFrame()
 	{
 		if (dataLength >= 2)
 		{
-			this->devid = this->data[0];
-			this->fnCode = this->data[1];
-			this->regAddr = this->data[2];
-			this->regAddr <<= 8;
-			this->regAddr |= this->data[3];
-			this->regLength = this->data[4];
-			this->regLength <<= 8;
-			this->regLength |= this->data[5];
-
-			this->frameLength = 8;
-			switch (this->fnCode)
-			{
-			case 0X02:
-			case 0X01:
-			case 0X05:
-			case 0X04:
-			case 0X03:
-			case 0X06:
-				frameLength = 8;
-				break;
-			case 0x0F://多个写 0 区输出继电器(指令代码: 0X0F) 
-			case 0x10://多个写 4 区输出寄存器(指令代码: 0X10) 
-				frameLength = data[6] + frameLength;
-				break;
-			default:
-				frameLength = 0;
-				break;
-			}
-			if (frameLength == 0)
-			{
-				//非法指令，直接清空缓冲区
-				frameLength = this->dataLength;
-				this->RemoveOneFrame();
-				return false;
-			}
-			//需要的长度不够，直接返回
-			if (frameLength > this->dataLength)
-				return false;
-			this->checkSum = this->data[this->frameLength - 1];
-			this->checkSum <<= 8;
-			this->checkSum |= this->data[this->frameLength - 2];
-
-			if (this->VerifyCheckCode())
+			auto end = this->SearchATEnd(dataLength);
+			if (end >= 0)
+				this->frameLength = end + 1;
+			else
+				return false;			
+			
+			/*if (this->VerifyCheckCode())
 			{
 				return true;
 			}
@@ -113,25 +61,9 @@ public:
 			{
 				this->RemoveOneFrame();
 				return false;
-			}
+			}*/
 		}
 		return false;
-	}
-	void CreateCheckCode()
-	{
-		checkSum = fnCode + frameLength;
-		for (uint8_t i = 0; i < frameLength; i++)
-			checkSum += data[i];
-	}
-	DataFrameEspDemoLink& operator=(const DataFrameEspDemoLink &df)
-	{
-		fnCode = df.fnCode;
-		dataLength = df.dataLength;
-		isUpdated = df.isUpdated;
-		checkSum = df.checkSum;
-		for (uint8_t i = 0; i < dataLength; i++)
-			data[i] = df.data[i];
-		return *this;
 	}
 };
 
--- Libraries/DriversCom/EspDemoLink.cpp
+++ Libraries/DriversCom/EspDemoLink.cpp
@@ -72,16 +72,16 @@ bool EspDemoLink::Send()
 		return false;
 	//if (!com.SendByte(txFrame.header))  //send frame header
 	//	return false;
-	if (txFrame.fnCode > MAX_FN_CODE || !com.SendByte(txFrame.fnCode))  //send function code
-		return false;
-	txFrame.dataLength = DATA_LENGTH[txFrame.fnCode][DIRECTION_SEND];
-	if (!com.SendByte(txFrame.dataLength))  //send data length
-		return false;
-	if (!com.SendBytes(txFrame.data, txFrame.dataLength)) //send data;
-		return false;
-	txFrame.CreateCheckCode();
-	if (!com.SendByte(txFrame.checkSum))    //send check code
-		return false;
+	//if (txFrame.fnCode > MAX_FN_CODE || !com.SendByte(txFrame.fnCode))  //send function code
+	//	return false;
+	//txFrame.dataLength = DATA_LENGTH[txFrame.fnCode][DIRECTION_SEND];
+	//if (!com.SendByte(txFrame.dataLength))  //send data length
+	//	return false;
+	//if (!com.SendBytes(txFrame.data, txFrame.dataLength)) //send data;
+	//	return false;
+	//txFrame.CreateCheckCode();
+	//if (!com.SendByte(txFrame.checkSum))    //send check code
+	//	return false;
 	txFrame.isUpdated = false;
 	return true;
 }
