2a195516fab320b719f5331b2a6da88577181a9b
Ace <258613763@qq.com>
2018-12-02 20:10:22 +0800

    优化LED指示灯使用方法

--- User/Bsp/Bsp.cpp
+++ User/Bsp/Bsp.cpp
@@ -5,70 +5,12 @@
 
 char *AppVersion = "0.1.2018.1115"; //版本号
 
-#ifdef STM32F0
-OutputPort led1(PC6, true);
-OutputPort led2(PC7, true);
-OutputPort led3(PC8, true);
-OutputPort led4(PC9, true);
-
-OutputPort ledss[] =
-{
-	led1, led2, led3, led4
-};
-#elif defined STM32F1
-#if 1
-OutputPort led1(PB0, false);
-OutputPort led2(PF7, false);
-OutputPort led3(PF8, false);
-#else 
-OutputPort led1(PD0, false);
-OutputPort led2(PD1, false);
-OutputPort led3(PD2, false);
-OutputPort ledLCD(PD12, false);
-#endif 
-#elif defined STM32F4
-#if 1
-//正点原子开发板F4
-OutputPort led1(PF9, false);
-OutputPort led2(PF10, false);
-#else 
-//安福莱开发板F4-V5
-OutputPort led1(PI10, false);
-OutputPort led2(PF7, false);
-OutputPort led3(PC2, false);
-OutputPort led4(PC2, false);
-#endif 
-#endif
+OutputPort led1;
 
 void LedTask(void *param)
 {
-#ifdef STM32F0
-	static uint8_t flag = 1;
-	auto leds = (OutputPort*)param;
-
-	flag <<= 1;
-	flag &= 0x0f;
-	if (flag == 0)
-	{
-		flag = 1;
-	}
-
-	led1 = !led1;
-	led2 = !led2;
-	led3 = !led3;
-	led4 = !led4;
-#elif defined STM32F1
 	auto leds = (OutputPort*)param;
 	*leds = !* leds;
-	//    led2 = key0;
-#elif defined STM32F4
-	OutputPort *leds = (OutputPort*)param;
-	*leds = !* leds;
-	//led2 = !led2;
-	//    led3 = !led3;
-	//    led4 = !led4;
-	//led2 = keyup;
-#endif 
 }
 
 USART usart111(USART1, 115200);
@@ -91,10 +33,23 @@ void Com1test(void *param)
 void EspDemoLinkTestInit();
 void BspInit()
 {
+	led1.Set(PB0);
+	
+	led1.Open();
+
 	led1 = 1;
-	led2 = 1;
+	
 	Sys.AddTask(LedTask, &led1, 0, 500, "LedTask");
 
 	EspDemoLinkTestInit();
 	usart111.OnReceive = Com1test;
 }
+
+/*
+			LED1	LED2	LED3	LED4
+STM32F0		PC6		PC7		PC8		PC9
+STM32F1		PB0		PF7		PF8
+STM32F4正点 PF9		PF10
+F4安富莱F4-V5 PI10	PF7		PC2		PC2
+
+*/
