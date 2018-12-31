f38b5ebf7bd0ac9548678a3f9a350fa764e22449
Ace <258613763@qq.com>
2018-12-31 18:55:29 +0800

    整理

--- StdOS/DriversCom/ModbusLink/ModbusMaster.cpp
+++ StdOS/DriversCom/ModbusLink/ModbusMaster.cpp
@@ -5,8 +5,13 @@ ModbusMasterLink::ModbusMasterLink(USART &uart) :ModbusBase(uart)
 	
 }
 
-bool ModbusMasterLink::GetValue()
+bool ModbusMasterLink::GetValue(uint8_t id, uint16_t addr, uint16_t len)
 {
+	uint8_t buf[10];
+	this->txFrame.Address = id;
+	this->txFrame.regAddr = addr;
+	this->txFrame.Length = len;
+	
 
 	Sys.Sleep(100);
 }
