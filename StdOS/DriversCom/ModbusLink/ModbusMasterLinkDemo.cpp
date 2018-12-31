94b25fa8517c4d6a6b1af20664c5de03d4e4b49f
Ace <258613763@qq.com>
2018-12-31 17:09:39 +0800

    整理

--- StdOS/DriversCom/ModbusLink/ModbusMasterLinkDemo.cpp
+++ StdOS/DriversCom/ModbusLink/ModbusMasterLinkDemo.cpp
@@ -8,10 +8,15 @@
 #define _MODBUSMASTERLINKEST_CPP
 
 USART usart222(USART2, 9600);
+OutputPort u485dr;
 ModbusMasterLink modMaster(usart222);
 void ModbusMasterLinkTestInit()
 {
-
+	u485dr.Set(PA1);
+	u485dr.OpenDrain = false;
+	u485dr.Invert = false;
+	u485dr.Open();
+	u485dr = 1;
 }
 
 #endif // !_MODBUSMASTERLINKEST_CPP
