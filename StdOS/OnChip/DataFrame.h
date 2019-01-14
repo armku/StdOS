024c51f6a611a1d35ea326972c9ab0e6da2cd2ca
Ace <258613763@qq.com>
2019-01-14 20:18:44 +0800

    删除无用代码

--- StdOS/OnChip/DataFrame.h
+++ StdOS/OnChip/DataFrame.h
@@ -13,7 +13,7 @@ struct DataFrame
 {
 public:
 	const uint8_t header;
-	uint8_t fnCode;
+	/*uint8_t fnCode;*/
 	uint8_t dataLength; //当前数据长度
 	uint8_t data[MAX_FRAME_DATA_LENGTH];//当前数据
 	bool isUpdated;
