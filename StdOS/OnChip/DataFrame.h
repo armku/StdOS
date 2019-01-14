50a6746512dcb1a188d7731c126d4928c81b3e18
Ace <258613763@qq.com>
2019-01-14 20:19:08 +0800

    删除无用内容

--- StdOS/OnChip/DataFrame.h
+++ StdOS/OnChip/DataFrame.h
@@ -13,11 +13,9 @@ struct DataFrame
 {
 public:
 	const uint8_t header;
-	/*uint8_t fnCode;*/
 	uint8_t dataLength; //当前数据长度
 	uint8_t data[MAX_FRAME_DATA_LENGTH];//当前数据
 	bool isUpdated;
-	uint8_t checkSum;
 
 public:
 	DataFrame() :header(FRAME_HEADER)                //constructor
