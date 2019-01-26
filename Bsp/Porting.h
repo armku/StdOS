f58b0a9066b9fc07e81081fe4434084a2e4575ab
Ace <258613763@qq.com>
2019-01-26 11:44:01 +0800

    整理

--- Bsp/Porting.h
+++ Bsp/Porting.h
@@ -1,6 +1,9 @@
 #ifndef _Port_H_
 #define _Port_H_
 
+#ifdef STM32F0
+
+#elif defined STM32F1
 #include "../Bsp/ST/STM32F1/Porting/mcuGpio.h"
 #include "../Bsp/ST/STM32F1/Porting/USART.h"
 #include "../Bsp/ST/STM32F1/Porting/BspPlatform.h"
@@ -8,5 +11,8 @@
 
 #include "../Bsp/ST/STM32F1/Porting/FIFOBuffer.h"
 #include "../Bsp/ST/STM32F1/Porting/Configuration.h"
+#elif defined STM32F4
+
+#endif
 
 #endif //_Port_H_
