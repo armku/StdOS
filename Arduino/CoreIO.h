52c92fe6d616fddef770c17fd0357a73349854d8
Ace <258613763@qq.com>
2019-02-01 15:50:30 +0800

    整理

--- Arduino/CoreIO.h
+++ Arduino/CoreIO.h
@@ -2,11 +2,15 @@
 #define _COREIO_H
 
 #include <stdint.h>
+#include "../component/Type.h"
+#include "../Porting/Pin_STM32F1.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+	void pinMode(Pin pin, uint8_t mode);
+	void digitalWrite(Pin pin,uint8_t value);
+	int8_t digitalRead();
 
 #ifdef __cplusplus
 }
