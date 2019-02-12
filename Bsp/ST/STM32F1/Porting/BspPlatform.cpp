aba1a2189afb053fa38959d861b9bd178498fa8b
Ace <258613763@qq.com>
2019-02-12 18:23:16 +0800

    整理

--- Bsp/ST/STM32F1/Porting/BspPlatform.cpp
+++ Bsp/ST/STM32F1/Porting/BspPlatform.cpp
@@ -61,3 +61,53 @@ int StdPrintf(const char *format, ...)
 #endif
 	return n;
 }
+#ifdef __cplusplus
+extern "C" {
+#endif
+	//////////////////////////////////////////////////////////////////
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+#if 0
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+	struct __FILE
+	{
+		int handle;
+
+	};
+
+	FILE __stdout;
+	//定义_sys_exit()以避免使用半主机模式    
+	_sys_exit(int x)
+	{
+		x = x;
+	}
+	//重定义fputc函数 
+	int fputc(int ch, FILE *f)
+	{
+		while ((USART1->SR & 0X40) == 0);//循环发送,直到发送完毕   
+		USART1->DR = (u8)ch;
+		return ch;
+	}
+#else 
+
+	/*使用microLib的方法*/
+	 
+	int fputc(int ch, FILE *f)
+	{
+		USART_SendData(USART1, (uint8_t) ch);
+
+		while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {}
+
+		return ch;
+	}
+	int GetKey (void)  {
+
+		while (!(USART1->SR & USART_FLAG_RXNE));
+
+		return ((int)(USART1->DR & 0x1FF));
+	}
+	
+#endif
+#ifdef __cplusplus
+}
+#endif
