6194519e9745436373a80008b90320802b9d2eb7
Ace <258613763@qq.com>
2019-02-12 22:58:47 +0800

    整理

--- wiring/wiring.c
+++ wiring/wiring.c
@@ -89,20 +89,6 @@ void delay( uint32_t ms )
     	yield();
 }
 #endif
-#if defined ( __ICCARM__ ) /* IAR Ewarm 5.41+ */
-extern signed int putchar( signed int c ) ;
-/**
- * \brief
- *
- * \param c  Character to output.
- *
- * \return The character that was output.
- */
-extern WEAK signed int putchar( signed int c )
-{
-    return c ;
-}
-#endif /* __ICCARM__ */
 
 #ifdef __cplusplus
 }
