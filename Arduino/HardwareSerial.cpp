053bb1e77fe6c46b0a07e6be44b64890b01b1b0d
Ace <258613763@qq.com>
2019-02-14 16:54:18 +0800

    整理

--- Arduino/HardwareSerial.cpp
+++ Arduino/HardwareSerial.cpp
@@ -36,80 +36,12 @@
 // this is so I can support Attiny series and any other chip without a uart
 #if defined(HAVE_HWSERIAL0) || defined(HAVE_HWSERIAL1) || defined(HAVE_HWSERIAL2) || defined(HAVE_HWSERIAL3)
 
-// SerialEvent functions are weak, so when the user doesn't define them,
-// the linker just sets their address to 0 (which is checked below).
-// The Serialx_available is just a wrapper around Serialx.available(),
-// but we can refer to it weakly so we don't pull in the entire
-// HardwareSerial_T instance if the user doesn't also refer to it.
-//#if defined(HAVE_HWSERIAL0)
-//  void serialEvent() __attribute__((weak));
-//  bool Serial0_available() __attribute__((weak));
-//#endif
-
-//#if defined(HAVE_HWSERIAL1)
-//  void serialEvent1() __attribute__((weak));
-//  bool Serial1_available() __attribute__((weak));
-//#endif
-
-//#if defined(HAVE_HWSERIAL2)
-//  void serialEvent2() __attribute__((weak));
-//  bool Serial2_available() __attribute__((weak));
-//#endif
-
-//#if defined(HAVE_HWSERIAL3)
-//  void serialEvent3() __attribute__((weak));
-//  bool Serial3_available() __attribute__((weak));
-//#endif
-
-//void serialEventRun(void)
-//{
-//#if defined(HAVE_HWSERIAL0)
-//  if (Serial0_available && serialEvent && Serial0_available()) serialEvent();
-//#endif
-//#if defined(HAVE_HWSERIAL1)
-//  if (Serial1_available && serialEvent1 && Serial1_available()) serialEvent1();
-//#endif
-//#if defined(HAVE_HWSERIAL2)
-//  if (Serial2_available && serialEvent2 && Serial2_available()) serialEvent2();
-//#endif
-//#if defined(HAVE_HWSERIAL3)
-//  if (Serial3_available && serialEvent3 && Serial3_available()) serialEvent3();
-//#endif
-//}
-
-// macro to guard critical sections when needed for large TX buffer sizes
-//#if (SERIAL_TX_BUFFER_SIZE>256)
-//#define TX_BUFFER_ATOMIC ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-//#else
-//#define TX_BUFFER_ATOMIC
-//#endif
-
-// Actual interrupt handlers //////////////////////////////////////////////////////////////
-
 void HardwareSerial_T::_tx_udr_empty_irq(void)
 {
   // If interrupts are enabled, there must be more data in the output
   // buffer. Send the next byte
   unsigned char c = _tx_buffer[_tx_buffer_tail];
   _tx_buffer_tail = (_tx_buffer_tail + 1) % SERIAL_TX_BUFFER_SIZE;
-
-//  *_udr = c;
-
-  // clear the TXC bit -- "can be cleared by writing a one to its bit
-  // location". This makes sure flush() won't return until the bytes
-  // actually got written. Other r/w bits are preserved, and zeroes
-  // written to the rest.
-
-//#ifdef MPCM0
-//  *_ucsra = ((*_ucsra) & ((1 << U2X0) | (1 << MPCM0))) | (1 << TXC0);
-//#else
-//  *_ucsra = ((*_ucsra) & ((1 << U2X0) | (1 << TXC0)));
-//#endif
-
-  //if (_tx_buffer_head == _tx_buffer_tail) {
-  //  // Buffer empty, so disable interrupts
-  //  cbi(*_ucsrb, UDRIE0);
-  //}
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
@@ -123,11 +55,6 @@ void HardwareSerial_T::end()
 {
   // wait for transmission of outgoing data
   flush();
-
-//  cbi(*_ucsrb, RXEN0);
-//  cbi(*_ucsrb, TXEN0);
-//  cbi(*_ucsrb, RXCIE0);
-//  cbi(*_ucsrb, UDRIE0);
   
   // clear any received data
   _rx_buffer_head = _rx_buffer_tail;
@@ -164,86 +91,19 @@ int HardwareSerial_T::availableForWrite(void)
   tx_buffer_index_t head;
   tx_buffer_index_t tail;
 
-//  TX_BUFFER_ATOMIC {
-//    head = _tx_buffer_head;
-//    tail = _tx_buffer_tail;
-//  }
   if (head >= tail) return SERIAL_TX_BUFFER_SIZE - 1 - head + tail;
   return tail - head - 1;
 }
 
 void HardwareSerial_T::flush()
 {
-  // If we have never written a byte, no need to flush. This special
-  // case is needed since there is no way to force the TXC (transmit
-  // complete) bit to 1 during initialization
   if (!_written)
     return;
-
-//  while (bit_is_set(*_ucsrb, UDRIE0) || bit_is_clear(*_ucsra, TXC0)) {
-//    if (bit_is_clear(SREG, SREG_I) && bit_is_set(*_ucsrb, UDRIE0))
-//	// Interrupts are globally disabled, but the DR empty
-//	// interrupt should be enabled, so poll the DR empty flag to
-//	// prevent deadlock
-//	if (bit_is_set(*_ucsra, UDRE0))
-//	  _tx_udr_empty_irq();
-//  }
-  // If we get here, nothing is queued anymore (DRIE is disabled) and
-  // the hardware finished tranmission (TXC is set).
 }
 
 size_t HardwareSerial_T::write(uint8_t c)
 {
   _written = true;
-  // If the buffer and the data register is empty, just write the byte
-  // to the data register and be done. This shortcut helps
-  // significantly improve the effective datarate at high (>
-  // 500kbit/s) bitrates, where interrupt overhead becomes a slowdown.
-//  if (_tx_buffer_head == _tx_buffer_tail && bit_is_set(*_ucsra, UDRE0)) {
-//    // If TXC is cleared before writing UDR and the previous byte
-//    // completes before writing to UDR, TXC will be set but a byte
-//    // is still being transmitted causing flush() to return too soon.
-//    // So writing UDR must happen first.
-//    // Writing UDR and clearing TC must be done atomically, otherwise
-//    // interrupts might delay the TXC clear so the byte written to UDR
-//    // is transmitted (setting TXC) before clearing TXC. Then TXC will
-//    // be cleared when no bytes are left, causing flush() to hang
-//    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-//      *_udr = c;
-//#ifdef MPCM0
-//      *_ucsra = ((*_ucsra) & ((1 << U2X0) | (1 << MPCM0))) | (1 << TXC0);
-//#else
-//      *_ucsra = ((*_ucsra) & ((1 << U2X0) | (1 << TXC0)));
-//#endif
-//    }
-//    return 1;
-//  }
-//  tx_buffer_index_t i = (_tx_buffer_head + 1) % SERIAL_TX_BUFFER_SIZE;
-//	
-//  // If the output buffer is full, there's nothing for it other than to 
-//  // wait for the interrupt handler to empty it a bit
-//  while (i == _tx_buffer_tail) {
-//    if (bit_is_clear(SREG, SREG_I)) {
-//      // Interrupts are disabled, so we'll have to poll the data
-//      // register empty flag ourselves. If it is set, pretend an
-//      // interrupt has happened and call the handler to free up
-//      // space for us.
-//      if(bit_is_set(*_ucsra, UDRE0))
-//	_tx_udr_empty_irq();
-//    } else {
-//      // nop, the interrupt handler will free up space for us
-//    }
-//  }
-
-//  _tx_buffer[_tx_buffer_head] = c;
-
-//  // make atomic to prevent execution of ISR between setting the
-//  // head pointer and setting the interrupt flag resulting in buffer
-//  // retransmission
-//  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-//    _tx_buffer_head = i;
-//    sbi(*_ucsrb, UDRIE0);
-//  }
   
   return 1;
 }
