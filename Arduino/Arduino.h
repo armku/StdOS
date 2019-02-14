5d850faa74586b9a392614445bbd77ab48876002
Ace <258613763@qq.com>
2019-02-14 16:47:59 +0800

    整理

--- Arduino/Arduino.h
+++ Arduino/Arduino.h
@@ -85,8 +85,6 @@ extern "C"{
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 
-typedef unsigned int word;
-
 #define bit(b) (1UL << (b))
 
 typedef bool boolean;
