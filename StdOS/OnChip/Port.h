d80cb12c008ef03f9502fcfa033ce49a47248812
Ace <258613763@qq.com>
2019-03-27 22:18:50 +0800

    整理

--- StdOS/OnChip/Port.h
+++ StdOS/OnChip/Port.h
@@ -23,6 +23,8 @@ public:
     Pin		_Pin;		// 引脚  
 	bool	Opened;		// 是否已经打开
 
+	Port();
+	Port& Set(Pin pin);	// 设置引脚
 	void SetPinMode(GPIOMode_T mode);//设置管脚模式
 
 	bool ReadInput() const;// Read/ReadInput 的区别在于，前者读输出后者读输入，在开漏输出的时候有很大区别
@@ -30,9 +32,7 @@ public:
 	void Write(bool value) const;
 	Port& operator=(bool value) { Write(value); return *this; }
 	Port& operator=(Port& port) { Write(port.Read()); return *this; }
-	Port();
-
-    Port& Set(Pin pin);	// 设置引脚
+	
 	bool Empty() const;
 
 	bool Open();
