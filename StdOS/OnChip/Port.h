2f402730c438c943c488eb136de5ea603781b104
Ace <258613763@qq.com>
2019-03-27 22:17:26 +0800

    整理

--- StdOS/OnChip/Port.h
+++ StdOS/OnChip/Port.h
@@ -25,7 +25,6 @@ public:
 
 	void SetPinMode(GPIOMode_T mode);//设置管脚模式
 
-	/*operator bool() const { return Read(); }*/
 	bool ReadInput() const;// Read/ReadInput 的区别在于，前者读输出后者读输入，在开漏输出的时候有很大区别
 	static void Write(Pin pin, bool value);
 	void Write(bool value) const;
