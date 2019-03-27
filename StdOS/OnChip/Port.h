0cb35857dae69f93414143152f84cfe56507de6e
Ace <258613763@qq.com>
2019-03-27 22:16:01 +0800

    整理

--- StdOS/OnChip/Port.h
+++ StdOS/OnChip/Port.h
@@ -32,7 +32,7 @@ public:
 		UP = 0x01,	// 上拉电阻
 		DOWN = 0x02,	// 下拉电阻
 	}PuPd;
-	bool	Floating;	// 是否浮空输入
+	//bool	Floating;	// 是否浮空输入
 	//PuPd	Pull;	// 上拉下拉电阻
 	/*operator bool() const { return Read(); }*/
 	bool ReadInput() const;// Read/ReadInput 的区别在于，前者读输出后者读输入，在开漏输出的时候有很大区别
