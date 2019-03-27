eb5db2c2b946e063355b6bbddead5d55e4e5635c
Ace <258613763@qq.com>
2019-03-27 21:59:59 +0800

    整理

--- StdOS/OnChip/Port.h
+++ StdOS/OnChip/Port.h
@@ -60,8 +60,7 @@ public:
 	operator bool() const { return Read(); }	
 protected:
     // ≈‰÷√π˝≥Ã
-    virtual void OnOpenInputPort(void* param);
-	virtual void OnOpenOutputPort(void* param);
+    virtual void OnOpenOutputPort(void* param);
 	virtual void OpenPinOutputPort(void* param);
 };
 
