12f4f4b39c80cc2491914b5cce56e5d38418f63b
Ace <258613763@qq.com>
2019-03-31 21:25:25 +0800

    整理

--- StdOS/Drivers/SPI/W25QXXX/Example/W25QXXXDemo.cpp
+++ StdOS/Drivers/SPI/W25QXXX/Example/W25QXXXDemo.cpp
@@ -12,31 +12,7 @@ SPI25QXX s25xx(&spi);
     {       
 		spi.SetPin(PA5,PA6,PA7);
 		spi.SetNss(PA4);
-
-		//GPIO_InitTypeDef GPIO_InitStructure;
-		///*!< Configure SPI_FLASH_SPI_CS_PIN pin: SPI_FLASH Card CS pin */
-		//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-		//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-		//GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-		//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-		//GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-		///*!< Configure SPI_FLASH_SPI pins: SCK */
-		//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-		//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-		//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-		//GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-		///*!< Configure SPI_FLASH_SPI pins: MISO */
-		//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-		//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-		//GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-		///*!< Configure SPI_FLASH_SPI pins: MOSI */
-		//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-		//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-		//GPIO_Init(GPIOA, &GPIO_InitStructure);
-		s25xx.Test();
-		
+				
+		s25xx.Test();		
     }
 #endif
