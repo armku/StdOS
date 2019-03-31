8912ebcaa24766b07fee79434ff5e495363fcd3d
Ace <258613763@qq.com>
2019-03-31 21:50:01 +0800

    整理

--- StdOS/OnChip/Spi.cpp
+++ StdOS/OnChip/Spi.cpp
@@ -108,230 +108,6 @@ void Spi::Stop()
 		this->_nss = 1;
 	}
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////SpiSoft////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-SpiSoft::SpiSoft(CPOLTYPE cpol, CPHATYPE cpha, uint32_t speedHz)
-{	
-	//this->delayus=speedHz;
-#ifdef STM32F0
-	this->delayus = 0;
-#elif defined STM32F1
-	this->delayus = 10;
-#elif defined STM32F4
-	this->delayus = 0;
-#endif
-	this->CPOL = cpol;
-	this->CPHA = cpha;
-}
-
-void SpiSoft::SetPin(Pin clk, Pin miso, Pin mosi)
-{
-	this->_clk.pinMode(GPIO_Out_PP);
-	this->_mosi.pinMode(GPIO_Out_PP);//此处设置错误将导致异常
-	this->_miso.pinMode(GPIO_Out_OD);
-
-	this->_clk.SetPin(clk);
-	this->_mosi.SetPin(mosi);
-	this->_miso.SetPin(miso);
-}
-void SpiSoft::SetNss(Pin nss)
-{
-	this->_nss.pinMode(GPIO_Out_PP);
-
-	this->_nss.SetPin(nss);
-}
-
-/*---------------------------------------------------------
-忙状态判断，最长等待时间，200 X 10 ms=2S
----------------------------------------------------------*/
-uint8_t SpiSoft::WaitBusy()
-{
-	uint16_t i;
-	this->Start();
-	i = 0;
-	while (this->_miso.Read() > 0)
-	{
-		delay(10);
-		i++;
-		if (i > 200)
-			return 1;
-	}
-	this->Stop();
-	return 0;
-}
-
-//SPI写字节
-uint8_t SpiSoft::Write(uint8_t data)
-{
-	uint8_t i;
-	uint8_t ret = 0;
-	if (this->CPOL == CPOL_Low)
-	{
-		//时钟极性，空闲时为低
-		if (this->CPHA == CPHA_1Edge)
-		{
-			//时钟相位 在串行同步时钟的第一个跳变沿（上升或下降）数据被采样
-			//测试通过
-			for (i = 0; i < 8; i++)
-			{
-				if (data &(1 << (8 - i - 1)))
-				{
-					this->_mosi = 1;
-				}
-				else
-				{
-					this->_mosi = 0;
-				}
-				delayMicroseconds(this->delayus);
-				this->_clk = 1;
-				delayMicroseconds(this->delayus);
-				this->_clk = 0;
-				ret <<= 1;
-				if (this->_miso.Read())
-				{
-					ret |= 1;
-				}
-			}
-		}
-		else if (this->CPHA == CPHA_2Edge)
-		{
-			//时钟相位 在串行同步时钟的第二个跳变沿（上升或下降）数据被采样
-			//需要检查
-			for (i = 0; i < 8; i++)
-			{
-				this->_clk = 1;
-				delayMicroseconds(this->delayus);
-				if (data &(1 << (8 - i - 1)))
-				{
-					this->_mosi = 1;
-				}
-				else
-				{
-					this->_mosi = 0;
-				}
-				delayMicroseconds(this->delayus);
-				this->_clk = 0;
-				ret <<= 1;
-				if (this->_miso.Read())
-				{
-					ret |= 1;
-				}
-			}
-		}
-	}
-	else if (this->CPOL == CPOL_High)
-	{
-		//时钟极性，空闲时为高
-		if (this->CPHA == CPHA_1Edge)
-		{
-			//时钟相位 在串行同步时钟的第一个跳变沿（上升或下降）数据被采样
-			for (i = 0; i < 8; i++)
-			{
-				if (data &(1 << (8 - i - 1)))
-				{
-					this->_mosi = 1;
-				}
-				else
-				{
-					this->_mosi = 0;
-				}
-				delayMicroseconds(this->delayus);
-				this->_clk = 0;
-				delayMicroseconds(this->delayus);
-				this->_clk = 1;
-				ret <<= 1;
-				if (this->_miso.Read())
-				{
-					ret |= 1;
-				}
-			}
-		}
-		else if (this->CPHA == CPHA_2Edge)
-		{
-			//时钟相位 在串行同步时钟的第二个跳变沿（上升或下降）数据被采样
-			//测试通过			
-			for (i = 0; i < 8; i++)
-			{
-				this->_clk = 0;
-				delayMicroseconds(this->delayus);
-				if (data &(1 << (8 - i - 1)))
-				{
-					this->_mosi = 1;
-				}
-				else
-				{
-					this->_mosi = 0;
-				}
-				delayMicroseconds(this->delayus);
-				this->_clk = 1;
-				delayMicroseconds(this->delayus);
-				ret <<= 1;
-				if (this->_miso.Read())
-				{
-					ret |= 1;
-				}
-			}
-		}
-		else
-		{
-		}
-	}
-	else
-	{
-	}
-	return ret;
-}
-
-void SpiSoft::Open()
-{
-	if (this->CPOL)
-	{
-		this->_clk = 1;
-	}
-	else
-	{
-		this->_clk = 0;
-	}
-	this->Stop();
-	/*this->_clk.Open();
-	this->_miso.Open();
-	this->_mosi.Open();
-	this->_nss.Open();*/
-}
-void SpiSoft::Close()
-{
-	//测试，等待删除
-	for (int i = 0; i < 8; i++)
-	{
-		this->_clk = 0;
-		this->_mosi = 1;
-		delayMicroseconds(this->delayus);
-		this->_clk = 1;
-		this->_mosi = 0;
-	}
-}
-
-// 拉低NSS，开始传输
-void SpiSoft::Start()
-{
-	if (!this->_nss.Empty())
-	{
-		this->_nss = 0;
-	}
-	// 开始新一轮事务操作，错误次数清零
-	//Error = 0;
-}
-
-// 拉高NSS，停止传输
-void SpiSoft::Stop()
-{
-	if (!this->_nss.Empty())
-	{
-		this->_nss = 1;
-	}
-}
 // 基础读写
 uint8_t Spi::Write(uint8_t data)
 {
@@ -877,10 +653,10 @@ void Spi::OnClose()
 	debug_printf("    NSS : ");
 	this->_nss.SetPin(P0);
 
-//	this->Pins[0] = P0;
-//	this->Pins[1] = P0;
-//	this->Pins[2] = P0;
-//	this->Pins[3] = P0;
+	//	this->Pins[0] = P0;
+	//	this->Pins[1] = P0;
+	//	this->Pins[2] = P0;
+	//	this->Pins[3] = P0;
 #elif defined STM32F4
 	this->Stop();
 	switch (this->_index)
@@ -996,4 +772,228 @@ uint16_t Spi::Write16(uint16_t data)
 		return 0;
 	}
 #endif
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////SpiSoft////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+SpiSoft::SpiSoft(CPOLTYPE cpol, CPHATYPE cpha, uint32_t speedHz)
+{	
+	//this->delayus=speedHz;
+#ifdef STM32F0
+	this->delayus = 0;
+#elif defined STM32F1
+	this->delayus = 10;
+#elif defined STM32F4
+	this->delayus = 0;
+#endif
+	this->CPOL = cpol;
+	this->CPHA = cpha;
+}
+
+void SpiSoft::SetPin(Pin clk, Pin miso, Pin mosi)
+{
+	this->_clk.pinMode(GPIO_Out_PP);
+	this->_mosi.pinMode(GPIO_Out_PP);//此处设置错误将导致异常
+	this->_miso.pinMode(GPIO_Out_OD);
+
+	this->_clk.SetPin(clk);
+	this->_mosi.SetPin(mosi);
+	this->_miso.SetPin(miso);
+}
+void SpiSoft::SetNss(Pin nss)
+{
+	this->_nss.pinMode(GPIO_Out_PP);
+
+	this->_nss.SetPin(nss);
+}
+
+/*---------------------------------------------------------
+忙状态判断，最长等待时间，200 X 10 ms=2S
+---------------------------------------------------------*/
+uint8_t SpiSoft::WaitBusy()
+{
+	uint16_t i;
+	this->Start();
+	i = 0;
+	while (this->_miso.Read() > 0)
+	{
+		delay(10);
+		i++;
+		if (i > 200)
+			return 1;
+	}
+	this->Stop();
+	return 0;
+}
+
+//SPI写字节
+uint8_t SpiSoft::Write(uint8_t data)
+{
+	uint8_t i;
+	uint8_t ret = 0;
+	if (this->CPOL == CPOL_Low)
+	{
+		//时钟极性，空闲时为低
+		if (this->CPHA == CPHA_1Edge)
+		{
+			//时钟相位 在串行同步时钟的第一个跳变沿（上升或下降）数据被采样
+			//测试通过
+			for (i = 0; i < 8; i++)
+			{
+				if (data &(1 << (8 - i - 1)))
+				{
+					this->_mosi = 1;
+				}
+				else
+				{
+					this->_mosi = 0;
+				}
+				delayMicroseconds(this->delayus);
+				this->_clk = 1;
+				delayMicroseconds(this->delayus);
+				this->_clk = 0;
+				ret <<= 1;
+				if (this->_miso.Read())
+				{
+					ret |= 1;
+				}
+			}
+		}
+		else if (this->CPHA == CPHA_2Edge)
+		{
+			//时钟相位 在串行同步时钟的第二个跳变沿（上升或下降）数据被采样
+			//需要检查
+			for (i = 0; i < 8; i++)
+			{
+				this->_clk = 1;
+				delayMicroseconds(this->delayus);
+				if (data &(1 << (8 - i - 1)))
+				{
+					this->_mosi = 1;
+				}
+				else
+				{
+					this->_mosi = 0;
+				}
+				delayMicroseconds(this->delayus);
+				this->_clk = 0;
+				ret <<= 1;
+				if (this->_miso.Read())
+				{
+					ret |= 1;
+				}
+			}
+		}
+	}
+	else if (this->CPOL == CPOL_High)
+	{
+		//时钟极性，空闲时为高
+		if (this->CPHA == CPHA_1Edge)
+		{
+			//时钟相位 在串行同步时钟的第一个跳变沿（上升或下降）数据被采样
+			for (i = 0; i < 8; i++)
+			{
+				if (data &(1 << (8 - i - 1)))
+				{
+					this->_mosi = 1;
+				}
+				else
+				{
+					this->_mosi = 0;
+				}
+				delayMicroseconds(this->delayus);
+				this->_clk = 0;
+				delayMicroseconds(this->delayus);
+				this->_clk = 1;
+				ret <<= 1;
+				if (this->_miso.Read())
+				{
+					ret |= 1;
+				}
+			}
+		}
+		else if (this->CPHA == CPHA_2Edge)
+		{
+			//时钟相位 在串行同步时钟的第二个跳变沿（上升或下降）数据被采样
+			//测试通过			
+			for (i = 0; i < 8; i++)
+			{
+				this->_clk = 0;
+				delayMicroseconds(this->delayus);
+				if (data &(1 << (8 - i - 1)))
+				{
+					this->_mosi = 1;
+				}
+				else
+				{
+					this->_mosi = 0;
+				}
+				delayMicroseconds(this->delayus);
+				this->_clk = 1;
+				delayMicroseconds(this->delayus);
+				ret <<= 1;
+				if (this->_miso.Read())
+				{
+					ret |= 1;
+				}
+			}
+		}
+		else
+		{
+		}
+	}
+	else
+	{
+	}
+	return ret;
+}
+
+void SpiSoft::Open()
+{
+	if (this->CPOL)
+	{
+		this->_clk = 1;
+	}
+	else
+	{
+		this->_clk = 0;
+	}
+	this->Stop();
+	/*this->_clk.Open();
+	this->_miso.Open();
+	this->_mosi.Open();
+	this->_nss.Open();*/
+}
+void SpiSoft::Close()
+{
+	//测试，等待删除
+	for (int i = 0; i < 8; i++)
+	{
+		this->_clk = 0;
+		this->_mosi = 1;
+		delayMicroseconds(this->delayus);
+		this->_clk = 1;
+		this->_mosi = 0;
+	}
+}
+
+// 拉低NSS，开始传输
+void SpiSoft::Start()
+{
+	if (!this->_nss.Empty())
+	{
+		this->_nss = 0;
+	}
+	// 开始新一轮事务操作，错误次数清零
+	//Error = 0;
+}
+
+// 拉高NSS，停止传输
+void SpiSoft::Stop()
+{
+	if (!this->_nss.Empty())
+	{
+		this->_nss = 1;
+	}
 }
