16c42dc199598d3b2d3a052b60228ffbf5487e0c
Ace <258613763@qq.com>
2019-04-01 21:33:56 +0800

    整理

--- StdOS/Drivers/SPI/W25QXXX/Example/W25QXXXDemo.cpp
+++ StdOS/Drivers/SPI/W25QXXX/Example/W25QXXXDemo.cpp
@@ -4,13 +4,21 @@
 
 #ifdef _W25QXX_TEST_CPP
 
+#define SPISOFT
+
+#ifdef SPISOFT
+SpiSoft spi;
+#else
 Spi spi(Spi1);
+#endif
 
 SPI25QXX s25xx;
 void w25qxxxtest()
 {
-	/*spi.SetPin(PA5,PA6,PA7);
-	spi.SetNss(PA4);*/
+#ifdef SPISOFT
+	spi.SetPin(PA5,PA6,PA7);
+	spi.SetNss(PA4);
+#endif
 	spi.Init(CPOL_High, CPHA_2Edge);
 
 	s25xx.SetSpi(&spi);
