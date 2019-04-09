45eb23971fbbe6a4acf4c5f3c6c4a95e647667e9
Ace <258613763@qq.com>
2019-04-09 22:34:46 +0800

    整理

---
+++ StdOS/ArduinoAPI/main1.cpp
@@ -0,0 +1,26 @@
+#include "FileGroup.h"
+
+void setup() {
+	// put your setup code here, to run once:
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+}
+
+/**
+  * @brief  Main Function
+  * @param  None
+  * @retval None
+  */
+int main1(void)
+{
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+	GPIO_JTAG_Disable();
+//  SysClock_Init(F_CPU_128MHz);
+    Delay_Init();
+    ADCx_Init(ADC1);
+//	ADCx_DMA_Init();
+    setup();
+    for(;;)loop();
+}
