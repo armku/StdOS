fd120b508835a91ae023dca615a266abfe35a72b
Ace <258613763@qq.com>
2019-05-11 16:51:36 +0800

    代码整理

--- HAL/STM32F1/ARCH_UART.h
+++ HAL/STM32F1/ARCH_UART.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include "type.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct
 	{
 		//uint8_t data[1024];
@@ -13,6 +17,28 @@ typedef struct
 	_data_Q_t;
 	
 	typedef void (*pFun_UART)(uint8_t c); 
+	
+	
+	void UART1_Dis_IRQ(void);
+	void UART2_Dis_IRQ(void);
+	void UART3_Dis_IRQ(void);
+	void UART4_Dis_IRQ(void);
+	void UART5_Dis_IRQ(void);
+	
+	void UART1_En_IRQ(void);
+	void UART2_En_IRQ(void);
+	void UART3_En_IRQ(void);
+	void UART4_En_IRQ(void);
+	void UART5_En_IRQ(void);
+	
+	void UART_1_send_byte(uint8_t c);
+	void UART_2_send_byte(uint8_t c);
+	void UART_3_send_byte(uint8_t c);
+	void UART_4_send_byte(uint8_t c);
+	void UART_5_send_byte(uint8_t c);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
