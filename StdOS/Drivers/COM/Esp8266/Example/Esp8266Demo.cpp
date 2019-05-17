5c3bf92bb5d22f9266c26aef5ddc1681da483455
Ace <258613763@qq.com>
2019-05-17 22:31:01 +0800

    整理

--- StdOS/Drivers/COM/Esp8266/Example/Esp8266Demo.cpp
+++ StdOS/Drivers/COM/Esp8266/Example/Esp8266Demo.cpp
@@ -6,42 +6,17 @@
 
 #define _ESP8266EST_CPP
 #ifdef _ESP8266EST_CPP
-//测试 01 03 00 00 00 0A C5 CD
+
 USARTHAL usart222(COM3,115200);
-//ModbusSlaveLink modbusSlave(usart222);
-//Port p485dr;
-//uint16_t RegInputu16[144]; //输入寄存器
-//uint16_t RegHoilding16[60];
 
 void Esp8266Routin(void* param)
 {
-	//if ((modbusSlave.com.RxSize() > 0)&& modbusSlave.CheckFrame())
-	//{
-	//	modbusSlave.DealFrame();
-	//	modbusSlave.rxFrame.RemoveOneFrame();
-	//}
-	//else if ((modbusSlave.com.RxSize() > 0 || (modbusSlave.rxFrame.Length > 0)) && (millis() - modbusSlave.com.LastRcvTime) > 5)
-	//{
-	//	debug_printf("Clear RcvBuffer rxFrame:%d  com.RxSize:%d\n", modbusSlave.rxFrame.Length,modbusSlave.com.RxSize());
-	//	modbusSlave.rxFrame.Length = 0;
-	//	//modbusSlave.com.ClearRxBuf();
-	//}
-	//else {}
+
 }
 
 void Esp8266TestInit()
 {
-	//p485dr.SetPin(PC2);
-	//p485dr.pinMode(GPIO_Out_PP);
-	//p485dr = 0;//接收模式
-	//modbusSlave.com.RS485 = &p485dr;
-	//modbusSlave.id = 1;
-	//modbusSlave.SetRegInput(0, ArrayLength(RegInputu16), RegInputu16, 0);
-	//modbusSlave.SetRegHoid(0, ArrayLength(RegHoilding16), RegHoilding16, 0);
-
-	//RegInputu16[0] = 12;
-	//RegInputu16[1] = 13;
-
+	
 	Sys.AddTask(Esp8266Routin, 0, 0, 1, "Esp8266Routin");
 }
 #endif
