827302af2363998caf9cbeddb379e6d5fcc99107
Ace <258613763@qq.com>
2019-05-17 23:07:30 +0800

    整理

--- StdOS/Drivers/COM/Esp8266/Example/Esp8266Demo.cpp
+++ StdOS/Drivers/COM/Esp8266/Example/Esp8266Demo.cpp
@@ -60,8 +60,8 @@ void SendData(char * bufin,int len)
 	char buf[5];
 	buf[0] = bufin[0];
 	buf[1] = bufin[1];
-	buf[2] = 0x0d;
-	buf[3] = 0x0a;
+	buf[2] = 0x0a;
+	buf[3] = 0x0d;
 	buf[4] = 0;
 
 	UART3_send_data((uint8_t*)buf, 5);
