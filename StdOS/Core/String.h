321d771e2d66ae7d139a4b6a33da5b2d26601e4d
Ace <258613763@qq.com>
2019-06-16 23:46:32 +0800

    整理

---
+++ StdOS/Core/String.h
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#ifndef _STRINGS_H
+#define _STRINGS_H
+
+//#ifdef __cplusplus
+//extern "C"
+//{
+////#include "dtostrf.h"
+
+//}
+
+
+class Strings
+{
+public:
+	Strings();
+	~Strings();
+
+private:
+
+};
+
+Strings::Strings()
+{
+}
+
+Strings::~Strings()
+{
+}
+
+
+#endif // !_STRINGS_H
