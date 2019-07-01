ddff75c97230cb92083bfe148285a7823bba7020
Ace <258613763@qq.com>
2019-07-01 17:20:13 +0800

    删除无用代码

--- StdOS/Drivers/COM/ModbusLink/ModbusLink.cpp
+++ StdOS/Drivers/COM/ModbusLink/ModbusLink.cpp
@@ -12,16 +12,7 @@ ModbusSlaveLink::ModbusSlaveLink()
 }
 #include "Buffer.h"
 bool ModbusSlaveLink::CheckFrame()
-{
-	int rxlen = 0;// com.RxSize();
-
-	/*if (com.GetBytes(&rxFrame.data[rxFrame.Length], rxlen))
-	{
-		rxFrame.Length += rxlen;
-	}*/
-	//判断数据帧最小长度要求
-	if (rxFrame.Length < 8)
-		return false;
+{		
 #ifdef  DEBUG
 	Buffer(rxFrame.data, rxFrame.Length).ShowHex(true);
 #endif //  DEBUG	
