a7a86efe9ea9f23b7b5e1f12fce0b760974ce2b1
Ace <258613763@qq.com>
2019-07-07 12:43:31 +0800

    整理

--- StdOS/Core/Type.h
+++ StdOS/Core/Type.h
@@ -10,23 +10,11 @@ typedef const char*		cstring;
 
 // 逐步使用char替代byte，在返回类型中使用char*替代void*
 // 因为格式化输出时，可以用%c输出char，用%s输出char*
-// 根对象
-//class Object
-//{
-//public:
-//	
-//};
+
 void assert_failed(uint8_t *file, uint32_t line, char *errstr);
 // 数组长度
 #define ArrayLength(arr) (int)(sizeof(arr)/sizeof(arr[0]))
 
 int StdPrintf(const char *format, ...); //代替 printf
-
-// 弱函数
-#if defined(_MSC_VER)
-#define WEAK
-#else
-#define	WEAK	__attribute__((weak))
-#endif
 
 #endif
