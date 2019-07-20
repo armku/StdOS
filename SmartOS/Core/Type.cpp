a43f6ea18b494c9b9b96b988be95951271acd627
Ace <258613763@qq.com>
2019-07-21 00:10:49 +0800

    调整代码

--- SmartOS/Core/Type.cpp
+++ SmartOS/Core/Type.cpp
@@ -11,35 +11,35 @@ using namespace ::std;
 /******************************** Object ********************************/
 
 // 输出对象的字符串表示方式
-//String& Object::ToStr(String& str) const
-//{
-//	auto name = typeid(*this).name();
-//	while(*name >= '0' && *name <= '9') name++;
+String& Object::ToStr(String& str) const
+{
+	auto name = typeid(*this).name();
+	while(*name >= '0' && *name <= '9') name++;
 
-//	str	+= name;
+	str	+= name;
 
-//	return str;
-//}
+	return str;
+}
 
 // 输出对象的字符串表示方式。支持RVO优化。
 // 该方法直接返回给另一个String作为初始值，只有一次构造，没有多余构造、拷贝和析构。
-//String Object::ToString() const
-//{
-//	String str;
-//	ToStr(str);
+String Object::ToString() const
+{
+	String str;
+	ToStr(str);
 
-//	return str;
-//}
+	return str;
+}
 
-//void Object::Show(bool newLine) const
-//{
-//	// 为了减少堆分配，采用较大的栈缓冲区
-//	char cs[0x200];
-//	String str(cs, ArrayLength(cs));
-//	str.SetLength(0);
-//	ToStr(str);
-//	str.Show(newLine);
-//}
+void Object::Show(bool newLine) const
+{
+	// 为了减少堆分配，采用较大的栈缓冲区
+	char cs[0x200];
+	String str(cs, ArrayLength(cs));
+	str.SetLength(0);
+	ToStr(str);
+	str.Show(newLine);
+}
 
 const Type Object::GetType() const
 {
@@ -64,14 +64,15 @@ Type::Type()
 	//Size	= size;
 }
 
-//const String Type::Name() const
-//{
-//	auto ti	= (const type_info*)_info;
-//	auto name = ti->name();
-//	while(*name >= '0' && *name <= '9') name++;
+const String Type::Name() const
+{
+	auto ti	= (const type_info*)_info;
+	auto name = ti->name();
+	while(*name >= '0' && *name <= '9') name++;
 
-//	return String(name);
-//}
+	return String(name);
+}
+
 void assert_failed2(cstring msg, cstring file, unsigned int line)
 {
     debug_printf("%s Line %d, %s\r\n", msg, line, file);
