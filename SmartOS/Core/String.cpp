bddb1da8a1670869731360c872ebfba2f585a8f8
Ace <258613763@qq.com>
2019-07-21 00:03:01 +0800

    增加代码

--- SmartOS/Core/String.cpp
+++ SmartOS/Core/String.cpp
@@ -1,2 +1,17 @@
-#include "SString.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdarg.h>
 
+// gcc里面strncasecmp位于strings.h
+#ifdef __GNUC__
+#include <strings.h>
+#endif
+
+#include "_Core.h"
+
+#include "ByteArray.h"
+
+#include "SString.h"
