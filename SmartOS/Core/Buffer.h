4c99750773c1187669d2567915596cd9c5844f73
Ace <258613763@qq.com>
2019-07-20 22:16:16 +0800

    升级

--- SmartOS/Core/Buffer.h
+++ SmartOS/Core/Buffer.h
@@ -3,7 +3,25 @@
 
 #include "Type.h"
 
-class Buffer 
+/*
+数据区设计原则：
+1，固定数据区封装 Buffer	=> Object
+2，变长数据区封装 Array		=> Buffer
+3，自带初始缓冲区封装 ByteArray/String	=> Array
+
+赋值运算符原则：
+1，赋值运算拷贝长度和数据，核心语义为拷贝一份数据区
+2，左值长度大于等于右值时，左值长度变小
+3，左值长度小于右值时，左值尝试SetLength扩容
+4，如果扩容失败，调试版断言失败，发行版左值长度保持不变
+*/
+
+// 内存数据区。包装指针和长度
+// 参考C#的Byte[]，主要实现对象与指针的相互转化、赋值、拷贝、设置、截取、比较等操作。
+// 内部指针指向的内存和长度，都由外部传入，内部不会自动分配。
+// 所有的进出拷贝都是针对内部指针和最大长度，不会自动扩容，除非子类继承扩展SetLength。
+// 拷贝的原则是尽力而为，有多少可用空间就拷贝多少长度。
+class Buffer //: public Object
 {
 public:
 	// 打包一个指针和长度指定的数据区
@@ -17,6 +35,16 @@ protected:
 public:
 	virtual void Show(bool newLine = false) const;
 	virtual void ShowHex(bool newLine = false, char sep = 0X20) const;
+};
+
+// 带引用计数的缓冲区
+class BufferRef : public Buffer
+{
+public:
+	int	Ref	= 1;	// 引用计数
+
+	// 打包一个指针和长度指定的数据区
+	void Set(void* ptr, int len);
 };
 
 #endif
--- SmartOS/Core/Type.h
+++ SmartOS/Core/Type.h
@@ -1,20 +1,70 @@
 #ifndef __Type_H__
 #define __Type_H__
-#include <stdint.h>
-/* 类型定义 */
-typedef const char*		cstring;
 
-#define NULL 0
+/* 类型定义 */
+typedef char			sbyte;
+typedef unsigned char	byte;
+typedef unsigned short	ushort;
+typedef unsigned int	uint;
+typedef unsigned long long	UInt64;
+typedef long long		Int64;
+typedef const char*		cstring;
 
 #define UInt64_Max 0xFFFFFFFFFFFFFFFFull
 
 // 逐步使用char替代byte，在返回类型中使用char*替代void*
 // 因为格式化输出时，可以用%c输出char，用%s输出char*
 
-void assert_failed(uint8_t *file, uint32_t line, char *errstr);
+class String;
+class Type;
+
+// 根对象
+class Object
+{
+public:
+	// 输出对象的字符串表示方式
+	virtual String& ToStr(String& str) const;
+	// 输出对象的字符串表示方式。支持RVO优化
+	virtual String ToString() const;
+	// 显示对象。默认显示ToString
+	virtual void Show(bool newLine = false) const;
+
+	const Type GetType() const;
+};
+
+// 类型
+class Type
+{
+private:
+	const void* _info;
+
+	friend class Object;
+
+	Type();
+
+public:
+	int		Size;	// 大小
+	//String	Name;	// 名称
+
+	const String Name() const;	// 名称
+};
+
 // 数组长度
 #define ArrayLength(arr) (int)(sizeof(arr)/sizeof(arr[0]))
+// 数组清零，固定长度
+//#define ArrayZero(arr) memset(arr, 0, sizeof(arr))
 
+// 弱函数
+#if defined(_MSC_VER)
+#define WEAK
+#else
+#define	WEAK	__attribute__((weak))
+#endif
+
+
+//以下需要升级
+#include <stdint.h>
+#define NULL 0
+void assert_failed(uint8_t *file, uint32_t line, char *errstr);
 int StdPrintf(const char *format, ...); //代替 printf
-
 #endif
