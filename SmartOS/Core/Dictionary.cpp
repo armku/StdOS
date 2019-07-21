86ee4983ca37b17f525406c555940ea4193218b8
Ace <258613763@qq.com>
2019-07-21 12:08:18 +0800

    增加代码

--- SmartOS/Core/Dictionary.cpp
+++ SmartOS/Core/Dictionary.cpp
@@ -1,2 +1,122 @@
+#include "Type.h"
+#include "Buffer.h"
+#include "SString.h"
+
+#include "List.h"
 #include "Dictionary.h"
 
+IDictionary::IDictionary(IComparer comparer)
+{
+	_Keys.Comparer	= comparer;
+}
+
+IDictionary::IDictionary(const IDictionary& dic)
+{
+	operator=(dic);
+}
+
+IDictionary::IDictionary(IDictionary&& dic)
+{
+	move(dic);
+}
+
+IDictionary& IDictionary::operator=(const IDictionary& dic)
+{
+	_Keys	= dic._Keys;
+	_Values	= dic._Values;
+
+	return *this;
+}
+
+IDictionary& IDictionary::operator=(IDictionary&& dic)
+{
+	move(dic);
+
+	return *this;
+}
+
+void IDictionary::move(IDictionary& dic)
+{
+	_Keys	= dic._Keys;
+	_Values	= dic._Values;
+}
+
+//int IDictionary::Count() const { return _Keys.Count(); }
+//const IList& IDictionary::Keys()	const { return _Keys; }
+//const IList& IDictionary::Values()	const { return _Values; }
+
+// 添加单个元素
+void IDictionary::Add(PKey key, void* value)
+{
+	// 判断一下，如果已存在，则覆盖
+	int idx	= _Keys.FindIndex(key);
+	if(idx >= 0)
+	{
+		_Values[idx]	= value;
+	}
+	else
+	{
+		_Keys.Add((void*)key);
+		_Values.Add(value);
+	}
+}
+
+// 删除指定元素
+void IDictionary::Remove(PKey key)
+{
+	int idx = _Keys.Remove(key);
+	if(idx >= 0) _Values.RemoveAt(idx);
+}
+
+void IDictionary::Clear()
+{
+	_Keys.Clear();
+	_Values.Clear();
+}
+
+// 是否包含指定项
+bool IDictionary::ContainKey(PKey key) const
+{
+	return _Keys.FindIndex(key) >= 0;
+}
+
+// 尝试获取值
+bool IDictionary::TryGetValue(PKey key, void*& value) const
+{
+	int idx	= _Keys.FindIndex(key);
+	if(idx < 0) return false;
+
+	value	= _Values[idx];
+
+	return true;
+}
+
+
+// 重载索引运算符[]，返回指定元素的第一个
+void* IDictionary::operator[](PKey key) const
+{
+	int idx	= _Keys.FindIndex(key);
+	if(idx < 0) return nullptr;
+
+	return _Values[idx];
+}
+
+void*& IDictionary::operator[](PKey key)
+{
+	int idx	= _Keys.FindIndex(key);
+	if(idx < 0)
+	{
+		static void* dummy;
+		return dummy;
+	}
+
+	return _Values[idx];
+}
+
+const String IDictionary::GetString(PKey key) const
+{
+	void* p	= nullptr;
+	TryGetValue(key, p);
+
+	return String((cstring)p);
+}
---
+++ SmartOS/Core/Dictionary.h
@@ -0,0 +1,91 @@
+#ifndef _Dictionary_H_
+#define _Dictionary_H_
+
+// 字典。仅用于存储指针。
+// 内置两个List用于存储键值集合，添加删除时对它们进行同步操作
+class IDictionary
+{
+	typedef const void*	PKey;
+	typedef void*		PValue;
+public:
+    IDictionary(IComparer comparer = nullptr);
+    IDictionary(const IDictionary& dic);
+    IDictionary(IDictionary&& dic);
+
+	IDictionary& operator=(const IDictionary& dic);
+	IDictionary& operator=(IDictionary&& dic);
+
+	inline int Count()				const { return _Keys.Count(); }
+	inline const IList& Keys()		const { return _Keys; }
+	inline const IList& Values()	const { return _Values; }
+
+	// 添加单个元素
+    void Add(PKey key, PValue value);
+
+	// 删除指定元素
+	void Remove(PKey key);
+
+	void Clear();
+
+	// 是否包含指定项
+	bool ContainKey(PKey key) const;
+
+	// 尝试获取值
+	bool TryGetValue(PKey key, PValue& value) const;
+
+    // 重载索引运算符[]，返回指定元素的第一个
+    PValue operator[](PKey key) const;
+    PValue& operator[](PKey key);
+
+	const String GetString(PKey key) const;
+
+#if DEBUG
+	static void Test();
+#endif
+
+private:
+	IList	_Keys;
+	IList	_Values;
+
+	void move(IDictionary& dic);
+};
+
+template<typename TKey, typename TValue>
+class Dictionary : public IDictionary
+{
+	static_assert(sizeof(TKey) <= 4, "Dictionary only support pointer or int");
+	static_assert(sizeof(TValue) <= 4, "Dictionary only support pointer or int");
+
+	typedef const TKey	PKey;
+	typedef TValue		PValue;
+public:
+    Dictionary(IComparer comparer = nullptr) : IDictionary(comparer) { }
+
+	const List<TKey>& Keys() const		{ return (List<TKey>&)	 IDictionary::Keys();	};
+	const List<TValue>& Values() const	{ return (List<TValue>&) IDictionary::Values();	};
+
+	// 添加单个元素
+    void Add(PKey key, PValue value) { IDictionary::Add((const void*)key, (void*)value); }
+
+	// 删除指定元素
+	void Remove(PKey key) { IDictionary::Remove((const void*)key); }
+
+	// 是否包含指定项
+	bool ContainKey(PKey key) const { return IDictionary::ContainKey((const void*)key); }
+
+	// 尝试获取值
+	bool TryGetValue(PKey key, PValue& value) const
+	{
+		void* val	= nullptr;
+		bool rs	= IDictionary::TryGetValue((const void*)key, val);
+		value	= (PValue)val;
+
+		return rs;
+	}
+
+    // 重载索引运算符[]，返回指定元素的第一个
+    PValue operator[](PKey key) const	{ return (PValue)IDictionary::operator[]((const void*)key); }
+    PValue& operator[](PKey key)		{ return (PValue&)IDictionary::operator[]((const void*)key); }
+};
+
+#endif
