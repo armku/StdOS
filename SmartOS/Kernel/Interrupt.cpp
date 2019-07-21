5e2dd0b7fb1c9a3c192caec0ebde994ad42f8db5
Ace <258613763@qq.com>
2019-07-21 18:15:27 +0800

    增加代码

--- SmartOS/Kernel/Interrupt.cpp
+++ SmartOS/Kernel/Interrupt.cpp
@@ -11,11 +11,11 @@ void TInterrupt::Init() const
 {
 	//OnInit();
 }
-//bool TInterrupt::Activate(short irq, InterruptCallback isr, void* param)
-//{
-//	short irq2 = irq + 16; // exception = irq + 16
-////	Vectors[irq2] = isr;
-////	VectorParams[irq2] = param;
+bool TInterrupt::Activate(short irq, InterruptCallback isr, void* param)
+{
+	short irq2 = irq + 16; // exception = irq + 16
+	Vectors[irq2] = isr;
+	VectorParams[irq2] = param;
 
-//	return OnActivate(irq);
-//}
+	return OnActivate(irq);
+}
