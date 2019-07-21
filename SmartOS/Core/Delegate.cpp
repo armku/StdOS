2b308215a7939e93d92d926ac9829acdb6b4ae52
Ace <258613763@qq.com>
2019-07-21 12:05:27 +0800

    增加部分代码

--- SmartOS/Core/Delegate.cpp
+++ SmartOS/Core/Delegate.cpp
@@ -1,2 +1,17 @@
+#include "Type.h"
+#include "DateTime.h"
+#include "SString.h"
+
 #include "Delegate.h"
 
+/************************************************ Delegate ************************************************/
+/*Delegate::Delegate()
+{
+	Method	= nullptr;
+	Target	= nullptr;
+}
+
+Delegate::Delegate(Action func)	{ Method	= (void*)func; Target	= nullptr; }
+
+Delegate& Delegate::operator=(Action func)	{ Method	= (void*)func; return *this; }
+*/
