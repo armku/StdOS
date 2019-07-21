858b5d91456ab10496c741c29aa3fc3dc5b502f8
Ace <258613763@qq.com>
2019-07-21 12:14:32 +0800

    增加代码

--- SmartOS/Core/Environment.cpp
+++ SmartOS/Core/Environment.cpp
@@ -1,3 +1,27 @@
+#include "Buffer.h"
+
 #include "Environment.h"
 
+#if defined(__CC_ARM)
+	#include <time.h>
+#else
+	#include <ctime>
+#endif
 
+const TEnvironment Environment;
+
+/************************************************ TEnvironment ************************************************/
+
+TEnvironment::TEnvironment()
+{
+	
+}
+
+// 获取系统启动后经过的毫秒数
+UInt64 TEnvironment::TickCount() const { return clock(); }
+
+// 获取系统启动后经过的毫秒数
+UInt64 TEnvironment::Ms() const { return clock(); }
+
+// 获取当前计算机上的处理器数
+int TEnvironment::ProcessorCount() const { return 1; }
