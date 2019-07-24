934a0488b1bef1b52d49f29c455b33a67c750ac7
Ace <258613763@qq.com>
2019-07-24 23:33:17 +0800

    增加检查

--- SmartOS/Core/List.cpp
+++ SmartOS/Core/List.cpp
@@ -23,28 +23,28 @@ IList::IList(IList&& list)
 
 IList::~IList()
 {
-	if(_Arr && _Arr != Arr) delete[] _Arr;
+	if (_Arr && _Arr != Arr) delete[] _Arr;
 }
 
 void IList::Init()
 {
-	_Arr	= Arr;
-	_Count	= 0;
-	_Capacity	= ArrayLength(Arr);
+	_Arr = Arr;
+	_Count = 0;
+	_Capacity = ArrayLength(Arr);
 
-	Comparer	= nullptr;
+	Comparer = nullptr;
 }
 
 IList& IList::operator=(const IList& list)
 {
-	if(this != &list)
+	if (this != &list)
 	{
-		_Count	= list._Count;
-		Comparer	= list.Comparer;
+		_Count = list._Count;
+		Comparer = list.Comparer;
 
-		if(list._Arr != list.Arr) CheckCapacity(list._Count);
+		if (list._Arr != list.Arr) CheckCapacity(list._Count);
 
-		Buffer(_Arr, _Count << 2)	= list._Arr;
+		Buffer(_Arr, _Count << 2) = list._Arr;
 	}
 
 	return *this;
@@ -52,30 +52,30 @@ IList& IList::operator=(const IList& list)
 
 IList& IList::operator=(IList&& list)
 {
-	if(this != &list) move(list);
+	if (this != &list) move(list);
 
 	return *this;
 }
 
 void IList::move(IList& list)
 {
-	_Count	= list._Count;
-	_Capacity	= list._Capacity;
-	Comparer	= list.Comparer;
+	_Count = list._Count;
+	_Capacity = list._Capacity;
+	Comparer = list.Comparer;
 
 	// 如果list的缓冲区是自己的，则拷贝过来
 	// 如果不是自己的，则直接拿过来用
-	if(list._Arr == list.Arr)
+	if (list._Arr == list.Arr)
 	{
-		_Arr	= Arr;
-		Buffer(_Arr, _Count << 2)	= list._Arr;
+		_Arr = Arr;
+		Buffer(_Arr, _Count << 2) = list._Arr;
 	}
 	else
 	{
 		// 如果已有数据区，则释放
-		if(_Arr && _Arr != Arr) delete[] _Arr;
+		if (_Arr && _Arr != Arr) delete[] _Arr;
 
-		_Arr	= list._Arr;
+		_Arr = list._Arr;
 		list.Init();
 	}
 }
@@ -87,30 +87,30 @@ void IList::Add(void* item)
 {
 	CheckCapacity(_Count + 1);
 
-	_Arr[_Count++]	= item;
+	_Arr[_Count++] = item;
 }
 
 // 添加多个元素
 void IList::Add(void** items, int count)
 {
-	if(!items || !count) return;
+	if (!items || !count) return;
 
 	CheckCapacity(_Count + count);
 
-	while(count--) _Arr[_Count++]	= items++;
+	while (count--) _Arr[_Count++] = items++;
 }
 
 // 删除指定位置元素
 void IList::RemoveAt(int index)
 {
 	int len = _Count;
-	if(len <= 0 || index >= len) return;
+	if (len <= 0 || index >= len) return;
 
 	// 复制元素，最后一个不用复制
-	int remain	= len - 1 - index;
-	if(remain)
+	int remain = len - 1 - index;
+	if (remain)
 	{
-		len	= remain * sizeof(void*);
+		len = remain * sizeof(void*);
 		Buffer(&_Arr[index], len).Copy(0, &_Arr[index + 1], len);
 	}
 	_Count--;
@@ -120,17 +120,17 @@ void IList::RemoveAt(int index)
 int IList::Remove(const void* item)
 {
 	int idx = FindIndex(item);
-	if(idx >= 0) RemoveAt(idx);
+	if (idx >= 0) RemoveAt(idx);
 
 	return idx;
 }
 
 int IList::FindIndex(const void* item) const
 {
-	for(int i=0; i<_Count; i++)
+	for (int i = 0; i < _Count; i++)
 	{
-		if(_Arr[i] == item) return i;
-		if(Comparer && Comparer(_Arr[i], item) == 0) return i;
+		if (_Arr[i] == item) return i;
+		if (Comparer && Comparer(_Arr[i], item) == 0) return i;
 	}
 
 	return -1;
@@ -139,9 +139,9 @@ int IList::FindIndex(const void* item) const
 // 释放所有指针指向的内存
 IList& IList::DeleteAll()
 {
-	for(int i=0; i < _Count; i++)
+	for (int i = 0; i < _Count; i++)
 	{
-		if(_Arr[i]) delete (int*)_Arr[i];
+		if (_Arr[i]) delete (int*)_Arr[i];
 	}
 
 	return *this;
@@ -155,14 +155,14 @@ void IList::Clear()
 // 重载索引运算符[]，返回指定元素的第一个
 void* IList::operator[](int i) const
 {
-	if(i<0 || i>=_Count) return nullptr;
+	if (i < 0 || i >= _Count) return nullptr;
 
 	return _Arr[i];
 }
 
 void*& IList::operator[](int i)
 {
-	if(i<0 || i>=_Count)
+	if (i < 0 || i >= _Count)
 	{
 		static void* dummy;
 		return dummy;
@@ -174,24 +174,24 @@ void*& IList::operator[](int i)
 bool IList::CheckCapacity(int count)
 {
 	// 是否超出容量
-	if(_Arr && count <= _Capacity) return true;
+	if (_Arr && count <= _Capacity) return true;
 
 	// 自动计算合适的容量
 	int sz = 0x40 >> 2;
-	while(sz < count) sz <<= 1;
+	while (sz < count) sz <<= 1;
 
 	void* p = new byte[sz << 2];
-	if(!p) return false;
+	if (!p) return false;
 
 	// 需要备份数据
-	if(_Count > 0 && _Arr)
+	if (_Count > 0 && _Arr)
 		// 为了安全，按照字节拷贝
 		Buffer(p, sz << 2).Copy(0, _Arr, _Count << 2);
 
-	if(_Arr && _Arr != Arr) delete[] _Arr;
+	if (_Arr && _Arr != Arr) delete[] _Arr;
 
-	_Arr		= (void**)p;
-	_Capacity	= sz;
+	_Arr = (void**)p;
+	_Capacity = sz;
 
 	return true;
 }
--- SmartOS/Core/List.h
+++ SmartOS/Core/List.h
@@ -7,11 +7,11 @@ typedef int (*IComparer)(const void* v1, const void* v2);
 class IList
 {
 public:
-    IComparer	Comparer;	// 比较器
+	IComparer	Comparer;	// 比较器
 
 	IList();
-    IList(const IList& list);
-    IList(IList&& list);
+	IList(const IList& list);
+	IList(IList&& list);
 	virtual ~IList();
 
 	IList& operator=(const IList& list);
@@ -20,10 +20,10 @@ public:
 	inline int Count()	const { return _Count; }
 
 	// 添加单个元素
-    void Add(void* item);
+	void Add(void* item);
 
 	// 添加多个元素
-    void Add(void** items, int count);
+	void Add(void** items, int count);
 
 	// 删除指定位置元素
 	void RemoveAt(int index);
@@ -39,20 +39,20 @@ public:
 	// 释放所有指针指向的内存
 	IList& DeleteAll();
 
-    // 重载索引运算符[]，返回指定元素的第一个
-    void* operator[](int i) const;
-    void*& operator[](int i);
+	// 重载索引运算符[]，返回指定元素的第一个
+	void* operator[](int i) const;
+	void*& operator[](int i);
 
 #if DEBUG
 	static void Test();
 #endif
 
 private:
-	void**	_Arr;
+	void** _Arr;
 	int		_Count;
 	int		_Capacity;
 
-	void*	Arr[0x04];
+	void* Arr[0x04];
 
 	void Init();
 	bool CheckCapacity(int count);
@@ -67,7 +67,7 @@ public:
 	virtual ~List() { };
 
 	// 添加单个元素
-    void Add(T item) { IList::Add((void*)item); }
+	void Add(T item) { IList::Add((void*)item); }
 
 	// 删除指定元素
 	int Remove(const T item) { return IList::Remove((const void*)item); }
@@ -75,9 +75,9 @@ public:
 	// 查找指定项。不存在时返回-1
 	int FindIndex(const T item) const { return IList::FindIndex((const void*)item); }
 
-    // 重载索引运算符[]，返回指定元素的第一个
-    T operator[](int i) const	{ return (T)IList::operator[](i); }
-    T& operator[](int i)		{ return (T&)IList::operator[](i); }
+	// 重载索引运算符[]，返回指定元素的第一个
+	T operator[](int i) const { return (T)IList::operator[](i); }
+	T& operator[](int i) { return (T&)IList::operator[](i); }
 };
 
 #endif
