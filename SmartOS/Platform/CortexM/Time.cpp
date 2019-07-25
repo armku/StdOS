d239dfc1eac2d12e8066635326d97ece0bbf8057
Ace <258613763@qq.com>
2019-07-25 21:58:03 +0800

    整理

--- SmartOS/Platform/CortexM/Time.cpp
+++ SmartOS/Platform/CortexM/Time.cpp
@@ -7,6 +7,9 @@
 
 #define TIME_DEBUG 0
 
+uint RCC_GetPCLK();
+uint RCC_GetPCLK2();
+
 /************************************************ TTime ************************************************/
 
 #define SYSTICK_MAXCOUNT       SysTick_LOAD_RELOAD_Msk	//((1<<24) - 1)	/* SysTick MaxCount */
@@ -15,6 +18,79 @@
 static TIM_TypeDef * const g_Timers[] = TIMS;
 static uint	gTicks;
 
+void TTime::Init()
+{
+	// 准备使用外部时钟，Systick时钟=HCLK/8
+	uint clk = Sys.Clock / 8;
+	// 48M时，每秒48M/8=6M个滴答，1us=6滴答
+	// 72M时，每秒72M/8=9M个滴答，1us=9滴答
+	// 96M时，每秒96M/8=12M个滴答，1us=12滴答
+	// 120M时，每秒120M/8=15M个滴答，1us=15滴答
+	// 168M时，每秒168M/8=21M个滴答，1us=21滴答
+	gTicks = clk / 1000000;	// 每微秒的时钟滴答数
+
+	//uint ticks = SYSTICK_MAXCOUNT;
+	// ticks为每次中断的嘀嗒数，也就是重载值
+	//SysTick_Config(ticks);
+	// 上面的函数会打开中断
+	uint ticks = gTicks * 1000;	// 1000微秒，便于跟毫秒叠加
+	SysTick->LOAD = ticks - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
+	//Interrupt.Disable(SysTick_IRQn);
+
+	TIM_TypeDef* tim = g_Timers[Index];
+	/*#if defined(STM32F0) || defined(GD32F150)
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM14, ENABLE);
+	#else
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+	#endif*/
+	Timer::ClockCmd(Index, true);
+
+	// 获取当前频率
+#if defined(STM32F0) || defined(GD32F150)
+	uint prd = 1000;
+	uint psc = Sys.Clock / 1000;
+#else
+	clk = RCC_GetPCLK();
+	if ((uint)tim & 0x00010000) clk = RCC_GetPCLK2();
+	clk <<= 1;
+
+	// 120M时，分频系数必须是120K才能得到1k的时钟，超过了最大值64k
+	// 因此，需要增加系数
+	uint prd = 1000;
+	uint psc = clk / 1000;
+	Div = 0;
+	while (psc > 0xFFFF)
+	{
+		prd <<= 1;
+		psc >>= 1;
+		Div++;
+	}
+#endif
+
+	// 配置时钟。1毫秒计时，1000毫秒中断
+	TIM_TimeBaseInitTypeDef tr;
+	TIM_TimeBaseStructInit(&tr);
+	tr.TIM_Period = prd - 1;
+	tr.TIM_Prescaler = psc - 1;
+	//tr.TIM_ClockDivision = 0x0;
+	tr.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(tim, &tr);
+
+	// 打开中断
+	TIM_ITConfig(tim, TIM_IT_Update, ENABLE);
+	// 清除标志位  必须要有！！ 否则 开启中断立马中断给你看
+	TIM_ClearFlag(tim, TIM_FLAG_Update);
+
+	const byte irqs[] = TIM_IRQns;
+	byte irq = irqs[Index];
+	Interrupt.SetPriority(irq, 0);
+	Interrupt.Activate(irq, OnHandler, tim);
+
+	// 打开计数
+	TIM_Cmd(tim, ENABLE);
+}
 
 
 
@@ -102,140 +178,6 @@ INROOT uint TTime::UsToTicks(uint us) const { return !us ? 0 : (us * gTicks); }
 
 
 
-
-
-
-void TTime::Init()
-{
-#if defined STM32F0	
-	// 初始化为输出
-	this->Index = 2;
-
-	gTicks = (Sys.Clock >> 3) / 0xF4240u;
-	//初始化延迟函数
-	//SYSTICK的时钟固定为HCLK时钟的1/8
-	//SYSCLK:系统时钟
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
-	SysTick_Config(9000); //配置SysTick tick is 9ms	9000
-	NVIC_SetPriority(SysTick_IRQn, 0);
-	switch (this->Index)
-	{
-	case 2:
-		NVIC_SetPriority(TIM2_IRQn, 0);
-		break;
-	case 5:
-		break;
-	case 6:
-		//NVIC_SetPriority(TIM7_IRQn, 0);
-		break;
-	default:
-		break;
-	}
-#elif defined STM32F1
-	this->Index = 5;
-	gTicks = (Sys.Clock >> 3) / 0xF4240u;
-
-	SysTick_Config(9000); //配置SysTick tick is 9ms	9000
-	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;                    /* Enable SysTick IRQ and SysTick Timer */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE); //关闭jtag，保留swd	
-#elif defined STM32F4	
-	// 初始化为输出
-	this->Index = 2;
-	gTicks = (Sys.Clock >> 3) / 0xF4240u;
-	//初始化延迟函数
-	//SYSTICK的时钟固定为HCLK时钟的1/8
-	//SYSCLK:系统时钟
-//        uint SYSCLK = 168;
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //选择外部时钟  HCLK/8
-	SysTick_Config(9000); //配置SysTick tick is 9ms	9000
-	NVIC_SetPriority(SysTick_IRQn, 0);
-	switch (this->Index)
-	{
-	case 2:
-		NVIC_SetPriority(TIM3_IRQn, 0);
-		break;
-	case 5:
-		break;
-	case 6:
-		NVIC_SetPriority(TIM7_IRQn, 0);
-		break;
-	default:
-		break;
-	}
-#endif
-	// 准备使用外部时钟，Systick时钟=HCLK/8
-	uint clk = Sys.Clock / 8;
-	// 48M时，每秒48M/8=6M个滴答，1us=6滴答
-	// 72M时，每秒72M/8=9M个滴答，1us=9滴答
-	// 96M时，每秒96M/8=12M个滴答，1us=12滴答
-	// 120M时，每秒120M/8=15M个滴答，1us=15滴答
-	// 168M时，每秒168M/8=21M个滴答，1us=21滴答
-	gTicks = clk / 1000000;	// 每微秒的时钟滴答数
-
-	//uint ticks = SYSTICK_MAXCOUNT;
-	// ticks为每次中断的嘀嗒数，也就是重载值
-	//SysTick_Config(ticks);
-	// 上面的函数会打开中断
-	uint ticks = gTicks * 1000;	// 1000微秒，便于跟毫秒叠加
-	SysTick->LOAD  = ticks - 1;
-	SysTick->VAL   = 0;
-	SysTick->CTRL  = SysTick_CTRL_ENABLE_Msk;
-	//Interrupt.Disable(SysTick_IRQn);
-
-	TIM_TypeDef* tim = g_Timers[Index];
-/*#if defined(STM32F0) || defined(GD32F150)
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM14, ENABLE);
-#else
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
-#endif*/
-//	Timer::ClockCmd(Index, true);
-
-    // 获取当前频率
-#if defined(STM32F0) || defined(GD32F150)
-	uint prd	= 1000;
-	uint psc	= Sys.Clock / 1000;
-#else
-	clk =8000;// RCC_GetPCLK();
-	if((uint)tim & 0x00010000) clk =8000;// RCC_GetPCLK2();
-	clk <<= 1;
-
-	// 120M时，分频系数必须是120K才能得到1k的时钟，超过了最大值64k
-	// 因此，需要增加系数
-	uint prd	= 1000;
-	uint psc	= clk / 1000;
-	Div = 0;
-	while(psc > 0xFFFF)
-	{
-		prd	<<= 1;
-		psc	>>= 1;
-		Div++;
-	}
-#endif
-
-	// 配置时钟。1毫秒计时，1000毫秒中断
-	TIM_TimeBaseInitTypeDef tr;
-	TIM_TimeBaseStructInit(&tr);
-	tr.TIM_Period		= prd - 1;
-	tr.TIM_Prescaler	= psc - 1;
-	//tr.TIM_ClockDivision = 0x0;
-	tr.TIM_CounterMode	= TIM_CounterMode_Up;
-	TIM_TimeBaseInit(tim, &tr);
-
-	// 打开中断
-	TIM_ITConfig(tim, TIM_IT_Update, ENABLE);
-	// 清除标志位  必须要有！！ 否则 开启中断立马中断给你看
-	TIM_ClearFlag(tim, TIM_FLAG_Update);
-
-	const byte irqs[] = TIM_IRQns;
-	byte irq	= irqs[Index];
-	Interrupt.SetPriority(irq, 0);
-	Interrupt.Activate(irq, OnHandler, tim);
-
-	// 打开计数
-	TIM_Cmd(tim, ENABLE);
-}
 
 #include "OnChip\Configuration.h"
 // 当前毫秒数
