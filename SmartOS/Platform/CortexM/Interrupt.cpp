79ccf1ae8b5d66f44913cf7778c92bea08132058
Ace <258613763@qq.com>
2019-07-25 21:38:36 +0800

    增加进度

--- SmartOS/Platform/CortexM/Interrupt.cpp
+++ SmartOS/Platform/CortexM/Interrupt.cpp
@@ -8,15 +8,15 @@
 #define IS_IRQ(irq) (irq >= -16 && irq <= VectorySize - 16)
 
 #ifdef STM32F1
-    #define VectorySize 84
+#define VectorySize 84
 #elif defined(STM32F0)
-    #define VectorySize 48
+#define VectorySize 48
 #elif defined(GD32F150)
-    #define VectorySize 64
+#define VectorySize 64
 #elif defined(STM32F4)
-    #define VectorySize (86 + 16 + 1)
+#define VectorySize (86 + 16 + 1)
 #else
-    #define VectorySize 32
+#define VectorySize 32
 #endif
 
 InterruptCallback Vectors[VectorySize];      // 对外的中断向量表
@@ -26,44 +26,44 @@ bool TInterrupt::OnActivate(short irq)
 {
 	assert_param(IS_IRQ(irq));
 
-    __DMB(); // asure table is written
-    //NVIC->ICPR[irq >> 5] = 1 << (irq & 0x1F); // clear pending bit
-    //NVIC->ISER[irq >> 5] = 1 << (irq & 0x1F); // set enable bit
-    if(irq >= 0)
-    {
-        NVIC_ClearPendingIRQ((IRQn_Type)irq);
-        NVIC_EnableIRQ((IRQn_Type)irq);
-    }
+	__DMB(); // asure table is written
+	//NVIC->ICPR[irq >> 5] = 1 << (irq & 0x1F); // clear pending bit
+	//NVIC->ISER[irq >> 5] = 1 << (irq & 0x1F); // set enable bit
+	if (irq >= 0)
+	{
+		NVIC_ClearPendingIRQ((IRQn_Type)irq);
+		NVIC_EnableIRQ((IRQn_Type)irq);
+	}
 
-    return true;
+	return true;
 }
 
 bool TInterrupt::OnDeactivate(short irq)
 {
 	assert_param(IS_IRQ(irq));
 
-    //NVIC->ICER[irq >> 5] = 1 << (irq & 0x1F); // clear enable bit */
-    if(irq >= 0) NVIC_DisableIRQ((IRQn_Type)irq);
-    return true;
+	//NVIC->ICER[irq >> 5] = 1 << (irq & 0x1F); // clear enable bit */
+	if (irq >= 0) NVIC_DisableIRQ((IRQn_Type)irq);
+	return true;
 }
 
 void TInterrupt::SetPriority(short irq, uint priority) const
 {
 	assert_param(IS_IRQ(irq));
 
-    NVIC_SetPriority((IRQn_Type)irq, priority);
+	NVIC_SetPriority((IRQn_Type)irq, priority);
 }
 
 void TInterrupt::GetPriority(short irq) const
 {
 	assert_param(IS_IRQ(irq));
 
-    NVIC_GetPriority((IRQn_Type)irq);
+	NVIC_GetPriority((IRQn_Type)irq);
 }
 
-void TInterrupt::GlobalEnable()	{ __enable_irq(); }
-void TInterrupt::GlobalDisable(){ __disable_irq(); }
-bool TInterrupt::GlobalState()	{ return __get_PRIMASK(); }
+void TInterrupt::GlobalEnable() { __enable_irq(); }
+void TInterrupt::GlobalDisable() { __disable_irq(); }
+bool TInterrupt::GlobalState() { return __get_PRIMASK(); }
 
 // 关键性代码，放到开头
 INROOT
@@ -71,18 +71,18 @@ INROOT
 #if defined ( __CC_ARM   )
 __ASM uint GetIPSR()
 {
-    mrs     r0,IPSR               // exception number
-    bx      lr
+	mrs     r0, IPSR               // exception number
+		bx      lr
 }
 #elif (defined (__GNUC__))
-uint32_t GetIPSR(void) __attribute__( ( naked ) );
+uint32_t GetIPSR(void) __attribute__((naked));
 uint32_t GetIPSR(void)
 {
-  uint32_t result=0;
+	uint32_t result = 0;
 
-  __ASM volatile ("MRS r0, IPSR\n\t" 
-                  "BX  lr     \n\t"  : "=r" (result) );
-  return(result);
+	__ASM volatile ("MRS r0, IPSR\n\t"
+		"BX  lr     \n\t"  : "=r" (result));
+	return(result);
 }
 #endif
 
@@ -94,14 +94,14 @@ void FaultHandler() { }
 #else
 void UserHandler()
 {
-    uint num = GetIPSR();
-	//Interrupt.Process(num);
+	uint num = GetIPSR();
+	Interrupt.Process(num);
 }
 #endif
 
 void assert_failed(uint8_t* file, unsigned int line)
 {
-    debug_printf("Assert Failed! Line %d, %s\r\n", line, file);
+	debug_printf("Assert Failed! Line %d, %s\r\n", line, file);
 
-	//TInterrupt::Halt();
+	TInterrupt::Halt();
 }
