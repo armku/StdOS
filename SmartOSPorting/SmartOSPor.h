93f51e17e8d7a5b2cbb05036c738304856ce4aa8
Ace <258613763@qq.com>
2019-07-29 21:18:48 +0800

    回退一个版本，运行错误

--- SmartOSPorting/SmartOSPor.h
+++ SmartOSPorting/SmartOSPor.h
@@ -1,33 +1,7 @@
 #ifndef _SmartOsPorting_H
 #define _SmartOsPorting_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define GD32_PLL_MASK    0x20000000
-
-#define CFGR_PLLMull_Mask         ((uint32_t)0x003C0000)
-#define CFGR_PLLSRC_Mask          ((uint32_t)0x00010000)
-#define CFGR_PLLXTPRE_Mask        ((uint32_t)0x00020000)
-#define CFGR_SWS_Mask             ((uint32_t)0x0000000C)
-#define CFGR_SW_Mask              ((uint32_t)0xFFFFFFFC)
-#define CFGR_HPRE_Reset_Mask      ((uint32_t)0xFFFFFF0F)
-#define CFGR_HPRE_Set_Mask        ((uint32_t)0x000000F0)
-#define CFGR_PPRE1_Reset_Mask     ((uint32_t)0xFFFFF8FF)
-#define CFGR_PPRE1_Set_Mask       ((uint32_t)0x00000700)
-#define CFGR_PPRE2_Reset_Mask     ((uint32_t)0xFFFFC7FF)
-#define CFGR_PPRE2_Set_Mask       ((uint32_t)0x00003800)
-#define CFGR_ADCPRE_Reset_Mask    ((uint32_t)0xFFFF3FFF)
-#define CFGR_ADCPRE_Set_Mask      ((uint32_t)0x0000C000)
-/******************************** 增加专有函数 ********************************/
-uint32_t RCC_GetSysClock(void);
-uint32_t RCC_GetHCLK(void);
-uint32_t RCC_GetPCLK(void);
-uint32_t RCC_GetPCLK2(void);
-
-#ifdef __cplusplus
-}
-#endif
+uint RCC_GetPCLK();
+uint RCC_GetPCLK2();
 
 #endif // !_SmartOsPorting_H
